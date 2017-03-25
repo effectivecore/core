287e4b6358f2e26623084de69780edea83c460bc
Maxim Rysevets <root@effcore.com>
2017-03-25 17:37:01 +0300

    sprint_201_18

--- system/module_core/backend/Events.php
+++ system/module_core/backend/Events.php
@@ -4,14 +4,6 @@ namespace effectivecore {
           use \effectivecore\modules\page\page;
           abstract class events {
 
-  static function on_page_install() {
-  # if (version_compare(phpversion(), '5.6.0') < 0) print "PHP is too old!\nCurrent version: ".phpversion()."\nRequired version: 5.6.0+";
-    $call_stack = factory::collect_by_property(settings::$data['module'], 'on_install');
-    foreach (factory::array_sort_by_weight($call_stack) as $c_event) {
-      call_user_func($c_event->handler);
-    }
-  }
-
   static function on_page_modules() {
     $data = [];
     foreach (settings::$data['module'] as $c_module) {
