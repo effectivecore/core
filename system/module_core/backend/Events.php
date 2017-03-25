3ee7e6cbd9093264fe8dc310b2e822f53ad5486d
Maxim Rysevets <root@effcore.com>
2017-03-25 18:04:23 +0300

    sprint_201_22

--- system/module_core/backend/Events.php
+++ system/module_core/backend/Events.php
@@ -1,25 +1,6 @@
 <?php
 
 namespace effectivecore {
-          use \effectivecore\modules\page\page;
           abstract class events {
-
-  static function on_show_modules() {
-    $head = ['Title', 'Id', 'Path', 'Description', 'Version', 'Is embed', 'Is always on'];
-    $data = [];
-    foreach (settings::$data['module'] as $c_module) {
-      $data[] = [
-        $c_module->title,
-        $c_module->id,
-        $c_module->path,
-        $c_module->description,
-        $c_module->version,
-        $c_module->is_embed ? 'Yes' : 'No',
-        $c_module->is_always_on ? 'Yes' : 'No'
-      ];
-    }
-    $data_markup = new html_table([], $data, $head);
-    page::add_element($data_markup);
-  }
 
 }}
--- system/module_core/backend/Events_Page.php
+++ system/module_core/backend/Events_Page.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace effectivecore {
+          use \effectivecore\modules\page\page;
           abstract class events_page extends events {
 
   static function on_show_install() {
@@ -9,6 +10,24 @@ namespace effectivecore {
     foreach (factory::array_sort_by_weight($call_stack) as $c_event) {
       call_user_func($c_event->handler);
     }
+  }
+
+  static function on_show_modules() {
+    $head = ['Title', 'Id', 'Path', 'Description', 'Version', 'Is embed', 'Is always on'];
+    $data = [];
+    foreach (settings::$data['module'] as $c_module) {
+      $data[] = [
+        $c_module->title,
+        $c_module->id,
+        $c_module->path,
+        $c_module->description,
+        $c_module->version,
+        $c_module->is_embed ? 'Yes' : 'No',
+        $c_module->is_always_on ? 'Yes' : 'No'
+      ];
+    }
+    $data_markup = new html_table([], $data, $head);
+    page::add_element($data_markup);
   }
 
 }}
