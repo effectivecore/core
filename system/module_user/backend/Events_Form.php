765d1033643c023e307cc414ecd9e79651239667
Maxim Rysevets <root@effcore.com>
2017-03-27 14:12:00 +0300

    sprint_201_45

--- system/module_user/backend/Events_Form.php
+++ system/module_user/backend/Events_Form.php
@@ -4,54 +4,81 @@ namespace effectivecore\modules\user {
           use \effectivecore\urls;
           use \effectivecore\messages;
           use \effectivecore\modules\data\db;
+          use const \effectivecore\format_datetime;
           abstract class events_form extends \effectivecore\events_form {
 
-  function on_submit_user_login($page_args, $form_args, $post_args) {
-    $db_user = table_user::select_one(['*'], [
-      'password_hash' => sha1($post_args['password']),
-      'email'         => $post_args['email'],
-    ]);
-    if (isset($db_user['id'])) {
-      session::init($db_user['id']);
-      urls::go('/user/'.$db_user['id']);
-    } else {
-      messages::add_new('Incorrect email or password!', 'error');
-    }
-  }
-
   function on_submit_user_n_delete($page_args, $form_args, $post_args) {
-    $user_id = $page_args['user_id'];
-    if ($post_args['button'] == 'delete') {
-      if (table_user::delete(['id' => $user_id])) {
-        table_session::delete(['user_id' => $user_id]);
-        messages::add_new('User with id "'.$user_id.'" was delited.');
-      }
+    $back_url = urldecode(urls::$current->get_args('back', 'query'));
+    $user_id  = $page_args['user_id'];
+    switch ($post_args['button']) {
+      case 'delete':
+        $result = table_user::delete(['id' => $user_id]);
+        if ($result) {
+          table_session::delete(['user_id' => $user_id]);
+          messages::add_new('User with id = '.$user_id.' was deleted.');
+          urls::go($back_url ?: '/admin/users');
+        } else {
+          messages::add_new('User is not deleted!', 'error');
+        }
+        break;
+      case 'cancel':
+        urls::go($back_url ?: '/admin/users');
+        break;
     }
-  # redirect in any case
-    $back_url = urls::$current->get_args('back', 'query');
-    urls::go($back_url ? urldecode($back_url) : '/admin/users');
   }
 
   function on_submit_user_n_edit($page_args, $form_args, $post_args) {
-    if (table_user::update(['password_hash' => sha1($args['password'])], ['id' => $args['user_id']])) {
-      messages::add_new('Parameters of user with id = '.$args['user_id'].' was updated.');
+    $back_url      = urldecode(urls::$current->get_args('back', 'query'));
+    $user_id       = $page_args['user_id'];
+    $password_hash = sha1($post_args['password']);
+    switch ($post_args['button']) {
+      case 'save':
+        $result = table_user::update(['password_hash' => $password_hash], ['id' => $user_id]);
+        if ($result) {
+          messages::add_new('Parameters of user with id = '.$user_id.' was updated.');
+          urls::go($back_url ?: '/user/'.$user_id);
+        } else {
+          messages::add_new('Parameters is not updated!', 'error');
+        }
+        break;
     }
-  # redirect to back
-    $back_url = urls::$current->get_args('back', 'query');
-    urls::go($back_url ? urldecode($back_url) : '/user/'.$args['user_id']);
+  }
+
+  function on_submit_user_login($page_args, $form_args, $post_args) {
+    $email         = $post_args['email'];
+    $password_hash = sha1($post_args['password']);
+    switch ($post_args['button']) {
+      case 'login':
+        $result = table_user::select_one(['id'], ['password_hash' => $password_hash, 'email' => $email]);
+        if (isset($result['id'])) {
+          session::init($result['id']);
+          urls::go('/user/'.$result['id']);
+        } else {
+          messages::add_new('Incorrect email or password!', 'error');
+        }
+        break;
+    }
   }
 
   function on_submit_user_register($page_args, $form_args, $post_args) {
-    if (table_user::select(['id'], ['email' => $args['email']]) == []) {
-      $new_user_id = table_user::insert([
-        'email'         => $args['email'],
-        'password_hash' => sha1($args['password']),
-        'created'       => date(format_datetime, time())
-      ]);
-      session::init($new_user_id);
-      urls::go('/user/'.$new_user_id);
-    } else {
-      messages::add_new('This email is already registered!', 'error');
+    $email         = $post_args['email'];
+    $password_hash = sha1($post_args['password']);
+    $created       = date(format_datetime, time());
+    switch ($post_args['button']) {
+      case 'register':
+        $is_exist = table_user::select_one(['id'], ['email' => $email]);
+        if ($is_exist) {
+          messages::add_new('User with this email is already exist!', 'error');
+        } else {
+          $new_id = table_user::insert(['email' => $email, 'password_hash' => $password_hash, 'created' => $created]);
+          if ($new_id) {
+            session::init($new_id);
+            urls::go('/user/'.$new_id);
+          } else {
+            messages::add_new('User is not created!', 'error');
+          }
+        }
+        break;
     }
   }
 
