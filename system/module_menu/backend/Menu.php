58d08ca84c5f8b96f33ff0624caf2f0f4b855706
Maxim Rysevets <root@effcore.com>
2017-03-29 23:30:39 +0300

    sprint_205

--- system/module_menu/backend/Menu.php
+++ system/module_menu/backend/Menu.php
@@ -1,10 +1,18 @@
 <?php
 
 namespace effectivecore {
+          use \effectivecore\modules\user\access;
           class menu extends \effectivecore\folder {
 
   function render() {
-    return (new html('menu', ['class' => $this->attributes->class], new html('ul', [], $this->children)))->render();
+    $rendered = [];
+    foreach ($this->children as $c_child) {
+      if (!isset($c_child->access) ||
+          (isset($c_child->access) && access::check($c_child->access))) {
+        $rendered[] = $c_child->render();
+      }
+    }
+    return (new html('menu', ['class' => $this->attributes->class], new html('ul', [], $rendered)))->render();
   }
 
 }}
