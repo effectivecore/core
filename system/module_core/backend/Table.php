1ad5679672fa7600270d5c3b9be1e65a9bb5640e
Maxim Rysevets <root@effcore.com>
2017-04-12 12:08:39 +0300

    sprint_234

--- system/module_core/backend/Table.php
+++ system/module_core/backend/Table.php
@@ -1,9 +1,31 @@
 <?php
 
 namespace effectivecore {
-          class table extends markup {
+          class table extends node {
 
-  function __construct() {
+  public $template = 'table';
+  public $template_head = 'table_head';
+  public $template_head_row = 'table_head_row';
+  public $template_head_row_cell = 'table_head_row_cell';
+  public $template_body = 'table_body';
+  public $template_body_row = 'table_body_row';
+  public $template_body_row_cell = 'table_body_row_cell';
+
+  public $head;
+  public $body;
+
+  function __construct($attributes = null, $body = [], $head = [], $weight = 0) {
+    parent::__construct(null, $attributes, null, $weight);
+    $this->body = $body;
+    $this->head = $head;
+  }
+
+  function render() {
+    $rendered = '';
+    foreach ($head as $c_cell) {
+      $rendered.= (new template($this->template_head_row_cell, ['cell' => $c_cell]))->render();
+    }
+    return $rendered;
   }
 
 }}
