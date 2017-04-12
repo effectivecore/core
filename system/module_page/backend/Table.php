ca749450fdf35fe164b0bfb30498c8eaf09c9bd2
Maxim Rysevets <root@effcore.com>
2017-04-12 14:38:17 +0300

    sprint_237

--- system/module_page/backend/Table.php
+++ system/module_page/backend/Table.php
@@ -21,11 +21,32 @@ namespace effectivecore {
   }
 
   function render() {
-    $rendered = '';
-    foreach ($head as $c_cell) {
-      $rendered.= (new template($this->template_head_row_cell, ['cell' => $c_cell]))->render();
+    $rendered_head = '';
+    $rendered_body = '';
+  # render table head
+    foreach ($this->head as $c_row) {
+      $buf = '';
+      foreach ($c_row as $c_cell) $buf.= $this->render_head_row_cell($c_cell);
+      $rendered_head.= $this->render_head_row($buf);
     }
-    return $rendered;
+  # render table body
+    foreach ($this->body as $c_row) {
+      $buf = '';
+      foreach ($c_row as $c_cell) $buf.= $this->render_body_row_cell($c_cell);
+      $rendered_body.= $this->render_body_row($buf);
+    }
+  # return rendered table
+    return (new template($this->template, [
+      'head' => $this->render_head($rendered_head),
+      'body' => $this->render_body($rendered_body),
+    ]))->render();
   }
+
+  function render_head($rows)          {return (new template($this->template_head,          ['rows' => $rows]))->render();}
+  function render_head_row($cells)     {return (new template($this->template_head_row,      ['cells' => $cells]))->render();}
+  function render_head_row_cell($cell) {return (new template($this->template_head_row_cell, ['cell' => $cell]))->render();}
+  function render_body($rows)          {return (new template($this->template_body,          ['rows' => $rows]))->render();}
+  function render_body_row($cells)     {return (new template($this->template_body_row,      ['cells' => $cells]))->render();}
+  function render_body_row_cell($cell) {return (new template($this->template_body_row_cell, ['cell' => $cell]))->render();}
 
 }}
