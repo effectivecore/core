fc6100faad102dbf793f614c4d9c1977d5841905
Maxim Rysevets <root@effcore.com>
2017-04-24 15:49:58 +0300

    sprint_263_14

--- system/module_core/backend/Events_Module.php
+++ system/module_core/backend/Events_Module.php
@@ -13,15 +13,10 @@ namespace effectivecore {
     translate::init();
     token::init();
     urls::init();
+    events::init();
     core::init();
   # init modules
     ob_start();
-    foreach (settings::$data['events'] as $c_module_events) {
-      foreach ($c_module_events as $c_type => $c_events) {
-        foreach ($c_events as $c_id => $c_event) static::$data->{$c_type}[$c_id] = $c_event;
-        factory::array_sort_by_weight(static::$data->{$c_type});
-      }
-    }
     console::set_log('init_core', '\effectivecore\events_module::on_init', 'Init calls');
     foreach (static::$data->on_init as $c_id => $c_event) {
       console::set_log($c_id, $c_event->handler, 'Init calls');
--- system/module_core/backend/Events_Page.php
+++ system/module_core/backend/Events_Page.php
@@ -5,8 +5,7 @@ namespace effectivecore {
           abstract class events_page extends events {
 
   static function on_show_install() {
-    $call_stack = factory::collect_by_property(settings::$data['module'], 'on_install');
-    foreach (factory::array_sort_by_weight($call_stack) as $c_event) {
+    foreach (static::$data->on_install as $c_event) {
       call_user_func($c_event->handler);
     }
   }
