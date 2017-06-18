e7dcabaa2240293d32b0a048f668d4a856667897
Maxim Rysevets <root@effcore.com>
2017-06-18 21:42:06 +0300

    sprint_304_6

--- system/module_page/backend/Events_Form.php
+++ system/module_page/backend/Events_Form.php
@@ -1,8 +1,22 @@
 <?php
 
 namespace effectivecore\modules\page {
+          use \effectivecore\markup;
           use \effectivecore\message_factory as messages;
+          use \effectivecore\modules\storage\storage_factory as storages;
           abstract class events_form extends \effectivecore\events_form {
+
+  static function on_init_admin_decoration($form, $elements) {
+    foreach (storages::get('settings')->select('palette') as $module_id => $c_palette) {
+      foreach ($c_palette as $c_color) {
+        $elements['field_bg_color']->add_child(
+          new markup('option', [
+            'value' => $c_color->value,
+            'style' => 'background-color:'.$c_color->value], $c_color->value)
+        );
+      }
+    }
+  }
 
   static function on_submit_admin_decoration($form, $elements) {
     messages::add_new('Test');
