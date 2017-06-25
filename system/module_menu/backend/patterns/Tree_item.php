7941488e86ec25e3b1ebc0cb331246e8040e72fa
Maxim Rysevets <root@effcore.com>
2017-06-25 14:14:52 +0300

    sprint_304_37

--- system/module_menu/backend/patterns/Tree_item.php
+++ system/module_menu/backend/patterns/Tree_item.php
@@ -8,6 +8,9 @@ namespace effectivecore {
           class tree_item extends \effectivecore\node {
 
   public $title;
+  public $template          = 'tree_item';
+  public $template_self     = 'tree_item_self';
+  public $template_children = 'tree_item_children';
 
   function __construct($title = '', $attributes = null, $children = null, $weight = 0) {
     parent::__construct($attributes, $children, $weight);
@@ -19,11 +22,11 @@ namespace effectivecore {
         (isset($this->access) && access::check($this->access))) {
       $rendered_children = '';
       if (count($this->children)) {
-        $rendered_children = (new template('tree_item_children', [
+        $rendered_children = (new template($this->template_children, [
           'children' => $this->render_children($this->children)
         ]))->render();
       }
-      return (new template('tree_item', [
+      return (new template($this->template, [
         'self'     => $this->render_self(),
         'children' => $rendered_children
       ]))->render();
@@ -38,7 +41,7 @@ namespace effectivecore {
         $attr->class = isset($attr->class) ? $attr->class.' active' : 'active';
       }
     }
-    return (new template('tree_item_self', [
+    return (new template($this->template_self, [
       'attributes' => factory::data_to_attr($attr, ' '),
       'title'      => tokens::replace(translations::get($this->title))
     ]))->render();
