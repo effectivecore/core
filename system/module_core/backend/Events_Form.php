22dcd2ec8b1b52c3a492c36abfb9cc02f1b16454
Maxim Rysevets <root@effcore.com>
2017-06-27 15:36:59 +0300

    sprint_304_56

--- system/module_core/backend/Events_Form.php
+++ system/module_core/backend/Events_Form.php
@@ -30,84 +30,81 @@ namespace effectivecore {
             case 'input':
               $c_type = isset($c_element->attributes->type) ?
                               $c_element->attributes->type : '';
+              if ($c_type) {
 
-            # not processed elements
-              if ($c_type && (
-                  $c_type == 'submit' || # <input type="submit">
-                  $c_type == 'reset'  || # <input type="reset">
-                  $c_type == 'image'  || # <input type="image">
-                  $c_type == 'button' || # <input type="button">
-                  $c_type == 'hidden')   # <input type="hidden">
-              ) {
-                continue;
-              }
-
-            # file
-              if ($c_type &&
-                  $c_type == 'file') {
-            # ... @todo: make functionality
-              }
-
-            # checkbox
-              if ($c_type &&
-                  $c_type == 'checkbox') {
-            # ... @todo: make functionality
-              }
-
-            # radio
-              if ($c_type &&
-                  $c_type == 'radio') {
-            # ... @todo: make functionality
-              }
-
-            # html4 elements: text|password
-            # html5 elements: search|email|url|tel|number|range|date|time|color
-              if ($c_type && (
-                  $c_type == 'text'     || # <input type="text">
-                  $c_type == 'password' || # <input type="password">
-                  $c_type == 'search'   || # <input type="search">
-                  $c_type == 'email'    || # <input type="email">
-                  $c_type == 'url'      || # <input type="url">
-                  $c_type == 'tel'      || # <input type="tel">
-                  $c_type == 'number'   || # <input type="number">
-                  $c_type == 'range'    || # <input type="range">
-                  $c_type == 'date'     || # <input type="date">
-                  $c_type == 'time'     || # <input type="time">
-                  $c_type == 'color')      # <input type="color">
-              ) {
-
-              # set post value
-                $c_element->attributes->value = $c_post;
-
-              # check required fields
-                if ($c_post == '' && isset($c_element->attributes->required)) {
-                  $form->add_error($c_id,
-                    $c_element->title.' field can not be blank!'
-                  );
+              # not processed elements
+                if ($c_type == 'submit' || # <input type="submit">
+                    $c_type == 'reset'  || # <input type="reset">
+                    $c_type == 'image'  || # <input type="image">
+                    $c_type == 'button' || # <input type="button">
+                    $c_type == 'hidden'    # <input type="hidden">
+                ) {
+                  continue;
                 }
-
-              # check min length
-                if ($c_post != '' && isset($c_element->attributes->minlength) &&
-                         strlen($c_post) < $c_element->attributes->minlength) {
-                  $form->add_error($c_id,
-                    $c_element->title.' field contain too few symbols! Minimum '.$c_element->attributes->minlength.' symbols.'
-                  );
+  
+              # file
+                if ($c_type == 'file') {
+              # ... @todo: make functionality
                 }
-
-              # check max length
-                if ($c_post != '' && isset($c_element->attributes->maxlength) &&
-                         strlen($c_post) > $c_element->attributes->maxlength) {
-                  $form->add_error($c_id,
-                    $c_element->title.' field contain too much symbols! Maximum '.$c_element->attributes->maxlength.' symbols.'
-                  );
+  
+              # checkbox
+                if ($c_type == 'checkbox') {
+              # ... @todo: make functionality
                 }
-
-              # check email field
-                if ($c_type == 'email' &&
-                    $c_post && !filter_var($c_post, FILTER_VALIDATE_EMAIL)) {
-                  $form->add_error($c_id,
-                    $c_element->title.' field contains an invalid email address!'
-                  );
+  
+              # radio
+                if ($c_type == 'radio') {
+              # ... @todo: make functionality
+                }
+  
+              # html4 elements: text|password
+              # html5 elements: search|email|url|tel|number|range|date|time|color
+                if ($c_type == 'text'     || # <input type="text">
+                    $c_type == 'password' || # <input type="password">
+                    $c_type == 'search'   || # <input type="search">
+                    $c_type == 'email'    || # <input type="email">
+                    $c_type == 'url'      || # <input type="url">
+                    $c_type == 'tel'      || # <input type="tel">
+                    $c_type == 'number'   || # <input type="number">
+                    $c_type == 'range'    || # <input type="range">
+                    $c_type == 'date'     || # <input type="date">
+                    $c_type == 'time'     || # <input type="time">
+                    $c_type == 'color'       # <input type="color">
+                ) {
+  
+                # set post value
+                  $c_element->attributes->value = $c_post;
+  
+                # check required fields
+                  if ($c_post == '' && isset($c_element->attributes->required)) {
+                    $form->add_error($c_id,
+                      $c_element->title.' field can not be blank!'
+                    );
+                  }
+  
+                # check min length
+                  if ($c_post != '' && isset($c_element->attributes->minlength) &&
+                           strlen($c_post) < $c_element->attributes->minlength) {
+                    $form->add_error($c_id,
+                      $c_element->title.' field contain too few symbols! Minimum '.$c_element->attributes->minlength.' symbols.'
+                    );
+                  }
+  
+                # check max length
+                  if ($c_post != '' && isset($c_element->attributes->maxlength) &&
+                           strlen($c_post) > $c_element->attributes->maxlength) {
+                    $form->add_error($c_id,
+                      $c_element->title.' field contain too much symbols! Maximum '.$c_element->attributes->maxlength.' symbols.'
+                    );
+                  }
+  
+                # check email field
+                  if ($c_type == 'email' &&
+                      $c_post && !filter_var($c_post, FILTER_VALIDATE_EMAIL)) {
+                    $form->add_error($c_id,
+                      $c_element->title.' field contains an invalid email address!'
+                    );
+                  }
                 }
               }
               break;
