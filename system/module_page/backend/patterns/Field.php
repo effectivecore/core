08fe4877a6b7a6641a50307a96c321b1db744f83
Maxim Rysevets <root@effcore.com>
2017-06-28 16:58:28 +0300

    sprint_304_59

--- system/module_page/backend/patterns/Field.php
+++ system/module_page/backend/patterns/Field.php
@@ -16,13 +16,18 @@ namespace effectivecore {
     if ($is_has_box) {
       $this->wr_attributes['class'][] = 'has-box';
     }
+    $description = [];
+    if (!empty($this->description))           $description[] = (new markup('p', ['class' => ['default']], $this->description))->render();
+    if (!empty($this->attributes->minlength)) $description[] = (new markup('p', ['class' => ['minlength']], 'Field should contain minimum '.$this->attributes->minlength.' symbols.'))->render();
+    if (!empty($this->attributes->maxlength)) $description[] = (new markup('p', ['class' => ['maxlength']], 'Field should contain maximum '.$this->attributes->maxlength.' symbols.'))->render();
+    $is_required_mark = !empty($this->attributes->required) ? new markup('spam', ['class' => 'required'], '*') : '';
     return (new template($this->template, [
       'wr_tag_name'   => $this->wr_tag_name,
       'wr_attributes' => factory::data_to_attr($this->wr_attributes, ' '),
       'attributes'    => factory::data_to_attr($this->attributes, ' '),
-      'title_t'       => $is_has_box != true ? (new markup('label', [], $this->title))->render() : '',
-      'title_b'       => $is_has_box == true ? (new markup('label', [], $this->title))->render() : '',
-      'description'   => $this->description ? (new markup('x-description', [], $this->description))->render() : '',
+      'title_t'       => $is_has_box != true ? (new markup('label',         [], [$this->title, $is_required_mark]))->render() : '',
+      'title_b'       => $is_has_box == true ? (new markup('label',         [], [$this->title]))->render()                    : '',
+      'description'   => count($description) ? (new markup('x-description', [], implode($description)))->render()             : '',
       'children'      => (new markup($this->tag_name, $this->attributes, $this->children))->render()
     ]))->render();
   }
