dfb029c67c676947e490e9945522a580bcd5ef0c
Maxim Rysevets <root@effcore.com>
2017-07-03 12:30:23 +0300

    sprint_304_74

--- system/module_develop/module_demo/backend/Events_Form.php
+++ system/module_develop/module_demo/backend/Events_Form.php
@@ -5,7 +5,6 @@ namespace effectivecore\modules\demo {
           abstract class events_form extends \effectivecore\events_form {
 
   static function on_init_demo($form, $elements) {
-    messages::add_new('Call \effectivecore\modules\demo\events_form::on_init_demo.');
   }
 
   static function on_submit_demo($form, $elements) {
