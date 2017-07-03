09ddbdcfcfc1917b4b73c6abb7e17defcd87bb24
Maxim Rysevets <root@effcore.com>
2017-07-03 13:42:57 +0300

    sprint_304_75

--- system/module_page/backend/Events_Form.php
+++ system/module_page/backend/Events_Form.php
@@ -11,22 +11,22 @@ namespace effectivecore\modules\page {
     foreach (storages::get('settings')->select('colors') as $module_id => $c_colors) {
       foreach ($c_colors as $c_color_id => $c_color_info) {
         $c_element_id = $elements['fieldset_default/field_bg_color']->child_insert(
-          new markup('input', [
+          new markup('input', (object)[
             'type'  => 'radio',
             'name'  => 'bg_color',
             'value' => $c_color_id,
             'title' => $c_color_id.' ('.$c_color_info->value.')',
-            'style' => ['background-color: '.$c_color_info->value]])
+            'style' => ['background-color: '.$c_color_info->value]]), $c_color_info->value
         );
       }
     }
   }
 
-  static function on_submit_admin_decoration($form, $elements) {
+  static function on_submit_admin_decoration($form, $elements, $values) {
     storages::get('settings')->changes_register_action('page', (object)[
       'action' => 'update',
       'npath'  => 'decoration/page/background_color',
-      'value'  => $_POST['bg_color']
+      'value'  => $values['bg_color']
     ]);
   }
 
