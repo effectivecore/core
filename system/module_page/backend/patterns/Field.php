992518c45d82ae47c9dd0e13f17594fde27c6db7
Maxim Rysevets <root@effcore.com>
2017-07-04 13:32:22 +0300

    sprint_304_81

--- system/module_page/backend/patterns/Field.php
+++ system/module_page/backend/patterns/Field.php
@@ -18,7 +18,7 @@ namespace effectivecore {
       $this->wr_attributes['class']['has_box'] = 'has-box'; # @todo: use attribute_insert
     }
     $description = [];
-    if (!empty($this->description))                   $description[] = (new markup('p', ['class' => ['default']],   translations::get($this->description)))->render();
+    if (!empty($this->description))                   $description[] = (new markup('p', ['class' => ['default']],   is_string($this->description) ? translations::get($this->description) : $this->description))->render();
     if (!empty($this->attribute_select('minlength'))) $description[] = (new markup('p', ['class' => ['minlength']], translations::get('Field should contain minimum %%_lenght symbols.', ['lenght' => $this->attribute_select('minlength')])))->render();
     if (!empty($this->attribute_select('maxlength'))) $description[] = (new markup('p', ['class' => ['maxlength']], translations::get('Field should contain maximum %%_lenght symbols.', ['lenght' => $this->attribute_select('maxlength')])))->render();
     $is_required_mark = !empty($this->attribute_select('required')) ? new markup('b', ['class' => 'required'], '*') : '';
