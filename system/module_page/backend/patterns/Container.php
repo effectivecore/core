45aa229630f5e3ba734af9673056db168fa2da76
Maxim Rysevets <root@effcore.com>
2017-07-10 18:07:44 +0300

    sprint_304_97_10

--- system/module_page/backend/patterns/Container.php
+++ system/module_page/backend/patterns/Container.php
@@ -20,15 +20,24 @@ namespace effectivecore {
   }
 
   function render_self() {
-    return $this->title ? (new markup('x-title', [],
-      $this->title
-    ))->render() : '';
+    return (new markup('label', [], [
+      $this->title,
+      $this->render_required_mark()
+    ]))->render();
+  }
+
+  function render_required_mark() {
+    return !empty($this->attribute_select('required')) ? (
+      new markup('b', ['class' => ['required' => 'required']], '*')
+    )->render() : '';
   }
 
   function render_description() {
-    return $this->description ? (new markup('x-description', [], new markup('p', [],
-      is_string($this->description) ? translations::get($this->description) : $this->description
-    )))->render() : '';
+    $descriptions = [];
+    if (!empty($this->description))                   $descriptions[] = (new markup('p', [], is_string($this->description) ? translations::get($this->description) : $this->description))->render();
+    if (!empty($this->attribute_select('minlength'))) $descriptions[] = (new markup('p', ['class' => ['minlength' => 'minlength']], translations::get('Field should contain minimum %%_lenght symbols.', ['lenght' => $this->attribute_select('minlength')])))->render();
+    if (!empty($this->attribute_select('maxlength'))) $descriptions[] = (new markup('p', ['class' => ['maxlength' => 'maxlength']], translations::get('Field should contain maximum %%_lenght symbols.', ['lenght' => $this->attribute_select('maxlength')])))->render();
+    return count($descriptions) ? (new markup('x-description', [], implode($descriptions)))->render() : '';
   }
 
 }}
