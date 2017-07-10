b94add7cb1fdcaa848eff54dc734125dc78bc37d
Maxim Rysevets <root@effcore.com>
2017-07-10 16:55:52 +0300

    sprint_304_97_9

--- system/module_page/backend/patterns/Container.php
+++ system/module_page/backend/patterns/Container.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace effectivecore {
+          use \effectivecore\translate_factory as translations;
           class form_container extends node {
 
   public $template = 'form_container';
@@ -12,10 +13,22 @@ namespace effectivecore {
     return (new template($this->template, [
       'attributes'  => factory::data_to_attr($this->attribute_select(), ' '),
       'tag_name'    => $this->tag_name,
-      'title'       => $this->title ? (new markup('x-title', [], $this->title))->render() : '',
-      'description' => $this->description ? (new markup('x-description', [], $this->description))->render() : '',
-      'children'    => $this->render_children($this->children)
+      'title'       => $this->render_self(),
+      'content'     => $this->render_children($this->children),
+      'description' => $this->render_description()
     ]))->render();
+  }
+
+  function render_self() {
+    return $this->title ? (new markup('x-title', [],
+      $this->title
+    ))->render() : '';
+  }
+
+  function render_description() {
+    return $this->description ? (new markup('x-description', [], new markup('p', [],
+      is_string($this->description) ? translations::get($this->description) : $this->description
+    )))->render() : '';
   }
 
 }}
