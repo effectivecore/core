15029ad18cab5e2e64ca34e6494ee59632184496
Maxim Rysevets <root@effcore.com>
2017-07-10 14:11:08 +0300

    sprint_304_97_3

--- system/module_page/backend/patterns/Field.php
+++ system/module_page/backend/patterns/Field.php
@@ -15,19 +15,26 @@ namespace effectivecore {
       'wr_tag_name'   => $this->wr_tag_name,
       'wr_attributes' => factory::data_to_attr($this->attribute_select('', 'wr_attributes'), ' '),
       'attributes'    => factory::data_to_attr($this->attribute_select(), ' '),
-      'title'         => (new markup('label', [], [$this->title, $this->_render_required_mark()]))->render(),
       'children'      => (new markup($this->tag_name, $this->attribute_select(), $this->children))->render(),
-      'description'   => $this->_render_description()
+      'description'   => $this->render_description(),
+      'title'         => $this->render_self()
     ]))->render();
   }
 
-  function _render_required_mark() {
+  function render_self() {
+    return (new markup('label', [], [
+      $this->title,
+      $this->render_required_mark()
+    ]))->render();
+  }
+
+  function render_required_mark() {
     return !empty($this->attribute_select('required')) ? (
       new markup('b', ['class' => ['required' => 'required']], '*')
     )->render() : '';
   }
 
-  function _render_description() {
+  function render_description() {
     $descriptions = [];
     if (!empty($this->description))                   $descriptions[] = (new markup('p', [], is_string($this->description) ? translations::get($this->description) : $this->description))->render();
     if (!empty($this->attribute_select('minlength'))) $descriptions[] = (new markup('p', ['class' => ['minlength' => 'minlength']], translations::get('Field should contain minimum %%_lenght symbols.', ['lenght' => $this->attribute_select('minlength')])))->render();
