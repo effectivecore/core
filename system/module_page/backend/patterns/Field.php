2770df6400bcea5f12a570a4a5da213f67d28035
Maxim Rysevets <root@effcore.com>
2017-07-11 14:25:21 +0300

    sprint_304_97_19

--- system/module_page/backend/patterns/Field.php
+++ system/module_page/backend/patterns/Field.php
@@ -10,7 +10,21 @@ namespace effectivecore {
   public $description;
 
   function render() {
-    return parent::render();
+    $attributes = [];
+    $description = $this->description;
+    $default = $this->child_select('default');
+    if ($default instanceof markup) {
+      if ($default->attribute_select('required')) $this->attribute_insert('required', 'required');
+      if (!empty($default->description))          $description.= $default->description;
+      if (!empty($default->attribute_select()))   $attributes += $default->attribute_select();
+    }
+    return (new template($this->template, [
+      'attributes'  => factory::data_to_attr($this->attribute_select(), ' '),
+      'tag_name'    => $this->tag_name,
+      'title'       => $this->render_self(),
+      'content'     => $this->render_children($this->children),
+      'description' => $this->render_description($description, $attributes)
+    ]))->render();
   }
 
 }}
