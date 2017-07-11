bcb392e4934389271bc6413c1eae3dbbe4354074
Maxim Rysevets <root@effcore.com>
2017-07-11 16:51:44 +0300

    sprint_304_97_20

--- system/module_page/backend/patterns/Field.php
+++ system/module_page/backend/patterns/Field.php
@@ -10,12 +10,12 @@ namespace effectivecore {
   public $description;
 
   function render() {
-    $attributes = [];
-    $description = $this->description;
-    $default = $this->child_select('default');
+    $attributes   = [];
+    $descriptions = $this->description ? [$this->description] : [];
+    $default      = $this->child_select('default');
     if ($default instanceof markup) {
       if ($default->attribute_select('required')) $this->attribute_insert('required', 'required');
-      if (!empty($default->description))          $description.= $default->description;
+      if (!empty($default->description))          $descriptions[] = $default->description;
       if (!empty($default->attribute_select()))   $attributes += $default->attribute_select();
     }
     return (new template($this->template, [
@@ -23,7 +23,7 @@ namespace effectivecore {
       'tag_name'    => $this->tag_name,
       'title'       => $this->render_self(),
       'content'     => $this->render_children($this->children),
-      'description' => $this->render_description($description, $attributes)
+      'description' => $this->render_description($descriptions, $attributes)
     ]))->render();
   }
 
