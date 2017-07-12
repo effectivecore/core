d06a225e23faae1bf531151c25ec9d61140c4c68
Maxim Rysevets <root@effcore.com>
2017-07-12 14:44:51 +0300

    sprint_304_97_25

--- system/module_page/backend/patterns/Field_Checkboxes.php
+++ system/module_page/backend/patterns/Field_Checkboxes.php
@@ -4,10 +4,7 @@ namespace effectivecore {
           use \effectivecore\translate_factory as translations;
           class form_field_checkboxes extends form_field {
 
-  public $template = 'form_field';
   public $tag_name = 'x-field';
-  public $title;
-  public $description;
 
   function render() {
     $this->attributes['class']['is-box'] = 'is-box'; # @todo: use attribute_insert
