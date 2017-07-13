0b9714b9d90aca3a6b2be5189846a8ef46cd88bf
Maxim Rysevets <root@effcore.com>
2017-07-13 12:46:07 +0300

    sprint_304_102

--- system/module_page/backend/patterns/Container.php
+++ system/module_page/backend/patterns/Container.php
@@ -26,9 +26,9 @@ namespace effectivecore {
   }
 
   function render_self() {
-    return empty($this->title) ? '' : (new template('form_label', [
+    return empty($this->title) ? '' : (new template('form_title', [
       'tag_name'      => $this->tag_name == 'fieldset' ? 'legend' : 'x-title',
-      'label'         => translations::get($this->title),
+      'title'         => translations::get($this->title),
       'required_mark' => $this->attribute_select('required') ? $this->render_required_mark() : ''
     ]))->render();
   }
