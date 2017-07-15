577edaf6a796864c6be684e3027a7c63b6589040
Maxim Rysevets <root@effcore.com>
2017-07-15 23:29:41 +0300

    sprint_304_110

--- system/module_menu/backend/patterns/Tree.php
+++ system/module_menu/backend/patterns/Tree.php
@@ -3,11 +3,11 @@
 namespace effectivecore {
           class tree extends \effectivecore\node {
 
-  public $title;
+  public $title = '';
   public $template = 'tree';
 
   function __construct($title = '', $attributes = [], $children = [], $weight = 0) {
-    $this->title = $title;
+    if ($title) $this->title = $title;
     parent::__construct($attributes, $children, $weight);
   }
 
