739f3b3ca1e59ae945d9526d41d7a3da81e24f35
Maxim Rysevets <root@effcore.com>
2017-07-18 13:50:30 +0300

    sprint_304_123

--- system/module_core/backend/interfaces/marker--call_construct.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace effectivecore {
-          interface call_construct {
-
- # this interface indicates that the __construct() of class
- # with this interface sholud be called on automatic load
-
-}}
---
+++ system/module_core/backend/markers.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace effectivecore {
+
+  interface call_construct {
+    # indicates that the __construct() of class with this interface sholud be called on automatic load
+  }
+
+}
