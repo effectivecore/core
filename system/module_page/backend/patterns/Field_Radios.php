4a622aa129323989c73976cdc45e3309360afad6
Maxim Rysevets <root@effcore.com>
2017-07-18 21:50:26 +0300

    sprint_304_130

--- system/module_page/backend/patterns/Field_Radios.php
+++ system/module_page/backend/patterns/Field_Radios.php
@@ -1,13 +1,11 @@
 <?php
 
 namespace effectivecore {
-          class form_field_radios extends form_container
-          implements late_constructor {
+          class form_field_radios extends form_container {
 
   public $values = [];
 
-  function __construct($tag_name = '', $title = '', $description = '', $attributes = [], $children = [], $weight = 0) {
-    parent::__construct($tag_name, $title, $description, $attributes, $children, $weight);
+  function init() {
     $this->attribute_insert('class', ['boxes' => 'boxes', 'radios' => 'radios']);
     foreach ($this->values as $value => $title) {
       $this->child_insert(new form_field(), $value);
