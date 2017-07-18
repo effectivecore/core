64b121894f1a235e0be693e5f8a91e4e782f2e74
Maxim Rysevets <root@effcore.com>
2017-07-18 14:10:35 +0300

    sprint_304_124

--- system/module_page/backend/patterns/Field_Radios.php
+++ system/module_page/backend/patterns/Field_Radios.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace effectivecore {
-          class form_field_radios extends form_container implements call_construct {
+          class form_field_radios extends form_container
+          implements call_construct {
 
   function __construct($tag_name = '', $title = '', $description = '', $attributes = [], $children = [], $weight = 0) {
     parent::__construct($tag_name, $title, $description, $attributes, $children, $weight);
@@ -9,7 +10,7 @@ namespace effectivecore {
     if (!empty($this->values)) {
       foreach ($this->values as $value => $title) {
         $this->child_insert(new form_field(), $value);
-        $this->child_select($value)->child_insert(new form_element('input', $title, '', ['type' => 'radio', 'name' => $value]), 'default');
+        $this->child_select($value)->child_insert(new form_element('input', $title, '', ['type' => 'radio', 'name' => $this->attribute_select('name'), 'value' => $value]), 'default');
         $this->child_select($value)->child_select('default')->title_position = 'right';
       }
     }
