771b9890f0ad1846d75b534e28935c46c3e0b6e2
Maxim Rysevets <root@effcore.com>
2017-07-20 12:35:10 +0300

    sprint_304_134

---
+++ system/module_page/backend/patterns/Field_Palette.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace effectivecore {
+          use \effectivecore\translate_factory as translations;
+          use \effectivecore\modules\storage\storage_factory as storages;
+          class form_field_palette extends form_field_radios {
+
+  function build() {
+    $this->attribute_insert('class', ['palette' => 'palette']);
+    foreach (storages::get('settings')->select('colors') as $module_id => $c_colors) {
+      foreach ($c_colors as $c_color_id => $c_color_info) {
+        $this->item_insert('', [
+          'value' => $c_color_id,
+          'title' => translations::get('Color ID = %%_id (value = %%_value)', ['id' => $c_color_id, 'value' => $c_color_info->value]),
+          'style' => ['background-color: '.$c_color_info->value]
+        ]);
+      }
+    }
+  }
+
+  function item_insert($title = '', $attr = []) {
+    return parent::item_insert($title, $attr + ['name' => $this->name]);
+  }
+
+}}
