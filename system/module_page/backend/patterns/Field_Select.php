c3086ca52fd0bf242c5b8c77f707d0f1ad37dec8
Maxim Rysevets <root@effcore.com>
2017-07-22 22:48:50 +0300

    sprint_311_2

--- system/module_page/backend/patterns/Field_Select.php
+++ system/module_page/backend/patterns/Field_Select.php
@@ -3,14 +3,15 @@
 namespace effectivecore {
           class form_field_select extends form_field {
 
-  function render() {
+  public $values = [];
+
+  function build() {
     $this->child_insert(new markup('select'), 'default');
     foreach ($this->values as $value => $title) {
       $this->child_select('default')->child_insert(
         new markup('option', ['value' => $value], $title), $value
       );
     }
-    return parent::render();
   }
 
 }}
