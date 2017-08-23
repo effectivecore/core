3d85ee743b4abb53149572264ce9ebbdbb24e902
Maxim Rysevets <root@effcore.com>
2017-08-23 18:17:37 +0300

    sprint_311_67

--- system/module_page/frontend/page.js
+++ system/module_page/frontend/page.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', function(){
 
+  effcore = {};
+  effcore.tokens = [];
+  effcore.tokens['color'] = '%%_color';
+  effcore.tokens['color_bg'] = '%%_color_bg';
+
+  NodeList.prototype.each = function(func){
+    for (var i = 0; i < this.length; i++) {
+      func.call(this[i]);
+    }
+  };
+
 });
