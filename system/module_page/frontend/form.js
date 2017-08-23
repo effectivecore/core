630b6c5f2173c3eb59bc29c040f92986e30561de
Maxim Rysevets <root@effcore.com>
2017-08-23 18:17:37 +0300

    sprint_311_67

--- system/module_page/frontend/form.js
+++ system/module_page/frontend/form.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', function(){
 
+  document.querySelectorAll('input[type=range]')
+          .each(function(){
+    var c_value_box = document.createElement('div');
+    this.parentNode.insertBefore(c_value_box, this.nextSibling);
+    this.addEventListener('mousemove', function(){
+      c_value_box.innerHTML = this.value;
+    });
+  });
+
 });
