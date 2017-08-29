ffc787ee0d7a1e2a17e5dae38ec3ae3a1fb91806
Maxim Rysevets <root@effcore.com>
2017-08-29 17:44:28 +0300

    sprint_311_73

--- system/module_page/frontend/page.js
+++ system/module_page/frontend/page.js
@@ -11,4 +11,7 @@ document.addEventListener('DOMContentLoaded', function(){
     }
   };
 
+/* this code activate hover state on ios devices */
+  document.addEventListener('touchstart', function(){}, false);
+
 });
