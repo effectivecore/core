e087f9e2b0b7ac97e83b4ae352bb2921a4cd07ef
Maxim Rysevets <root@effcore.com>
2017-09-03 13:02:03 +0300

    sprint_311_98_3

--- system/module_storage/backend/Events_Storage.php
+++ system/module_storage/backend/Events_Storage.php
@@ -9,22 +9,22 @@ namespace effectivecore\modules\storage {
           use \effectivecore\console_factory as console;
           abstract class events_storage {
 
-  static function on_storage_init_before($id) {
+  static function on_storage_init_before(&$instance) {
     timers::tap('init_pdo');
   }
 
-  static function on_storage_init_after($id) {
+  static function on_storage_init_after(&$instance) {
     timers::tap('init_pdo');
     console::add_log(
-      'query', 'The database "'.$id.'" was initialized on first request.', 'ok', timers::get_period('init_pdo', 0, 1)
+      'query', 'The database "'.$instance->id.'" was initialized on first request.', 'ok', timers::get_period('init_pdo', 0, 1)
     );
   }
 
-  static function on_query_before(&$query) {
+  static function on_query_before(&$instance, &$query) {
     timers::tap('query_'.md5($query));
   }
 
-  static function on_query_after($query, &$result, $errors) {
+  static function on_query_after(&$instance, &$query, &$result, &$errors) {
     timers::tap('query_'.md5($query));
     console::add_log(
       'query', $query, $errors[0] == '00000' ? 'ok' : 'error', timers::get_period('query_'.md5($query), -1, -2)
