9a909f72ed1a34830f7f9440bbd2c3487c7817c7
Maxim Rysevets <root@effcore.com>
2017-09-12 00:54:44 +0300

    sprint_311_106_1

---
+++ system/module_page/backend/patterns/Actions_list.php
@@ -0,0 +1,32 @@
+<?php
+
+  #############################################################
+  ### Copyright © 2017 Maxim Rysevets. All rights reserved. ###
+  #############################################################
+
+namespace effectivecore {
+          class actions_list extends markup {
+
+  public $tag_name = 'x-actions-list';
+  public $values = [];
+  public $active = [];
+
+  function __construct($values = [], $active = [], $attributes = [], $weight = 0) {
+    $this->values = factory::array_values_map_to_keys($values);
+    $this->active = factory::array_values_map_to_keys($active);
+    parent::__construct($attributes, [], $weight);
+  }
+
+  function render() {
+    foreach ($this->values as $c_value) {
+      $c_attr = isset($this->active[$c_value]) ? ['class' => ['active']] : [];
+      $this->child_insert(
+        new markup('x-action', $c_attr,
+          new markup('a', ['href' => '?action='.$c_value], $c_value)
+        )
+      );
+    }
+    return parent::render();
+  }
+
+}}
