4cad9d61960c72c3923a2e838043521da6b25be1
Maxim Rysevets <root@effcore.com>
2017-09-11 10:51:52 +0300

    sprint_311_104

--- system/module_locale/backend/Languages__factory.php
+++ system/module_locale/backend/Languages__factory.php
@@ -12,7 +12,7 @@ namespace effectivecore {
   protected static $current_code;
 
   static function init() {
-    static::$current_code = storages::get('settings')->select('languages')['translate']->current;
+    static::$current_code = storages::get('settings')->select('languages')['locales']->current;
     foreach (storages::get('settings')->select('languages') as $module_id => $languages) {
       foreach ($languages->available as $c_lang) {
         static::$data[$c_lang->code] = $c_lang;
