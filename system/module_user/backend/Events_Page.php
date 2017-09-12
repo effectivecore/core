1e556cac061c89bcb6edf1e8794b017dcb4ae147
Maxim Rysevets <root@effcore.com>
2017-09-12 12:52:40 +0300

    sprint_311_106_4

--- system/module_user/backend/Events_Page.php
+++ system/module_user/backend/Events_Page.php
@@ -16,38 +16,7 @@ namespace effectivecore\modules\user {
           use \effectivecore\modules\user\users_factory as users;
           abstract class events_page extends \effectivecore\events_page {
 
-  static function on_show_admin_roles() {
-    $head = [['ID', 'Title', 'Is embed']];
-    $body = entities::get('role')->select_set();
-    foreach ($body as $c_row) {
-      $c_row->is_embed = $c_row->is_embed ? 'Yes' : 'No';
-    }
-    return new table([], $body, $head);
-  }
-
-  static function on_show_admin_users() {
-    $pager = new pager();
-    if ($pager->has_error) {
-      factory::send_header_and_exit('not_found',
-        'Page not found!'
-      );
-    } else {
-      $head = [['ID', 'EMail', 'Password hash', 'Created', 'Is embed', 'Actions']];
-      $body = entities::get('user')->select_set();
-      foreach ($body as $c_row) {
-        $c_actions = new markup('ul', ['class' => ['actions' => 'actions']]);
-        $c_actions->child_insert( new markup('li', [], new markup('a', ['href' => (new url('/user/'.$c_row->id))->get_full()], 'view') ) );
-        $c_actions->child_insert( new markup('li', [], new markup('a', ['href' => (new url('/user/'.$c_row->id.'/edit?'.urls::make_back_part()))->get_full()], 'edit') ) );
-        if ($c_row->is_embed != 1) $c_actions->child_insert( new markup('li', [], new markup('a', ['href' => (new url('/admin/users/delete/'.$c_row->id.'?'.urls::make_back_part()))->get_full()], 'delete') ) );
-        $c_row->is_embed = $c_row->is_embed ? 'Yes' : 'No';
-        $c_row->password_hash = '*****';
-        $c_row->actions = $c_actions;
-      }
-      return new table([], $body, $head);
-    }
-  }
-
-  static function on_show_admin_users_delete_n($user_id) {
+  static function on_check_access_user_n_delete($user_id) {
     $user = (new entity_instance('entities/user/user', ['id' => $user_id]))->select();
     if ($user) {
       if ($user->is_embed == 1) {
@@ -62,11 +31,63 @@ namespace effectivecore\modules\user {
     }
   }
 
-  static function on_show_user_n($id) {
+  static function on_check_access_user_n_edit($user_id) {
+    $user = (new entity_instance('entities/user/user', ['id' => $user_id]))->select();
+    if ($user) {
+      if (!($user->id == users::get_current()->id ||                # not owner or
+                   isset(users::get_current()->roles['admins']))) { # not admin
+        factory::send_header_and_exit('access_denided',
+          'Access denided!'
+        );
+      }
+    } else {
+      factory::send_header_and_exit('not_found',
+        'User not found!'
+      );
+    }
+  }
+
+  static function on_show_block_admin_roles() {
+    $block = new markup('x-block', ['id' => 'admin_roles']);
+    $thead = [['ID', 'Title', 'Is embed']];
+    $tbody = entities::get('role')->select_set();
+    foreach ($tbody as $c_row) {
+      $c_row->is_embed = $c_row->is_embed ? 'Yes' : 'No';
+    }
+    $block->child_insert(new table([], $tbody, $thead));
+    return $block;
+  }
+
+  static function on_show_block_admin_users() {
+    $pager = new pager();
+    if ($pager->has_error) {
+      factory::send_header_and_exit('not_found',
+        'Page not found!'
+      );
+    } else {
+      $block = new markup('x-block', ['id' => 'admin_users']);
+      $thead = [['ID', 'EMail', 'Password hash', 'Created', 'Is embed', 'Actions']];
+      $tbody = entities::get('user')->select_set();
+      foreach ($tbody as $c_row) {
+        $c_actions = new markup('ul', ['class' => ['actions' => 'actions']]);
+        $c_actions->child_insert( new markup('li', [], new markup('a', ['href' => (new url('/user/'.$c_row->id))->get_full()], 'view') ) );
+        $c_actions->child_insert( new markup('li', [], new markup('a', ['href' => (new url('/user/'.$c_row->id.'/edit?'.urls::make_back_part()))->get_full()], 'edit') ) );
+        if ($c_row->is_embed != 1) $c_actions->child_insert( new markup('li', [], new markup('a', ['href' => (new url('/admin/users/delete/'.$c_row->id.'?'.urls::make_back_part()))->get_full()], 'delete') ) );
+        $c_row->is_embed = $c_row->is_embed ? 'Yes' : 'No';
+        $c_row->password_hash = '*****';
+        $c_row->actions = $c_actions;
+      }
+      $block->child_insert(new table([], $tbody, $thead));
+      return $block;
+    }
+  }
+
+  static function on_show_block_user_n($id) {
     $user = (new entity_instance('entities/user/user', ['id' => $id]))->select();
     if ($user) {
       if ($user->id == users::get_current()->id ||               # owner
                  isset(users::get_current()->roles['admins'])) { # admin
+        $block = new markup('x-block', ['id' => 'user']);
       # get roles
         $roles = [];
         $db_roles = entities::get('relation_role_ws_user')->select_set(['user_id' => $id]);
@@ -81,26 +102,11 @@ namespace effectivecore\modules\user {
         $values['password_hash'] = '*****';
         $values['is_embed'] = $values['is_embed'] ? 'Yes' : 'No';
       # show table
-        $head = [['Parameter', 'Value']];
-        $body = factory::array_rotate([array_keys($values), array_values($values)]);
-        return new table([], $body, $head);
+        $thead = [['Parameter', 'Value']];
+        $tbody = factory::array_rotate([array_keys($values), array_values($values)]);
+        $block->child_insert(new table([], $tbody, $thead));
+        return $block;
       } else {
-        factory::send_header_and_exit('access_denided',
-          'Access denided!'
-        );
-      }
-    } else {
-      factory::send_header_and_exit('not_found',
-        'User not found!'
-      );
-    }
-  }
-
-  static function on_show_user_n_edit($user_id) {
-    $user = (new entity_instance('entities/user/user', ['id' => $user_id]))->select();
-    if ($user) {
-      if (!($user->id == users::get_current()->id ||                # not owner or
-                   isset(users::get_current()->roles['admins']))) { # not admin
         factory::send_header_and_exit('access_denided',
           'Access denided!'
         );
