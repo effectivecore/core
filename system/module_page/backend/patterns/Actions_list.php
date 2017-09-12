692eebfd4c0e32d443c116508ca31b713ca00607
Maxim Rysevets <root@effcore.com>
2017-09-13 01:00:14 +0300

    sprint_311_106_8

--- system/module_page/backend/patterns/Actions_list.php
+++ system/module_page/backend/patterns/Actions_list.php
@@ -7,20 +7,25 @@
 namespace effectivecore {
           class actions_list extends markup {
 
-  public $tag_name = 'x-actions-list';
+  public $tag_name = 'x-actions-control';
+  public $title = null;
   public $values = [];
   public $active = [];
 
-  function __construct($values = [], $active = [], $attributes = [], $weight = 0) {
+  function __construct($values = [], $active = [], $title = null, $attributes = [], $weight = 0) {
     $this->values = factory::array_values_map_to_keys($values);
     $this->active = factory::array_values_map_to_keys($active);
+    if (!is_null($title)) $this->title = $title;
     parent::__construct($attributes, [], $weight);
   }
 
   function render() {
+    $this->child_insert(new markup('x-title', [], !is_null($this->title) ? $this->title : 'actions'), 'title');
+    $this->child_insert(new markup('x-action-list', [], []), 'action_list');
+    $list = $this->child_select('action_list');
     foreach ($this->values as $c_value) {
       $c_attr = isset($this->active[$c_value]) ? ['class' => ['active']] : [];
-      $this->child_insert(
+      $list->child_insert(
         new markup('x-action', $c_attr, [
           new markup('a', ['href' => '?action='.$c_value], $c_value)
         ])
