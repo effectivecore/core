4278034bef002408cde519cfcac6ccba4c22f091
Maxim Rysevets <root@effcore.com>
2017-09-12 12:25:44 +0300

    sprint_311_106_3

--- system/module_develop/module_demo/backend/Events_Page.php
+++ system/module_develop/module_demo/backend/Events_Page.php
@@ -6,18 +6,27 @@
 
 namespace effectivecore\modules\develop {
           use \effectivecore\markup;
+          use \effectivecore\table as table;
+          use \effectivecore\table_body_row as table_body_row;
+          use \effectivecore\table_body_row_cell as table_body_row_cell;
           abstract class events_page extends \effectivecore\events_page {
 
   static function on_show_block_demo_dynamic() {
-    $block = new markup('div', ['id' => 'block_demo_dynamic', 'class' => ['block']]);
-    $block->child_insert(new markup('h2', [], 'Dynamic block'));
-    $block->child_insert(new markup('div', [],
-      'test test test test test test test test test test test test test test test'.
-      'test test test test test test test test test test test test test test test'.
-      'test test test test test test test test test test test test test test test'.
-      'test test test test test test test test test test test test test test test'.
-      'test test test test test test test test test test test test test test test'
-    ));
+    $block = new markup('x-block', ['id' => 'demo_dynamic']);
+  # title
+    $block->child_insert(
+      new markup('h2', [], 'Dynamic block')
+    );
+  # table
+    $thead = [['th 1', 'th 2', 'th 3']];
+    $tbody = [
+      ['td 1.1', 'td 1.2', 'td 1.3'],
+      ['td 2.1', 'td 2.2', new table_body_row_cell([], 'td 2.3')],
+      new table_body_row([], ['td 3.1', 'td 3.2', new table_body_row_cell([], 'td 3.3')])
+    ];
+    $block->child_insert(
+      new table(['class' => 'table'], $tbody, $thead)
+    );
     return $block;
   }
 
