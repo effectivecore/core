2480fffbd2e85c566f5b10e42f930a6175df338a
Maxim Rysevets <root@effcore.com>
2017-09-13 16:02:13 +0300

    sprint_311_106_17

--- system/module_page/backend/patterns/Table.php
+++ system/module_page/backend/patterns/Table.php
@@ -5,22 +5,14 @@
   #############################################################
 
 namespace effectivecore {
-          class table extends \effectivecore\node {
+          class table extends \effectivecore\markup {
 
-  public $template = 'table';
+  public $tag_name = 'table';
 
   function __construct($attributes = [], $tbody = [], $thead = [], $weight = 0) {
-    parent::__construct($attributes, null, $weight);
-    $this->child_insert(new table_head(null, $thead), 'head');
-    $this->child_insert(new table_body(null, $tbody), 'body');
-  }
-
-  function render() {
-    return (new template($this->template, [
-      'attributes' => factory::data_to_attr($this->attribute_select()),
-      'head'       => isset($this->children['head']) ? $this->children['head']->render() : '',
-      'body'       => isset($this->children['body']) ? $this->children['body']->render() : '',
-    ]))->render();
+    parent::__construct($this->tag_name, $attributes, null, $weight);
+    if ($thead) $this->child_insert(new table_head(null, $thead), 'head');
+    if ($tbody) $this->child_insert(new table_body(null, $tbody), 'body');
   }
 
 }}
