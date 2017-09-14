ff6181f1ac5db3d72059063976c128932c74e588
Maxim Rysevets <root@effcore.com>
2017-09-14 22:02:40 +0300

    sprint_311_106_34

---
+++ system/module_page/backend/patterns/Markup_simple.php
@@ -0,0 +1,25 @@
+<?php
+
+  #############################################################
+  ### Copyright © 2017 Maxim Rysevets. All rights reserved. ###
+  #############################################################
+
+namespace effectivecore {
+          class markup_simple extends \effectivecore\node_simple {
+
+  public $tag_name = 'div';
+  public $template = 'markup_element_simple';
+
+  function __construct($tag_name = null, $attributes = [], $weight = 0) {
+    if ($tag_name) $this->tag_name = $tag_name;
+    parent::__construct($attributes, $weight);
+  }
+
+  function render() {
+    return (new template($template, [
+      'tag_name'   => $this->tag_name,
+      'attributes' => factory::data_to_attr($this->attribute_select()),
+    ]))->render();
+  }
+
+}}
