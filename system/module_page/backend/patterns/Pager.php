9deedf9ed12ef68733b99076b3cf30dafa370982
Maxim Rysevets <root@effcore.com>
2017-09-14 15:57:34 +0300

    sprint_311_106_31

--- system/module_page/backend/patterns/Pager.php
+++ system/module_page/backend/patterns/Pager.php
@@ -10,32 +10,35 @@ namespace effectivecore {
   public $id;
   public $tag_name = 'x-pager';
   public $has_error = false;
+  public $page_num_min = 1;
+  public $page_num_max = 100;
+  public $page_num_cur = 50;
 
   function __construct($attributes = [], $weight = 0) {
     parent::__construct($attributes, [], $weight);
   }
 
-  function get_current_page_num() {
-    return 1;
+  function get_page_num_cur() {
+    return $this->page_num_cur;
   }
 
   function render() {
     $pager = new markup($this->tag_name);
-    $pager->child_insert(new markup('a', ['href' => '#'], new text(1)));
-    $pager->child_insert(new markup('a', ['href' => '#'], new text('...')));
-    $pager->child_insert(new markup('a', ['href' => '#'], new text(25)));
-    $pager->child_insert(new markup('a', ['href' => '#'], new text('...')));
-    $pager->child_insert(new markup('a', ['href' => '#'], new text(47)));
-    $pager->child_insert(new markup('a', ['href' => '#'], new text(48)));
-    $pager->child_insert(new markup('a', ['href' => '#'], new text(49)));
-    $pager->child_insert(new markup('a', ['href' => '#', 'class' => ['active']], new text(50)));
-    $pager->child_insert(new markup('a', ['href' => '#'], new text(51)));
-    $pager->child_insert(new markup('a', ['href' => '#'], new text(52)));
-    $pager->child_insert(new markup('a', ['href' => '#'], new text(53)));
-    $pager->child_insert(new markup('a', ['href' => '#'], new text('...')));
-    $pager->child_insert(new markup('a', ['href' => '#'], new text(75)));
-    $pager->child_insert(new markup('a', ['href' => '#'], new text('...')));
-    $pager->child_insert(new markup('a', ['href' => '#'], new text(100)));
+    $pager->child_insert(new markup('a', ['href' => '#'], '1'));
+    $pager->child_insert(new text('...'));
+    $pager->child_insert(new markup('a', ['href' => '#'], '25'));
+    $pager->child_insert(new text('...'));
+    $pager->child_insert(new markup('a', ['href' => '#'], '47'));
+    $pager->child_insert(new markup('a', ['href' => '#'], '48'));
+    $pager->child_insert(new markup('a', ['href' => '#'], '49'));
+    $pager->child_insert(new markup('a', ['href' => '#', 'class' => ['active']], '50'));
+    $pager->child_insert(new markup('a', ['href' => '#'], '51'));
+    $pager->child_insert(new markup('a', ['href' => '#'], '52'));
+    $pager->child_insert(new markup('a', ['href' => '#'], '53'));
+    $pager->child_insert(new text('...'));
+    $pager->child_insert(new markup('a', ['href' => '#'], '75'));
+    $pager->child_insert(new text('...'));
+    $pager->child_insert(new markup('a', ['href' => '#'], '100'));
     return $pager->render();
   }
 
