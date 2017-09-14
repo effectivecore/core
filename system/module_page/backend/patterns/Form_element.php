839b09a1b02dafe5418bd0766d47ed598f1bd459
Maxim Rysevets <root@effcore.com>
2017-09-15 00:36:35 +0300

    sprint_311_106_41

--- system/module_page/backend/patterns/Form_element.php
+++ system/module_page/backend/patterns/Form_element.php
@@ -8,29 +8,29 @@ namespace effectivecore {
           use \effectivecore\translations_factory as translations;
           class form_element extends \effectivecore\markup {
 
-  public $template_element = 'form_element';
-  public $template_title = 'form_title';
-  public $title = '';
-  public $description = '';
+  public $wr_template = 'form_element';
+  public $wr_title_template = 'form_title';
+  public $title = null;
+  public $description = null;
 
-  function __construct($tag_name = '', $title = '', $description = '', $attributes = [], $children = [], $weight = 0) {
+  function __construct($tag_name = null, $title = null, $description = null, $attributes = [], $weight = 0) {
     if ($title)       $this->title       = $title;
     if ($description) $this->description = $description;
-    parent::__construct($tag_name, $attributes, $children, $weight);
+    parent::__construct($tag_name, $attributes, [], $weight);
   }
 
   function render() {
     $is_right = !empty($this->title_position) && $this->title_position == 'right';
-    return (new template($this->template_element, [
+    return (new template($this->wr_template, [
       'title_t'     => $is_right ? '' : $this->render_self(),
       'title_b'     => $is_right ?      $this->render_self() : '',
-      'element'     => parent::render(),
+      'content'     => parent::render(),
       'description' => $this->render_description()
     ]))->render();
   }
 
   function render_self() {
-    return empty($this->title) ? '' : (new template($this->template_title, [
+    return empty($this->title) ? '' : (new template($this->wr_title_template, [
       'tag_name'      => 'label',
       'title'         => translations::get($this->title),
       'required_mark' => $this->attribute_select('required') ? $this->render_required_mark() : ''
