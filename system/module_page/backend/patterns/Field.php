d9a9b57d74d1defe9fa01c169b16907ebc670a09
Maxim Rysevets <root@effcore.com>
2017-09-15 16:43:09 +0300

    sprint_311_106_42_4

---
+++ system/module_page/backend/patterns/Field.php
@@ -0,0 +1,12 @@
+<?php
+
+  #############################################################
+  ### Copyright © 2017 Maxim Rysevets. All rights reserved. ###
+  #############################################################
+
+namespace effectivecore {
+          class form_field extends \effectivecore\form_box {
+
+  public $tag_name = 'x-form-field';
+
+}}
