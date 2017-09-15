4a0d9904056d88ea6da1815d869937f20667aead
Maxim Rysevets <root@effcore.com>
2017-09-15 17:25:14 +0300

    sprint_311_106_42_5

--- system/module_page/backend/patterns/Field.php
+++ system/module_page/backend/patterns/Field.php
@@ -5,8 +5,20 @@
   #############################################################
 
 namespace effectivecore {
+          use \effectivecore\translations_factory as translations;
           class form_field extends \effectivecore\form_box {
 
   public $tag_name = 'x-form-field';
+
+  function render_description() {
+    $return = [];
+    $default = $this->child_select('default');
+    if ($default && !empty($default->attributes['minlength'])) $return[] = new markup('p', ['class' => ['minlength' => 'minlength']], translations::get('Field must contain a minimum of %%_lenght characters.', ['lenght' => $default->attributes['minlength']]));
+    if ($default && !empty($default->attributes['maxlength'])) $return[] = new markup('p', ['class' => ['maxlength' => 'maxlength']], translations::get('Field must contain a maximum of %%_lenght characters.', ['lenght' => $default->attributes['maxlength']]));
+    if ($this->description)                                    $return[] = new markup('p', [],        is_string($this->description) ? translations::get($this->description) : $this->description);
+    if (count($return)) {
+      return (new markup('x-description', [], $return))->render();
+    }
+  }
 
 }}
