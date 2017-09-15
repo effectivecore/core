d483bed00dedc44239891b203d55b8128c888eb9
Maxim Rysevets <root@effcore.com>
2017-09-15 15:40:32 +0300

    sprint_311_106_42_2

--- system/module_page/backend/patterns/Markup.php
+++ system/module_page/backend/patterns/Markup.php
@@ -5,7 +5,6 @@
   #############################################################
 
 namespace effectivecore {
-          use \effectivecore\translations_factory as translations;
           class markup extends \effectivecore\node {
 
   public $tag_name = 'div';
@@ -27,22 +26,6 @@ namespace effectivecore {
       'attributes' => factory::data_to_attr($this->attribute_select()),
       'content'    => $this->render_children($this->children)
     ]))->render();
-  }
-
-  ###########################################
-  ### functionality for inherited classes ###
-  ###########################################
-
-  function render_required_mark() {
-    return (new markup('b', ['class' => ['required' => 'required']], '*'))->render();
-  }
-
-  function render_description() {
-    $return = [];
-    if (!empty($this->description))             $return[] = new markup('p', [], is_string($this->description) ? translations::get($this->description) : $this->description);
-    if (!empty($this->attributes['minlength'])) $return[] = new markup('p', ['class' => ['minlength' => 'minlength']], translations::get('Field must contain a minimum of %%_lenght characters.', ['lenght' => $this->attributes['minlength']]));
-    if (!empty($this->attributes['maxlength'])) $return[] = new markup('p', ['class' => ['maxlength' => 'maxlength']], translations::get('Field must contain a maximum of %%_lenght characters.', ['lenght' => $this->attributes['maxlength']]));
-    return count($return) ? (new markup('x-description', [], $return))->render() : '';
   }
 
 }}
