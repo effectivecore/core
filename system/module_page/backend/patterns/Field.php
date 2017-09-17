3844bdf59973b67b2c067905544656d630f986f0
Maxim Rysevets <root@effcore.com>
2017-09-17 13:15:21 +0300

    sprint_311_106_42_7

--- system/module_page/backend/patterns/Field.php
+++ system/module_page/backend/patterns/Field.php
@@ -15,7 +15,7 @@ namespace effectivecore {
     $default = $this->child_select('default');
     if ($default && !empty($default->attributes['minlength'])) $return[] = new markup('p', ['class' => ['minlength' => 'minlength']], translations::get('Field must contain a minimum of %%_lenght characters.', ['lenght' => $default->attributes['minlength']]));
     if ($default && !empty($default->attributes['maxlength'])) $return[] = new markup('p', ['class' => ['maxlength' => 'maxlength']], translations::get('Field must contain a maximum of %%_lenght characters.', ['lenght' => $default->attributes['maxlength']]));
-    if ($this->description)                                    $return[] = new markup('p', [],        is_string($this->description) ? translations::get($this->description) : $this->description);
+    if ($this->description)                                    $return[] = new markup('p', [], $this->description);
     if (count($return)) {
       return (new markup('x-description', [], $return))->render();
     }
