ca6308ed3d293f6d5527f0b4625045cffd76abcb
Maxim Rysevets <root@effcore.com>
2017-09-17 13:33:13 +0300

    sprint_311_106_42_8

--- system/module_page/backend/patterns/Box_Palette.php
+++ system/module_page/backend/patterns/Box_Palette.php
@@ -7,13 +7,13 @@
 namespace effectivecore {
           use \effectivecore\translations_factory as translations;
           use \effectivecore\modules\storage\storages_factory as storages;
-          class form_field_palette extends \effectivecore\form_container_radios {
+          class form_box_palette extends \effectivecore\form_box_radios {
 
   function build() {
     $this->attribute_insert('class', ['palette' => 'palette']);
     foreach (storages::get('settings')->select('colors') as $module_id => $c_colors) {
       foreach ($c_colors as $c_color_id => $c_color_info) {
-        $this->item_insert('', [
+        $this->field_insert('', [
           'value' => $c_color_id,
           'title' => translations::get('Color ID = %%_id (value = %%_value)', ['id' => $c_color_id, 'value' => $c_color_info->value]),
           'style' => ['background-color: '.$c_color_info->value]
@@ -22,8 +22,8 @@ namespace effectivecore {
     }
   }
 
-  function item_insert($title = '', $attr = []) {
-    return parent::item_insert($title, $attr + ['name' => $this->name]);
+  function field_insert($title = '', $attr = []) {
+    return parent::field_insert($title, $attr + ['name' => $this->name]);
   }
 
 }}
