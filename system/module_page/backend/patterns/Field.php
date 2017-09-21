6c81e27d4f143f0bb257061ee359b6749cc2840e
Maxim Rysevets <root@effcore.com>
2017-09-22 00:44:50 +0300

    sprint_311_119_3

--- system/module_page/backend/patterns/Field.php
+++ system/module_page/backend/patterns/Field.php
@@ -11,11 +11,18 @@ namespace effectivecore {
   public $tag_name = 'x-field';
   public $title_tag_name = 'label';
 
+  function render() {
+    $default = $this->child_select('default');
+    if ($default instanceof node_simple && $default->attribute_select('disabled')) $this->attribute_insert('class', ['disabled' => 'disabled']);
+    if ($default instanceof node_simple && $default->attribute_select('required')) $this->attribute_insert('class', ['required' => 'required']);
+    return parent::render();
+  }
+
   function render_self() {
     $default = $this->child_select('default');
     if ($this->title) {
-      $required_mark = $this->attribute_select('required') ||
-                    $default->attribute_select('required') ? $this->render_required_mark() : '';
+      $required_mark = $this->attribute_select('required') || ($default instanceof node_simple && $default->attribute_select('required')) ?
+                       $this->render_required_mark() : '';
       return (new markup($this->title_tag_name, [], [
         $this->title, $required_mark
       ]))->render();
@@ -25,9 +32,9 @@ namespace effectivecore {
   function render_description() {
     $return = [];
     $default = $this->child_select('default');
-    if ($default && !empty($default->attributes['minlength'])) $return[] = new markup('p', ['class' => ['minlength' => 'minlength']], translations::get('Field must contain a minimum of %%_lenght characters.', ['lenght' => $default->attributes['minlength']]));
-    if ($default && !empty($default->attributes['maxlength'])) $return[] = new markup('p', ['class' => ['maxlength' => 'maxlength']], translations::get('Field must contain a maximum of %%_lenght characters.', ['lenght' => $default->attributes['maxlength']]));
-    if ($this->description)                                    $return[] = new markup('p', [], $this->description);
+    if ($default instanceof node_simple && !empty($default->attributes['minlength'])) $return[] = new markup('p', ['class' => ['minlength' => 'minlength']], translations::get('Field must contain a minimum of %%_lenght characters.', ['lenght' => $default->attributes['minlength']]));
+    if ($default instanceof node_simple && !empty($default->attributes['maxlength'])) $return[] = new markup('p', ['class' => ['maxlength' => 'maxlength']], translations::get('Field must contain a maximum of %%_lenght characters.', ['lenght' => $default->attributes['maxlength']]));
+    if ($this->description)                                                           $return[] = new markup('p', [], $this->description);
     if (count($return)) {
       return (new markup($this->description_tag_name, [], $return))->render();
     }
