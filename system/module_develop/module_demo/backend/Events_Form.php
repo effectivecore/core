34a06cf82255b1bfe88cb8de32643006023122e9
Maxim Rysevets <root@effcore.com>
2017-09-23 13:28:49 +0300

    sprint_311_119_9_6

--- system/module_develop/module_demo/backend/Events_Form.php
+++ system/module_develop/module_demo/backend/Events_Form.php
@@ -9,12 +9,12 @@ namespace effectivecore\modules\demo {
           abstract class events_form extends \effectivecore\events_form {
 
   static function on_init_demo($form, $fields) {
-    $fields['fieldset_default/field_select_macro']->option_insert('Option 1.5 (inserted from init)', 'option_1_5', [], 'group_1_1');
-    $fields['fieldset_default/field_select_macro']->option_insert('Option 1.6 (inserted + disabled from init)', 'option_1_6', ['disabled' => 'disabled'], 'group_1_1');
-    $fields['fieldset_default/field_select_macro']->optgroup_insert('group_1_2', 'Group 1.2 (inserted from init)');
-    $fields['fieldset_default/field_select_macro']->option_insert('Option 1.7 (inserted from init)', 'option_1_7', [], 'group_1_2');
-    $fields['fieldset_default/field_select_macro']->option_insert('Option 1.8 (inserted from init)', 'option_1_8', [], 'group_1_2');
-    $fields['fieldset_default/field_select_macro']->option_insert('Option 1.9 (inserted from init)', 'option_1_9', [], 'group_1_2');
+    $fields['fieldset_default/field_select_macro']->option_insert('Option 2.5 (inserted + disabled from init)', 'option_2_5', ['disabled' => 'disabled'], 'group_2_1');
+    $fields['fieldset_default/field_select_macro']->option_insert('Option 2.6 (inserted from init)', 'option_2_6', [], 'group_2_1');
+    $fields['fieldset_default/field_select_macro']->optgroup_insert('group_2_2', 'Group 2.2 (inserted from init)');
+    $fields['fieldset_default/field_select_macro']->option_insert('Option 2.7 (inserted from init)', 'option_2_7', [], 'group_2_2');
+    $fields['fieldset_default/field_select_macro']->option_insert('Option 2.8 (inserted from init)', 'option_2_8', [], 'group_2_2');
+    $fields['fieldset_default/field_select_macro']->option_insert('Option 2.9 (inserted from init)', 'option_2_9', [], 'group_2_2');
   }
 
   static function on_submit_demo($form, $fields, &$values) {
