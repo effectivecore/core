803029c253f6bcc2e1093efa38c1290a93cc30c8
Maxim Rysevets <root@effcore.com>
2017-10-02 12:46:06 +0300

    sprint_311_167

--- system/module_core/backend/patterns/Entity_instance.php
+++ system/module_core/backend/patterns/Entity_instance.php
@@ -20,7 +20,6 @@ namespace effectivecore {
   function __get($name)         {return $this->values[$name];}
   function __set($name, $value) {$this->values[$name] = $value;}
 
-  function set_entity_name($entity_name) {$this->entity_name = $entity_name;}
   function set_values($values) {$this->values = $values;}
   function get_values($names = []) {
     if (count($names)) {
@@ -34,28 +33,30 @@ namespace effectivecore {
     }
   }
 
-  function get_entity()        {return entities::get($this->entity_name);}
-  function get_entity_name()   {return $this->get_entity()->get_name();}
-  function get_entity_fields() {return $this->get_entity()->get_fields();}
-  function get_entity_ids()    {return $this->get_entity()->get_ids();}
+  function get_entity()            {return entities::get($this->entity_name);}
+  function get_entity_name()       {return $this->get_entity()->get_name();}
+  function get_entity_fields()     {return $this->get_entity()->get_fields();}
+  function get_entity_ids()        {return $this->get_entity()->get_ids();}
+  function get_entity_storage_id() {return $this->get_entity()->get_storage_id();}
+  function set_entity_name($entity_name) {$this->entity_name = $entity_name;}
 
   function select($custom_ids = []) {
-    $storage = storages::get($this->get_entity()->get_storage_id());
+    $storage = storages::get($this->get_entity_storage_id());
     return $storage->select_instance($this, $custom_ids);
   }
 
   function insert() {
-    $storage = storages::get($this->get_entity()->get_storage_id());
+    $storage = storages::get($this->get_entity_storage_id());
     return $storage->insert_instance($this);
   }
 
   function update() {
-    $storage = storages::get($this->get_entity()->get_storage_id());
+    $storage = storages::get($this->get_entity_storage_id());
     return $storage->update_instance($this);
   }
 
   function delete() {
-    $storage = storages::get($this->get_entity()->get_storage_id());
+    $storage = storages::get($this->get_entity_storage_id());
     return $storage->delete_instance($this);
   }
 
