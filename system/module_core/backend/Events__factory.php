826f6e07e063a651fd9292683d3cbb44316347f2
Maxim Rysevets <root@effcore.com>
2017-10-04 19:04:35 +0300

    sprint_311_175_15_1

--- system/module_core/backend/Events__factory.php
+++ system/module_core/backend/Events__factory.php
@@ -14,15 +14,17 @@ namespace effectivecore {
 
   static function init() {
     console::add_log('event', 'init.', 'the event system was initialized', '-');
-    foreach (storages::get('settings')->select_group('events') as $module_id => $c_grp_events) {
-      foreach ($c_grp_events as $c_type => $c_events) {
-        foreach ($c_events as $c_id => $c_event) {
-          static::$data->{$c_type}[$c_id] = $c_event;
+    foreach (storages::get('settings')->select_group('events') as $c_events_by_module) {
+      foreach ($c_events_by_module as $c_type => $c_events_by_type) {
+        foreach ($c_events_by_type as $c_event) {
+          static::$data[$c_type][$c_event->for][] = $c_event;
         }
       }
     }
-    foreach (static::$data as &$c_type) {
-      factory::array_sort_by_weight($c_type);
+    foreach (static::$data as $c_type => $c_grp_events) {
+      foreach ($c_grp_events as &$c_events) {
+        factory::array_sort_by_weight($c_events);
+      }
     }
   }
 
@@ -33,15 +35,17 @@ namespace effectivecore {
 
   static function start($type, $id = null, $args = []) {
     $return = [];
-    if (!empty(static::get()->{$type})) {
-      foreach (static::get()->{$type} as $c_id => $c_info) {
-        if ($id == null || $id == $c_id) {
-          timers::tap('event call: '.$c_id);
-          $return[$c_info->handler][] = $c_return = call_user_func_array($c_info->handler, $args);
-          timers::tap('event call: '.$c_id);
-          console::add_log(
-            'event', 'call', ltrim($c_info->handler, '\\'), $c_return ? 'ok' : '-', timers::get_period('event call: '.$c_id, -1, -2)
-          );
+    if (!empty(static::get()[$type])) {
+      foreach (static::get()[$type] as $c_for => $c_events) {
+        foreach ($c_events as $c_event) {
+          if ($id == $c_for || $id == null) {
+            timers::tap('event call: '.$c_for);
+            $return[$c_event->handler][] = $c_return = call_user_func_array($c_event->handler, $args);
+            timers::tap('event call: '.$c_for);
+            console::add_log(
+              'event', 'call', ltrim($c_event->handler, '\\'), $c_return ? 'ok' : '-', timers::get_period('event call: '.$c_for, -1, -2)
+            );
+          }
         }
       }
     }
