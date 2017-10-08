515112e0b480cf2c92d506c5366ba6beaec7f7e9
Maxim Rysevets <root@effcore.com>
2017-10-08 13:49:54 +0300

    sprint_311_177_4

--- system/module_menu/backend/patterns/Tree_item.php
+++ system/module_menu/backend/patterns/Tree_item.php
@@ -12,6 +12,8 @@ namespace effectivecore {
           class tree_item extends \effectivecore\node {
 
   public $id;
+  public $parent_id;
+  public $parent_type;
   public $parent_npath = '';
   public $title = '';
   public $template = 'tree_item';
