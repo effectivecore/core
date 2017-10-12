a8503f502c37242ca765b7fa6b9209449170716c
Maxim Rysevets <root@effcore.com>
2017-10-12 16:14:36 +0300

    sprint_311_184_7

--- system/module_storage/backend/Events_Storage.php
+++ system/module_storage/backend/Events_Storage.php
@@ -10,22 +10,22 @@ namespace effectivecore\modules\storage {
           use \effectivecore\translations_factory as translations;
           abstract class events_storage {
 
-  static function on_storage_init_before(&$instance) {
+  static function on_storage_init_before($instance) {
     timers::tap('storage init');
   }
 
-  static function on_storage_init_after(&$instance) {
+  static function on_storage_init_after($instance) {
     timers::tap('storage init');
     console::add_log(
       'storage', 'init.', 'the storage "%%_name" was initialized', 'ok', timers::get_period('storage init', 0, 1), ['name' => $instance->id]
     );
   }
 
-  static function on_query_before(&$instance, &$query) {
+  static function on_query_before($instance, $query) {
     timers::tap('storage query: '.$query);
   }
 
-  static function on_query_after(&$instance, &$query, &$result, &$errors) {
+  static function on_query_after($instance, $query, $result, $errors) {
     timers::tap('storage query: '.$query);
     console::add_log(
       'storage', 'query', $query, $errors[0] == '00000' ? 'ok' : 'error', timers::get_period('storage query: '.$query, -1, -2)
