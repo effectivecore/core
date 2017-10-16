646a459c7a812edde6003014097342be962e57d5
Maxim Rysevets <root@effcore.com>
2017-10-16 17:48:24 +0300

    sprint_311_184_18

--- system/module_develop/module_demo/backend/Events_Form.php
+++ system/module_develop/module_demo/backend/Events_Form.php
@@ -13,12 +13,12 @@ namespace effectivecore\modules\demo {
   ##################
 
   static function on_init_demo($form, $fields) {
-    $fields['fieldset_default/field_select_macro']->option_insert('Option 2.5 (inserted + disabled from init)', 'option_2_5', ['disabled' => 'disabled'], 'group_2_1');
-    $fields['fieldset_default/field_select_macro']->option_insert('Option 2.6 (inserted from init)', 'option_2_6', [], 'group_2_1');
-    $fields['fieldset_default/field_select_macro']->optgroup_insert('group_2_2', 'Group 2.2 (inserted from init)');
-    $fields['fieldset_default/field_select_macro']->option_insert('Option 2.7 (inserted from init)', 'option_2_7', [], 'group_2_2');
-    $fields['fieldset_default/field_select_macro']->option_insert('Option 2.8 (inserted from init)', 'option_2_8', [], 'group_2_2');
-    $fields['fieldset_default/field_select_macro']->option_insert('Option 2.9 (inserted from init)', 'option_2_9', [], 'group_2_2');
+    $fields['form_elements/select_macro']->option_insert('Option 2.5 (inserted + disabled from init)', 'option_2_5', ['disabled' => 'disabled'], 'group_2_1');
+    $fields['form_elements/select_macro']->option_insert('Option 2.6 (inserted from init)', 'option_2_6', [], 'group_2_1');
+    $fields['form_elements/select_macro']->optgroup_insert('group_2_2', 'Group 2.2 (inserted from init)');
+    $fields['form_elements/select_macro']->option_insert('Option 2.7 (inserted from init)', 'option_2_7', [], 'group_2_2');
+    $fields['form_elements/select_macro']->option_insert('Option 2.8 (inserted from init)', 'option_2_8', [], 'group_2_2');
+    $fields['form_elements/select_macro']->option_insert('Option 2.9 (inserted from init)', 'option_2_9', [], 'group_2_2');
   }
 
   static function on_submit_demo($form, $fields, &$values) {
