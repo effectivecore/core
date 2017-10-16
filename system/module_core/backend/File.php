ed159fa3ccb93315c78058834a0899ad8e5e526e
Maxim Rysevets <root@effcore.com>
2017-10-16 16:03:59 +0300

    sprint_311_184_16

--- system/module_core/backend/File.php
+++ system/module_core/backend/File.php
@@ -57,12 +57,13 @@ namespace effectivecore {
   }
 
   function is_exist()          {return file_exists($this->get_path_full());}
+
   function get_dirs_info()     {return $this->dirs;}
-  function get_file_info()     {return $this->file;}
   function get_dirs_full()     {return $this->dirs->full;}
+  function get_dirs_relative() {return $this->dirs->relative;}
+  function get_file_info()     {return $this->file;}
   function get_file_full()     {return $this->file->full;}
   function get_path_full()     {return $this->dirs->full.'/'.$this->file->full;}
-  function get_dirs_relative() {return $this->dirs->relative;}
   function get_path_relative() {return $this->dirs->relative.'/'.$this->file->full;}
   function get_dir_parent()    {return ltrim(strrchr($this->dirs->full, '/'), '/');}
   function get_hash()          {return md5_file($this->get_path_relative());}
