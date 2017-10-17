95f2a27103689f455500437302e26d61bc7e88da
Maxim Rysevets <root@effcore.com>
2017-10-17 20:32:51 +0300

    sprint_311_184_64

--- system/module_user/backend/Events_Token.php
+++ system/module_user/backend/Events_Token.php
@@ -14,18 +14,18 @@ namespace effectivecore\modules\user {
   static function on_replace($match, $args = []) {
     if (!empty(users::get_current()->id)) {
       switch ($match) {
-        case '%%_user_id'    : return users::get_current()->id;
-        case '%%_user_email' : return users::get_current()->email;
-        case '%%_nick'       : return users::get_current()->nick;
+        case '%%_user_id' : return users::get_current()->id;
+        case '%%_email'   : return users::get_current()->email;
+        case '%%_nick'    : return users::get_current()->nick;
         case '%%_user_id_context':
-        case '%%_user_email_context':
+        case '%%_email_context':
         case '%%_nick_context':
           $arg_num = $args[0];
           $user_id = urls::get_current()->get_args($arg_num);
           $user = (new instance('user', ['id' => $user_id]))->select();
-          if ($user && $match == '%%_user_id_context')    return $user->id;
-          if ($user && $match == '%%_user_email_context') return $user->email;
-          if ($user && $match == '%%_nick_context')       return $user->nick;
+          if ($user && $match == '%%_user_id_context') return $user->id;
+          if ($user && $match == '%%_email_context')   return $user->email;
+          if ($user && $match == '%%_nick_context')    return $user->nick;
           return '[unknown uid]';
       }
     }
