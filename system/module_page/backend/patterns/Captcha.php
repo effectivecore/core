5cf1d53776584268ebe0cb3ef31f1146a323b2ff
Maxim Rysevets <root@effcore.com>
2017-11-09 23:57:21 +0300

    sprint_311_226

--- system/module_page/backend/patterns/Captcha.php
+++ system/module_page/backend/patterns/Captcha.php
@@ -10,11 +10,8 @@ namespace effectivecore {
   public $length = 8;
 
   function render() {
-    $canvas = new canvas_svg(20, 10);
+    $canvas = new canvas_svg(40, 15, 5);
     $canvas->fill_noise();
-  # $canvas->pixel_set(0, 0);
-  # $canvas->pixel_set(2, 0);
-  # $canvas->pixel_set(4, 0);
     return $canvas->render();
   }
 
