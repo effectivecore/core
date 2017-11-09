27a60fb5bdfc13de2bf23158b841de216fdd25ca
Maxim Rysevets <root@effcore.com>
2017-11-09 19:59:19 +0300

    sprint_311_221

---
+++ system/module_page/backend/patterns/SVG.php
@@ -0,0 +1,43 @@
+<?php
+
+  #############################################################
+  ### Copyright © 2017 Maxim Rysevets. All rights reserved. ###
+  #############################################################
+
+namespace effectivecore {
+          class svg extends \effectivecore\node_simple {
+
+  public $template = 'svg';
+  public $color_bg = 'white';
+  public $width = 0;
+  public $height = 0;
+  public $canvas = [];
+
+  function __construct($attributes = [], $weight = 0) {
+    parent::__construct($attributes, $weight);
+  }
+
+  function pixel_set($x, $y, $state = 1) {
+    $this->canvas[$x][$y] = $state;
+  }
+
+  function render() {
+    return (new template($this->template, [
+      'color_bg' => $this->color_bg,
+      'width'    => $this->width,
+      'height'   => $this->height,
+      'canvas'   => $this->render_canvas($this->canvas),
+    ]))->render();
+  }
+
+  function render_canvas($canvas) {
+    $return = [];
+    foreach ($this->canvas as $x => $x_row) {
+      foreach ($x_row as $y => $c_value) {
+        $return[] = '<rect style="fill:#000000" width="10" height="10" x="'.($x * 10).'" y="'.($y * 10).'" />';
+      }
+    }
+    return implode(nl, $return);
+  }
+
+}}
