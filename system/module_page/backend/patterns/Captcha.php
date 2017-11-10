73a27daa5134df1b25111172b1601ecef3cd7e43
Maxim Rysevets <root@effcore.com>
2017-11-10 20:13:08 +0300

    sprint_311_230

--- system/module_page/backend/patterns/Captcha.php
+++ system/module_page/backend/patterns/Captcha.php
@@ -8,21 +8,40 @@ namespace effectivecore {
           use \effectivecore\modules\storage\storage_factory as storage;
           class captcha extends \effectivecore\node_simple {
 
-  public $length = 8;
+  public $length = 6;
 
   function render() {
-    $characters = storage::get('settings')->select_group('captcha')['page']->characters;
-    $canvas = new canvas_svg(40, 15, 5);
+    foreach (storage::get('settings')->select_group('captcha')['page']->characters as $c_character) {
+      foreach ($c_character->glyphs as $c_glyph) {
+        $characters[$c_glyph] = $c_character->character;
+      }
+    }
+    $canvas = new canvas_svg(5 * $this->length, 15, 5);
     $canvas->fill('#000000', .9);
-    $canvas->glyph_set(rand(0,   1), rand(1, 5), $characters['ch0']->glyphs['default']);
-    $canvas->glyph_set(rand(4,   5), rand(1, 5), $characters['ch1']->glyphs['default']);
-    $canvas->glyph_set(rand(9,  11), rand(1, 5), $characters['ch2']->glyphs['default']);
-    $canvas->glyph_set(rand(14, 16), rand(1, 5), $characters['ch3']->glyphs['default']);
-    $canvas->glyph_set(rand(19, 21), rand(1, 5), $characters['ch4']->glyphs['default']);
-    $canvas->glyph_set(rand(24, 26), rand(1, 5), $characters['ch5']->glyphs['default']);
-    $canvas->glyph_set(rand(29, 31), rand(1, 5), $characters['ch6']->glyphs['default']);
-    $canvas->glyph_set(rand(34, 36), rand(1, 5), $characters['ch7']->glyphs['default']);
+    for ($i = 0; $i < $this->length; $i++) {
+      $canvas->glyph_set(
+        rand(0, 2) - 1 + ($i * 5),
+        rand(1, 5),
+        array_rand($characters)
+      );
+    }
     return $canvas->render();
   }
+
+   function render_demo() {
+     $canvas = new canvas_svg(105, 15, 5);
+     $canvas->fill('#ffffff');
+     $canvas->glyph_set( 5, 3, '01110|10001|10001|10001|10001|10001|10001|10001|10001|01110'); # 0
+     $canvas->glyph_set(15, 3, '00001|00001|00001|00001|00001|10001|01001|00101|00010|00001'); # 1
+     $canvas->glyph_set(25, 3, '11111|10000|01000|00100|00010|00001|00001|00001|00001|11110'); # 2
+     $canvas->glyph_set(35, 3, '01000|00100|00010|00001|11111|01000|00100|00010|00001|11111'); # 3
+     $canvas->glyph_set(45, 3, '00001|00001|00001|00001|01111|10001|01001|00101|00010|00001'); # 4
+     $canvas->glyph_set(55, 3, '01000|00100|00010|00001|01111|10000|10000|10000|10000|01111'); # 5
+     $canvas->glyph_set(65, 3, '01110|10001|10001|10001|10001|01110|10000|01000|00100|00010'); # 6
+     $canvas->glyph_set(75, 3, '10000|10000|10000|10000|10000|01000|00100|00010|00001|11111'); # 7
+     $canvas->glyph_set(85, 3, '01110|10001|10001|10001|10001|01110|10001|10001|10001|01110'); # 8
+     $canvas->glyph_set(95, 3, '01000|00100|00010|00001|01110|10001|10001|10001|10001|01110'); # 9
+     return $canvas->render();
+   }
 
 }}
