15395f3a7a28b6b25cbf5f43e0e0852371603bf8
Maxim Rysevets <root@effcore.com>
2017-11-10 19:16:12 +0300

    sprint_311_229

--- system/module_page/backend/patterns/Captcha.php
+++ system/module_page/backend/patterns/Captcha.php
@@ -5,21 +5,23 @@
   #############################################################
 
 namespace effectivecore {
+          use \effectivecore\modules\storage\storage_factory as storage;
           class captcha extends \effectivecore\node_simple {
 
   public $length = 8;
 
   function render() {
+    $characters = storage::get('settings')->select_group('captcha')['page']->characters;
     $canvas = new canvas_svg(40, 15, 5);
     $canvas->fill('#000000', .9);
-    $canvas->glyph_set(rand(0,   1), rand(1, 5), '00001|00001|00001|00001|00001|10001|01001|00101|00011|00001');
-    $canvas->glyph_set(rand(4,   5), rand(1, 5), '11111|10000|01000|00100|00010|00001|00001|00001|00001|11111');
-    $canvas->glyph_set(rand(9,  11), rand(1, 5), '01000|00100|00010|00001|11111|01000|00100|00010|00001|11111');
-    $canvas->glyph_set(rand(14, 16), rand(1, 5), '00001|00001|00001|00001|01111|10001|01001|00100|00011|00001');
-    $canvas->glyph_set(rand(19, 21), rand(1, 5), '01000|00100|00010|00001|11111|10000|10000|10000|10000|11111');
-    $canvas->glyph_set(rand(24, 26), rand(1, 5), '11111|10001|10001|10001|11111|10000|01000|00100|00010|00001');
-    $canvas->glyph_set(rand(29, 31), rand(1, 5), '10000|10000|10000|10000|10000|01000|00100|00010|00001|11111');
-    $canvas->glyph_set(rand(34, 36), rand(1, 5), '11111|10001|10001|10001|11111|10001|10001|10001|10001|11111');
+    $canvas->glyph_set(rand(0,   1), rand(1, 5), $characters['ch0']->glyphs['default']);
+    $canvas->glyph_set(rand(4,   5), rand(1, 5), $characters['ch1']->glyphs['default']);
+    $canvas->glyph_set(rand(9,  11), rand(1, 5), $characters['ch2']->glyphs['default']);
+    $canvas->glyph_set(rand(14, 16), rand(1, 5), $characters['ch3']->glyphs['default']);
+    $canvas->glyph_set(rand(19, 21), rand(1, 5), $characters['ch4']->glyphs['default']);
+    $canvas->glyph_set(rand(24, 26), rand(1, 5), $characters['ch5']->glyphs['default']);
+    $canvas->glyph_set(rand(29, 31), rand(1, 5), $characters['ch6']->glyphs['default']);
+    $canvas->glyph_set(rand(34, 36), rand(1, 5), $characters['ch7']->glyphs['default']);
     return $canvas->render();
   }
 
