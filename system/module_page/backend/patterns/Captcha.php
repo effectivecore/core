489d619788484c5dc5e34b5c3fe70915449fa81b
Maxim Rysevets <root@effcore.com>
2017-11-10 23:30:54 +0300

    sprint_311_233

--- system/module_page/backend/patterns/Captcha.php
+++ system/module_page/backend/patterns/Captcha.php
@@ -11,14 +11,15 @@ namespace effectivecore {
   public $title = 'Captcha';
   public $g_length = 6;
   public $g_characters = [];
-  static public $characters;
+  public $attributes = ['class' => ['captcha' => 'captcha']];
+  public static $glyphs;
 
   static function init() {
     foreach (storage::get('settings')->select_group('captcha') as $c_settings) {
       foreach ($c_settings as $c_characters) {
         foreach ($c_characters as $c_character) {
           foreach ($c_character->glyphs as $c_glyph) {
-            static::$characters[$c_glyph] = $c_character->character;
+            static::$glyphs[$c_glyph] = $c_character->character;
           }
         }
       }
@@ -26,21 +27,22 @@ namespace effectivecore {
   }
 
   function build() {
-    if (!static::$characters) static::init();
+    if (!static::$glyphs) static::init();
     $canvas = new canvas_svg(5 * $this->g_length, 15, 5);
     $canvas->fill('#000000', .9);
     for ($i = 0; $i < $this->g_length; $i++) {
+      $c_glyph = array_rand(static::$glyphs);
+      $this->g_characters[] = static::$glyphs[$c_glyph];
       $canvas->glyph_set(
         rand(0, 2) - 1 + ($i * 5),
-        rand(1, 5),
-        array_rand(static::$characters)
+        rand(1, 5), $c_glyph
       );
     }
     $this->child_insert($canvas, 'captcha');
-    $this->child_insert(new markup('input'), 'element');
+    $this->child_insert(new markup('input', ['size' => $this->g_length]), 'element');
   }
 
-  function render_captcha_demo() {
+  function build_demo() {
     $canvas = new canvas_svg(105, 15, 5);
     $canvas->fill('#ffffff');
     $canvas->glyph_set( 5, 3, '01110|10001|10001|10001|10001|10001|10001|10001|10001|01110'); # 0
@@ -53,7 +55,7 @@ namespace effectivecore {
     $canvas->glyph_set(75, 3, '10000|10000|10000|10000|10000|01000|00100|00010|00001|11111'); # 7
     $canvas->glyph_set(85, 3, '01110|10001|10001|10001|10001|01110|10001|10001|10001|01110'); # 8
     $canvas->glyph_set(95, 3, '01000|00100|00010|00001|01110|10001|10001|10001|10001|01110'); # 9
-    return $canvas->render();
+    return $canvas;
   }
 
 }}
