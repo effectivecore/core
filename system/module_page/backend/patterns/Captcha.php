8174cabac052aa009cf45bd1e01888353ac99764
Maxim Rysevets <root@effcore.com>
2017-11-10 22:46:50 +0300

    sprint_311_232

--- system/module_page/backend/patterns/Captcha.php
+++ system/module_page/backend/patterns/Captcha.php
@@ -26,15 +26,6 @@ namespace effectivecore {
   }
 
   function build() {
-    $this->child_insert(new markup('input'), 'element');
-  }
-
-  function render() {
-    return static::render_captcha().
-           parent::render();
-  }
-
-  function render_captcha() {
     if (!static::$characters) static::init();
     $canvas = new canvas_svg(5 * $this->g_length, 15, 5);
     $canvas->fill('#000000', .9);
@@ -45,23 +36,24 @@ namespace effectivecore {
         array_rand(static::$characters)
       );
     }
-    return $canvas->render();
+    $this->child_insert($canvas, 'captcha');
+    $this->child_insert(new markup('input'), 'element');
   }
 
-   function render_captcha_demo() {
-     $canvas = new canvas_svg(105, 15, 5);
-     $canvas->fill('#ffffff');
-     $canvas->glyph_set( 5, 3, '01110|10001|10001|10001|10001|10001|10001|10001|10001|01110'); # 0
-     $canvas->glyph_set(15, 3, '00001|00001|00001|00001|00001|10001|01001|00101|00010|00001'); # 1
-     $canvas->glyph_set(25, 3, '11111|10000|01000|00100|00010|00001|00001|00001|00001|11110'); # 2
-     $canvas->glyph_set(35, 3, '01000|00100|00010|00001|11111|01000|00100|00010|00001|11111'); # 3
-     $canvas->glyph_set(45, 3, '00001|00001|00001|00001|01111|10001|01001|00101|00010|00001'); # 4
-     $canvas->glyph_set(55, 3, '01000|00100|00010|00001|01111|10000|10000|10000|10000|01111'); # 5
-     $canvas->glyph_set(65, 3, '01110|10001|10001|10001|10001|01110|10000|01000|00100|00010'); # 6
-     $canvas->glyph_set(75, 3, '10000|10000|10000|10000|10000|01000|00100|00010|00001|11111'); # 7
-     $canvas->glyph_set(85, 3, '01110|10001|10001|10001|10001|01110|10001|10001|10001|01110'); # 8
-     $canvas->glyph_set(95, 3, '01000|00100|00010|00001|01110|10001|10001|10001|10001|01110'); # 9
-     return $canvas->render();
-   }
+  function render_captcha_demo() {
+    $canvas = new canvas_svg(105, 15, 5);
+    $canvas->fill('#ffffff');
+    $canvas->glyph_set( 5, 3, '01110|10001|10001|10001|10001|10001|10001|10001|10001|01110'); # 0
+    $canvas->glyph_set(15, 3, '00001|00001|00001|00001|00001|10001|01001|00101|00010|00001'); # 1
+    $canvas->glyph_set(25, 3, '11111|10000|01000|00100|00010|00001|00001|00001|00001|11110'); # 2
+    $canvas->glyph_set(35, 3, '01000|00100|00010|00001|11111|01000|00100|00010|00001|11111'); # 3
+    $canvas->glyph_set(45, 3, '00001|00001|00001|00001|01111|10001|01001|00101|00010|00001'); # 4
+    $canvas->glyph_set(55, 3, '01000|00100|00010|00001|01111|10000|10000|10000|10000|01111'); # 5
+    $canvas->glyph_set(65, 3, '01110|10001|10001|10001|10001|01110|10000|01000|00100|00010'); # 6
+    $canvas->glyph_set(75, 3, '10000|10000|10000|10000|10000|01000|00100|00010|00001|11111'); # 7
+    $canvas->glyph_set(85, 3, '01110|10001|10001|10001|10001|01110|10001|10001|10001|01110'); # 8
+    $canvas->glyph_set(95, 3, '01000|00100|00010|00001|01110|10001|10001|10001|10001|01110'); # 9
+    return $canvas->render();
+  }
 
 }}
