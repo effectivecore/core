091f3d6f7532cff2eb808686dc7e74bbfe394106
Maxim Rysevets <root@effcore.com>
2017-11-11 13:20:16 +0300

    sprint_311_234

--- system/module_page/backend/patterns/Field_Captcha.php
+++ system/module_page/backend/patterns/Field_Captcha.php
@@ -9,6 +9,7 @@ namespace effectivecore {
           class form_field_captcha extends \effectivecore\form_field {
 
   public $title = 'Captcha';
+  public $description = 'Write the characters from the picture.';
   public $g_length = 6;
   public $g_characters = [];
   public $attributes = ['class' => ['captcha' => 'captcha']];
@@ -39,23 +40,7 @@ namespace effectivecore {
       );
     }
     $this->child_insert($canvas, 'captcha');
-    $this->child_insert(new markup('input', ['size' => $this->g_length]), 'element');
-  }
-
-  function build_demo() {
-    $canvas = new canvas_svg(105, 15, 5);
-    $canvas->fill('#ffffff');
-    $canvas->glyph_set( 5, 3, '01110|10001|10001|10001|10001|10001|10001|10001|10001|01110'); # 0
-    $canvas->glyph_set(15, 3, '00001|00001|00001|00001|00001|10001|01001|00101|00010|00001'); # 1
-    $canvas->glyph_set(25, 3, '11111|10000|01000|00100|00010|00001|00001|00001|00001|11110'); # 2
-    $canvas->glyph_set(35, 3, '01000|00100|00010|00001|11111|01000|00100|00010|00001|11111'); # 3
-    $canvas->glyph_set(45, 3, '00001|00001|00001|00001|01111|10001|01001|00101|00010|00001'); # 4
-    $canvas->glyph_set(55, 3, '01000|00100|00010|00001|01111|10000|10000|10000|10000|01111'); # 5
-    $canvas->glyph_set(65, 3, '01110|10001|10001|10001|10001|01110|10000|01000|00100|00010'); # 6
-    $canvas->glyph_set(75, 3, '10000|10000|10000|10000|10000|01000|00100|00010|00001|11111'); # 7
-    $canvas->glyph_set(85, 3, '01110|10001|10001|10001|10001|01110|10001|10001|10001|01110'); # 8
-    $canvas->glyph_set(95, 3, '01000|00100|00010|00001|01110|10001|10001|10001|10001|01110'); # 9
-    return $canvas;
+    $this->child_insert(new markup_simple('input', ['required' => 'required', 'size' => $this->g_length]), 'element');
   }
 
 }}
