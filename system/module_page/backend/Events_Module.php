6a5bccbb2e49bba1e5d615791e42bbe8abdb7670
Maxim Rysevets <root@effcore.com>
2017-11-11 15:59:05 +0300

    sprint_311_236

--- system/module_page/backend/Events_Module.php
+++ system/module_page/backend/Events_Module.php
@@ -5,11 +5,22 @@
   #############################################################
 
 namespace effectivecore\modules\page {
+          use \effectivecore\entity_factory as entity;
+          use \effectivecore\message_factory as message;
+          use \effectivecore\translation_factory as translation;
           use \effectivecore\modules\page\page_factory as page;
           abstract class events_module extends \effectivecore\events_module {
 
   static function on_start() {
     return page::find_and_render();
+  }
+
+  static function on_install() {
+  # install entities
+    foreach (entity::get_all_by_module('page') as $c_entity) {
+      if ($c_entity->install()) message::add_new(translation::get('Entity %%_name was installed.',     ['name' => $c_entity->get_name()]));
+      else                      message::add_new(translation::get('Entity %%_name was not installed!', ['name' => $c_entity->get_name()]), 'error');
+    }
   }
 
 }}
