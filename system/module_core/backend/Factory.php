5a11c0b717353d35229fc4ba388d853a7f5b2e84
Maxim Rysevets <root@effcore.com>
2017-11-12 20:33:31 +0300

    sprint_311_247

--- system/module_core/backend/Factory.php
+++ system/module_core/backend/Factory.php
@@ -226,4 +226,20 @@ namespace effectivecore {
     return sha1($data);
   }
 
+  function binstr2hexstr($binstr) {
+    $hexstr = '';
+    foreach (str_split($binstr, 8) as $c_chunk) {
+      $hexstr.= str_pad(base_convert(str_pad($c_chunk, 8, '0'), 2, 16), 2, '0', STR_PAD_LEFT);
+    }
+    return $hexstr;
+  }
+
+  function hexstr2binstr($hexstr) {
+    $binstr = '';
+    foreach (str_split($hexstr, 2) as $c_chunk) {
+      $binstr.= str_pad(base_convert($c_chunk, 16, 2), 8, '0', STR_PAD_LEFT);
+    }
+    return $binstr;
+  }
+
 }}
