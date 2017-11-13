92ca86d6612b35980d9d563c60b75aaca92aa665
Maxim Rysevets <root@effcore.com>
2017-11-13 22:08:35 +0300

    sprint_311_252

--- system/module_page/backend/patterns/Field_Captcha.php
+++ system/module_page/backend/patterns/Field_Captcha.php
@@ -13,8 +13,9 @@ namespace effectivecore {
   public $description = 'Write the characters from the picture.';
   public $attributes = ['class' => ['captcha' => 'captcha']];
 
+  public $id;
   public $length = 6;
-  public $characters = '';
+  public $attempts = 1;
   public static $glyphs;
 
   static function init() {
@@ -29,35 +30,45 @@ namespace effectivecore {
     }
   }
 
-  function build() {
-  # generate new captcha
+  function captcha_generate() {
     if (!static::$glyphs) static::init();
-    $canvas_0 = new canvas_svg(5 * $this->length, 15, 5);
-    $canvas_0->fill('#000000', 0, 0, null, null, 1);
+    $characters = '';
+    $canvas = new canvas_svg(5 * $this->length, 15, 5);
+    $canvas->fill('#000000', 0, 0, null, null, 1);
     for ($i = 0; $i < $this->length; $i++) {
       $c_glyph = array_rand(static::$glyphs);
-      $this->characters.= static::$glyphs[$c_glyph];
-      $canvas_0->glyph_set($c_glyph, rand(0, 2) - 1 + ($i * 5), rand(1, 5));
+      $characters.= static::$glyphs[$c_glyph];
+      $canvas->glyph_set($c_glyph, rand(0, 2) - 1 + ($i * 5), rand(1, 5));
     }
-  # save captcha to storage
-    $captcha_0 = new instance('captcha', [
-      'characters' => $this->characters,
-      'canvas'     => $canvas_0->clmask_to_hexstr(),
-      'attempts'   => 1
+    $captcha = new instance('captcha', [
+      'characters'  => $characters,
+      'canvas'      => $canvas,
+      'canvas_data' => $canvas->clmask_to_hexstr(),
+      'attempts'    => $this->attempts
     ]);
-    $captcha_0->insert();
-    message::add_new('New captcha id = '.$captcha_0->id);
+    return $captcha;
+  }
 
+  function captcha_load($id) {
+    $captcha = (new instance('captcha', ['id' => $id]))->select();
+    $captcha->canvas = new canvas_svg(5 * $this->length, 15, 5);
+    $captcha->canvas->matrix_set(
+      $captcha->canvas->hexstr_to_clmask($captcha->canvas_data)
+    );
+    return $captcha;
+  }
 
-  # load captcha from storage
-    $captcha_1 = (new instance('captcha', [
-      'id' => $captcha_0->id
-    ]))->select();
-    $canvas_1 = new canvas_svg(5 * $this->length, 15, 5);
-    $canvas_1->matrix_set($canvas_1->hexstr_to_clmask($captcha_1->canvas));
+  function build() {
+    $captcha_0 = $this->captcha_generate();
+    $captcha_1 = $this->captcha_load('123');
   # build form elements
-    $this->child_insert($canvas_0, 'canvas_0');
-    $this->child_insert($canvas_1, 'canvas_1');
+    $this->child_insert($captcha_0->canvas, 'canvas_0');
+    $this->child_insert($captcha_1->canvas, 'canvas_1');
+    $this->child_insert(new markup_simple('input', [
+      'type'  => 'hidden',
+      'name'  => 'captcha_id',
+      'value' => $this->id
+    ]), 'captcha_id');
     $this->child_insert(new markup_simple('input', [
       'type' => 'text',
       'name' => 'captcha',
