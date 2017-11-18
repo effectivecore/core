06d079e5e07e227890edb3eb02d4f6c4df6ebfb5
Maxim Rysevets <root@effcore.com>
2017-11-18 16:30:26 +0300

    sprint_311_264

--- system/module_develop/module_demo/backend/Events_Module.php
+++ system/module_develop/module_demo/backend/Events_Module.php
@@ -7,6 +7,7 @@
 namespace effectivecore\modules\demo {
           use \effectivecore\entity_factory as entity;
           use \effectivecore\message_factory as message;
+          use \effectivecore\instance_factory as instance;
           use \effectivecore\translation_factory as translation;
           abstract class events_module extends \effectivecore\events_module {
 
@@ -18,6 +19,11 @@ namespace effectivecore\modules\demo {
     foreach (entity::get_all_by_module('demo') as $c_entity) {
       if ($c_entity->install()) message::add_new(translation::get('Entity %%_name was installed.',     ['name' => $c_entity->get_name()]));
       else                      message::add_new(translation::get('Entity %%_name was not installed!', ['name' => $c_entity->get_name()]), 'error');
+    }
+  # insert instances
+    foreach (instance::get_by_module('demo') as $c_instance) {
+      if ($c_instance->insert()) message::add_new(translation::get('Instances of entity %%_name was added.',     ['name' => $c_entity->get_name()]));
+      else                       message::add_new(translation::get('Instances of entity %%_name was not added!', ['name' => $c_entity->get_name()]), 'error');
     }
   }
 
