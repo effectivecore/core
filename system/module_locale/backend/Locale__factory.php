795b5d3d841e7610a4a8127e9983aa8ead35a0b6
Maxim Rysevets <root@effcore.com>
2017-11-30 22:25:04 +0300

    sprint_311_306

--- system/module_locale/backend/Locale__factory.php
+++ system/module_locale/backend/Locale__factory.php
@@ -41,12 +41,25 @@ namespace effectivecore {
   static function format_msecond($number, $precision = 6) {return static::format_number(floatval($number), $precision);}
   static function format_version($number)                 {return static::format_number(floatval($number), 2);}
 
-  static function format_number($number, $precision = 0, $decimal_point = null, $thousands_separator = null) {
+  static function format_number($number, $precision = 0, $dec_point = null, $thousands = null, $no_zeros = true) {
     $current = static::get_settings();
-    return number_format($number, $precision,
-      is_null($decimal_point)       ? $current->decimal_point       : $decimal_point,
-      is_null($thousands_separator) ? $current->thousands_separator : $thousands_separator
-    );
+    $precision = $precision ? $precision + 5 : 0; # disable the rounding effect
+    $dec_point = is_null($dec_point) ? $current->decimal_point       : $dec_point;
+    $thousands = is_null($thousands) ? $current->thousands_separator : $thousands;
+    $return = $precision ? substr(number_format($number, $precision, $dec_point, $thousands), 0, -5) :
+                                  number_format($number, $precision, $dec_point, $thousands);
+    if ($no_zeros) {
+      $return = rtrim($return, '0');
+      $return = rtrim($return, $dec_point);
+    }
+    return $return;
+  }
+
+  static function format_human_bytes($bytes, $decimals = 2) {
+    $pow = $bytes == 0 ? 0 : (int)log($bytes, 1024);
+    $character = ['B', 'K', 'M', 'G', 'T'][$pow];
+    $value = $bytes >= 1024 ? $bytes / pow(1024, $pow) : $bytes;
+    return static::format_number($value, $decimals).$character;
   }
 
 }}
