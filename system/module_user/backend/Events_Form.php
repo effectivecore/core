e9cf3f785bf87fdf5b7b897ebb7c5efe755316ff
Maxim Rysevets <root@effcore.com>
2017-12-03 14:06:46 +0300

    sprint_311_314

--- system/module_user/backend/Events_Form.php
+++ system/module_user/backend/Events_Form.php
@@ -66,7 +66,7 @@ namespace effectivecore\modules\user {
           $id = page::$args['id_user'];
         # check security
           $test_pass = (new instance('user', ['id' => $id]))->select();
-          if ($test_pass->password_hash !== factory::hash_get($values['password'])) {
+          if ($test_pass->password_hash !== factory::hash_get($values['password'][0])) {
             $form->add_error('credentials/password/element',
               translation::get('Field "%%_title" contains incorrect value!', [
                 'title' => translation::get($fields['credentials/password']->title)
@@ -75,22 +75,22 @@ namespace effectivecore\modules\user {
             return;
           }
         # test email
-          $test_email = (new instance('user', ['email' => strtolower($values['email'])]))->select();
+          $test_email = (new instance('user', ['email' => strtolower($values['email'][0])]))->select();
           if ($test_email &&
               $test_email->id != $id) {
             $form->add_error('credentials/email/element', 'User with this EMail was already registered!');
             return;
           }
         # test nick
-          $test_nick = (new instance('user', ['nick' => strtolower($values['nick'])]))->select();
+          $test_nick = (new instance('user', ['nick' => strtolower($values['nick'][0])]))->select();
           if ($test_nick &&
               $test_nick->id != $id) {
             $form->add_error('credentials/nick/element', 'User with this Nick was already registered!');
             return;
           }
         # test new password
-          if ($values['password'] ==
-              $values['password_new']) {
+          if ($values['password'][0] ==
+              $values['password_new'][0]) {
             $form->add_error('credentials/password_new/element',
               'New password must be different from the current password!'
             );
@@ -106,10 +106,10 @@ namespace effectivecore\modules\user {
     switch ($form->clicked_button_name) {
       case 'save':
         $user = (new instance('user', ['id' => $id]))->select();
-        $user->email = strtolower($values['email']);
-        $user->nick  = strtolower($values['nick']);
-        if ($values['password_new']) {
-          $user->password_hash = factory::hash_get($values['password_new']);
+        $user->email = strtolower($values['email'][0]);
+        $user->nick  = strtolower($values['nick'][0]);
+        if ($values['password_new'][0]) {
+          $user->password_hash = factory::hash_get($values['password_new'][0]);
         }
         if ($user->update()) {
           message::add_new(
