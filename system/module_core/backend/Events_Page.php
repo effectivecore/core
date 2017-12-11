889ffdd34b47817620139533b20a6f4d632cd68e
Maxim Rysevets <root@effcore.com>
2017-12-11 12:38:00 +0300

    sprint_311_357

---
+++ system/module_core/backend/Events_Page.php
@@ -0,0 +1,30 @@
+<?php
+
+  #############################################################
+  ### Copyright © 2017 Maxim Rysevets. All rights reserved. ###
+  #############################################################
+
+namespace effectivecore\modules\core {
+          use \effectivecore\table as table;
+          use \effectivecore\locale_factory as locale;
+          use \effectivecore\control_switcher as switcher;
+          use \effectivecore\modules\storage\storage_factory as storage;
+          abstract class events_page extends \effectivecore\events_page {
+
+  static function on_show_modules() {
+    $thead = [['Title', 'ID', 'Path', 'Description', 'Ver.', 'State.']];
+    $tbody = [];
+    foreach (storage::get('settings')->select_group('module') as $c_module) {
+      $tbody[] = [
+        $c_module->title,
+        $c_module->id,
+        $c_module->path,
+        $c_module->description,
+        locale::format_version($c_module->version),
+        $c_module->state != 'always_on' ? new switcher($c_module->state) : '&nbsp;',
+      ];
+    }
+    return new table([], $tbody, $thead);
+  }
+
+}}
--- system/module_core/backend/Events_Page__global.php
+++ system/module_core/backend/Events_Page__global.php
@@ -5,25 +5,6 @@
   #############################################################
 
 namespace effectivecore {
-          use \effectivecore\locale_factory as locale;
-          use \effectivecore\control_switcher as switcher;
-          use \effectivecore\modules\storage\storage_factory as storage;
           abstract class events_page {
-
-  static function on_show_modules() {
-    $thead = [['Title', 'ID', 'Path', 'Description', 'Ver.', 'State.']];
-    $tbody = [];
-    foreach (storage::get('settings')->select_group('module') as $c_module) {
-      $tbody[] = [
-        $c_module->title,
-        $c_module->id,
-        $c_module->path,
-        $c_module->description,
-        locale::format_version($c_module->version),
-        $c_module->state != 'always_on' ? new switcher($c_module->state) : '&nbsp;',
-      ];
-    }
-    return new table([], $tbody, $thead);
-  }
 
 }}
