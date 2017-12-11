3a3c84084306ef8f78c75105888465f05a7ce129
Maxim Rysevets <root@effcore.com>
2017-12-11 15:58:01 +0300

    sprint_311_378

--- system/module_menu/backend/patterns/Tree.php
+++ system/module_menu/backend/patterns/Tree.php
@@ -5,6 +5,7 @@
   #############################################################
 
 namespace effectivecore {
+          use \effectivecore\modules\storage\storage as storage;
           class tree extends \effectivecore\node {
 
   public $id;
@@ -15,5 +16,32 @@ namespace effectivecore {
     if ($title) $this->title = $title;
     parent::__construct($attributes, $children, $weight);
   }
+
+  ######################
+  ### static methods ###
+  ######################
+
+  protected static $trees;
+  protected static $tree_items;
+
+  static function init() {
+    $trees      = storage::get('settings')->select_group('trees');
+    $tree_items = storage::get('settings')->select_group('tree_items');
+    foreach ($trees as $c_trees_by_module) {
+      foreach ($c_trees_by_module as $c_tree) {
+        static::$trees[$c_tree->id] = $c_tree;
+      }
+    }
+    foreach ($tree_items as $c_items_by_module) {
+      foreach ($c_items_by_module as $c_item) {
+        static::$tree_items[$c_item->id] = $c_item;
+      }
+    }
+  }
+
+  static function get_trees()        {return static::$trees;}
+  static function get_tree_items()   {return static::$tree_items;}
+  static function get_tree($id)      {return static::$trees[$id];}
+  static function get_tree_item($id) {return static::$tree_items[$id];}
 
 }}
