2953cd1569a90d64426d674070c0c33338e50643
Maxim Rysevets <root@effcore.com>
2017-12-11 13:38:50 +0300

    sprint_311_365

--- system/module_locale/backend/Locale__factory.php
+++ system/module_locale/backend/Locale__factory.php
@@ -20,12 +20,12 @@ namespace effectivecore {
     }
   }
 
-  static function get_countries() {
+  static function select_countries() {
     if (!static::$countries) static::init();
     return static::$countries;
   }
 
-  static function get_settings() {
+  static function select_settings() {
     if (!static::$settings) static::init();
     return static::$settings;
   }
@@ -34,21 +34,21 @@ namespace effectivecore {
   ### formats ###
   ###############
 
-  static function format_time($time)                      {return \DateTime::createFromFormat('H:i:s',       $time,     new \DateTimeZone('UTC'))->setTimezone(new \DateTimeZone(static::get_settings()->timezone))->format(static::get_settings()->format_time);}
-  static function format_date($date)                      {return \DateTime::createFromFormat('Y-m-d',       $date,     new \DateTimeZone('UTC'))->setTimezone(new \DateTimeZone(static::get_settings()->timezone))->format(static::get_settings()->format_date);}
-  static function format_datetime($datetime)              {return \DateTime::createFromFormat('Y-m-d H:i:s', $datetime, new \DateTimeZone('UTC'))->setTimezone(new \DateTimeZone(static::get_settings()->timezone))->format(static::get_settings()->format_datetime);}
+  static function format_time($time)                      {return \DateTime::createFromFormat('H:i:s',       $time,     new \DateTimeZone('UTC'))->setTimezone(new \DateTimeZone(static::select_settings()->timezone))->format(static::select_settings()->format_time);}
+  static function format_date($date)                      {return \DateTime::createFromFormat('Y-m-d',       $date,     new \DateTimeZone('UTC'))->setTimezone(new \DateTimeZone(static::select_settings()->timezone))->format(static::select_settings()->format_date);}
+  static function format_datetime($datetime)              {return \DateTime::createFromFormat('Y-m-d H:i:s', $datetime, new \DateTimeZone('UTC'))->setTimezone(new \DateTimeZone(static::select_settings()->timezone))->format(static::select_settings()->format_datetime);}
   static function format_persent($number, $precision = 2) {return static::format_number(floatval($number), $precision).'%';}
   static function format_msecond($number, $precision = 6) {return static::format_number(floatval($number), $precision);}
   static function format_version($number)                 {return static::format_number(floatval($number), 2);}
 
   static function format_number($number, $precision = 0, $dec_point = null, $thousands = null, $no_zeros = true) {
-    $dec_point = is_null($dec_point) ? static::get_settings()->decimal_point       : $dec_point;
-    $thousands = is_null($thousands) ? static::get_settings()->thousands_separator : $thousands;
+    $dec_point = is_null($dec_point) ? static::select_settings()->decimal_point       : $dec_point;
+    $thousands = is_null($thousands) ? static::select_settings()->thousands_separator : $thousands;
     return factory::format_number($number, $precision, $dec_point, $thousands, $no_zeros);
   }
 
   static function format_human_bytes($bytes, $decimals = 2) {
-    return factory::bytes_to_human($bytes, $decimals, static::get_settings()->decimal_point);
+    return factory::bytes_to_human($bytes, $decimals, static::select_settings()->decimal_point);
   }
 
 }}
--- system/module_page/backend/patterns/Page.php
+++ system/module_page/backend/patterns/Page.php
@@ -130,9 +130,9 @@ namespace effectivecore {
     console::add_information('User roles', implode(', ', user::select_current()->roles));
     console::add_information('Server load (sys_getloadavg)', locale::format_msecond(sys_getloadavg()[0]));
     console::add_information('Memory for php (bytes)', locale::format_number(memory_get_usage(true), 0, null, ' '));
-    console::add_information('Current language', locale::get_settings()->lang_code);
+    console::add_information('Current language', locale::select_settings()->lang_code);
 
-    $template->set_var('attributes', factory::data_to_attr(['lang' => locale::get_settings()->lang_code]));
+    $template->set_var('attributes', factory::data_to_attr(['lang' => locale::select_settings()->lang_code]));
     $template->set_var('console', console::render()); # @todo: only for admins
     $template->set_var('messages', message::render_all());
     $template->set_var('meta', implode(nl, $rendered_meta));
