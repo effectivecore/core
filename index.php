2b9208c1922c2308c4abfc6b829ae485c8950234
Maxim Rysevets <root@effcore.com>
2017-12-11 15:40:58 +0300

    sprint_311_375

--- index.php
+++ index.php
@@ -23,8 +23,8 @@ namespace effectivecore {
   spl_autoload_register('\effectivecore\factory::autoload');
 
   use \effectivecore\url as url;
+  use \effectivecore\timer as timer;
   use \effectivecore\token_factory as token;
-  use \effectivecore\timer_factory as timer;
   use \effectivecore\event_factory as event;
   use \effectivecore\modules\storage\storage as storage;
   timer::tap('total');
