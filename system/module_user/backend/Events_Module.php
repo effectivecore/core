604106625c23e2d920e74c07a4ef6a40e9ff6799
Maxim Rysevets <root@effcore.com>
2017-12-18 16:38:49 +0300

    sprint_311_412

--- system/module_user/backend/Events_Module.php
+++ system/module_user/backend/Events_Module.php
@@ -6,12 +6,12 @@
 
 namespace effectivecore\modules\user {
           use \effectivecore\entity as entity;
+          use \effectivecore\session as session;
           use \effectivecore\factory as factory;
           use \effectivecore\message as message;
           use \effectivecore\instance as instance;
           use \effectivecore\translation as translation;
           use \effectivecore\modules\user\user as user;
-          use \effectivecore\modules\user\session as session;
           abstract class events_module extends \effectivecore\events_module {
 
   static function on_start() {
