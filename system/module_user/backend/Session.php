5017648e4dec43cd57b1672760283d5cbe47fa24
Maxim Rysevets <root@effcore.com>
2017-12-18 20:46:16 +0300

    sprint_311_415_2

--- system/module_user/backend/Session.php
+++ system/module_user/backend/Session.php
@@ -7,30 +7,6 @@
 namespace effectivecore {
           abstract class session {
 
-  static function id_regenerate($type, $expire = 60 * 60 * 24 * 30) {
-    $hex_type = $type; # 'a' - anonymous user | 'f' - authenticated user
-    $hex_expire = dechex(time() + $expire);
-    $hex_ip = factory::ip_to_hex($_SERVER['REMOTE_ADDR']);
-    $hex_uagent_hash_8 = substr(md5($_SERVER['HTTP_USER_AGENT']), 0, 8);
-    $hex_random = str_pad(dechex(rand(0, 0xffffffff)), 8, '0', STR_PAD_LEFT);
-    $session_id = $hex_type.          # strlen == 1
-                  $hex_expire.        # strlen == 8
-                  $hex_ip.            # strlen == 8
-                  $hex_uagent_hash_8. # strlen == 8
-                  $hex_random;        # strlen == 8
-    setcookie('session_id', ($_COOKIE['session_id'] = $session_id), $expire ? time() + $expire : 0, '/');
-    setcookie('cookies_is_on', 'true', $expire ? time() + $expire : 0, '/');
-    return $session_id;
-  }
-
-  static function id_get() {
-    $c_value = factory::filter_session_id(
-      isset($_COOKIE['session_id']) ?
-            $_COOKIE['session_id'] : '');
-    if ($c_value) return $c_value;
-    else          return static::id_regenerate('a');
-  }
-
   static function select() {
     $session_id = static::id_get();
     if ($session_id[0] == 'f') {
@@ -67,6 +43,41 @@ namespace effectivecore {
       'id_user' => $id_user
     ]))->delete();
     static::id_regenerate('a');
+  }
+
+  ############################
+  ### session_id functions ###
+  ############################
+
+  static function id_regenerate($type, $expire = 60 * 60 * 24 * 30) {
+    $hex_type = $type; # 'a' - anonymous user | 'f' - authenticated user
+    $hex_expire = dechex(time() + $expire);
+    $hex_ip = factory::ip_to_hex($_SERVER['REMOTE_ADDR']);
+    $hex_uagent_hash_8 = substr(md5($_SERVER['HTTP_USER_AGENT']), 0, 8);
+    $hex_random = str_pad(dechex(rand(0, 0xffffffff)), 8, '0', STR_PAD_LEFT);
+    $session_id = $hex_type.          # strlen == 1
+                  $hex_expire.        # strlen == 8
+                  $hex_ip.            # strlen == 8
+                  $hex_uagent_hash_8. # strlen == 8
+                  $hex_random;        # strlen == 8
+    setcookie('session_id', ($_COOKIE['session_id'] = $session_id), $expire ? time() + $expire : 0, '/');
+    setcookie('cookies_is_on', 'true', $expire ? time() + $expire : 0, '/');
+    return $session_id;
+  }
+
+  static function id_get() {
+    if (static::id_check(
+          isset($_COOKIE['session_id']) ?
+                $_COOKIE['session_id'] : '')) {
+      return    $_COOKIE['session_id']; } else {
+      return static::id_regenerate('a');
+    }
+  }
+
+  static function id_check($value) {
+    if (factory::filter_session_id($value)) {
+      return true;
+    }
   }
 
 }}
