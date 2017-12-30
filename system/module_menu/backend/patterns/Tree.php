2a5576db3061e835b735b38ce0dc8be4234b3bed
Maxim Rysevets <root@effcore.com>
2017-12-30 16:13:59 +0300

    sprint_311_426_36

--- system/module_menu/backend/patterns/Tree.php
+++ system/module_menu/backend/patterns/Tree.php
@@ -24,7 +24,7 @@ namespace effectivecore {
   static protected $cache_tree_items;
 
   static function init() {
-    $trees      = storage::get('files')->select('trees');
+    $trees      = storage::get('files')->select('tree');
     $tree_items = storage::get('files')->select('tree_items');
     foreach ($trees as $c_module_id => $c_module_trees) {
       foreach ($c_module_trees as $c_row_id => $c_tree) {
