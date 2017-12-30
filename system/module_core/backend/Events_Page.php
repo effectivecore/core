0a47d3ad85be91aaa3ac021261f6fceb5240af29
Maxim Rysevets <root@effcore.com>
2017-12-30 18:59:02 +0300

    sprint_311_426_56

--- system/module_core/backend/Events_Page.php
+++ system/module_core/backend/Events_Page.php
@@ -15,14 +15,18 @@ namespace effectivecore\modules\core {
           use \effectivecore\control_switcher as switcher;
           use \effectivecore\table_head_row_cell as table_head_row_cell;
           use \effectivecore\table_body_row_cell as table_body_row_cell;
+          use \effectivecore\control_actions_list as control_actions_list;
           abstract class events_page extends \effectivecore\events_page {
 
   static function on_show_modules($page) {
-    $thead = [[
-      new table_head_row_cell(['class' => ['info' => 'info']], 'Module information'),
-      new table_head_row_cell(['class' => ['action' => 'action']], 'Actions')]];
+    $thead = [['Module information', '']];
     $tbody = [];
     foreach (module::get_all() as $c_module) {
+      $c_action_list = new control_actions_list();
+      $c_action_list->action_add('enable', 'enable');
+      $c_action_list->action_add('disable', 'disable'); // if ($c_module->state != 'always_on')
+      $c_action_list->action_add('install', 'install');
+      $c_action_list->action_add('uninstall', 'uninstall');
       $tbody[] = [
         new table_body_row_cell(['class' => ['info' => 'info']],
           translation::get('ID')         .': '.$c_module->id.br.
@@ -30,8 +34,7 @@ namespace effectivecore\modules\core {
           translation::get('Title')      .': '.translation::get($c_module->title).br.
           translation::get('Description').': '.translation::get($c_module->description).br.
           translation::get('Path')       .': '.$c_module->path),
-        new table_body_row_cell(['class' => ['action' => 'action']],
-          $c_module->state != 'always_on' ? new switcher($c_module->state) : '&nbsp;')
+        new table_body_row_cell(['class' => ['action' => 'action']], $c_action_list)
       ];
     }
     return new markup('x-block', ['id' => 'modules_info'], [
