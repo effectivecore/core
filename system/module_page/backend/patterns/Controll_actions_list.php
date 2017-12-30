0151994588ba0b45b8e10e52f572a7fd43967952
Maxim Rysevets <root@effcore.com>
2017-12-30 18:59:02 +0300

    sprint_311_426_56

--- system/module_page/backend/patterns/Controll_actions_list.php
+++ system/module_page/backend/patterns/Controll_actions_list.php
@@ -9,26 +9,30 @@ namespace effectivecore {
 
   public $tag_name = 'x-actions-control';
   public $title = null;
-  public $values = [];
+  public $actions = [];
   public $active = [];
 
-  function __construct($values = [], $active = [], $title = null, $attributes = [], $weight = 0) {
-    $this->values = factory::array_values_map_to_keys($values);
+  function __construct($actions = [], $active = [], $title = null, $attributes = [], $weight = 0) {
     $this->active = factory::array_values_map_to_keys($active);
+    foreach ($actions as $id => $c_action) $this->action_add($id, $c_action);
     if (!is_null($title)) $this->title = $title;
     parent::__construct($this->tag_name, $attributes, [], $weight);
+  }
+
+  function action_add($id, $title) {
+    $this->actions[$id] = $title;
   }
 
   function render() {
     $this->child_insert(new markup('x-title', [], $this->title ?: 'actions'), 'title');
     $this->child_insert(new markup('x-action-list'), 'action_list');
     $list = $this->child_select('action_list');
-    foreach ($this->values as $c_value) {
-      $c_attr = isset($this->active[$c_value]) ? ['class' => ['active']] : [];
+    foreach ($this->actions as $c_action) {
+      $c_attr = isset($this->active[$c_action]) ? ['class' => ['active']] : [];
       $list->child_insert(
         new markup('x-action', $c_attr,
-        new markup('a', ['href' => '?action='.$c_value], $c_value))
-      );
+          new markup('a', ['href' => '?action='.$c_action], $c_action)
+        ));
     }
     return parent::render();
   }
