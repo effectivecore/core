b88e721f35bd858e4f402654d304432406260936
Maxim Rysevets <root@effcore.com>
2017-12-31 20:55:56 +0300

    sprint_311_426_83

--- system/module_core/backend/Events_Page.php
+++ system/module_core/backend/Events_Page.php
@@ -12,7 +12,6 @@ namespace effectivecore\modules\core {
           use \effectivecore\locale as locale;
           use \effectivecore\translation as translation;
           use \effectivecore\control_switcher as switcher;
-          use \effectivecore\table_head_row_cell as table_head_row_cell;
           use \effectivecore\table_body_row_cell as table_body_row_cell;
           use \effectivecore\control_actions_list as control_actions_list;
           abstract class events_page extends \effectivecore\events_page {
@@ -22,10 +21,10 @@ namespace effectivecore\modules\core {
     $tbody = [];
     foreach (module::get_all() as $c_module) {
       $c_action_list = new control_actions_list([], [], null);
-      $c_action_list->action_add('enable', 'enable');
-      $c_action_list->action_add('disable', 'disable'); // if ($c_module->state != 'always_on')
-      $c_action_list->action_add('install', 'install');
-      $c_action_list->action_add('uninstall', 'uninstall');
+      $c_action_list->action_add('/admin/modules/'.$c_module->id.'/enable', 'enable',       $c_module->state != 'always_on');
+      $c_action_list->action_add('/admin/modules/'.$c_module->id.'/disable', 'disable',     $c_module->state != 'always_on');
+      $c_action_list->action_add('/admin/modules/'.$c_module->id.'/install', 'install',     $c_module->state != 'always_on');
+      $c_action_list->action_add('/admin/modules/'.$c_module->id.'/uninstall', 'uninstall', $c_module->state != 'always_on');
       $tbody[] = [
         new table_body_row_cell(['class' => ['info' => 'info']],
           translation::get('ID')         .': '.$c_module->id.br.
@@ -33,7 +32,7 @@ namespace effectivecore\modules\core {
           translation::get('Title')      .': '.translation::get($c_module->title).br.
           translation::get('Description').': '.translation::get($c_module->description).br.
           translation::get('Path')       .': '.$c_module->path),
-        new table_body_row_cell(['class' => ['action' => 'action']], $c_action_list)
+        new table_body_row_cell(['class' => ['actions' => 'actions']], $c_action_list)
       ];
     }
     return new markup('x-block', ['id' => 'modules_info'], [
