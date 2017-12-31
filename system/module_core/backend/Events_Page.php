8fb0c5fac0bc88d4575f1df564353dc7985f139c
Maxim Rysevets <root@effcore.com>
2017-12-31 21:11:49 +0300

    sprint_311_426_85

--- system/module_core/backend/Events_Page.php
+++ system/module_core/backend/Events_Page.php
@@ -11,19 +11,17 @@ namespace effectivecore\modules\core {
           use \effectivecore\module as module;
           use \effectivecore\locale as locale;
           use \effectivecore\translation as translation;
-          use \effectivecore\control_switcher as switcher;
           use \effectivecore\table_body_row_cell as table_body_row_cell;
           use \effectivecore\control_actions_list as control_actions_list;
           abstract class events_page extends \effectivecore\events_page {
 
   static function on_show_modules($page) {
-    $thead = [['Module information', '']];
+    $thead = [['Module information', 'State', '']];
     $tbody = [];
     foreach (module::get_all() as $c_module) {
       $c_action_list = new control_actions_list([], [], null);
       $c_action_list->action_add('/admin/modules/'.$c_module->id.'/enable', 'enable',       $c_module->state != 'always_on');
       $c_action_list->action_add('/admin/modules/'.$c_module->id.'/disable', 'disable',     $c_module->state != 'always_on');
-      $c_action_list->action_add('/admin/modules/'.$c_module->id.'/install', 'install',     $c_module->state != 'always_on');
       $c_action_list->action_add('/admin/modules/'.$c_module->id.'/uninstall', 'uninstall', $c_module->state != 'always_on');
       $tbody[] = [
         new table_body_row_cell(['class' => ['info' => 'info']],
@@ -32,6 +30,7 @@ namespace effectivecore\modules\core {
           translation::get('Title')      .': '.translation::get($c_module->title).br.
           translation::get('Description').': '.translation::get($c_module->description).br.
           translation::get('Path')       .': '.$c_module->path),
+        new table_body_row_cell(['class' => ['state' => 'state']], $c_module->state),
         new table_body_row_cell(['class' => ['actions' => 'actions']], $c_action_list)
       ];
     }
