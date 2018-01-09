5b0ce09f6a31180141aecdc3b1a03765f028b1e5
Maxim Rysevets <root@effcore.com>
2018-01-10 01:31:32 +0300

    sprint_311_426_96

--- system/module_core/backend/Markdown.php
+++ system/module_core/backend/Markdown.php
@@ -9,8 +9,6 @@ namespace effectivecore {
 
   static function simple_markdown_to_markup($markdown) {
     $stack = new node();
-    $p_lists = [];
-    $p_quote = null;
     $strings = explode(nl, $markdown);
     foreach ($strings as $c_num => $c_string) {
       $c_string = str_replace(tb, '    ', $c_string);
@@ -33,24 +31,6 @@ namespace effectivecore {
                                  '([_][ ]{0,2}){3,})'.
                        '(?<noises>[ ]{0,})$%S', $c_string)) {
         $stack->child_insert(new markup_simple('hr', []));
-        $p_lists = [];
-        continue;
-      }
-
-    # find blockquotes
-    # ─────────────────────────────────────────────────────────────────────
-      if (preg_match('%^(?<indent>[ ]{0,3})'.
-                       '(?<marker>[>][ ]{0,1})'.
-                       '(?<return>.+)$%S', $c_string, $c_matches)) {
-      # create new blockquote container
-        if (empty($p_quote)) {
-          $p_quote = new markup('blockquote');
-          $stack->child_insert($p_quote);
-        }
-      # insert new blockquote string
-        $p_quote->child_insert(
-          new text($c_matches['return'])
-        );
         continue;
       }
 
@@ -60,20 +40,66 @@ namespace effectivecore {
                        '(?<marker>[*+-]|[0-9]+(?<dot>.))'.
                        '(?<noises>[ ]{1,})'.
                        '(?<return>[^ ].+)$%S', $c_string, $c_matches)) {
+      # detect level and clear old pointers to containers
         $c_level = floor(((strlen($c_matches['indent']) - 1) / 4) + 1.25);
-      # remove pointers to old list containers
-        for ($c_i = $c_level + 1; $c_i < count($p_lists) + 1; $c_i++) {
-          unset($p_lists[$c_i]);
+        for ($i = $c_level + 1; $i < count($c_root->_p) + 1; $i++) {
+          unset($c_root->_p[$i]);
         }
       # create new list container
-        if (empty($p_lists[$c_level])) {
-          $p_lists[$c_level] = new markup($c_matches['dot'] ? 'ol' : 'ul');
-          if ($c_level == 1) $stack->child_insert($p_lists[1]);
-          if ($c_level >= 2) $p_lists[$c_level-1]->child_select_last()->child_insert($p_lists[$c_level]);
+        $c_root = $stack->child_select_last();
+        if (!($c_root instanceof markup &&
+             ($c_root->tag_name == 'ol' ||
+              $c_root->tag_name == 'ul'))) {
+          $c_root = new markup($c_matches['dot'] ? 'ol' : 'ul');
+          $c_root->_p[1] = $c_root;
+          $stack->child_insert($c_root);
+        }
+        if (empty($c_root->_p[$c_level-0]) &&
+           !empty($c_root->_p[$c_level-1])) {
+          $c_list = new markup($c_matches['dot'] ? 'ol' : 'ul');
+          $c_root->_p[$c_level-0] = $c_list;
+          $c_root->_p[$c_level-1]->child_select_last()->child_insert($c_list);
         }
       # insert new list item
-        $p_lists[$c_level]->child_insert(
+        $c_root->_p[$c_level]->child_insert(
           new markup('li', [], $c_matches['return'])
+        );
+        continue;
+      }
+
+    # find blockquotes
+    # ─────────────────────────────────────────────────────────────────────
+      if (preg_match('%^(?<indent>[ ]{0,3})'.
+                       '(?<marker>[>][ ]{0,1})'.
+                       '(?<return>.+)$%S', $c_string, $c_matches)) {
+      # create new blockquote container
+        $c_quote = $stack->child_select_last();
+        if (!($c_quote instanceof markup &&
+              $c_quote->tag_name == 'blockquote')) {
+          $c_quote = new markup('blockquote');
+          $stack->child_insert($c_quote);
+        }
+      # insert new blockquote string
+        $c_quote->child_insert(
+          new text($c_matches['return'])
+        );
+        continue;
+      }
+
+    # find code
+    # ─────────────────────────────────────────────────────────────────────
+      if (preg_match('%^(?<indent>[ ]{4})'.
+                       '(?<return>.+)$%S', $c_string, $c_matches)) {
+      # create new code container
+        $c_code = $stack->child_select_last();
+        if (!($c_code instanceof markup &&
+              $c_code->tag_name == 'code')) {
+          $c_code = new markup('code');
+          $stack->child_insert($c_code);
+        }
+      # insert new code string
+        $c_code->child_insert(
+          new text($c_matches['return'])
         );
         continue;
       }
