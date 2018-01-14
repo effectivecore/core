295bb43198f924640b711a1ed5ef9232fbb079a9
Maxim Rysevets <root@effcore.com>
2018-01-14 14:17:27 +0300

    sprint_311_426_106

--- system/module_core/backend/Markdown.php
+++ system/module_core/backend/Markdown.php
@@ -14,15 +14,15 @@ namespace effectivecore {
       $c_string = str_replace(tb, '    ', $c_string);
       $c_indent = strspn($c_string, ' ');
       $c_level = floor((($c_indent - 1) / 4) + 1.25);
-      $c_last = $pool->child_select_last();
+      $c_item = $pool->child_select_last();
       $c_matches = [];
 
     # headers
     # ─────────────────────────────────────────────────────────────────────
       if (preg_match('%^(?<marker>[-=]+)[ ]*$%S', $c_string, $c_matches)) {
-        if ($c_last instanceof markup &&
-            $c_last->tag_name == 'p'  &&
-            $c_last->child_select_first() instanceof text)
+        if ($c_item instanceof markup &&
+            $c_item->tag_name == 'p'  &&
+            $c_item->child_select_first() instanceof text)
         $pool->child_delete($pool->child_select_last_id());
         $pool->child_insert(new markup($c_matches['marker'] == '=' ? 'h1' : 'h2', [], $strings[$c_num-1]));
         continue;
@@ -49,27 +49,31 @@ namespace effectivecore {
                        '(?<marker>[*+-]|[0-9]+(?<dot>[.]))'.
                        '(?<noises>[ ]{1,})'.
                        '(?<return>[^ ].+)$%S', $c_string, $c_matches)) {
-      # create new root list container (ol/ul) if $c_last is not a container
-        if (!($c_last instanceof markup &&
-             ($c_last->tag_name == 'ol' ||
-              $c_last->tag_name == 'ul'))) {
-          $c_last = new markup($c_matches['dot'] ? 'ol' : 'ul');
-          $c_last->_p[1] = $c_last;
-          $pool->child_insert($c_last);
+      # special cases: paragraph near list, blockquote near list, code near list
+        if ($c_item instanceof markup && $c_item->tag_name == 'p')          {$c_item->child_insert(new text(nl.$c_string));       continue;}
+        if ($c_item instanceof markup && $c_item->tag_name == 'blockquote') {$c_item->child_insert(new text(nl.$c_string));       continue;}
+        if ($c_item instanceof markup && $c_item->tag_name == 'pre')        {$pool->child_insert(new markup('p', [], $c_string)); continue;}
+      # create new root list container (ol/ul) if $c_item is not a container
+        if (!($c_item instanceof markup &&
+             ($c_item->tag_name == 'ol' ||
+              $c_item->tag_name == 'ul'))) {
+          $c_item = new markup($c_matches['dot'] ? 'ol' : 'ul');
+          $c_item->_p[1] = $c_item;
+          $pool->child_insert($c_item);
         }
       # create new list sub container (ol/ul)
-        if (empty($c_last->_p[$c_level-0]) &&
-           !empty($c_last->_p[$c_level-1])) {
+        if (empty($c_item->_p[$c_level-0]) &&
+           !empty($c_item->_p[$c_level-1])) {
           $c_cont = new markup($c_matches['dot'] ? 'ol' : 'ul');
-          $c_last->_p[$c_level-0] = $c_cont;
-          $c_last->_p[$c_level-1]->child_select_last()->child_insert($c_cont);
+          $c_item->_p[$c_level-0] = $c_cont;
+          $c_item->_p[$c_level-1]->child_select_last()->child_insert($c_cont);
         }
       # remove old pointers to list containers (ol/ul)
-        for ($i = $c_level + 1; $i < count($c_last->_p) + 1; $i++) {
-          unset($c_last->_p[$i]);
+        for ($i = $c_level + 1; $i < count($c_item->_p) + 1; $i++) {
+          unset($c_item->_p[$i]);
         }
       # insert new list item (li)
-        $c_last->_p[$c_level]->child_insert(
+        $c_item->_p[$c_level]->child_insert(
           new markup('li', [], $c_matches['return'])
         );
         continue;
@@ -81,13 +85,13 @@ namespace effectivecore {
                        '(?<marker>[>][ ]{0,1})'.
                        '(?<return>.+)$%S', $c_string, $c_matches)) {
       # create new blockquote container
-        if (!($c_last instanceof markup &&
-              $c_last->tag_name == 'blockquote')) {
-          $c_last = new markup('blockquote'); # @todo: add blockquote/p
-          $pool->child_insert($c_last);
+        if (!($c_item instanceof markup &&
+              $c_item->tag_name == 'blockquote')) {
+          $c_item = new markup('blockquote'); # @todo: add blockquote/p
+          $pool->child_insert($c_item);
         }
       # insert new blockquote string
-        $c_last->child_insert(
+        $c_item->child_insert(
           new text($c_matches['return'])
         );
         continue;
@@ -96,8 +100,8 @@ namespace effectivecore {
     # paragraphs
     # ─────────────────────────────────────────────────────────────────────
       if (trim($c_string) == '') {
-        if ($c_last instanceof text && trim($c_last->text_get(), nl) == '') {
-          $c_last->text_set($c_last->text_get().nl);
+        if ($c_item instanceof text && trim($c_item->text_get(), nl) == '') {
+          $c_item->text_set($c_item->text_get().nl);
           continue;
         } else {
           $pool->child_insert(new text(nl));
@@ -105,13 +109,13 @@ namespace effectivecore {
         }
       }
       if (trim($c_string) != '') {
-        if ($c_last instanceof markup && $c_last->tag_name == 'p')          {$c_last->child_insert(new text(nl.$c_string)); continue;}
-        if ($c_last instanceof markup && $c_last->tag_name == 'blockquote') {$c_last->child_insert(new text(nl.$c_string)); continue;}
-        if ($c_last instanceof markup && (
-            $c_last->tag_name == 'ol' ||
-            $c_last->tag_name == 'ul')) {
-          if (!empty($c_last->_p[$c_level]))           {$c_last->_p[$c_level]          ->child_select_last()->child_insert(new text(nl.$c_string)); continue;}
-          if (!empty($c_last->_p[count($c_last->_p)])) {$c_last->_p[count($c_last->_p)]->child_select_last()->child_insert(new text(nl.$c_string)); continue;}
+        if ($c_item instanceof markup && $c_item->tag_name == 'p')          {$c_item->child_insert(new text(nl.$c_string)); continue;}
+        if ($c_item instanceof markup && $c_item->tag_name == 'blockquote') {$c_item->child_insert(new text(nl.$c_string)); continue;}
+        if ($c_item instanceof markup && (
+            $c_item->tag_name == 'ol' ||
+            $c_item->tag_name == 'ul')) {
+          if (!empty($c_item->_p[$c_level]))           {$c_item->_p[$c_level]          ->child_select_last()->child_insert(new text(nl.$c_string)); continue;}
+          if (!empty($c_item->_p[count($c_item->_p)])) {$c_item->_p[count($c_item->_p)]->child_select_last()->child_insert(new text(nl.$c_string)); continue;}
         }
         if ($c_indent < 4) {
           $pool->child_insert(new markup('p', [], $c_string));
@@ -125,14 +129,14 @@ namespace effectivecore {
                        '(?<noises>[ ]{0,})'.
                        '(?<return>[^ ].*)$%S', $c_string, $c_matches)) {
       # create new code container
-        if (!($c_last instanceof markup &&
-              $c_last->tag_name == 'pre')) {
-          $c_last = new markup('pre');
-          $c_last->child_insert(new markup('code'), 'code');
-          $pool->child_insert($c_last);
+        if (!($c_item instanceof markup &&
+              $c_item->tag_name == 'pre')) {
+          $c_item = new markup('pre');
+          $c_item->child_insert(new markup('code'), 'code');
+          $pool->child_insert($c_item);
         }
       # insert new code string
-        $c_last->child_select('code')->child_insert(
+        $c_item->child_select('code')->child_insert(
           new text($c_matches['return'])
         );
         continue;
