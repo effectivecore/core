910dace4504b7f16bff1ccee80d642e78f9b5839
Maxim Rysevets <root@effcore.com>
2018-02-24 19:34:08 +0300

    sprint_311_426_142_1

--- system/module_core/backend/Console.php
+++ system/module_core/backend/Console.php
@@ -30,34 +30,22 @@ namespace effectivecore {
 
   static function render() {
     return (new markup('x-console', [], [
-      new markup('h2', [], 'Execute plan'), static::render_logs(),
-      new markup('h2', [], 'Total load'),   static::render_diagram_load(),
-      new markup('h2', [], 'Information'),  static::render_information()
+      new markup('h2', [], 'Current page information'), static::get_markup_information(),
+      new markup('h2', [], 'Total load'),               static::get_markup_diagram_load(),
+      new markup('h2', [], 'Execute plan'),             static::get_markup_logs()
     ]))->render();
   }
 
-  static function render_logs() {
-    $thead = [['Time', 'Object', 'Action', 'Description', 'Val.']];
-    $tbody = [];
-    foreach (static::select_all_logs() as $c_log) {
-      $c_row_class = factory::to_css_class($c_log['object']);
-      $c_value_class = $c_log['value'] === 'error' ?
-        ['value' => 'value', 'value-error' => 'value-error'] :
-        ['value' => 'value'];
-      $tbody[] = new table_body_row(['class' => [$c_row_class => $c_row_class]], [
-        new table_body_row_cell(['class' => ['time'        => 'time']],        locale::format_msecond($c_log['time'])),
-        new table_body_row_cell(['class' => ['object'      => 'object']],      translation::get($c_log['object'],      $c_log['args'])),
-        new table_body_row_cell(['class' => ['action'      => 'action']],      translation::get($c_log['action'],      $c_log['args'])),
-        new table_body_row_cell(['class' => ['description' => 'description']], translation::get($c_log['description'], $c_log['args'])),
-        new table_body_row_cell(['class' => $c_value_class],                   translation::get($c_log['value']))
-      ]);
+  static function get_markup_information() {
+    $info = new markup('dl', ['class' => ['info' => 'info']]);
+    foreach (static::select_all_information() as $c_param => $c_value) {
+      $info->child_insert(new markup('dt', [], $c_param));
+      $info->child_insert(new markup('dd', [], $c_value));
     }
-    return (
-      new table(['class' => ['logs' => 'logs']], $tbody, $thead)
-    )->render();
+    return $info;
   }
 
-  static function render_diagram_load() {
+  static function get_markup_diagram_load() {
     $statistics = [];
     $total = 0;
     foreach (static::$data as $c_log) {
@@ -80,16 +68,28 @@ namespace effectivecore {
         ])
       ]));
     }
-    return $diagram->render();
+    return $diagram;
   }
 
-  static function render_information() {
-    $info = new markup('dl', ['class' => ['info' => 'info']]);
-    foreach (static::select_all_information() as $c_param => $c_value) {
-      $info->child_insert(new markup('dt', [], $c_param));
-      $info->child_insert(new markup('dd', [], $c_value));
+  static function get_markup_logs() {
+    $thead = [['Time', 'Object', 'Action', 'Description', 'Val.']];
+    $tbody = [];
+    foreach (static::select_all_logs() as $c_log) {
+      $c_row_class = factory::to_css_class($c_log['object']);
+      $c_value_class = $c_log['value'] === 'error' ?
+        ['value' => 'value', 'value-error' => 'value-error'] :
+        ['value' => 'value'];
+      $tbody[] = new table_body_row(['class' => [$c_row_class => $c_row_class]], [
+        new table_body_row_cell(['class' => ['time'        => 'time']],        locale::format_msecond($c_log['time'])),
+        new table_body_row_cell(['class' => ['object'      => 'object']],      translation::get($c_log['object'],      $c_log['args'])),
+        new table_body_row_cell(['class' => ['action'      => 'action']],      translation::get($c_log['action'],      $c_log['args'])),
+        new table_body_row_cell(['class' => ['description' => 'description']], translation::get($c_log['description'], $c_log['args'])),
+        new table_body_row_cell(['class' => $c_value_class],                   translation::get($c_log['value']))
+      ]);
     }
-    return $info->render();
+    return (
+      new table(['class' => ['logs' => 'logs']], $tbody, $thead)
+    );
   }
 
 }}
