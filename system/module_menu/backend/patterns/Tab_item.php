829f715e0a683f3df038113396ff9b1af4b6313e
Maxim Rysevets <root@effcore.com>
2018-03-14 23:42:47 +0300

    sprint_312_1

---
+++ system/module_menu/backend/patterns/Tab_item.php
@@ -0,0 +1,17 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2018 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore {
+          class tab_item extends \effcore\tree_item {
+
+  public $id;
+  public $id_parent;
+  public $parent_is_tree;
+  public $title = '';
+  public $template = 'tab_item';
+  public $template_children = 'tab_item_children';
+
+}}
