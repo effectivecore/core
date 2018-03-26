41046070ebcb0cebad75747bca4c0401ad77742b
Maxim Rysevets <root@effcore.com>
2018-03-26 19:47:14 +0300

    sprint_312_93

---
+++ system/module_develop/module_demo/backend/Demo_Interface.php
@@ -0,0 +1,12 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2018 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore {
+
+  interface demo_interface {
+  }
+
+}
