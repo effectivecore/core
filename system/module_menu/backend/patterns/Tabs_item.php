9de914dba5103dd377bfab5e7a9f0c86b7025a39
Maxim Rysevets <root@effcore.com>
2018-04-03 14:01:42 +0300

    sprint_312_105

--- system/module_menu/backend/patterns/Tabs_item.php
+++ system/module_menu/backend/patterns/Tabs_item.php
@@ -7,20 +7,11 @@
 namespace effcore {
           class tabs_item extends \effcore\tree_item {
 
-  public $id;
-  public $id_parent;
   public $parent_is_tab;
   public $action_name;
   public $title = '';
   public $template = 'tabs_item';
-  public $template_children = null;
-
-  function render() {
-    return (new template($this->template, [
-      'self'     => $this->render_self(),
-      'children' => $this->render_children($this->children)
-    ]))->render();
-  }
+  public $template_children = 'tabs_item_children';
 
   function render_self() {
     $href = page::get_current()->args_get('base').'/'.$this->action_name;
