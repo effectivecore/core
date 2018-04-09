a1fa315b3595a7d223c90d180398b2c9be80651d
Maxim Rysevets <root@effcore.com>
2018-04-09 21:59:48 +0300

    sprint_312_134

--- system/module_core/backend/Events_Page__global.php
+++ system/module_core/backend/Events_Page__global.php
@@ -12,7 +12,7 @@ namespace effcore {
     if (empty($user->id)) {
       return new markup('x-block', ['id' => 'user_menu'], [
         storage::get('files')->select('trees/user/user_anonymous'),
-        new markup('img', ['id' => 'avatar', 'src' => '/system/module_page/frontend/avatar-anonymous.svgd'])
+        new markup('img', ['id' => 'avatar', 'src' => '/system/module_page/frontend/images/avatar-anonymous.svgd'])
       ]);
     } else {
       return new markup('x-block', ['id' => 'user_menu'], [
@@ -21,7 +21,7 @@ namespace effcore {
           new markup('img', [
             'id' => 'avatar',
             'src' => $user->avatar_path_relative ?
-                 '/'.$user->avatar_path_relative : '/system/module_page/frontend/avatar-logged_in.svgd']
+                 '/'.$user->avatar_path_relative : '/system/module_page/frontend/images/avatar-logged_in.svgd']
         ))
       ]);
     }
