1ed8e27cd763988fcb145bef68b66c7cca519004
Maxim Rysevets <root@effcore.com>
2018-04-11 13:44:17 +0300

    sprint_312_138

--- system/module_page/backend/patterns/Field_Password.php
+++ system/module_page/backend/patterns/Field_Password.php
@@ -5,17 +5,18 @@
   ##################################################################
 
 namespace effcore {
-          class form_field_email extends form_field {
+          class form_field_password extends form_field {
 
-  public $title = 'EMail';
+  public $title = 'Password';
 
   function build() {
     $this->child_insert(new markup_simple('input', $this->element_attributes + [
-      'type'      => 'email',
-      'name'      => 'email',
-      'required'  => 'required',
-      'minlength' => 5,
-      'maxlength' => 64
+      'type'         => 'password',
+      'name'         => 'password',
+      'required'     => 'required',
+      'autocomplete' => 'off',
+      'minlength'    => 5,
+      'maxlength'    => 255
     ]), 'element');
   }
 
