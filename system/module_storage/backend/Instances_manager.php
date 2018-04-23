0b7a94a6c0dbf14b7b9e3636bad5141b02044440
Maxim Rysevets <root@effcore.com>
2018-04-23 13:01:26 +0300

    sprint_312_202

--- system/module_storage/backend/Instances_manager.php
+++ system/module_storage/backend/Instances_manager.php
@@ -12,7 +12,7 @@ namespace effcore {
     $links = new markup('ul');
     foreach ($entities as $c_entity) {
       $links->child_insert(
-        new markup('li', [], new markup('a', ['href' => '/admin/instances/'.$c_entity->name.'/insert'], $c_entity->title))
+        new markup('li', [], new markup('a', ['href' => '/manage/instances/'.$c_entity->name.'/insert'], $c_entity->title))
       );
     }
     return $links;
@@ -37,7 +37,7 @@ namespace effcore {
         $c_action_list = new control_actions_list();
         $c_action_list->action_add('/user/'.$c_user->id, 'view');
         $c_action_list->action_add('/user/'.$c_user->id.'/edit?'.url::make_back_part(), 'edit');
-        $c_action_list->action_add('/admin/users/delete/'.$c_user->id.'?'.url::make_back_part(), 'delete', !$c_user->is_embed);
+        $c_action_list->action_add('/manage/users/delete/'.$c_user->id.'?'.url::make_back_part(), 'delete', !$c_user->is_embed);
         $tbody[] = [
           new table_body_row_cell(['class' => ['id' => 'id']], $c_user->id),
           new table_body_row_cell(['class' => ['email' => 'email']], $c_user->email),
@@ -47,7 +47,7 @@ namespace effcore {
           new table_body_row_cell(['class' => ['actions' => 'actions']], $c_action_list)
         ];
       }
-      return new markup('x-block', ['class' => ['users_admin']],
+      return new markup('x-block', ['class' => ['users']],
         new table([], $tbody, $thead)
       );
     }
