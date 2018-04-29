3aeb93fcb93568544646e941feb0f772f9731351
Maxim Rysevets <root@effcore.com>
2018-04-29 15:41:14 +0300

    sprint_312_223_3

--- system/module_page/backend/patterns/Field_Number.php
+++ system/module_page/backend/patterns/Field_Number.php
@@ -5,21 +5,17 @@
   ##################################################################
 
 namespace effcore {
-          class field_number extends field {
+          class field_number extends field_simple {
 
   public $title = 'Number';
-
-  function build() {
-    $this->child_insert(new markup_simple('input', [
-      'type'     => 'number',
-      'name'     => 'number',
-      'required' => 'required',
-      'value'    => 0,
-      'step'     => 1,
-      'min'      => -10000000000,
-      'max'      => +10000000000
-    ]), 'element');
-    parent::build();
-  }
+  public $element_attributes_default = [
+    'type'     => 'number',
+    'name'     => 'number',
+    'required' => 'required',
+    'value'    => 0,
+    'step'     => 1,
+    'min'      => -10000000000,
+    'max'      => +10000000000
+  ];
 
 }}
