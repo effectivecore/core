b533dad6d87041a8d84bd0a6861fe7f2501d2e7a
Maxim Rysevets <root@effcore.com>
2018-04-29 17:27:09 +0300

    sprint_312_223_15

--- system/module_page/backend/patterns/Field_Textarea.php
+++ system/module_page/backend/patterns/Field_Textarea.php
@@ -5,21 +5,27 @@
   ##################################################################
 
 namespace effcore {
-          class field_textarea extends field {
+          class field_textarea extends field_simple {
 
   public $title = 'Textarea';
+  public $element_class = '\\effcore\\markup';
+  public $element_tag_name = 'textarea';
+  public $element_attributes_default = [
+    'name'      => 'textarea',
+    'required'  => 'required',
+    'rows'      => 5,
+    'minlength' => 5,
+    'maxlength' => 255,
+  ];
 
   function build() {
-    $value = $this->attribute_select('value', 'element_attributes') ?: '';
-    $this->attribute_delete('value', 'element_attributes');
-    $this->child_insert(new markup('textarea', [
-      'name'      => 'textarea',
-      'required'  => 'required',
-      'rows'      => 5,
-      'minlength' => 5,
-      'maxlength' => 255,
-    ], ['content' => new text($value)]), 'element');
+    $value_def = $this->attribute_select('value', 'element_attributes_default') ?: '';
+    $value     = $this->attribute_select('value', 'element_attributes')         ?: '';
+                 $this->attribute_delete('value', 'element_attributes_default');
+                 $this->attribute_delete('value', 'element_attributes');
     parent::build();
+    $element = $this->child_select('element');
+    $element->child_insert(new text_simple($value ?: $value_def), 'content');
   }
 
 }}
