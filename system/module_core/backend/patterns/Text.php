637d1700c3dd9a2299dd0d6db867dec46c0267c3
Maxim Rysevets <root@effcore.com>
2018-05-02 18:00:00 +0300

    sprint_312_224

--- system/module_core/backend/patterns/Text.php
+++ system/module_core/backend/patterns/Text.php
@@ -7,6 +7,16 @@
 namespace effcore {
           class text extends text_simple {
 
+  public $args = [];
+
+  function __construct($text = '', $args = [], $weight = 0) {
+    parent::__construct($text, $weight);
+    if ($args) $this->args_set($args);
+  }
+
+  function args_get() {return $this->args;}
+  function args_set($args) {$this->args = $args;}
+
   function render() {
     return translation::get($this->text, $this->args);
   }
--- system/module_core/backend/patterns/Text_simple.php
+++ system/module_core/backend/patterns/Text_simple.php
@@ -8,12 +8,10 @@ namespace effcore {
           class text_simple {
 
   public $text = '';
-  public $args = [];
   public $weight = 0;
 
-  function __construct($text = '', $args = [], $weight = 0) {
+  function __construct($text = '', $weight = 0) {
     if ($text !== '') $this->text_update($text);
-    if ($args)        $this->args_set($args);
     if ($weight)      $this->weight = $weight;
   }
 
@@ -21,15 +19,8 @@ namespace effcore {
   function text_update($text) {$this->text = $text;}
   function text_append($text) {$this->text.= $text;}
 
-  function args_get() {return $this->args;}
-  function args_set($args) {$this->args = $args;}
-
   function render() {
-    $return = $this->text;
-    foreach ($this->args as $c_key => $c_value) {
-      $return = str_replace('%%_'.$c_key, $c_value, $return);
-    }
-    return $return;
+    return $this->text;
   }
 
 }}
