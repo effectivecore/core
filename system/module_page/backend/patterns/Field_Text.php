6171d80208b36acef253117e771d07645e370ca3
Maxim Rysevets <root@effcore.com>
2018-05-07 14:31:54 +0300

    sprint_312_246_4

--- system/module_page/backend/patterns/Field_Text.php
+++ system/module_page/backend/patterns/Field_Text.php
@@ -21,11 +21,18 @@ namespace effcore {
     $name = $this->get_element_name();
     $type = $this->get_element_type();
     if ($name && $type) {
+      if (static::validate_is_disabled($this, $element)) return true;
+      if (static::validate_is_readonly($this, $element)) return true;
       $index = !isset(static::$indexes[$name]) ?
                      (static::$indexes[$name] = 0) :
                     ++static::$indexes[$name];
-      if (!static::validate_disabled($element)) return false;
-      if (!static::validate_readonly($element)) return false;
+      $new_value = !isset($_POST[$name]) ? '' :
+               (is_string($_POST[$name]) ? $_POST[$name] : 
+                (is_array($_POST[$name]) &&
+                    isset($_POST[$name][$index]) ?
+                          $_POST[$name][$index] : ''));
+      $element->attribute_insert('value', $new_value);
+      if (!static::validate_text_required($this, $element, $new_value, $form, $dpath)) return false;
       return true;
     }
   }
@@ -36,12 +43,21 @@ namespace effcore {
 
   static protected $indexes = [];
 
-  static function validate_disabled($element) {
-    return $element->attribute_select('disabled') ? false : true;
+  static function validate_is_disabled($field, $element) {
+    return $element->attribute_select('disabled') ? true : false;
   }
 
-  static function validate_readonly($element) {
-    return $element->attribute_select('readonly') ? false : true;
+  static function validate_is_readonly($field, $element) {
+    return $element->attribute_select('readonly') ? true : false;
+  }
+
+  static function validate_text_required($field, $element, $new_value, $form, $dpath) {
+    if ($element->attribute_select('required') && strlen($new_value) == 0) {
+      $form->add_error($dpath.'/element',
+        translation::get('Field "%%_title" can not be blank!', ['title' => translation::get($field->title)])
+      );
+      return;
+    }
   }
 
 }}
