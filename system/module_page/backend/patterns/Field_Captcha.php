328d3837465862a32dbcfc3ea15c828df681ad46
Maxim Rysevets <root@effcore.com>
2018-05-09 13:54:09 +0300

    sprint_312_246_37

--- system/module_page/backend/patterns/Field_Captcha.php
+++ system/module_page/backend/patterns/Field_Captcha.php
@@ -5,7 +5,7 @@
   ##################################################################
 
 namespace effcore {
-          class field_captcha extends field {
+          class field_captcha extends field_text {
 
   public $title = 'Captcha';
   public $description = 'Write the characters from the picture.';
@@ -95,8 +95,6 @@ namespace effcore {
   ### static declarations ###
   ###########################
 
-  static protected $glyphs;
-
   # note:
   # ─────────────────────────────────────────────────────────────────────
   # 1. function id_get:
@@ -106,6 +104,8 @@ namespace effcore {
   #    on each submit and this action will create a great variety of unique
   #    captcha_id in the storage and will make it overflowed)
   # ─────────────────────────────────────────────────────────────────────
+
+  static protected $glyphs;
 
   static function id_get() {
     return $_SERVER['REMOTE_ADDR'];
@@ -130,6 +130,34 @@ namespace effcore {
   static function captcha_cleaning() {
     $storage = $s = storage::get(entity::get('captcha')->get_storage_id());
     $storage->query('DELETE', 'FROM', $s->tables('captcha'), 'WHERE', $s->condition('created', factory::datetime_get('-1 hour'), '<'));
+  }
+
+  static function validate($field, $form, $dpath) {
+    $element = $field->child_select('element');
+    $name = $field->get_element_name();
+    $type = $field->get_element_type();
+    if ($name && $type) {
+      if (static::is_disabled($field, $element)) return true;
+      if (static::is_readonly($field, $element)) return true;
+      $cur_index = static::get_cur_index($name);
+      $new_value = static::get_new_value($name, $cur_index);
+      $result = static::validate_required ($field, $form, $dpath, $element, $new_value) &&
+                static::validate_minlength($field, $form, $dpath, $element, $new_value) &&
+                static::validate_maxlength($field, $form, $dpath, $element, $new_value) &&
+                static::validate_value    ($field, $form, $dpath, $element, $new_value);
+      $element->attribute_insert('value', $new_value);
+      return $result;
+    }
+  }
+
+  static function validate_value($field, $form, $dpath, $element, &$new_value) {
+    if (!$field->captcha_check($new_value)) {
+      $form->add_error($dpath.'/element',
+        translation::get('Field "%%_title" contains an incorrect characters from image!', ['title' => translation::get($field->title)])
+      );
+    } else {
+      return true;
+    }
   }
 
 }}
