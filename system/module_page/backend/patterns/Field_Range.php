191b4c1dece450572e170fd9adcc4017e31c5737
Maxim Rysevets <root@effcore.com>
2018-05-09 13:04:30 +0300

    sprint_312_246_35

--- system/module_page/backend/patterns/Field_Range.php
+++ system/module_page/backend/patterns/Field_Range.php
@@ -7,14 +7,17 @@
 namespace effcore {
           class field_range extends field_number {
 
+  const input_min_range = 0;
+  const input_max_range = 100;
+
   public $title = 'Range';
   public $attributes = ['x-type' => 'range'];
   public $element_attributes_default = [
     'type'     => 'range',
     'name'     => 'range',
     'required' => 'required',
-    'min'      => 0,
-    'max'      => 100,
+    'min'      => self::input_min_range,
+    'max'      => self::input_max_range,
     'step'     => 1,
     'value'    => 0
   ];
@@ -23,7 +26,7 @@ namespace effcore {
   ### static declarations ###
   ###########################
 
-  static function get_min_value($element) {return $element->attribute_select('min') !== null ? $element->attribute_select('min') : 0;}
-  static function get_max_value($element) {return $element->attribute_select('max') !== null ? $element->attribute_select('max') : 100;}
+  static function get_min_value($element) {return $element->attribute_select('min') !== null ? $element->attribute_select('min') : self::input_min_range;}
+  static function get_max_value($element) {return $element->attribute_select('max') !== null ? $element->attribute_select('max') : self::input_max_range;}
 
 }}
