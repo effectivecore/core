aff61fb8c32165073d16b49c1a448891438cedc6
Maxim Rysevets <root@effcore.com>
2018-06-13 13:14:50 +0300

    sprint_312_246_183

--- system/module_develop/module_demo/backend/Events_Form.php
+++ system/module_develop/module_demo/backend/Events_Form.php
@@ -16,12 +16,12 @@ namespace effcore\modules\demo {
   ##################
 
   static function on_init_demo($form, $items) {
-    $items['form_elements/select_macro']->option_insert('Option 2.5 (inserted + disabled from init)', 'option_2_5', ['disabled' => 'disabled'], 'group_2_1');
-    $items['form_elements/select_macro']->option_insert('Option 2.6 (inserted from init)', 'option_2_6', [], 'group_2_1');
-    $items['form_elements/select_macro']->optgroup_insert('group_2_2', 'Group 2.2 (inserted from init)');
-    $items['form_elements/select_macro']->option_insert('Option 2.7 (inserted from init)', 'option_2_7', [], 'group_2_2');
-    $items['form_elements/select_macro']->option_insert('Option 2.8 (inserted from init)', 'option_2_8', [], 'group_2_2');
-    $items['form_elements/select_macro']->option_insert('Option 2.9 (inserted from init)', 'option_2_9', [], 'group_2_2');
+    $items['form_elements/select']->option_insert('Option 5 (inserted + disabled from code)', 'option_5', ['disabled' => 'disabled'], 'group_1');
+    $items['form_elements/select']->option_insert('Option 6 (inserted from code)', 'option_6', [], 'group_1');
+    $items['form_elements/select']->optgroup_insert('group_2', 'Group 2 (inserted from code)');
+    $items['form_elements/select']->option_insert('Option 7 (inserted from code)', 'option_7', [], 'group_2');
+    $items['form_elements/select']->option_insert('Option 8 (inserted from code)', 'option_8', [], 'group_2');
+    $items['form_elements/select']->option_insert('Option 9 (inserted from code)', 'option_9', [], 'group_2');
     $items['form_elements/file']->pool_values_init_old(
       data::select('files_demo') ?: []
     );
