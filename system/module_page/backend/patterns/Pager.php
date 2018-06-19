75ae95e061ecca53f0254c8dd0d0b7f44765fadd
Maxim Rysevets <root@effcore.com>
2018-06-19 20:07:43 +0300

    sprint_312_246_214

--- system/module_page/backend/patterns/Pager.php
+++ system/module_page/backend/patterns/Pager.php
@@ -7,39 +7,55 @@
 namespace effcore {
           class pager extends markup {
 
-  public $id;
+  public $id = 0;
+  public $prefix = 'page';
   public $tag_name = 'x-pager';
   public $has_error = false;
-  public $page_num_min = 1;
-  public $page_num_max = 100;
-  public $page_num_cur = 50;
+  public $min = 1;
+  public $max = 10;
+  public $cur = 1;
 
   function __construct($attributes = [], $weight = 0) {
     parent::__construct($this->tag_name, $attributes, [], $weight);
+    $this->cur = (int)url::current_get()->query_arg_get($this->pager_name_get());
+    if ($this->cur > $this->max) {
+        $this->cur = $this->max;
+        $this->has_error = true;
+    }
   }
 
-  function get_page_num_cur() {
-    return $this->page_num_cur;
+  function min_get() {return $this->min;}
+  function max_get() {return $this->max;}
+  function cur_get() {return $this->cur;}
+
+  function pager_name_get() {
+    return $this->id ? $this->prefix.$this->id :
+                       $this->prefix;
   }
 
-  function render() { # @todo: make functionality
+  function render() {
     $pager = new markup($this->tag_name);
-    $pager->child_insert(new markup('a', ['href' => '#'], 1));
-    $pager->child_insert(new text('...'));
-    $pager->child_insert(new markup('a', ['href' => '#'], 25));
-    $pager->child_insert(new text('...'));
-    $pager->child_insert(new markup('a', ['href' => '#'], 47));
-    $pager->child_insert(new markup('a', ['href' => '#'], 48));
-    $pager->child_insert(new markup('a', ['href' => '#'], 49));
-    $pager->child_insert(new markup('a', ['href' => '#', 'class' => ['active']], 50));
-    $pager->child_insert(new markup('a', ['href' => '#'], 51));
-    $pager->child_insert(new markup('a', ['href' => '#'], 52));
-    $pager->child_insert(new markup('a', ['href' => '#'], 53));
-    $pager->child_insert(new text('...'));
-    $pager->child_insert(new markup('a', ['href' => '#'], 75));
-    $pager->child_insert(new text('...'));
-    $pager->child_insert(new markup('a', ['href' => '#'], 100));
-    $pager->child_insert(new markup('div', [], 'UNDER CONSTRUCTION'));
+    $pager_name = $this->pager_name_get();
+    $url = url::current_get();
+    if ($this->min == 1) {
+      $url->query_arg_set($pager_name, $this->min);
+      $pager->child_insert(new markup('a', ['href' => $url->relative_get()], $this->min));
+    }
+    if ($this->max >= 3) {
+      $start_pos = $this->cur - 8 > $this->min ?
+                   $this->cur - 8 : $this->min + 1;
+      for ($i = $start_pos;
+           $i < $start_pos + 16; $i++) {
+        if ($i > $this->min && $i < $this->max) {
+          $url->query_arg_set($pager_name, $i);
+          $pager->child_insert(new markup('a', ['href' => $url->relative_get()], $i));
+        }
+      }
+    }
+    if ($this->max >= 2) {
+      $url->query_arg_set($pager_name, $this->max);
+      $pager->child_insert(new markup('a', ['href' => $url->relative_get()], $this->max));
+    }
     return $pager->render();
   }
 
