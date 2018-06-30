cb145898cb08d60c7e78a74e79baa678446645a0
Maxim Rysevets <root@effcore.com>
2018-06-30 11:21:23 +0300

    sprint_312_246_252

--- system/module_page/backend/Template.php
+++ system/module_page/backend/Template.php
@@ -44,18 +44,16 @@ namespace effcore {
     if ($this->type == 'text' ||
         $this->type == 'file') {
       $rendered = $this->data;
-      if (count($this->args)) {
-        $rendered = preg_replace_callback('%(?<spacer>[ ]*)'.
-                                           '(?<prefix>\\%\\%_)'.
-                                           '(?<name>[a-z0-9_]+)'.
-                                           '(?<args>\\{[a-z0-9_,]+\\}|)%S', function($matches) {
-          return isset($matches['prefix']) &&
-                 isset($matches['name']) &&
-                 isset($this->args[$matches['name']]) &&
-                       $this->args[$matches['name']] !== '' ? $matches['spacer'].
-                       $this->args[$matches['name']] : '';
-        }, $rendered);
-      }
+      $rendered = preg_replace_callback('%(?<spacer>[ ]*)'.
+                                         '(?<prefix>\\%\\%_)'.
+                                         '(?<name>[a-z0-9_]+)'.
+                                         '(?<args>\\{[a-z0-9_,]+\\}|)%S', function($matches) {
+        return isset($matches['prefix']) &&
+               isset($matches['name']) &&
+               isset($this->args[$matches['name']]) &&
+                     $this->args[$matches['name']] !== '' ? $matches['spacer'].
+                     $this->args[$matches['name']] : '';
+      }, $rendered);
       return $rendered;
     }
     if ($this->type == 'code') {
