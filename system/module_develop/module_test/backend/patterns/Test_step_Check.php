e04e2c70b95a129e8fc06b9265bac45d28df7108
Maxim Rysevets <root@effcore.com>
2018-07-31 13:53:54 +0300

    sprint_312_246_321

--- system/module_develop/module_test/backend/patterns/Test_step_Check.php
+++ system/module_develop/module_test/backend/patterns/Test_step_Check.php
@@ -13,42 +13,46 @@ namespace effcore {
   public $on_failure;
 
   function run(&$test, &$c_scenario, &$c_step, &$c_results) {
-    if ($this->where == 'http_code' && array_key_exists('http_code', $c_results['request']['info']) &&
-        $this->match ==                                              $c_results['request']['info']['http_code']) {
-      if (isset($this->on_success)) {
-        $c_scenario = $this->on_success;
-        $c_step = reset($c_scenario);
-        $c_results['is_continue'] = true;
-        $c_results['reports'][] = translation::get('checking on "%%_name" = "%%_value"', ['name' => 'http_code', 'value' => $c_results['request']['info']['http_code']]);
-        $c_results['reports'][] = translation::get('&ndash; result of checking is = "%%_result"', ['result' => 'on_success']);
-        return;
-      }
-      if (isset($this->on_failure)) {
-        $c_scenario = $this->on_failure;
-        $c_step = reset($c_scenario);
-        $c_results['is_continue'] = true;
-        $c_results['reports'][] = translation::get('checking on "%%_name" = "%%_value"', ['name' => 'http_code', 'value' => $c_results['request']['info']['http_code']]);
-        $c_results['reports'][] = translation::get('&ndash; result of checking is = "%%_result"', ['result' => 'on_failure']);
-        return;
+  # check on 'http_code'
+    if ($this->where == 'http_code') {
+      if (array_key_exists('http_code', $c_results['request']['info']) &&
+          $this->match ==               $c_results['request']['info']['http_code']) {
+        if (isset($this->on_success)) {
+          $c_scenario = $this->on_success;
+          $c_step = reset($c_scenario);
+          $c_results['is_continue'] = true;
+          $c_results['reports'][] = translation::get('checking on "%%_name" = "%%_value"', ['name' => 'http_code', 'value' => $this->match]);
+          $c_results['reports'][] = translation::get('&ndash; result of checking is = "%%_result"', ['result' => 'success']);
+        }
+      } else {
+        if (isset($this->on_failure)) {
+          $c_scenario = $this->on_failure;
+          $c_step = reset($c_scenario);
+          $c_results['is_continue'] = true;
+          $c_results['reports'][] = translation::get('checking on "%%_name" = "%%_value"', ['name' => 'http_code', 'value' => $this->match]);
+          $c_results['reports'][] = translation::get('&ndash; result of checking is = "%%_result"', ['result' => 'failure']);
+        }
       }
     }
-    if ($this->where == 'subm_errs' && array_key_exists('X-Submit-Errors-Count', $c_results['request']['headers']) &&
-        $this->match ==                                                          $c_results['request']['headers']['X-Submit-Errors-Count']) {
-      if (isset($this->on_success)) {
-        $c_scenario = $this->on_success;
-        $c_step = reset($c_scenario);
-        $c_results['is_continue'] = true;
-        $c_results['reports'][] = translation::get('checking on "%%_name" = "%%_value"', ['name' => 'subm_errs', 'value' => $c_results['request']['headers']['X-Submit-Errors-Count']]);
-        $c_results['reports'][] = translation::get('&ndash; result of checking is = "%%_result"', ['result' => 'on_success']);
-        return;
-      }
-      if (isset($this->on_failure)) {
-        $c_scenario = $this->on_failure;
-        $c_step = reset($c_scenario);
-        $c_results['is_continue'] = true;
-        $c_results['reports'][] = translation::get('checking on "%%_name" = "%%_value"', ['name' => 'subm_errs', 'value' => $c_results['request']['headers']['X-Submit-Errors-Count']]);
-        $c_results['reports'][] = translation::get('&ndash; result of checking is = "%%_result"', ['result' => 'on_failure']);
-        return;
+  # check on 'subm_errs'
+    if ($this->where == 'subm_errs') {
+      if (array_key_exists('X-Submit-Errors-Count', $c_results['request']['headers']) &&
+          $this->match ==                           $c_results['request']['headers']['X-Submit-Errors-Count']) {
+        if (isset($this->on_success)) {
+          $c_scenario = $this->on_success;
+          $c_step = reset($c_scenario);
+          $c_results['is_continue'] = true;
+          $c_results['reports'][] = translation::get('checking on "%%_name" = "%%_value"', ['name' => 'subm_errs', 'value' => $this->match]);
+          $c_results['reports'][] = translation::get('&ndash; result of checking is = "%%_result"', ['result' => 'success']);
+        }
+      } else {
+        if (isset($this->on_failure)) {
+          $c_scenario = $this->on_failure;
+          $c_step = reset($c_scenario);
+          $c_results['is_continue'] = true;
+          $c_results['reports'][] = translation::get('checking on "%%_name" = "%%_value"', ['name' => 'subm_errs', 'value' => $this->match]);
+          $c_results['reports'][] = translation::get('&ndash; result of checking is = "%%_result"', ['result' => 'failure']);
+        }
       }
     }
   }
