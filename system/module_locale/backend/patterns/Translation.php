6aabdf19b8d9bc542a89fdaaa641476ab8eb740e
Maxim Rysevets <root@effcore.com>
2018-08-12 19:11:08 +0300

    sprint_312_246_362

--- system/module_locale/backend/patterns/Translation.php
+++ system/module_locale/backend/patterns/Translation.php
@@ -41,9 +41,28 @@ namespace effcore {
     if (!isset(static::$cache[$c_code])) static::init($c_code);
     $string = isset(static::$cache[$c_code][$string]) ?
                     static::$cache[$c_code][$string] : $string;
-    return preg_replace_callback('%\\%\\%_(?<name>[a-z0-9_]+)(?:\\{(?<args>[a-z0-9_,=\'"]+)\\}|)%S', function($c_match) use ($args) {
+    return preg_replace_callback('%\\%\\%_(?<name>[a-z0-9_]+)(?:\\{(?<args>[a-z0-9_,=\'"-]+)\\}|)%S', function($c_match) use ($c_code, $args) {
       $c_name =       $c_match['name'];
       $c_args = isset($c_match['args']) ? explode(',', $c_match['args']) : [];
+    # plurals functionality
+      if ($c_name == 'plural') {
+        if (isset($c_args[0]) &&
+            isset($c_args[1])) {
+          $c_number_name = $c_args[0];
+          $c_plural_type = $c_args[1];
+          $c_plurals     = language::get_plurals($c_code);
+          if (isset($c_plurals[$c_plural_type]) &&
+                   isset($args[$c_number_name])) {
+            $p_plural_info = $c_plurals[$c_plural_type];
+            $p_matches     = [];
+            if (preg_match($p_plural_info->formula, (string)$args[$c_number_name], $p_matches)) {
+              $replacement = array_intersect_key($p_plural_info->matches, $p_matches);
+              return reset($replacement);
+            }
+          }
+        }
+      }
+    # default case
       return isset($args[$c_name]) ?
                    $args[$c_name] : $c_match[0];
     }, $string);
