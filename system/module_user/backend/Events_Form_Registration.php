71c168cba14a31b08451c6a225ca1abc254d1870
Maxim Rysevets <root@effcore.com>
2018-08-27 23:32:51 +0300

    sprint_312_246_422

---
+++ system/module_user/backend/Events_Form_Registration.php
@@ -0,0 +1,58 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2019 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\user {
+          use \effcore\core;
+          use \effcore\instance;
+          use \effcore\message;
+          use \effcore\session;
+          use \effcore\url;
+          abstract class events_form_registration extends \effcore\events_form {
+
+  static function on_validate_registration($form, $items) {
+    switch ($form->clicked_button->value_get()) {
+      case 'register':
+        if ($form->total_errors_count_get() == 0) {
+        # test email
+          if ((new instance('user', ['email' => strtolower($items['#email']->value_get())]))->select()) {
+            $items['#email']->error_set(
+              'User with this EMail was already registered!'
+            );
+            return;
+          }
+        # test nick
+          if ((new instance('user', ['nick' => strtolower($items['#nick']->value_get())]))->select()) {
+            $items['#nick']->error_set(
+              'User with this Nick was already registered!'
+            );
+            return;
+          }
+        }
+        break;
+    }
+  }
+
+  static function on_submit_registration($form, $items) {
+    switch ($form->clicked_button->value_get()) {
+      case 'register':
+        $user = (new instance('user', [
+          'email'         => strtolower($items['#email']->value_get()),
+          'nick'          => strtolower($items['#nick']->value_get()),
+          'password_hash' => core::hash_password_get($items['#password']->value_get())
+        ]))->insert();
+        if ($user) {
+          session::insert($user->id,
+            core::array_kmap($items['*session_params']->values_get())
+          );
+          url::go('/user/'.$user->id);
+        } else {
+          message::insert('User was not registered!', 'error');
+        }
+        break;
+    }
+  }
+
+}}
