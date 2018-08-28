1ff1e58208f11cc9273e159c4ef40fcd1cafa11a
Maxim Rysevets <root@effcore.com>
2018-08-28 12:04:32 +0300

    sprint_312_246_428

--- system/module_page/backend/Events_Form_Decoration.php
+++ system/module_page/backend/Events_Form_Decoration.php
@@ -7,15 +7,15 @@
 namespace effcore\modules\page {
           use \effcore\message;
           use \effcore\storage;
-          abstract class events_form_decoration extends \effcore\events_form {
+          abstract class events_form_decoration {
 
-  static function on_init_decoration($form, $items) {
+  static function on_init($form, $items) {
     $settings = storage::get('files')->select('settings');
     $items['*color_id'   ]->value_set($settings['page']->color_id   );
     $items['*color_bg_id']->value_set($settings['page']->color_bg_id);
   }
 
-  static function on_submit_decoration($form, $items) {
+  static function on_submit($form, $items) {
     switch ($form->clicked_button->value_get()) {
       case 'save':
         storage::get('files')->changes_insert('page', 'update', 'settings/page/color_id',    $items['*color_id'   ]->value_get(), false);
