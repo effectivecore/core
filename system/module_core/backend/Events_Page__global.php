2fc3fb57fcc4b15f1d0bc5b1b0689b84151e721e
Maxim Rysevets <root@effcore.com>
2018-08-28 14:07:52 +0300

    sprint_312_246_436

--- system/module_core/backend/Events_Page__global.php
+++ system/module_core/backend/Events_Page__global.php
@@ -7,36 +7,6 @@
 namespace effcore {
           abstract class events_page {
 
-  static function on_show_block_menu_user($page) {
-    $block_menu = null;
-    $user = user::current_get();
-    if (empty($user->id)) {
-      $block_menu = new block('', ['class' => ['menu-user' => 'menu-user']], [
-        storage::get('files')->select('trees/user/user_anonymous'),
-        new markup_simple('img', [
-          'class' => ['avatar' => 'avatar'],
-          'alt' => 'avatar',
-          'src' => '/'.module::get('page')->path_get().'frontend/images/avatar-anonymous.svgd'
-        ])
-      ]);
-    } else {
-      $block_menu = new block('', ['class' => ['menu-user' => 'menu-user']], [
-        storage::get('files')->select('trees/user/user_logged_in'),
-        new markup('a', ['href' => '/user/'.$user->id],
-          new markup_simple('img', [
-            'class' => ['avatar' => 'avatar'],
-            'alt' => 'avatar',
-            'src' => $user->avatar_path_relative ?
-                 '/'.$user->avatar_path_relative :
-                 '/'.module::get('page')->path_get().'frontend/images/avatar-logged_in.svgd'
-          ])
-        )
-      ]);
-    }
-    $block_menu->content_tag_name = null;
-    return $block_menu;
-  }
-
   static function on_show_block_title($page) {
     return new markup('h1', ['id' => 'title'],
       token::replace(translation::get($page->title))
