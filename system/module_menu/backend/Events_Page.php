acc55582ce1e000182f49f5646411aeb8b870973
Maxim Rysevets <root@effcore.com>
2018-09-17 00:11:32 +0300

    sprint_312_247

--- system/module_menu/backend/Events_Page.php
+++ system/module_menu/backend/Events_Page.php
@@ -14,28 +14,20 @@ namespace effcore\modules\menu {
           abstract class events_page {
 
   static function on_show_block_menu_user($page) {
-    $block_menu = null;
     $user = user::current_get();
     if (empty($user->id)) {
+      $src = '/'.module::get('page')->path_get().'frontend/images/avatar-anonymous.svgd';
       $block_menu = new block('', ['class' => ['menu-user' => 'menu-user']], [
         storage::get('files')->select('trees/user/user_anonymous'),
-        new markup_simple('img', [
-          'class' => ['avatar' => 'avatar'],
-          'alt' => 'avatar',
-          'src' => '/'.module::get('page')->path_get().'frontend/images/avatar-anonymous.svgd'
-        ])
+        new markup_simple('img', ['class' => ['avatar' => 'avatar'], 'alt' => 'avatar', 'src' => $src])
       ]);
     } else {
+      $src = $user->avatar_path_relative ?
+         '/'.$user->avatar_path_relative : '/'.module::get('page')->path_get().'frontend/images/avatar-logged_in.svgd';
       $block_menu = new block('', ['class' => ['menu-user' => 'menu-user']], [
         storage::get('files')->select('trees/user/user_logged_in'),
         new markup('a', ['href' => '/user/'.$user->id],
-          new markup_simple('img', [
-            'class' => ['avatar' => 'avatar'],
-            'alt' => 'avatar',
-            'src' => $user->avatar_path_relative ?
-                 '/'.$user->avatar_path_relative :
-                 '/'.module::get('page')->path_get().'frontend/images/avatar-logged_in.svgd'
-          ])
+          new markup_simple('img', ['class' => ['avatar' => 'avatar'], 'alt' => 'avatar', 'src' => $src])
         )
       ]);
     }
