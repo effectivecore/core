3b3173e069078ac8694654245189d710d336cb4b
Maxim Rysevets <root@effcore.com>
2018-09-17 13:58:28 +0300

    sprint_312_255

--- system/module_menu/backend/patterns/Tabs.php
+++ system/module_menu/backend/patterns/Tabs.php
@@ -86,11 +86,20 @@ namespace effcore {
     return static::$cache_tabs_items;
   }
 
-  static function insert_item($title, $id, $id_parent, $action_name, $action_default_name = null, $attributes = [], $weight = 0) {
+  static function subitem_insert($title, $id, $id_parent, $action_name, $action_default_name = null, $attributes = [], $weight = 0) {
     $new_item = new tabs_item($title, $id, $id_parent, $action_name, $action_default_name, $attributes, $weight);
     static::$cache_tabs_items[$id] = $new_item;
     static::$cache_tabs_items[$id]->module_id = null;
     static::build([$new_item]);
+  }
+
+  static function subitem_delete($id) {
+    if (isset(static::$cache_tabs_items[$id])) {
+      $id_parent = static::$cache_tabs_items[$id]->id_parent;
+      if (isset(static::$cache_tabs_items[$id_parent]))
+                static::$cache_tabs_items[$id_parent]->child_delete($id);
+          unset(static::$cache_tabs_items[$id]);
+    }
   }
 
   static function build($items = null) {
--- system/module_storage/backend/Instances_manager.php
+++ system/module_storage/backend/Instances_manager.php
@@ -9,7 +9,7 @@ namespace effcore {
 
   static function instance_insert($page) {
     foreach (entity::all_get(false) as $c_entity) {
-      tabs::insert_item($c_entity->title, 'insert_'.$c_entity->name, 'insert', $c_entity->name);
+      tabs::subitem_insert($c_entity->title, 'insert_'.$c_entity->name, 'insert', $c_entity->name);
     }
   }
 
