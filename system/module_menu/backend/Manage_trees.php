11b3d28753f632478e2f0e0df13d34dbf258509e
Maxim Rysevets <root@effcore.com>
2018-09-21 17:25:00 +0300

    sprint_312_285

--- system/module_menu/backend/Manage_trees.php
+++ system/module_menu/backend/Manage_trees.php
@@ -9,9 +9,20 @@ namespace effcore {
 
   static function tree_select($page) {
     $trees = tree::all_get();
-    if (!$page->args_get('id')) url::go($page->args_get('base').'/select/'.reset($trees)->id);
+    $id = $page->args_get('id');
+    if (!$id) url::go($page->args_get('base').'/select/'.reset($trees)->id);
     foreach ($trees as $c_tree) {
       tabs::item_insert($c_tree->title, 'tree_select_'.$c_tree->id, 'tree_select', 'select/'.$c_tree->id);
+    }
+    if ($id) {
+      $tree = clone tree::get($id);
+      $tree->attribute_delete('class');
+      $tree->title_state = 'cutted';
+      foreach ($tree->children_select_recursive() as $c_item) {
+        $c_item->access = null;
+        $c_item->href = '';
+      }
+      return $tree;
     }
   }
 
