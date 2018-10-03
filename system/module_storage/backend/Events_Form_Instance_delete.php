bf74336dee56e91886406bdc236b6dd2a3cb154c
Maxim Rysevets <root@effcore.com>
2018-10-03 18:32:10 +0300

    sprint_400_23

---
+++ system/module_storage/backend/Events_Form_Instance_delete.php
@@ -0,0 +1,14 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2019 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\storage {
+          abstract class events_form_instance_delete {
+
+  static function on_submit($form, $items) {
+    // manage_instances::instance_delete_by_entity_name_and_instance_id()
+  }
+
+}}
