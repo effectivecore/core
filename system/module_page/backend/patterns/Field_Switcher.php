a365fbb3c063224b68b13f5427b325c0e20f6b9e
Maxim Rysevets <root@effcore.com>
2018-10-03 23:56:39 +0300

    sprint_400_25

---
+++ system/module_page/backend/patterns/Field_Switcher.php
@@ -0,0 +1,18 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2019 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore {
+          class field_switcher extends field_checkbox {
+
+  public $title = 'Checkbox';
+  public $title_position = 'bottom';
+  public $attributes = ['data-type' => 'switcher'];
+  public $element_attributes_default = [
+    'type' => 'checkbox',
+    'name' => 'checkbox'
+  ];
+
+}}
