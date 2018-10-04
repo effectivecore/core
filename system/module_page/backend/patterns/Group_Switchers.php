4ea2020d6c031b7143242c88b2a23dc9820f484e
Maxim Rysevets <root@effcore.com>
2018-10-04 14:50:57 +0300

    sprint_400_29

---
+++ system/module_page/backend/patterns/Group_Switchers.php
@@ -0,0 +1,12 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2019 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore {
+          class group_switchers extends group_checkboxes {
+
+  public $element_attributes = ['data-type' => 'switcher'];
+
+}}
