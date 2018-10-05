82e1d6aa926c97ff62da60fe82b338629ec5c2ec
Maxim Rysevets <root@effcore.com>
2018-10-05 14:45:47 +0300

    sprint_400_40

--- system/module_user/backend/Events_Form_Access.php
+++ system/module_user/backend/Events_Form_Access.php
@@ -13,17 +13,24 @@ namespace effcore\modules\user {
   static function on_init($form, $items) {
     $role_entity = entity::get('role');
     $role_instances = $role_entity->instances_select();
-    $action_list = new field_select();
-    $action_list->element_attributes = ['name' => 'permissions', 'required' => null];
-    $action_list->build();
-    $action_list->option_insert('- no -', 'not_selected');
-    $action_list->option_insert('select', 's');
-    $action_list->option_insert('select | update', 'su');
-    $action_list->option_insert('select | update | insert', 'sui');
-    $action_list->option_insert('select | update | insert | delete', 'suid');
+    $access = null;
+    switch ($form->__args['entity_name']) {
+      case 'page': $access = $form->__page->access; break;
+    }
     foreach ($role_instances as $c_role) {
+      $c_action_list = new field_select();
+      $c_action_list->element_attributes = ['name' => 'access_for_'.$c_role->id, 'required' => null];
+      $c_action_list->title = $c_role->title;
+      $c_action_list->build();
+      $c_action_list->option_insert('- no -', 'not_selected');
+      $c_action_list->option_insert('select', 's');
+      $c_action_list->option_insert('select | update', 'su');
+      $c_action_list->option_insert('select | update | insert', 'sui');
+      $c_action_list->option_insert('select | update | insert | delete', 'suid');
       $items['settings']->child_insert(
-        new markup('x-role', [], [$c_role->title, $action_list])
+        new markup('x-role', [], [
+          $c_action_list
+        ])
       );
     }
   }
