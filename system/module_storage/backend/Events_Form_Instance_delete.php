73dd8a51612972aad9195dcc6b56e6cbf05a05c7
Maxim Rysevets <root@effcore.com>
2018-10-08 00:17:45 +0300

    sprint_400_62

--- system/module_storage/backend/Events_Form_Instance_delete.php
+++ system/module_storage/backend/Events_Form_Instance_delete.php
@@ -7,11 +7,13 @@
 namespace effcore\modules\storage {
           use \effcore\manage_instances;
           use \effcore\message;
-          use \effcore\translation;
           use \effcore\page;
+          use \effcore\translation;
+          use \effcore\url;
           abstract class events_form_instance_delete {
 
   static function on_submit($form, $items) {
+    $base        = page::current_get()->args_get('base');
     $entity_name = page::current_get()->args_get('entity_name');
     $instance_id = page::current_get()->args_get('instance_id');
     switch ($form->clicked_button->value_get()) {
@@ -22,6 +24,10 @@ namespace effcore\modules\storage {
         if ($result) {
              message::insert(translation::get('Instance of entity "%%_entity_name" with id = "%%_instance_id" was deleted.',     ['entity_name' => $entity_name, 'instance_id' => $instance_id]));}
         else message::insert(translation::get('Instance of entity "%%_entity_name" with id = "%%_instance_id" was not deleted!', ['entity_name' => $entity_name, 'instance_id' => $instance_id]), 'error');
+        url::go(url::back_url_get() ?: $base.'/select/'.$entity_name);
+        break;
+      case 'cancel':
+        url::go(url::back_url_get() ?: $base.'/select/'.$entity_name);
         break;
     }
   }
