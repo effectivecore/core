85ce51524c537a2e1ee5c28b91a947325f928c87
Maxim Rysevets <root@effcore.com>
2018-10-09 01:39:18 +0300

    sprint_400_67_10

--- system/module_page/backend/patterns/Field_Nickname.php
+++ system/module_page/backend/patterns/Field_Nickname.php
@@ -9,12 +9,45 @@ namespace effcore {
 
   public $title = 'Nick';
   public $attributes = ['data-type' => 'nick'];
+  public $description = 'Field can contain only the next characters: "a-z", "0-9", "-", "_".';
   public $element_attributes_default = [
     'name'      => 'nick',
     'required'  => 'required',
-    'pattern'   => '%^[a-zA-Z0-9-_]{4,32}$%',
     'minlength' => 4,
     'maxlength' => 32
   ];
+
+  ###########################
+  ### static declarations ###
+  ###########################
+
+  static function validate($field, $form, $npath) {
+    $element = $field->child_select('element');
+    $name = $field->name_get();
+    $type = $field->type_get();
+    if ($name && $type) {
+      if ($field->disabled_get()) return true;
+      if ($field->readonly_get()) return true;
+      $new_value = static::request_value_get($name, static::cur_number_get($name), $form->source_get());
+      $new_value = strtolower($new_value);
+      $result = static::validate_required ($field, $form, $element, $new_value) &&
+                static::validate_minlength($field, $form, $element, $new_value) &&
+                static::validate_maxlength($field, $form, $element, $new_value) &&
+                static::validate_value    ($field, $form, $element, $new_value) &&
+                static::validate_pattern  ($field, $form, $element, $new_value);
+      $field->value_set($new_value);
+      return $result;
+    }
+  }
+
+  static function validate_value($field, $form, $element, &$new_value) {
+    if (!core::validate_nick($new_value)) {
+      $field->error_set(
+        translation::get('Field "%%_title" contains incorrect value!', ['title' => translation::get($field->title)])
+      );
+    } else {
+      return true;
+    }
+  }
 
 }}
