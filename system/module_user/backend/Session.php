5e63b74039ffdbbe7e1f2cc3793d5c52a2fc7655
Maxim Rysevets <root@effcore.com>
2018-10-11 15:57:53 +0300

    sprint_400_80_6

--- system/module_user/backend/Session.php
+++ system/module_user/backend/Session.php
@@ -7,8 +7,8 @@
 namespace effcore {
           abstract class session {
 
-  const period_expire_d = 60 * 60 * 24;
-  const period_expire_m = 60 * 60 * 24 * 30;
+  const period_expired_d = 60 * 60 * 24;
+  const period_expired_m = 60 * 60 * 24 * 30;
   const empty_ip = '::';
 
   static function select() {
@@ -30,14 +30,14 @@ namespace effcore {
   static function insert($nick, $session_params = []) {
     $is_remember = isset($session_params['is_remember']);
     $is_fixed_ip = isset($session_params['is_fixed_ip']);
-    $period = !$is_remember ? static::period_expire_d : static::period_expire_m;
+    $period = !$is_remember ? static::period_expired_d : static::period_expired_m;
     static::id_regenerate('f', $session_params);
     (new instance('session', [
       'id'          => static::id_get(),
       'nick'        => $nick,
       'is_remember' => $is_remember ? 1 : 0,
       'is_fixed_ip' => $is_fixed_ip ? 1 : 0,
-      'expire'      => core::datetime_get('+'.$period.' second'),
+      'expired'     => core::datetime_get('+'.$period.' second'),
     ]))->insert();
   }
 
@@ -70,15 +70,15 @@ namespace effcore {
   static function id_regenerate($hex_type, $session_params = []) {
     $is_remember = isset($session_params['is_remember']);
     $is_fixed_ip = isset($session_params['is_fixed_ip']);
-    $period      = $hex_type == 'f' && !$is_remember ? static::period_expire_d : static::period_expire_m;
+    $period      = $hex_type == 'f' && !$is_remember ? static::period_expired_d : static::period_expired_m;
     $ip          = $hex_type == 'f' && !$is_fixed_ip ? static::empty_ip : core::server_remote_addr_get();
   # $hex_type: a - anonymous user | f - authenticated user
-    $hex_expire        = static::id_hex_expire_get($period);
+    $hex_expired       = static::id_hex_expired_get($period);
     $hex_ip            = static::id_hex_ip_get($ip);
     $hex_uagent_hash_8 = static::id_hex_uagent_hash_8_get();
     $hex_random        = static::id_hex_random_get();
     $session_id = $hex_type.          # strlen == 1
-                  $hex_expire.        # strlen == 8
+                  $hex_expired.       # strlen == 8
                   $hex_ip.            # strlen == 32
                   $hex_uagent_hash_8. # strlen == 8
                   $hex_random;        # strlen == 8
@@ -94,13 +94,13 @@ namespace effcore {
       else return static::id_regenerate('a');
   }
 
-  static function id_hex_expire_get($period) {return dechex(time() + $period);}
-  static function id_hex_ip_get($ip)         {return core::ip_to_hex($ip);}
-  static function id_hex_uagent_hash_8_get() {return substr(md5(core::server_user_agent_get()), 0, 8);}
-  static function id_hex_random_get()        {return str_pad(dechex(random_int(0, 0x7fffffff)), 8, '0', STR_PAD_LEFT);}
-  static function id_hex_signature_get($id)  {return core::signature_get(substr($id, 0, 56 + 1), 8, 'session');}
+  static function id_hex_expired_get($period) {return dechex(time() + $period);}
+  static function id_hex_ip_get($ip)          {return core::ip_to_hex($ip);}
+  static function id_hex_uagent_hash_8_get()  {return substr(md5(core::server_user_agent_get()), 0, 8);}
+  static function id_hex_random_get()         {return str_pad(dechex(random_int(0, 0x7fffffff)), 8, '0', STR_PAD_LEFT);}
+  static function id_hex_signature_get($id)   {return core::signature_get(substr($id, 0, 56 + 1), 8, 'session');}
 
-  static function id_expire_extract($id)            {return hexdec(substr($id, 1, 8));}
+  static function id_expired_extract($id)           {return hexdec(substr($id, 1, 8));}
   static function id_hex_type_extract($id)          {return substr($id,      0,  1);}
   static function id_hex_ip_extract($id)            {return substr($id,  8 + 1, 32);}
   static function id_hex_uagent_hash_8_extract($id) {return substr($id, 40 + 1,  8);}
@@ -109,12 +109,12 @@ namespace effcore {
 
   static function id_check($id) {
     if (core::validate_hash($id, 65)) {
-      $expire            = static::id_expire_extract($id);
+      $expired           = static::id_expired_extract($id);
       $hex_type          = static::id_hex_type_extract($id);
       $hex_ip            = static::id_hex_ip_extract($id);
       $hex_uagent_hash_8 = static::id_hex_uagent_hash_8_extract($id);
       $hex_signature     = static::id_hex_signature_extract($id);
-      if ($expire >= time()                                         &&
+      if ($expired >= time()                                         &&
           $hex_uagent_hash_8 === static::id_hex_uagent_hash_8_get() &&
           $hex_signature     === static::id_hex_signature_get($id)) {
         if (($hex_type === 'a' && $hex_ip === core::ip_to_hex(core::server_remote_addr_get())) ||
