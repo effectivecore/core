954d6193055f40b3bd9413ef40afc9ec5dcc6d4f
Maxim Rysevets <root@effcore.com>
2018-10-19 18:28:07 +0300

    sprint_400_88

--- system/boot.php
+++ system/boot.php
@@ -79,7 +79,7 @@ namespace effcore {
         $data = token::replace($file->load());
         $etag = md5($data);
 
-      # send header '304 Not Modified' to the output buffer if HTTP_IF_NONE_MATCH header is received
+      # send header '304 Not Modified' if the data has not changed
         if (isset($_SERVER['HTTP_IF_NONE_MATCH']) &&
                   $_SERVER['HTTP_IF_NONE_MATCH'] === $etag) {
           header('HTTP/1.1 304 Not Modified');
@@ -106,6 +106,16 @@ namespace effcore {
     # ─────────────────────────────────────────────────────────────────────
       } else {
 
+        $last_modified = gmdate('D, d M Y H:i:s', filemtime($path)).' GMT';
+
+      # send header '304 Not Modified' if the data has not changed
+        if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) &&
+                  $_SERVER['HTTP_IF_MODIFIED_SINCE'] === $last_modified) {
+          header('HTTP/1.1 304 Not Modified');
+          console::log_store();
+          exit();
+        }
+
         # http ranges limits:
         # ─────────────────────────────────────────────────────────────────────
         #
@@ -123,41 +133,36 @@ namespace effcore {
         #
         # ─────────────────────────────────────────────────────────────────────
 
-        $file = new file($path);
-        $etag = md5_file($path);
         $length = filesize($path);
         $ranges = core::server_http_range_get();
         $min = $ranges->min !== null ? $ranges->min : 0;
         $max = $ranges->max !== null ? $ranges->max : $length - 1;
         if ($max >= $length) $max = $length - 1;
-        if (!($min >= 0 && $min <= $max && $min < $length)) {header('HTTP/1.1 416 Requested Range Not Satisfiable'); exit();}
-        if (!($max >= 0 && $max >= $min && $max < $length)) {header('HTTP/1.1 416 Requested Range Not Satisfiable'); exit();}
-        if (!($ranges->min === null && $ranges->max === null)) {
-          header('HTTP/1.1 206 Partial Content');
-          header('Content-Range: bytes '.$min.'-'.$max.'/'.$length);
-        }
+        if (!($min >= 0 && $min <= $max && $min < $length))   {header('HTTP/1.1 416 Requested Range Not Satisfiable'); exit();}
+        if (!($max >= 0 && $max >= $min && $max < $length))   {header('HTTP/1.1 416 Requested Range Not Satisfiable'); exit();}
+        if (!($ranges->min === null && $ranges->max === null)) header('HTTP/1.1 206 Partial Content');
+        if (!($ranges->min === null && $ranges->max === null)) header('Content-Range: bytes '.$min.'-'.$max.'/'.$length);
         header('Content-Length: '.($max - $min + 1));
         header('Accept-Ranges: bytes');
-        header('Cache-Control: private');
-        header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($path)).' GMT');
-        header('Etag: '.$etag);
+        header('Cache-Control: private, no-cache');
+        header('Last-Modified: '.$last_modified);
         if (!empty($file_types[$type]->headers)) {
           foreach ($file_types[$type]->headers as $c_key => $c_value) {
             header($c_key.': '.$c_value);
           }
         }
-        if ($file = fopen($path, 'rb')) {
+        if ($resource = fopen($path, 'rb')) {
           $c_print_length = $min;
-          if (fseek($file, $min) == 0) {
-            while (!feof($file)) {
-              $c_data = fread($file, 1024);
+          if (fseek($resource, $min) == 0) {
+            while (!feof($resource)) {
+              $c_data = fread($resource, 1024);
               for ($i = 0; $i < strlen($c_data); $i++, $c_print_length++) {
                 if ($c_print_length > $max) break 2;
                 print $c_data[$i];
               }
             }
           }
-          fclose($file);
+          fclose($resource);
         }
         console::log_store();
         exit();
