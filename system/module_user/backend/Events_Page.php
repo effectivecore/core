589be8ea5e6dedd8c54b12dc248b3eb1ecc06f57
Maxim Rysevets <root@effcore.com>
2018-10-21 14:02:21 +0300

    sprint_400_105_3

--- system/module_user/backend/Events_Page.php
+++ system/module_user/backend/Events_Page.php
@@ -6,15 +6,10 @@
 
 namespace effcore\modules\user {
           use \effcore\block;
-          use \effcore\core;
-          use \effcore\entity;
-          use \effcore\instance;
-          use \effcore\locale;
           use \effcore\markup_simple;
           use \effcore\markup;
           use \effcore\module;
           use \effcore\storage;
-          use \effcore\table;
           use \effcore\user;
           abstract class events_page {
 
@@ -38,36 +33,6 @@ namespace effcore\modules\user {
     }
     $block_menu->content_tag_name = null;
     return $block_menu;
-  }
-
-  static function on_show_block_user_info($page) {
-    $user = (new instance('user', [
-      'nick' => $page->args_get('nick')
-    ]))->select();
-    if ($user) {
-      if ($user->nick == user::current_get()->nick ||             # owner
-                   isset(user::current_get()->roles['admins'])) { # admin
-      # get values
-        $user_roles = user::id_roles_get($user->nick);
-        $values = $user->values_get();
-        $values['roles'] = $user_roles ? implode(', ', $user_roles) : '-';
-        $values['created'] = locale::format_datetime($values['created']);
-        $values['updated'] = locale::format_datetime($values['updated']);
-        $values['password_hash'] = '*****';
-        $values['is_embed'] = $values['is_embed'] ? 'Yes' : 'No';
-        $values['avatar_path_relative'] = $values['avatar_path_relative'] ?: '-';
-      # show table
-        $thead = [['Parameter', 'Value']];
-        $tbody = core::array_rotate([array_keys($values), array_values($values)]);
-        return new block('', ['class' => ['user-info' => 'user-info']],
-          new table([], $tbody, $thead)
-        );
-      } else {
-        core::send_header_and_exit('access_denided');
-      }
-    } else {
-      core::send_header_and_exit('page_not_found');
-    }
   }
 
 }}
---
+++ system/module_user/backend/Events_Page_User.php
@@ -0,0 +1,42 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2019 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\user {
+          use \effcore\block;
+          use \effcore\core;
+          use \effcore\instance;
+          use \effcore\locale;
+          use \effcore\table;
+          use \effcore\user;
+          abstract class events_page_user {
+
+  static function on_show_block_user_info($page) {
+    $user = (new instance('user', [
+      'nick' => $page->args_get('nick')
+    ]))->select();
+    if ($user) {
+      if ($user->nick == user::current_get()->nick ||             # owner
+                   isset(user::current_get()->roles['admins'])) { # admin
+      # get values
+        $user_roles = user::id_roles_get($user->nick);
+        $values = $user->values_get();
+        $values['roles'] = $user_roles ? implode(', ', $user_roles) : '-';
+        $values['created'] = locale::format_datetime($values['created']);
+        $values['updated'] = locale::format_datetime($values['updated']);
+        $values['password_hash'] = '*****';
+        $values['is_embed'] = $values['is_embed'] ? 'Yes' : 'No';
+        $values['avatar_path_relative'] = $values['avatar_path_relative'] ?: '-';
+      # show table
+        $thead = [['Parameter', 'Value']];
+        $tbody = core::array_rotate([array_keys($values), array_values($values)]);
+        return new block('', ['class' => ['user-info' => 'user-info']],
+          new table([], $tbody, $thead)
+        );
+      } else core::send_header_and_exit('access_denided');
+    }   else core::send_header_and_exit('page_not_found');
+  }
+
+}}
