8e58e583ee873fe0188877b7b022e3fcd3f09c39
Maxim Rysevets <root@effcore.com>
2018-10-22 16:37:39 +0300

    sprint_400_105_18

---
+++ system/module_locale/backend/Events_Form_Locales.php
@@ -0,0 +1,16 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2019 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\locales {
+          abstract class events_form_locales {
+
+  static function on_init($form, $items) {
+  }
+
+  static function on_submit($form, $items) {
+  }
+
+}}
