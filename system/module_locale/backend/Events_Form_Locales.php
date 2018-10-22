29e2b583a8101a7d8a9f2c4bb0a3e5e3a26fe0d7
Maxim Rysevets <root@effcore.com>
2018-10-22 17:04:21 +0300

    sprint_400_105_19

--- system/module_locale/backend/Events_Form_Locales.php
+++ system/module_locale/backend/Events_Form_Locales.php
@@ -5,12 +5,31 @@
   ##################################################################
 
 namespace effcore\modules\locales {
+          use \effcore\language;
+          use \effcore\storage;
           abstract class events_form_locales {
 
   static function on_init($form, $items) {
+    $languages = language::get_all();
+    foreach ($languages as $c_language) {
+      $title = $c_language->code == 'en' ?
+        $c_language->title->en :
+        $c_language->title->en.' ('.$c_language->title->native.')';
+      $items['#language']->option_insert($title, $c_language->code);
+    }
+    $items['#language']->value_set(
+      language::current_code_get()
+    );
   }
 
   static function on_submit($form, $items) {
+    switch ($form->clicked_button->value_get()) {
+      case 'save':
+        $code_new = $items['#language']->value_get();
+        language::current_code_set($code_new);
+        storage::get('files')->changes_insert('locales', 'update', 'settings/locales/lang_code', $code_new);
+        break;
+    }
   }
 
 }}
