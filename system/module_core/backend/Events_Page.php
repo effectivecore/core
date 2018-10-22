31ea8d4392afe9288b38eb42a03de0509996af61
Maxim Rysevets <root@effcore.com>
2018-10-22 09:37:03 +0300

    sprint_400_105_13

---
+++ system/module_core/backend/Events_Page.php
@@ -0,0 +1,18 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2019 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\core {
+          use \effcore\language;
+          abstract class events_page {
+
+  static function on_switch_language($page) {
+    $languages = language::get_all();
+    $code = $page->args_get('code');
+    if (!isset($languages[$code])) url::go($page->args_get('base').'/en');
+    language::current_code_set($code);
+  }
+
+}}
---
+++ system/module_locale/backend/Events_Page.php
@@ -0,0 +1,26 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2019 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\locales {
+          use \effcore\language;
+          use \effcore\markup;
+          abstract class events_page {
+
+  static function on_show_block_menu_languages($page) {
+    $menu = new markup('x-languages');
+    foreach (language::get_all() as $c_language) {
+      $title = $c_language->code == 'en' ?
+        $c_language->title->en :
+        $c_language->title->en.' ('.$c_language->title->native.')';
+      $href = $page->args_get('base').'/'.$c_language->code;
+      $link = new markup('a', ['href' => $href], $title);
+      $link_wrapper = new markup('x-language', ['class' => [$c_language->code => $c_language->code]], $link);
+      $menu->child_insert($link_wrapper, $c_language->code);
+    }
+    return $menu;
+  }
+
+}}
