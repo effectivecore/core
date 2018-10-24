b0109cab971cf3e3225b9f1cf88187f8e77b21c0
Maxim Rysevets <root@effcore.com>
2018-10-24 17:45:00 +0300

    sprint_400_105_31

--- system/module_core/backend/Events_Form_Modules.php
+++ system/module_core/backend/Events_Form_Modules.php
@@ -23,11 +23,11 @@ namespace effcore\modules\core {
       $c_switcher->checked_set($c_module->state != 'off');
       $c_switcher->disabled_set($c_module->state == 'always_on');
       $c_info->child_insert($c_switcher, 'switcher');
-      $c_info->child_insert(new markup('x-module-id',          [], [new markup('x-label', [], 'ID'),          ': ', new markup('x-value', [], $c_module->id.' ')]),                          'id');
-      $c_info->child_insert(new markup('x-module-title',       [], [new markup('x-label', [], 'Title'),       ': ', new markup('x-value', [], $c_module->title)]),                           'title');
-      $c_info->child_insert(new markup('x-module-version',     [], [new markup('x-label', [], 'Version'),     ': ', new markup('x-value', [], locale::format_version($c_module->version))]), 'version');
-      $c_info->child_insert(new markup('x-module-description', [], [new markup('x-label', [], 'Description'), ': ', new markup('x-value', [], $c_module->description)]),                     'description');
-      $c_info->child_insert(new markup('x-module-path',        [], [new markup('x-label', [], 'Path'),        ': ', new markup('x-value', [], $c_module->path)]),                            'path');
+      $c_info->child_insert(new markup('x-module-title',       [], [new markup('x-value', [], $c_module->title)]),                                                                           'title');
+      $c_info->child_insert(new markup('x-module-id',          [], [new markup('x-label', [], 'id'),          ': ', new markup('x-value', [], $c_module->id.' ')]),                          'id');
+      $c_info->child_insert(new markup('x-module-version',     [], [new markup('x-label', [], 'version'),     ': ', new markup('x-value', [], locale::format_version($c_module->version))]), 'version');
+      $c_info->child_insert(new markup('x-module-description', [], [new markup('x-label', [], 'description'), ': ', new markup('x-value', [], $c_module->description)]),                     'description');
+      $c_info->child_insert(new markup('x-module-path',        [], [new markup('x-label', [], 'path'),        ': ', new markup('x-value', [], $c_module->path)]),                            'path');
       $info->child_insert($c_info, 'module_'.$c_module->id);
     }
   }
