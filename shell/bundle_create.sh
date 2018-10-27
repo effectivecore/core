093e1de53935805be21c03bafc16a76afaa2ef27
Maxim Rysevets <root@effcore.com>
2018-10-27 15:58:53 +0300

    sprint_400_125

--- shell/bundle_create.sh
+++ shell/bundle_create.sh
@@ -3,16 +3,19 @@
 git -C ../ reset --hard
 git -C ../ pull
 
-build=$(grep 'build: [0-9]\{4,5\}' ../system/bundle.data | sed 's/  build: //g')
-bundle_name="../../effcore-$build.zip"
+cd ../
+build=$(grep 'build: [0-9]\{4,5\}' system/bundle.data | sed 's/  build: //g')
+bundle_name="../effcore-$build.zip"
 
-{ find .. -maxdepth 1 -type f -not -name '.*'             & \
-  find .. -maxdepth 1 -type f      -name '.editorconfig'  & \
-  find .. -maxdepth 1 -type f      -name '.gitattributes' & \
-  find .. -maxdepth 1 -type f      -name '.gitignore'     & \
-  find .. -maxdepth 1 -type f      -name '.htaccess'      & \
-  find .. -maxdepth 1 -type f      -name '.nginx'         & \
-  find ../dynamic     -type f      -name 'readme.md'      & \
-  find ../shell       -type f -not -name '.*'             & \
-  find ../system      -type f -not -name '.*'; \
+rm $bundle_name
+
+{ find . -maxdepth 1 -type f -not -name '.*'             & \
+  find . -maxdepth 1 -type f      -name '.editorconfig'  & \
+  find . -maxdepth 1 -type f      -name '.gitattributes' & \
+  find . -maxdepth 1 -type f      -name '.gitignore'     & \
+  find . -maxdepth 1 -type f      -name '.htaccess'      & \
+  find . -maxdepth 1 -type f      -name '.nginx'         & \
+  find dynamic       -type f      -name 'readme.md'      & \
+  find shell         -type f -not -name '.*'             & \
+  find system        -type f -not -name '.*';              \
 } | zip -9 $bundle_name -@
