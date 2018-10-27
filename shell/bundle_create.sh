d1c2bcb55a07577ac4db441af8d27d039874322e
Maxim Rysevets <root@effcore.com>
2018-10-27 11:18:31 +0300

    sprint_400_120

--- shell/bundle_create.sh
+++ shell/bundle_create.sh
@@ -1,14 +1,16 @@
+#!/usr/bin/env bash
+
 git -C ../ reset --hard
 git -C ../ pull
 ./cache_clear.sh
 
-{ find ../dynamic     -type f      -name 'readme.md'      & \
-  find ../shell       -type f -not -name '.*'             & \
-  find ../system      -type f -not -name '.*'             & \
-  find .. -maxdepth 1 -type f -not -name '.*'             & \
+{ find .. -maxdepth 1 -type f -not -name '.*'             & \
   find .. -maxdepth 1 -type f      -name '.editorconfig'  & \
   find .. -maxdepth 1 -type f      -name '.gitattributes' & \
   find .. -maxdepth 1 -type f      -name '.gitignore'     & \
   find .. -maxdepth 1 -type f      -name '.htaccess'      & \
-  find .. -maxdepth 1 -type f      -name '.nginx'; \
+  find .. -maxdepth 1 -type f      -name '.nginx'         & \
+  find ../dynamic     -type f      -name 'readme.md'      & \
+  find ../shell       -type f -not -name '.*'             & \
+  find ../system      -type f -not -name '.*'; \
 } | zip ../../bundle-0000.zip -@
