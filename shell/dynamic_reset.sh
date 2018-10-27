27b17ea5bdbf3d8af2bc341a9febc8c55e8aab59
Maxim Rysevets <root@effcore.com>
2018-10-27 14:55:40 +0300

    sprint_400_123

--- shell/dynamic_reset.sh
+++ shell/dynamic_reset.sh
@@ -1,3 +1,17 @@
-find ../dynamic/data/ -not -name 'readme.md' -delete
-cp /dev/null ../dynamic/data/data.sqlite
-./cache_clear.sh
+#!/usr/bin/env bash
+
+while true; do
+  read -p 'All data will be lost. Are you sure?: ' answer
+  case $answer in
+      y ) find ../dynamic/cache/   -not -name 'readme.md' -delete
+          find ../dynamic/data/    -not -name 'readme.md' -delete
+          find ../dynamic/files/   -not -name 'readme.md' -delete
+          find ../dynamic/logs/    -not -name 'readme.md' -delete
+          find ../dynamic/modules/ -not -name 'readme.md' -delete
+          find ../dynamic/tmp/     -not -name 'readme.md' -delete
+          cp /dev/null ../dynamic/data/data.sqlite
+          break;;
+      n ) exit;;
+      * ) echo 'Please answer "y" or "n".';;
+  esac
+done
