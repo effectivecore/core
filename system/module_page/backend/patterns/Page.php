89dcdc0317ea52ff718d95d10ef5e8e47d28d6a6
Maxim Rysevets <root@effcore.com>
2018-10-28 18:47:34 +0300

    sprint_400_140

--- system/module_page/backend/patterns/Page.php
+++ system/module_page/backend/patterns/Page.php
@@ -78,8 +78,10 @@ namespace effcore {
     $template->arg_set('head_styles',  $frontend->styles->render());
     $template->arg_set('head_scripts', $frontend->scripts->render());
     $template->arg_set('head_title', token::replace(translation::get($this->title)));
-    $template->arg_set('console', console::render()); # @todo: only for admins
     $template->arg_set('messages', message::render_all());
+    if (storage::get('files')->select('settings')['page']->console_display == 'yes') {
+      $template->arg_set('console', console::render());
+    }
     return $template->render();
   }
 
