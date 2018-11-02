451e407b6add54e437d12a7602a1cb554bb2cbfb
Maxim Rysevets <root@effcore.com>
2018-11-02 14:53:51 +0300

    sprint_400_146_14

--- system/module_core/backend/Events_Page_Install.php
+++ system/module_core/backend/Events_Page_Install.php
@@ -7,9 +7,9 @@
 namespace effcore\modules\core {
           use \effcore\language;
           use \effcore\url;
-          abstract class events_page {
+          abstract class events_page_install {
 
-  static function on_switch_language($page) {
+  static function on_page_init($page) {
     $languages = language::get_all();
     $code = $page->args_get('lang_code');
     if (!isset($languages[$code])) url::go($page->args_get('base').'/en');
