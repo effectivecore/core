7f8d30642861616b83bedc0ceef7b3b8bb583c5d
Maxim Rysevets <root@effcore.com>
2018-11-12 23:40:10 +0300

    sprint_400_146_77

--- system/module_core/backend/Core.php
+++ system/module_core/backend/Core.php
@@ -29,8 +29,8 @@ namespace effcore {
       return $cache;
     } else {
       $result = [];
-      $php_files = file::select_recursive(dir_system, '%^.*\\.php$%') +
-                   file::select_recursive(dir_modules, '%^.*\\.php$%');
+      $php_files = file::select_recursive(dir_system,  '%^.*\\.php$%') +
+                   file::select_recursive(dir_modules, '%^.*/modules/.*/.*\\.php$%');
       foreach ($php_files as $c_file) {
         $c_matches = [];
         preg_match_all('%(?:namespace (?<namespace>[a-z0-9_\\\\]+)\\s*[{;]\\s*(?<dependencies>.*?|)|)\\s*'.
