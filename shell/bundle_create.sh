d83f8af3d5e2a85314c22a15e7e92077140f55f8
Maxim Rysevets <root@effcore.com>
2018-11-12 13:28:42 +0300

    sprint_400_146_71

--- shell/bundle_create.sh
+++ shell/bundle_create.sh
@@ -10,18 +10,18 @@ bundle_name="../effcore-$build.zip"
 
 rm $bundle_name
 
-{ find .               -maxdepth 1 -type f -not -name '.*'             & \
-  find .               -maxdepth 1 -type f      -name '.editorconfig'  & \
-  find .               -maxdepth 1 -type f      -name '.gitattributes' & \
-  find .               -maxdepth 1 -type f      -name '.gitignore'     & \
-  find .               -maxdepth 1 -type f      -name '.htaccess'      & \
-  find .               -maxdepth 1 -type f      -name '.nginx'         & \
-  find dynamic/cache   -maxdepth 1 -type f      -name 'readme.md'      & \
-  find dynamic/data    -maxdepth 1 -type f      -name 'readme.md'      & \
-  find dynamic/files   -maxdepth 1 -type f      -name 'readme.md'      & \
-  find dynamic/logs    -maxdepth 1 -type f      -name 'readme.md'      & \
-  find dynamic/modules -maxdepth 1 -type f      -name 'readme.md'      & \
-  find dynamic/tmp     -maxdepth 1 -type f      -name 'readme.md'      & \
-  find shell                       -type f -not -name '.*'             & \
-  find system                      -type f -not -name '.*';              \
+{ find .             -maxdepth 1 -type f -not -name '.*'             & \
+  find .             -maxdepth 1 -type f      -name '.editorconfig'  & \
+  find .             -maxdepth 1 -type f      -name '.gitattributes' & \
+  find .             -maxdepth 1 -type f      -name '.gitignore'     & \
+  find .             -maxdepth 1 -type f      -name '.htaccess'      & \
+  find .             -maxdepth 1 -type f      -name '.nginx'         & \
+  find dynamic/cache -maxdepth 1 -type f      -name 'readme.md'      & \
+  find dynamic/data  -maxdepth 1 -type f      -name 'readme.md'      & \
+  find dynamic/files -maxdepth 1 -type f      -name 'readme.md'      & \
+  find dynamic/logs  -maxdepth 1 -type f      -name 'readme.md'      & \
+  find dynamic/tmp   -maxdepth 1 -type f      -name 'readme.md'      & \
+  find modules       -maxdepth 1 -type f      -name 'readme.md'      & \
+  find shell                     -type f -not -name '.*'             & \
+  find system                    -type f -not -name '.*';              \
 } | zip -9 $bundle_name -@
