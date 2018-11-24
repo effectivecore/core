c18e9f589ebd858ede1c235892a6a9ce677fc295
Maxim Rysevets <root@effcore.com>
2018-11-24 14:36:55 +0300

    sprint_400_146_117

--- system/module_locale/backend/Events_Form_Locales.php
+++ system/module_locale/backend/Events_Form_Locales.php
@@ -7,34 +7,48 @@
 namespace effcore\modules\locales {
           use \effcore\field_timezone;
           use \effcore\language;
+          use \effcore\locale;
           use \effcore\message;
           use \effcore\storage;
           use \effcore\url;
           abstract class events_form_locales {
 
   static function on_init($form, $items) {
-    $items['#timezone_server']->value_set(field_timezone::id_by_title_get(date_default_timezone_get()));
-    $languages = language::get_all();
-    foreach ($languages as $c_language) {
+    $settings = locale::settings_get();
+    foreach (language::get_all() as $c_language) {
       $title = $c_language->code == 'en' ?
                $c_language->title->en :
                $c_language->title->en.' ('.$c_language->title->native.')';
-      $items['#language']->option_insert($title, $c_language->code);
+      $items['#lang_code']->option_insert($title, $c_language->code);
     }
-    $items['#language']->value_set(
-      language::current_code_get()
-    );
+    $items['#lang_code'          ]->value_set($settings->lang_code);
+    $items['#format_date'        ]->value_set($settings->format_date);
+    $items['#format_time'        ]->value_set($settings->format_time);
+    $items['#format_datetime'    ]->value_set($settings->format_datetime);
+    $items['#decimal_point'      ]->value_set($settings->decimal_point);
+    $items['#thousands_separator']->value_set($settings->thousands_separator);
+    $items['#timezone_server'    ]->value_set(field_timezone::id_by_title_get(date_default_timezone_get()));
   }
 
   static function on_submit($form, $items) {
     switch ($form->clicked_button->value_get()) {
       case 'save':
-        storage::get('files')->changes_insert('locales', 'update', 'settings/locales/lang_code', $items['#language']->value_get());
+        storage::get('files')->changes_insert('locales', 'update', 'settings/locales/lang_code',           $items['#lang_code'          ]->value_get(), false);
+        storage::get('files')->changes_insert('locales', 'update', 'settings/locales/format_date',         $items['#format_date'        ]->value_get(), false);
+        storage::get('files')->changes_insert('locales', 'update', 'settings/locales/format_time',         $items['#format_time'        ]->value_get(), false);
+        storage::get('files')->changes_insert('locales', 'update', 'settings/locales/format_datetime',     $items['#format_datetime'    ]->value_get(), false);
+        storage::get('files')->changes_insert('locales', 'update', 'settings/locales/decimal_point',       $items['#decimal_point'      ]->value_get(), false);
+        storage::get('files')->changes_insert('locales', 'update', 'settings/locales/thousands_separator', $items['#thousands_separator']->value_get());
         message::insert('The changes was saved.');
         url::go('/manage/locales');
         break;
       case 'restore':
-        storage::get('files')->changes_delete('locales', 'update', 'settings/locales/lang_code');
+        storage::get('files')->changes_delete('locales', 'update', 'settings/locales/lang_code',       false);
+        storage::get('files')->changes_delete('locales', 'update', 'settings/locales/format_date',     false);
+        storage::get('files')->changes_delete('locales', 'update', 'settings/locales/format_time',     false);
+        storage::get('files')->changes_delete('locales', 'update', 'settings/locales/format_datetime', false);
+        storage::get('files')->changes_delete('locales', 'update', 'settings/locales/decimal_point',   false);
+        storage::get('files')->changes_delete('locales', 'update', 'settings/locales/thousands_separator');
         message::insert('The changes was deleted.');
         url::go('/manage/locales');
         break;
