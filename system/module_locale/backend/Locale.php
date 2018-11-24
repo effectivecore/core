1d37b285bbb0fe15cf4064e3847645d8de2f748e
Maxim Rysevets <root@effcore.com>
2018-11-24 15:58:47 +0300

    sprint_400_146_118

--- system/module_locale/backend/Locale.php
+++ system/module_locale/backend/Locale.php
@@ -26,10 +26,10 @@ namespace effcore {
   ### formats ###
   ###############
 
-  static function format_time($time)                      {return \DateTime::createFromFormat('H:i:s',       $time,     new \DateTimeZone('UTC'))->setTimezone(new \DateTimeZone( static::settings_get()->timezone ))->format( static::settings_get()->format_time );}
-  static function format_date($date)                      {return \DateTime::createFromFormat('Y-m-d',       $date,     new \DateTimeZone('UTC'))->setTimezone(new \DateTimeZone( static::settings_get()->timezone ))->format( static::settings_get()->format_date );}
-  static function format_datetime($datetime)              {return \DateTime::createFromFormat('Y-m-d H:i:s', $datetime, new \DateTimeZone('UTC'))->setTimezone(new \DateTimeZone( static::settings_get()->timezone ))->format( static::settings_get()->format_datetime );}
-  static function format_timestamp($timestamp)            {return \DateTime::createFromFormat('U',           $timestamp                         )->setTimezone(new \DateTimeZone( static::settings_get()->timezone ))->format( static::settings_get()->format_datetime );}
+  static function format_time($time)                      {return \DateTime::createFromFormat('H:i:s',       $time,     new \DateTimeZone('UTC'))->setTimezone(new \DateTimeZone( date_default_timezone_get() ))->format( static::settings_get()->format_time );}
+  static function format_date($date)                      {return \DateTime::createFromFormat('Y-m-d',       $date,     new \DateTimeZone('UTC'))->setTimezone(new \DateTimeZone( date_default_timezone_get() ))->format( static::settings_get()->format_date );}
+  static function format_datetime($datetime)              {return \DateTime::createFromFormat('Y-m-d H:i:s', $datetime, new \DateTimeZone('UTC'))->setTimezone(new \DateTimeZone( date_default_timezone_get() ))->format( static::settings_get()->format_datetime );}
+  static function format_timestamp($timestamp)            {return \DateTime::createFromFormat('U',           $timestamp                         )->setTimezone(new \DateTimeZone( date_default_timezone_get() ))->format( static::settings_get()->format_datetime );}
   static function format_persent($number, $precision = 2) {return static::format_number(floatval($number), $precision).'%';}
   static function format_msecond($number, $precision = 6) {return static::format_number(floatval($number), $precision);}
   static function format_version($number)                 {return static::format_number(floatval($number), 3, null, null, false);}
