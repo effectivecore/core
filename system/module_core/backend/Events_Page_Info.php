abb35458d19e7db25ff31492f1fe512a9751a389
Maxim Rysevets <root@effcore.com>
2018-11-24 15:58:47 +0300

    sprint_400_146_118

--- system/module_core/backend/Events_Page_Info.php
+++ system/module_core/backend/Events_Page_Info.php
@@ -53,6 +53,10 @@ namespace effcore\modules\core {
     $info->child_insert(new markup('dd', [], php_uname('n')));
     $info->child_insert(new markup('dt', [], 'Session expiration date'));
     $info->child_insert(new markup('dd', [], locale::format_timestamp(session::id_expired_extract(session::id_get()))));
+    $info->child_insert(new markup('dt', [], 'Server timezone'));
+    $info->child_insert(new markup('dd', [], date_default_timezone_get()));
+    $info->child_insert(new markup('dt', [], 'Server date-time'));
+    $info->child_insert(new markup('dd', [], locale::format_datetime(core::datetime_get())));
     $info->child_insert(new markup('dt', [], 'Cron URL'));
     $info->child_insert(new markup('dd', [], $cron_link));
     $info->child_insert(new markup('dt', [], 'Provisioning key'));
