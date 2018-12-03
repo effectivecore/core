dd9a397e018c2f34fea8dee86c07e34157f9a8e7
Maxim Rysevets <root@effcore.com>
2018-12-03 17:51:03 +0300

    sprint_400_146_187

--- system/module_page/backend/patterns/Field_DateTime_local.php
+++ system/module_page/backend/patterns/Field_DateTime_local.php
@@ -5,7 +5,7 @@
   ##################################################################
 
 namespace effcore {
-          class field_datetime_local extends field_text {
+          class field_datetime_local extends field_datetime {
 
   const input_min_datetime = '0000-01-01 12:00:00';
   const input_max_datetime = '9999-12-31 09:00:00';
@@ -22,8 +22,8 @@ namespace effcore {
   ];
 
   function build() {
-    parent::build();
-    $value = parent::value_get();
+    field_text::build();
+    $value = field_text::value_get();
     $element = $this->child_select('element');
     $min = $element->attribute_select('min');
     $max = $element->attribute_select('max');
@@ -34,52 +34,10 @@ namespace effcore {
   }
 
   function value_get() {
-    $value = parent::value_get();
+    $value = field_text::value_get();
     if ($this->is_return_utc != true && core::validate_T_datetime($value)) return                             core::T_datetime_to_datetime($value);
     if ($this->is_return_utc == true && core::validate_T_datetime($value)) return locale::datetime_loc_to_utc(core::T_datetime_to_datetime($value));
     return $value;
-  }
-
-  function value_set($value) {
-    if (core::validate_T_datetime($value))
-         parent::value_set(core::sanitize_T_datetime($value));
-    else parent::value_set($value);
-  }
-
-  ###########################
-  ### static declarations ###
-  ###########################
-
-  static function validate($field, $form, $npath) {
-    $element = $field->child_select('element');
-    $name = $field->name_get();
-    $type = $field->type_get();
-    if ($name && $type) {
-      if ($field->disabled_get()) return true;
-      if ($field->readonly_get()) return true;
-      $new_value = static::request_value_get($name, static::cur_number_get($name), $form->source_get());
-      $new_value = strlen($new_value) == 16 ? $new_value.':00' : $new_value;
-      $result = static::validate_required ($field, $form, $element, $new_value) &&
-                static::validate_minlength($field, $form, $element, $new_value) &&
-                static::validate_maxlength($field, $form, $element, $new_value) &&
-                static::validate_value    ($field, $form, $element, $new_value) &&
-                static::validate_min      ($field, $form, $element, $new_value) &&
-                static::validate_max      ($field, $form, $element, $new_value) &&
-                static::validate_pattern  ($field, $form, $element, $new_value);
-      $field->value_set($new_value);
-      return $result;
-    }
-  }
-
-  static function validate_value($field, $form, $element, &$new_value) {
-    if (strlen($new_value) && !core::validate_T_datetime($new_value)) {
-      $field->error_set(
-        translation::get('Field "%%_title" contains an incorrect date/time!', ['title' => translation::get($field->title)])
-      );
-    } else {
-      $new_value = core::sanitize_T_datetime($new_value);
-      return true;
-    }
   }
 
 }}
