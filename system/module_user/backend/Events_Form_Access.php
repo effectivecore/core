dc3a05dc9c5bd5185adf6088bab02b71405b1b2f
Maxim Rysevets <root@effcore.com>
2018-12-05 13:43:48 +0300

    sprint_400_146_200

--- system/module_user/backend/Events_Form_Access.php
+++ system/module_user/backend/Events_Form_Access.php
@@ -9,6 +9,8 @@ namespace effcore\modules\user {
           use \effcore\field_select;
           use \effcore\field_switcher;
           use \effcore\markup;
+          use \effcore\message;
+          use \effcore\translation;
           abstract class events_form_access {
 
   static function on_init($form, $items) {
@@ -21,10 +23,9 @@ namespace effcore\modules\user {
     foreach ($role_instances as $c_role) {
       $c_switcher = new field_switcher($c_role->title);
       $c_switcher->build();
-      $c_switcher->name_set('access_for_'.$c_role->id);
-      $c_switcher->value_set('on');
+      $c_switcher->name_set('access[]');
+      $c_switcher->value_set($c_role->id);
       $c_switcher->checked_set(isset($access->roles[$c_role->id]));
-      $c_switcher->disabled_set(true);
       $items['settings']->child_insert(
         new markup('x-role', [], [
           $c_switcher
@@ -37,6 +38,13 @@ namespace effcore\modules\user {
   }
 
   static function on_submit($form, $items) {
+    $role_entity = entity::get('role');
+    $role_instances = $role_entity->instances_select([], ['weight desc', 'title']);
+    foreach ($role_instances as $c_role) {
+      if ($items['#access:'.$c_role->id]->checked_get()) {
+        message::insert(translation::get('Role %%_id was selected.', ['id' => $c_role->id]));
+      }
+    }
   }
 
 }}
