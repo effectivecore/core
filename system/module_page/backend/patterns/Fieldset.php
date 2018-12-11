05559e623f9acdc09e045f7e0e6968e1eeb29013
Maxim Rysevets <root@effcore.com>
2018-12-11 17:46:16 +0300

    sprint_400_146_242

--- system/module_page/backend/patterns/Fieldset.php
+++ system/module_page/backend/patterns/Fieldset.php
@@ -11,7 +11,7 @@ namespace effcore {
   public $title_tag_name = 'label';
   public $content_tag_name = 'x-fieldset-content';
 # ─────────────────────────────────────────────────────────────────────
-  public $state = ''; # opened | closed[checked] | has_error
+  public $state = ''; # '' | opened | closed[checked]
   public $number = 0;
 
   function __construct($title = null, $description = null, $attributes = [], $children = [], $weight = 0) {
@@ -24,10 +24,14 @@ namespace effcore {
 
   function render_self() {
     if ($this->title) {
-      $opener = $this->state == 'opened' ? new markup_simple('input', ['type' => 'checkbox', 'data-opener-type' => 'fieldset', 'title' => translation::get('Show or hide content'), 'name' => 'f_opener_'.$this->number, 'id' => 'f_opener_'.$this->number,                       ]) : (
-                $this->state == 'closed' ? new markup_simple('input', ['type' => 'checkbox', 'data-opener-type' => 'fieldset', 'title' => translation::get('Show or hide content'), 'name' => 'f_opener_'.$this->number, 'id' => 'f_opener_'.$this->number, 'checked' => 'checked']) : null);
-      if ($opener && field::request_value_get('form_id') && field::request_value_get('f_opener_'.$this->number) == 'on') $opener->attribute_insert('checked', 'checked');
-      if ($opener && field::request_value_get('form_id') && field::request_value_get('f_opener_'.$this->number) != 'on') $opener->attribute_delete('checked');
+      switch ($this->state) {
+        case 'opened': $opener = new markup_simple('input', ['type' => 'checkbox', 'data-opener-type' => 'fieldset', 'title' => translation::get('Show or hide content'), 'name' => 'f_opener_'.$this->number, 'id' => 'f_opener_'.$this->number                        ]); break;
+        case 'closed': $opener = new markup_simple('input', ['type' => 'checkbox', 'data-opener-type' => 'fieldset', 'title' => translation::get('Show or hide content'), 'name' => 'f_opener_'.$this->number, 'id' => 'f_opener_'.$this->number, 'checked' => 'checked']); break;
+        default      : $opener = null;
+      }
+      if ($opener && $this->cform && $this->cform->attribute_select('id') == field::request_value_get('form_id') && field::request_value_get('f_opener_'.$this->number) == 'on') $opener->attribute_insert('checked', 'checked');
+      if ($opener && $this->cform && $this->cform->attribute_select('id') == field::request_value_get('form_id') && field::request_value_get('f_opener_'.$this->number) != 'on') $opener->attribute_delete('checked');
+      if ($opener && $this->has_error()                                                                                                                                        ) $opener->attribute_delete('checked');
       return $opener ? $opener->render().(new markup($this->title_tag_name,['for' => 'f_opener_'.$this->number], [$this->title]))->render() :
                                          (new markup($this->title_tag_name,[                                  ], [$this->title]))->render();
     }
@@ -41,14 +45,6 @@ namespace effcore {
 
   static function cur_number_get() {
     return static::$c_number++;
-  }
-
-  static function validate($fieldset, $form, $npath) {
-    if (($fieldset->state == 'opened'  ||
-         $fieldset->state == 'closed') &&
-         $fieldset->has_error()) {
-      $fieldset->state = 'has_error';
-    }
   }
 
 }}
