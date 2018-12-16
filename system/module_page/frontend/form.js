8f60ab46dfc59804226842f87e9851815f010d64
Maxim Rysevets <root@effcore.com>
2018-12-16 23:13:47 +0300

    sprint_400_146_262

--- system/module_page/frontend/form.js
+++ system/module_page/frontend/form.js
@@ -1,10 +1,3 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-  document.querySelectorAll('input[type=range]').forEach(function(range){
-    var x_value = range.parentNode.querySelector('x-value');
-    range.addEventListener('mousemove', function(){
-      x_value.innerText = range.value;
-    });
-  });
-
 });
