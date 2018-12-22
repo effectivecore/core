a338c6d9700255c6385cd85249e4bf063bea55ba
Maxim Rysevets <root@effcore.com>
2018-12-23 00:36:12 +0300

    sprint_400_146_282

--- system/module_core/backend/Events_Form_Modules_Uninstall.php
+++ system/module_core/backend/Events_Form_Modules_Uninstall.php
@@ -5,15 +5,24 @@
   ##################################################################
 
 namespace effcore\modules\core {
-          use \effcore\markup;
-          use \effcore\text;
+          use \effcore\core;
+          use \effcore\group_checkboxes;
+          use \effcore\module;
           abstract class events_form_modules_uninstall {
 
   static function on_init($form, $items) {
+    $installed_by_boot = core::boot_select('installed');
+    $embed = module::embed_get();
+    $modules = module::all_get();
+    $checkboxes = new group_checkboxes();
+    $checkboxes->build();
+    foreach ($modules as $c_module) {
+      $checkboxes->field_insert(
+        $c_module->title, ['name' => 'uninstall[]', 'value' => $c_module->id]
+      );
+    }
     $info = $form->child_select('info');
-    $info->child_insert(
-      new markup('div', [], new text('UNDER CONSTRUCTION'))
-    );
+    $info->child_insert($checkboxes, 'checkboxes');
   }
 
   static function on_submit($form, $items) {
