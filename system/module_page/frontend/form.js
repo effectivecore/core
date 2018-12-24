08c38557b5f090c690b0202e0e6855a68faba402
Maxim Rysevets <root@effcore.com>
2018-12-25 00:48:32 +0300

    sprint_400_146_291

--- system/module_page/frontend/form.js
+++ system/module_page/frontend/form.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-  effcore.select_multiple('input[type=range]').forEach(function(element){
-    effcore.select('x-value', element.parentNode).forEach(function(value){
+  effcore.select_multiple('input[type=range]').for_each(function(element){
+    effcore.select('x-value', element.parentNode).for_each(function(value){
       element.addEventListener('mousemove', function(){
         value.innerText = element.title = element.value;
       });
     });
   });
 
-  effcore.select_multiple('select[data-source=uagent-timezone]').forEach(function(element){
+  effcore.select_multiple('select[data-source=uagent-timezone]').for_each(function(element){
     if (element.value == '') {
       var timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
       if (timezone) element.value = timezone;
