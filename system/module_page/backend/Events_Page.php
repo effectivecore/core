dec2322e7542a19eafcfbe8f425eedaf767eb328
Maxim Rysevets <root@effcore.com>
2018-12-30 13:23:50 +0300

    sprint_400_146_312_5

--- system/module_page/backend/Events_Page.php
+++ system/module_page/backend/Events_Page.php
@@ -7,12 +7,17 @@
 namespace effcore\modules\page {
           use \effcore\markup;
           use \effcore\text;
+use \effcore\token;
+use \effcore\translation;
           abstract class events_page {
 
   static function on_show_title($page) {
-    $title = new text($page->title);
-    $title->is_apply_tokens = true;
-    return new markup('h1', ['id' => 'title'], $title);
+    // $title = new text($page->title);
+    // $title->is_apply_tokens = true;
+    // return new markup('h1', ['id' => 'title'], $title);
+    return new markup('h1', ['id' => 'title'],
+      token::replace(translation::get($page->title))
+    );
   }
 
 }}
