b9eeb1638d2b581ea9cf9a17327cf8e0f987e3bb
Maxim Rysevets <root@effcore.com>
2019-01-01 17:56:31 +0300

    sprint_400_154_2

--- system/module_develop/module_demo/backend/Events_Page_Demo.php
+++ system/module_develop/module_demo/backend/Events_Page_Demo.php
@@ -9,6 +9,7 @@ namespace effcore\modules\demo {
           use \effcore\canvas_svg;
           use \effcore\diagram;
           use \effcore\message;
+          use \effcore\node;
           use \effcore\table_body_row_cell;
           use \effcore\table_body_row;
           use \effcore\table;
@@ -31,6 +32,39 @@ namespace effcore\modules\demo {
     message::insert(translation::get('Error message #%%_number.',   ['number' => 1]), 'error');
     message::insert(translation::get('Error message #%%_number.',   ['number' => 2]), 'error');
     message::insert(translation::get('Error message #%%_number.',   ['number' => 3]), 'error');
+  }
+
+  static function on_show_block_demo_canvas($page) {
+    $canvas = new canvas_svg(105, 16, 5);
+    $canvas->glyph_set('01110|10001|10001|10001|10001|10001|10001|10001|10001|01110',  5, 3); # 0
+    $canvas->glyph_set('00001|00001|00001|00001|00001|10001|01001|00101|00010|00001', 15, 3); # 1
+    $canvas->glyph_set('11111|10000|01000|00100|00010|00001|00001|00001|00001|11110', 25, 3); # 2
+    $canvas->glyph_set('01000|00100|00010|00001|11111|01000|00100|00010|00001|11111', 35, 3); # 3
+    $canvas->glyph_set('00001|00001|00001|00001|01111|10001|01001|00101|00010|00001', 45, 3); # 4
+    $canvas->glyph_set('01000|00100|00010|00001|01111|10000|10000|10000|10000|01111', 55, 3); # 5
+    $canvas->glyph_set('01110|10001|10001|10001|10001|01110|10000|01000|00100|00010', 65, 3); # 6
+    $canvas->glyph_set('10000|10000|10000|10000|10000|01000|00100|00010|00001|11111', 75, 3); # 7
+    $canvas->glyph_set('01110|10001|10001|10001|10001|01110|10001|10001|10001|01110', 85, 3); # 8
+    $canvas->glyph_set('01000|00100|00010|00001|01110|10001|10001|10001|10001|01110', 95, 3); # 9
+    return new block('Canvas', ['class' => ['demo-canvas' => 'demo-canvas']], [
+      $canvas
+    ]);
+  }
+
+  static function on_show_block_demo_diagrams($page) {
+    $diagram_linear = new diagram('Title', 'linear');
+    $diagram_linear->slice_add('Parameter 1', 70, '0.07 sec.');
+    $diagram_linear->slice_add('Parameter 2', 20, '0.02 sec.');
+    $diagram_linear->slice_add('Parameter 3', 10, '0.01 sec.');
+    $diagram_radial = new diagram('Title', 'radial');
+    $diagram_radial->slice_add('Parameter 1', 40, '0.04 sec.', '#216ce4');
+    $diagram_radial->slice_add('Parameter 2', 30, '0.03 sec.', '#30c432');
+    $diagram_radial->slice_add('Parameter 3', 20, '0.02 sec.', '#fc5740');
+    $diagram_radial->slice_add('Parameter 4', 10, '0.01 sec.', '#fd9a1e');
+    return new node([], [
+      new block('Linear diagram', ['class' => ['demo-diagram-linear' => 'demo-diagram-linear']], $diagram_linear),
+      new block('Radial diagram', ['class' => ['demo-diagram-radial' => 'demo-diagram-radial']], $diagram_radial)
+    ]);
   }
 
   static function on_show_block_demo_dynamic($page) {
@@ -56,39 +90,6 @@ namespace effcore\modules\demo {
     ];
     return new block('Dynamic block', ['data-styled-title' => 'no', 'class' => ['demo-dynamic' => 'demo-dynamic']], [
       new table(['class' => ['table' => 'table']], $tbody, $thead)
-    ]);
-  }
-
-  static function on_show_block_demo_canvas($page) {
-    $canvas = new canvas_svg(105, 16, 5);
-    $canvas->glyph_set('01110|10001|10001|10001|10001|10001|10001|10001|10001|01110',  5, 3); # 0
-    $canvas->glyph_set('00001|00001|00001|00001|00001|10001|01001|00101|00010|00001', 15, 3); # 1
-    $canvas->glyph_set('11111|10000|01000|00100|00010|00001|00001|00001|00001|11110', 25, 3); # 2
-    $canvas->glyph_set('01000|00100|00010|00001|11111|01000|00100|00010|00001|11111', 35, 3); # 3
-    $canvas->glyph_set('00001|00001|00001|00001|01111|10001|01001|00101|00010|00001', 45, 3); # 4
-    $canvas->glyph_set('01000|00100|00010|00001|01111|10000|10000|10000|10000|01111', 55, 3); # 5
-    $canvas->glyph_set('01110|10001|10001|10001|10001|01110|10000|01000|00100|00010', 65, 3); # 6
-    $canvas->glyph_set('10000|10000|10000|10000|10000|01000|00100|00010|00001|11111', 75, 3); # 7
-    $canvas->glyph_set('01110|10001|10001|10001|10001|01110|10001|10001|10001|01110', 85, 3); # 8
-    $canvas->glyph_set('01000|00100|00010|00001|01110|10001|10001|10001|10001|01110', 95, 3); # 9
-    return new block('Canvas', ['class' => ['demo-canvas' => 'demo-canvas']], [
-      $canvas
-    ]);
-  }
-
-  static function on_show_block_demo_diagrams($page) {
-    $l_diagram = new diagram('Linear diagram', 'linear');
-    $l_diagram->slice_add('Parameter 1', 70, '0.07 sec.');
-    $l_diagram->slice_add('Parameter 2', 20, '0.02 sec.');
-    $l_diagram->slice_add('Parameter 3', 10, '0.01 sec.');
-    $c_diagram = new diagram('Radial diagram', 'radial');
-    $c_diagram->slice_add('Parameter 1', 40, '0.04 sec.', '#216ce4');
-    $c_diagram->slice_add('Parameter 2', 30, '0.03 sec.', '#30c432');
-    $c_diagram->slice_add('Parameter 3', 20, '0.02 sec.', '#fc5740');
-    $c_diagram->slice_add('Parameter 4', 10, '0.01 sec.', '#fd9a1e');
-    return new block('Diagrams', ['class' => ['demo-diagrams' => 'demo-diagrams']], [
-      $l_diagram,
-      $c_diagram
     ]);
   }
 
