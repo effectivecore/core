3cd08b12ff6eacd685d9fd52620b3050513e1ed9
Maxim Rysevets <root@effcore.com>
2019-01-02 20:44:31 +0300

    sprint_400_159

--- system/module_menu/backend/patterns/Tabs_item.php
+++ system/module_menu/backend/patterns/Tabs_item.php
@@ -28,10 +28,10 @@ namespace effcore {
   }
 
   function build() {
-    $this->children_delete_all();
     foreach (tabs::items_select() as $c_item) {
       if ($c_item->id_parent == $this->id) {
         $this->child_insert($c_item, $c_item->id);
+        $c_item->build();
       }
     }
   }
