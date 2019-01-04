7e0e02d8fd19747a14203cf36af78fcf20e544ff
Maxim Rysevets <root@effcore.com>
2019-01-04 17:07:50 +0300

    sprint_400_170

--- index.php
+++ index.php
@@ -4,10 +4,7 @@
   ### Copyright © 2017—2019 Maxim Rysevets. All rights reserved. ###
   ##################################################################
 
-  if (version_compare(phpversion(), '7.1.0', '<')) {
-    print 'Current version of PHP is: '.phpversion().'<br>';
-    print 'Required version is: 7.1.0+';
-  } else {
+  if (version_compare(PHP_VERSION, '7.1.0', '>=')) {
     $www_root = DIRECTORY_SEPARATOR == '\\' ? str_replace('\\', '/', __DIR__) : __DIR__;
     define('effcore\\dir_root',    $www_root.'/');
     define('effcore\\dir_cache',   $www_root.'/dynamic/cache/');
@@ -15,4 +12,7 @@
     define('effcore\\dir_modules', $www_root.'/modules/');
     define('effcore\\dir_system',  $www_root.'/system/');
     require_once('system/boot.php');
+  } else {
+    print 'The System requires a version of PHP not lower than 7.1.0<br>';
+    print 'The current version of PHP is '.PHP_VERSION;
   }
