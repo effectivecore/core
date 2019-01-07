6b76160a2c4ed0171b072fd198ea709fc570513e
Maxim Rysevets <root@effcore.com>
2019-01-08 00:03:58 +0300

    sprint_400_183

--- shell/dynamic_reset.sh
+++ shell/dynamic_reset.sh
@@ -7,7 +7,6 @@ while true; do
         find ../dynamic/data    -not -name 'readme.md' -not -path '../dynamic/data'    -delete
         find ../dynamic/files   -not -name 'readme.md' -not -path '../dynamic/files'   -delete
         find ../dynamic/logs    -not -name 'readme.md' -not -path '../dynamic/logs'    -delete
-        find ../dynamic/modules -not -name 'readme.md' -not -path '../dynamic/modules' -delete
         find ../dynamic/tmp     -not -name 'readme.md' -not -path '../dynamic/tmp'     -delete
         cp /dev/null ../dynamic/data/data.sqlite
         break;;
