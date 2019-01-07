81da6bf4490a71e087b49a7a8de4ee7dbc01be9c
Maxim Rysevets <root@effcore.com>
2019-01-07 23:36:13 +0300

    sprint_400_180

--- shell/update.sh
+++ shell/update.sh
@@ -3,15 +3,4 @@
 git -C ../ clean  -d  -f .
 git -C ../ reset --hard
 git -C ../ pull
-
-while true; do
-  read -p 'Apply stash? [y/n]: ' answer
-  case $answer in
-    y ) git -C ../ stash apply
-        break;;
-    n ) exit;;
-    * ) echo 'Please answer "y" or "n".';;
-  esac
-done
-
 ./cache_clear.sh
