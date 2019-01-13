4d4bc118c97ce27be5a22549fba0e91294e4783f
Maxim Rysevets <root@effcore.com>
2019-01-13 10:42:46 +0300

    sprint_500_13

--- system/module_core/backend/Events_Form_Modules_Install.php
+++ system/module_core/backend/Events_Form_Modules_Install.php
@@ -119,7 +119,7 @@ namespace effcore\modules\core {
         if ($modules_to_enable) {
           cache::update_global($include_paths);
           foreach ($modules_to_enable as $c_module) {
-            if (!$c_module->is_installed()) # p.s. module may not have the event "on_install"
+            if (!module::is_installed($c_module->id)) # p.s. module may not have the event "on_install"
             event::start('on_module_install', $c_module->id);
             event::start('on_module_enable',  $c_module->id);
           }
