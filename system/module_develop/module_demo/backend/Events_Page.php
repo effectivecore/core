625fdf259cbe52795b5349d64e4f385cadc940eb
Maxim Rysevets <root@effcore.com>
2019-01-13 11:30:49 +0300

    sprint_500_15

---
+++ system/module_develop/module_demo/backend/Events_Page.php
@@ -0,0 +1,13 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2019 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\demo {
+          abstract class events_page {
+
+  static function on_before_render($page, $template) {
+  }
+
+}}
