32fa49e4b2e0a5b89134fa3cdf603ce94c43ad84
Maxim Rysevets <root@effcore.com>
2019-01-15 23:50:22 +0300

    sprint_500_43_10

--- system/module_user/backend/Events_Page.php
+++ system/module_user/backend/Events_Page.php
@@ -14,25 +14,8 @@ namespace effcore\modules\user {
           abstract class events_page {
 
   static function on_show_block_menu_user($page) {
-    $user = user::current_get();
-    if (empty($user->nick)) {
-      $src = '/'.module::get('user')->path.'frontend/images/avatar-anonymous.svgd';
-      $block_menu = new block('', ['class' => ['menu-user' => 'menu-user']], [
-        storage::get('files')->select('trees/user/user_anonymous'),
-        new markup_simple('img', ['class' => ['avatar' => 'avatar'], 'alt' => 'avatar', 'src' => $src])
-      ]);
-    } else {
-      $src = $user->avatar_path ?
-         '/'.$user->avatar_path : '/'.module::get('user')->path.'frontend/images/avatar-registered.svgd';
-      $block_menu = new block('', ['class' => ['menu-user' => 'menu-user']], [
-        storage::get('files')->select('trees/user/user_registered'),
-        new markup('a', ['href' => '/user/'.$user->nick],
-          new markup_simple('img', ['class' => ['avatar' => 'avatar'], 'alt' => 'avatar', 'src' => $src])
-        )
-      ]);
-    }
-    $block_menu->content_tag_name = null;
-    return $block_menu;
+    // $user = user::current_get();
+    // $src = $user->avatar_path;
   }
 
 }}
