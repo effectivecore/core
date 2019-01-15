8d30f4c0c836dd99ff53a6ecd9b0443de0dcb972
Maxim Rysevets <root@effcore.com>
2019-01-15 15:46:13 +0300

    sprint_500_42

--- system/module_core/backend/File.php
+++ system/module_core/backend/File.php
@@ -202,7 +202,7 @@ namespace effcore {
   static function init() {
     foreach (storage::get('files')->select('file_types') as $c_module_id => $c_file_types) {
       foreach ($c_file_types as $c_row_id => $c_file_type) {
-        if (isset(static::$cache_file_types[$c_file_type->type])) console::log_about_duplicate_insert('file_types', $c_file_type->type);
+        if (isset(static::$cache_file_types[$c_file_type->type])) console::log_about_duplicate_insert('file_types', $c_file_type->type, $c_module_id);
         static::$cache_file_types[$c_file_type->type] = $c_file_type;
         static::$cache_file_types[$c_file_type->type]->module_id = $c_module_id;
       }
