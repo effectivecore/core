8b1593adecac41b3edf3b688645535eaea260801
Maxim Rysevets <root@effcore.com>
2019-01-17 14:47:18 +0300

    sprint_500_46

--- system/module_user/backend/Events_Form_Recovery.php
+++ system/module_user/backend/Events_Form_Recovery.php
@@ -5,12 +5,20 @@
   ##################################################################
 
 namespace effcore\modules\user {
+          use \effcore\instance;
           abstract class events_form_recovery {
 
   static function on_validate($form, $items) {
     switch ($form->clicked_button->value_get()) {
       case 'recovery':
-        break;
+        if (!$form->has_error()) {
+          if (!(new instance('user', ['email' => strtolower($items['#email']->value_get())]))->select()) {
+            $items['#email']->error_set(
+              'User with this EMail was not registered!'
+            );
+            return;
+          }
+        }
     }
   }
 
