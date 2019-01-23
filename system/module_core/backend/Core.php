fa9c8e9bafcf93d1dc508f841cefa8a25849de9d
Maxim Rysevets <root@effcore.com>
2019-01-23 16:06:54 +0300

    sprint_500_71

--- system/module_core/backend/Core.php
+++ system/module_core/backend/Core.php
@@ -721,9 +721,7 @@ namespace effcore {
     $matches = [];
     preg_match('%^(?<full_name>(?<name>[a-zA-Z0-9\\-]+)/(?<version>[a-zA-Z0-9\\.]+))|'.
                  '(?<full_name_unknown>.*)%', $_SERVER['SERVER_SOFTWARE'], $matches);
-    return !empty($matches['full_name']) ?
-                  $matches['name'].' '.
-                  $matches['version'] :
+    return !empty($matches['full_name']) ? $matches['name'].' '.$matches['version'] :
                   $matches['full_name_unknown'];
   }
 
