92b586ebfb82a79c7bad5fd2ead0865fce688558
Maxim Rysevets <root@effcore.com>
2019-01-27 12:44:27 +0300

    sprint_500_101

--- system/module_core/backend/Core.php
+++ system/module_core/backend/Core.php
@@ -622,6 +622,7 @@ namespace effcore {
   }
 
   static function password_generate($length = 8) {
+  # exclude to avoid misunderstanding: '0', 'O', 'o', 'L', 'l', 'I', 'i'
     $characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
                    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
                    '1', '2', '3', '4', '5', '6', '7', '8', '9', '@', '#', '%', '_', '*', '-', '+', '=', ':', ';', '!', '?'];
