688a7ec96b25443cc9bb77051a5f542c72ecf878
Maxim Rysevets <root@effcore.com>
2019-01-27 22:20:40 +0300

    sprint_500_113

--- system/module_develop/module_demo/backend/Events_File.php
+++ system/module_develop/module_demo/backend/Events_File.php
@@ -7,17 +7,31 @@
 namespace effcore\modules\demo {
           use const \effcore\nl;
           use \effcore\console;
+          use \effcore\session;
+          use \effcore\user;
           abstract class events_file {
 
-  static function process_demotype($type) {
-    $result = 'type: '.$type->type.nl;
-    $result.= 'call \effcore\modules\demo\events_file::process_demotype'.nl;
-    for ($i = 0; $i < 10; $i++) {
-      $result.= 'result string: '.$i.nl;
+  static function process_demotype($file_info) {
+    $session = session::select();
+    if ($session &&
+        $session->id_user) {
+      $user = user::current_get();
+      if (!isset($user->roles['registered'])) {
+        user::init($session->id_user, false); # false - do not load roles from the storage
+        $user = user::current_get();
+      }
+      if (isset($user->roles['registered'])) {
+        $result = 'dirs: '.$file_info->dirs.nl;
+        $result.= 'name: '.$file_info->name.nl;
+        $result.= 'type: '.$file_info->type.nl;
+        $result.= 'call \effcore\modules\demo\events_file::process_demotype'.nl;
+        $result.= 'current user: '.$user->nick.nl;
+        $result.= 'current user roles: '.implode(', ', $user->roles).nl;
+        header('Content-Length: '.strlen($result));
+        header('Cache-Control: private, no-cache');
+        print $result;
+      }
     }
-    header('Content-Length: '.strlen($result));
-    header('Cache-Control: private, no-cache');
-    print $result;
     console::log_store();
     exit();
   }
