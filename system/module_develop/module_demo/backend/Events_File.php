102fca033f7713336f878aa5b9bea9b613512799
Maxim Rysevets <root@effcore.com>
2019-01-28 22:47:43 +0300

    sprint_500_121

--- system/module_develop/module_demo/backend/Events_File.php
+++ system/module_develop/module_demo/backend/Events_File.php
@@ -7,6 +7,7 @@
 namespace effcore\modules\demo {
           use const \effcore\nl;
           use \effcore\console;
+          use \effcore\event;
           use \effcore\session;
           use \effcore\user;
           abstract class events_file {
@@ -21,22 +22,28 @@ namespace effcore\modules\demo {
         $user = user::current_get();
       }
       if (isset($user->roles['registered'])) {
-        $result = 'dirs: '.$file_info->dirs.nl;
-        $result.= 'name: '.$file_info->name.nl;
-        $result.= 'type: '.$file_info->type.nl;
-        $result.= 'call \effcore\modules\demo\events_file::process_demotype'.nl;
-        $result.= 'current user: '.$user->nick.nl;
-        $result.= 'current user roles: '.implode(', ', $user->roles).nl;
-        header('Content-Length: '.strlen($result));
+        $data = '';
+        event::start('on_file_process_demotype', null, [$file_info, &$data]);
+        header('Content-Length: '.strlen($data));
         header('Content-Type: application/octet-stream');
         header('Content-Disposition: attachment; filename=demo.txt');
         header('Cache-Control: private, no-cache, no-store, must-revalidate');
         header('Expires: 0');
-        print $result;
+        print $data;
       }
     }
     console::log_store();
     exit();
+  }
+
+  static function on_process_demotype($file_info, &$data) {
+    $user = user::current_get();
+    $data = 'dirs: '.$file_info->dirs.nl;
+    $data.= 'name: '.$file_info->name.nl;
+    $data.= 'type: '.$file_info->type.nl;
+    $data.= 'call \effcore\modules\demo\events_file::on_process_demotype'.nl;
+    $data.= 'current user: '.$user->nick.nl;
+    $data.= 'current user roles: '.implode(', ', $user->roles);
   }
 
 }}
