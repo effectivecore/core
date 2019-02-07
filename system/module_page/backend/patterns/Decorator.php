a51efd702204ed1eb304a8d1fb2cee499cdf0c1a
Maxim Rysevets <root@effcore.com>
2019-02-07 16:17:05 +0300

    sprint_502_16

--- system/module_page/backend/patterns/Decorator.php
+++ system/module_page/backend/patterns/Decorator.php
@@ -21,13 +21,13 @@ namespace effcore {
         $thead     = new table_head    ();
         $thead_row = new table_head_row();
         $tbody     = new table_body    ();
-        $thead->child_insert($thead_row);
+        $thead->child_insert($thead_row, 'head_row_main');
       # make thead
         foreach (reset($this->data) as $c_name => $c_info) {
           $thead_row->child_insert(
             new table_head_row_cell(['class' => [$c_name => $c_name]],
-              $c_info['title']
-            )
+               $c_info['title']
+            ), $c_name
           );
         }
       # make tbody
@@ -36,18 +36,17 @@ namespace effcore {
           foreach ($c_row as $c_name => $c_info) {
             $c_tbody_row->child_insert(
               new table_body_row_cell(['class' => [$c_name => $c_name]],
-                $c_info['value']
-              )
+                 $c_info['value']
+              ), $c_name
             );
           }
           $tbody->child_insert(
-            $c_tbody_row
+            $c_tbody_row, $c_rowid
           );
         }
       # return result
-        $table = new table([], $tbody, $thead);
         $result->child_insert(
-          $table
+          new table([], $tbody, $thead), 'result'
         );
         break;
 
@@ -59,12 +58,12 @@ namespace effcore {
           $c_list = new markup('ul', ['data-rowid' => $c_rowid]);
           foreach ($c_row as $c_name => $c_info) {
             $c_list->child_insert(new markup('li', ['class' => [$c_name => $c_name]], [
-              new markup('x-title', [], $c_info['title']),
-              new markup('x-value', [], $c_info['value'])
+              'title' => new markup('x-title', [], $c_info['title']),
+              'value' => new markup('x-value', [], $c_info['value'])
             ]), $c_name);
           }
           $result->child_insert(
-            $c_list
+            $c_list, $c_rowid
           );
         }
         break;
