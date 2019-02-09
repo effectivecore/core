26749e851f31fb490689c4943218882f4af3193e
Maxim Rysevets <root@effcore.com>
2019-02-09 16:47:52 +0300

    sprint_502_30

--- system/module_core/backend/Events_Page_Info.php
+++ system/module_core/backend/Events_Page_Info.php
@@ -37,22 +37,22 @@ namespace effcore\modules\core {
     $is_enabled_opcache_value = new markup('x-value', ['data-state' => $is_enabled_opcache ? 'ok' : 'warning'], $is_enabled_opcache ? 'yes' : 'no');
     $decorator = new decorator();
     $decorator->view_type = 'dl';
-    $decorator->data = ['info' => [
-      'system'        => ['title' => 'System',                 'value' => $logo_system],
-      'copyright'     => ['title' => 'Copyright',              'value' => '© 2017—2019 Maxim Rysevets. All rights reserved.'],
-      'build_number'  => ['title' => 'Build number',           'value' => $storage_files->select('bundle/system/build')],
-      'web_server'    => ['title' => 'Web server',             'value' => core::server_software_get()],
-      'php_version'   => ['title' => 'PHP Version',            'value' => phpversion().' ('.php_uname('m').')'],
+    $decorator->data = [[
+      'system'        => ['title' => 'System',                 'value' => $logo_system                                             ],
+      'copyright'     => ['title' => 'Copyright',              'value' => '© 2017—2019 Maxim Rysevets. All rights reserved.'       ],
+      'build_number'  => ['title' => 'Build number',           'value' => $storage_files->select('bundle/system/build')            ],
+      'web_server'    => ['title' => 'Web server',             'value' => core::server_software_get()                              ],
+      'php_version'   => ['title' => 'PHP Version',            'value' => phpversion().' ('.php_uname('m').')'                     ],
       'storage_sql'   => ['title' => 'Storage SQL',            'value' => $storage_sql->title_get().' '.$storage_sql->version_get()],
-      'os_name'       => ['title' => 'Operating System',       'value' => php_uname('s')],
-      'os_version'    => ['title' => 'OS Version',             'value' => php_uname('v')],
-      'hostname'      => ['title' => 'Hostname',               'value' => php_uname('n')],
-      'timezone'      => ['title' => 'Server timezone',        'value' => date_default_timezone_get()],
-      'datetime'      => ['title' => 'Server UTC date / time', 'value' => core::datetime_get()],
-      'opcache_state' => ['title' => 'OPcache is anebled',     'value' => $is_enabled_opcache_value],
-      'cron_url'      => ['title' => 'Cron URL',               'value' => $cron_link],
-      'prov_key'      => ['title' => 'Provisioning key',       'value' => 'not applicable'],
-      'subscr_to_upd' => ['title' => 'Subscribe to updates',   'value' => 'not applicable'],
+      'os_name'       => ['title' => 'Operating System',       'value' => php_uname('s')                                           ],
+      'os_version'    => ['title' => 'OS Version',             'value' => php_uname('v')                                           ],
+      'hostname'      => ['title' => 'Hostname',               'value' => php_uname('n')                                           ],
+      'timezone'      => ['title' => 'Server timezone',        'value' => date_default_timezone_get()                              ],
+      'datetime'      => ['title' => 'Server UTC date / time', 'value' => core::datetime_get()                                     ],
+      'opcache_state' => ['title' => 'OPcache is anebled',     'value' => $is_enabled_opcache_value                                ],
+      'cron_url'      => ['title' => 'Cron URL',               'value' => $cron_link                                               ],
+      'prov_key'      => ['title' => 'Provisioning key',       'value' => 'not applicable'                                         ],
+      'subscr_to_upd' => ['title' => 'Subscribe to updates',   'value' => 'not applicable'                                         ]
     ]];
     return new block('Shared information', ['class' => ['info' => 'info']], [
       $decorator->build()
