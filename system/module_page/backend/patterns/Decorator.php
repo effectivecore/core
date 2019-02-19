67adbf792af3a749a7f0ec802bc4dd0824b65538
Maxim Rysevets <root@effcore.com>
2019-02-19 17:53:19 +0300

    sprint_502_74

--- system/module_page/backend/patterns/Decorator.php
+++ system/module_page/backend/patterns/Decorator.php
@@ -22,97 +22,104 @@ namespace effcore {
   function build() {
     $this->children_delete_all();
     $this->attribute_insert('data-view-type', $this->view_type);
-    switch ($this->view_type) {
 
-    # ─────────────────────────────────────────────────────────────────────
-    # table
-    # ─────────────────────────────────────────────────────────────────────
-      case 'table':
-        $thead     = new table_head    ();
-        $thead_row = new table_head_row();
-        $tbody     = new table_body    ();
-        $thead->child_insert($thead_row, 'head_row_main');
-      # make thead
-        foreach (reset($this->data) as $c_name => $c_info) {
-          $c_cell_attributes = [];
-          if ($this->visibility_rowid == 'visible'                    ) $c_cell_attributes['data-cellid'] = $c_name;
-          if ($this->visibility_rowid == 'not_int' && !is_int($c_name)) $c_cell_attributes['data-cellid'] = $c_name;
-          if ($c_name != 'attributes') {
-            $thead_row->child_insert(
-              new table_head_row_cell($c_cell_attributes, $c_info['title']), $c_name
+    if ($this->data) {
+      switch ($this->view_type) {
+
+      # ─────────────────────────────────────────────────────────────────────
+      # table
+      # ─────────────────────────────────────────────────────────────────────
+        case 'table':
+          $thead     = new table_head    ();
+          $thead_row = new table_head_row();
+          $tbody     = new table_body    ();
+          $thead->child_insert($thead_row, 'head_row_main');
+        # make thead
+          foreach (reset($this->data) as $c_name => $c_info) {
+            $c_cell_attributes = [];
+            if ($this->visibility_rowid == 'visible'                    ) $c_cell_attributes['data-cellid'] = $c_name;
+            if ($this->visibility_rowid == 'not_int' && !is_int($c_name)) $c_cell_attributes['data-cellid'] = $c_name;
+            if ($c_name != 'attributes') {
+              $thead_row->child_insert(
+                new table_head_row_cell($c_cell_attributes, $c_info['title']), $c_name
+              );
+            }
+          }
+        # make tbody
+          foreach ($this->data as $c_rowid => $c_row) {
+            $c_row_attributes = static::attributes_shift($c_row);
+            if ($this->visibility_rowid == 'visible'                     ) $c_row_attributes['data-rowid'] = $c_rowid;
+            if ($this->visibility_rowid == 'not_int' && !is_int($c_rowid)) $c_row_attributes['data-rowid'] = $c_rowid;
+            $c_tbody_row = new table_body_row($c_row_attributes);
+            foreach ($c_row as $c_name => $c_info) {
+              $c_cell_attributes = static::attributes_shift($c_info);
+              if ($this->visibility_cellid == 'visible'                    ) $c_cell_attributes['data-cellid'] = $c_name;
+              if ($this->visibility_cellid == 'not_int' && !is_int($c_name)) $c_cell_attributes['data-cellid'] = $c_name;
+              $c_tbody_row->child_insert(
+                new table_body_row_cell($c_cell_attributes, $c_info['value']), $c_name
+              );
+            }
+            $tbody->child_insert(
+              $c_tbody_row, $c_rowid
             );
           }
-        }
-      # make tbody
-        foreach ($this->data as $c_rowid => $c_row) {
-          $c_row_attributes = static::attributes_shift($c_row);
-          if ($this->visibility_rowid == 'visible'                     ) $c_row_attributes['data-rowid'] = $c_rowid;
-          if ($this->visibility_rowid == 'not_int' && !is_int($c_rowid)) $c_row_attributes['data-rowid'] = $c_rowid;
-          $c_tbody_row = new table_body_row($c_row_attributes);
-          foreach ($c_row as $c_name => $c_info) {
-            $c_cell_attributes = static::attributes_shift($c_info);
-            if ($this->visibility_cellid == 'visible'                    ) $c_cell_attributes['data-cellid'] = $c_name;
-            if ($this->visibility_cellid == 'not_int' && !is_int($c_name)) $c_cell_attributes['data-cellid'] = $c_name;
-            $c_tbody_row->child_insert(
-              new table_body_row_cell($c_cell_attributes, $c_info['value']), $c_name
+        # make result
+          $this->child_insert(
+            new table($this->attributes_select('result_attributes'), $tbody, $thead), 'result'
+          );
+          break;
+
+      # ─────────────────────────────────────────────────────────────────────
+      # ul (unordered list)
+      # ─────────────────────────────────────────────────────────────────────
+        case 'ul':
+          foreach ($this->data as $c_rowid => $c_row) {
+            $c_row_attributes = $this->attributes_select('result_attributes') + static::attributes_shift($c_row);
+            if ($this->visibility_rowid == 'visible'                     ) $c_row_attributes['data-rowid'] = $c_rowid;
+            if ($this->visibility_rowid == 'not_int' && !is_int($c_rowid)) $c_row_attributes['data-rowid'] = $c_rowid;
+            $c_list = new markup('ul', $c_row_attributes);
+            foreach ($c_row as $c_name => $c_info) {
+              $c_cell_attributes = static::attributes_shift($c_info);
+              if ($this->visibility_cellid == 'visible'                    ) $c_cell_attributes['data-cellid'] = $c_name;
+              if ($this->visibility_cellid == 'not_int' && !is_int($c_name)) $c_cell_attributes['data-cellid'] = $c_name;
+              $c_list->child_insert(new markup('li', $c_cell_attributes, [
+                'title' => new markup('x-title', [], $c_info['title']),
+                'value' => new markup('x-value', [], $c_info['value'])
+              ]), $c_name);
+            }
+            $this->child_insert(
+              $c_list, $c_rowid
             );
           }
-          $tbody->child_insert(
-            $c_tbody_row, $c_rowid
-          );
-        }
-      # make result
-        $this->child_insert(
-          new table($this->attributes_select('result_attributes'), $tbody, $thead), 'result'
-        );
-        break;
+          break;
 
-    # ─────────────────────────────────────────────────────────────────────
-    # ul (unordered list)
-    # ─────────────────────────────────────────────────────────────────────
-      case 'ul':
-        foreach ($this->data as $c_rowid => $c_row) {
-          $c_row_attributes = $this->attributes_select('result_attributes') + static::attributes_shift($c_row);
-          if ($this->visibility_rowid == 'visible'                     ) $c_row_attributes['data-rowid'] = $c_rowid;
-          if ($this->visibility_rowid == 'not_int' && !is_int($c_rowid)) $c_row_attributes['data-rowid'] = $c_rowid;
-          $c_list = new markup('ul', $c_row_attributes);
-          foreach ($c_row as $c_name => $c_info) {
-            $c_cell_attributes = static::attributes_shift($c_info);
-            if ($this->visibility_cellid == 'visible'                    ) $c_cell_attributes['data-cellid'] = $c_name;
-            if ($this->visibility_cellid == 'not_int' && !is_int($c_name)) $c_cell_attributes['data-cellid'] = $c_name;
-            $c_list->child_insert(new markup('li', $c_cell_attributes, [
-              'title' => new markup('x-title', [], $c_info['title']),
-              'value' => new markup('x-value', [], $c_info['value'])
-            ]), $c_name);
+      # ─────────────────────────────────────────────────────────────────────
+      # dl (definition list)
+      # ─────────────────────────────────────────────────────────────────────
+        case 'dl':
+          foreach ($this->data as $c_rowid => $c_row) {
+            $c_row_attributes = $this->attributes_select('result_attributes') + static::attributes_shift($c_row);
+            if ($this->visibility_rowid == 'visible'                     ) $c_row_attributes['data-rowid'] = $c_rowid;
+            if ($this->visibility_rowid == 'not_int' && !is_int($c_rowid)) $c_row_attributes['data-rowid'] = $c_rowid;
+            $c_list = new markup('dl', $c_row_attributes);
+            foreach ($c_row as $c_name => $c_info) {
+              $c_cell_attributes = static::attributes_shift($c_info);
+              if ($this->visibility_cellid == 'visible'                    ) $c_cell_attributes['data-cellid'] = $c_name;
+              if ($this->visibility_cellid == 'not_int' && !is_int($c_name)) $c_cell_attributes['data-cellid'] = $c_name;
+              $c_list->child_insert(new markup('dt', $c_cell_attributes, $c_info['title']), 'title-'.$c_name);
+              $c_list->child_insert(new markup('dd', $c_cell_attributes, $c_info['value']), 'value-'.$c_name);
+            }
+            $this->child_insert(
+              $c_list, $c_rowid
+            );
           }
-          $this->child_insert(
-            $c_list, $c_rowid
-          );
-        }
-        break;
+          break;
 
-    # ─────────────────────────────────────────────────────────────────────
-    # dl (definition list)
-    # ─────────────────────────────────────────────────────────────────────
-      case 'dl':
-        foreach ($this->data as $c_rowid => $c_row) {
-          $c_row_attributes = $this->attributes_select('result_attributes') + static::attributes_shift($c_row);
-          if ($this->visibility_rowid == 'visible'                     ) $c_row_attributes['data-rowid'] = $c_rowid;
-          if ($this->visibility_rowid == 'not_int' && !is_int($c_rowid)) $c_row_attributes['data-rowid'] = $c_rowid;
-          $c_list = new markup('dl', $c_row_attributes);
-          foreach ($c_row as $c_name => $c_info) {
-            $c_cell_attributes = static::attributes_shift($c_info);
-            if ($this->visibility_cellid == 'visible'                    ) $c_cell_attributes['data-cellid'] = $c_name;
-            if ($this->visibility_cellid == 'not_int' && !is_int($c_name)) $c_cell_attributes['data-cellid'] = $c_name;
-            $c_list->child_insert(new markup('dt', $c_cell_attributes, $c_info['title']), 'title-'.$c_name);
-            $c_list->child_insert(new markup('dd', $c_cell_attributes, $c_info['value']), 'value-'.$c_name);
-          }
-          $this->child_insert(
-            $c_list, $c_rowid
-          );
-        }
-        break;
-
+      }
+    } else {
+      $this->child_insert(
+        new markup('x-no-result', [], 'no items')
+      );
     }
     return $this;
   }
