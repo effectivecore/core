e025915c151edc016f58b78763ef0afaf5423e34
Maxim Rysevets <root@effcore.com>
2019-02-20 16:23:46 +0300

    sprint_502_75

--- system/module_develop/module_demo/backend/Events_Module.php
+++ system/module_develop/module_demo/backend/Events_Module.php
@@ -5,12 +5,27 @@
   ##################################################################
 
 namespace effcore\modules\demo {
+          use \effcore\instance;
           use \effcore\module;
+          use \effcore\storage;
           abstract class events_module {
 
   static function on_install($demo_args = []) {
     $module = module::get('demo');
     $module->install();
+    if (count(storage::get('sql')->errors) == 0) {
+      for ($i = 1; $i <= 500; $i++) {
+        $c_base_date = new \DateTime('2030-01-01 00:08:20', new \DateTimeZone('UTC'));
+        $c_nick = 'user_'.str_pad((string)$i, 3, '0', STR_PAD_LEFT);
+        $c_created = $c_base_date->modify('-'.$i.' second')->format('Y-m-d H:i:s');
+        $c_is_even = $i % 2 ? 0 : 1;
+        $c_item = (new instance('demo_data', [
+          'nick'    => $c_nick,
+          'created' => $c_created,
+          'is_even' => $c_is_even
+        ]))->insert();
+      }
+    }
   }
 
   static function on_uninstall() {
