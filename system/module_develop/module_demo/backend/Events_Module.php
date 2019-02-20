863b26e68fa42b4d5e821ed87ffded871f204500
Maxim Rysevets <root@effcore.com>
2019-02-20 21:06:24 +0300

    sprint_502_80

--- system/module_develop/module_demo/backend/Events_Module.php
+++ system/module_develop/module_demo/backend/Events_Module.php
@@ -18,13 +18,18 @@ namespace effcore\modules\demo {
         $c_base_date = new \DateTime('2030-01-01 00:08:20', new \DateTimeZone('UTC'));
         $c_id = $i;
         $c_nick = 'user_'.str_pad((string)$i, 3, '0', STR_PAD_LEFT);
+        $c_email = $c_nick.'@example.com';
         $c_created = $c_base_date->modify('-'.$i.' second')->format('Y-m-d H:i:s');
         $c_is_even = $i % 2 ? 0 : 1;
-        $c_item = (new instance('demo_data', [
+        (new instance('demo_data', [
           'id'      => $c_id,
           'nick'    => $c_nick,
           'created' => $c_created,
           'is_even' => $c_is_even
+        ]))->insert();
+        (new instance('demo_data_join', [
+          'id_data' => $c_id,
+          'email'   => $c_email
         ]))->insert();
       }
     }
