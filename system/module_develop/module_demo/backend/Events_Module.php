596d44b2e06bc2a230c004053a54ec85b71bff5a
Maxim Rysevets <root@effcore.com>
2019-02-20 20:48:41 +0300

    sprint_502_79

--- system/module_develop/module_demo/backend/Events_Module.php
+++ system/module_develop/module_demo/backend/Events_Module.php
@@ -16,10 +16,12 @@ namespace effcore\modules\demo {
     if (count(storage::get('sql')->errors) == 0) {
       for ($i = 1; $i <= 500; $i++) {
         $c_base_date = new \DateTime('2030-01-01 00:08:20', new \DateTimeZone('UTC'));
+        $c_id = $i;
         $c_nick = 'user_'.str_pad((string)$i, 3, '0', STR_PAD_LEFT);
         $c_created = $c_base_date->modify('-'.$i.' second')->format('Y-m-d H:i:s');
         $c_is_even = $i % 2 ? 0 : 1;
         $c_item = (new instance('demo_data', [
+          'id'      => $c_id,
           'nick'    => $c_nick,
           'created' => $c_created,
           'is_even' => $c_is_even
