9d8586dc8935f77716aaf60e40e888c3aad2bbea
Maxim Rysevets <root@effcore.com>
2019-02-27 18:46:39 +0300

    sprint_502_128

--- system/module_page/frontend/form.js
+++ system/module_page/frontend/form.js
@@ -1,18 +1,27 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-  effcore.select_multiple('input[type=range]').for_each(function(element){
-    effcore.select('x-value', element.parentNode).for_each(function(value){
+  effcore.get_elements('input[type=range]').for_each(function(element){
+    effcore.get_element('x-value', element.parentNode).for_each(function(value){
       element.addEventListener('mousemove', function(){
         value.innerText = element.title = element.value;
       });
     });
   });
 
-  effcore.select_multiple('select[data-source=uagent-timezone]').for_each(function(element){
+  effcore.get_elements('select[data-source=uagent-timezone]').for_each(function(element){
     if (element.value == '') {
       var timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
       if (timezone) element.value = timezone;
     }
+  });
+
+  effcore.get_elements('x-group[data-type=palette]').for_each(function(wrapper){
+    var opener = effcore.get_element('input[data-opener-type=palette]', wrapper).select(0);
+    effcore.get_elements('x-field input', wrapper).for_each(function(element){
+      element.addEventListener('click', function(){
+        opener.style.backgroundColor = element.style.backgroundColor;
+      });
+    });
   });
 
 });
