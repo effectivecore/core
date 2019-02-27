12134a4a9ef726374d62d4ace27b5611be1227b6
Maxim Rysevets <root@effcore.com>
2019-02-27 16:06:59 +0300

    sprint_502_127

--- system/module_page/backend/patterns/Group_Palette.php
+++ system/module_page/backend/patterns/Group_Palette.php
@@ -7,6 +7,8 @@
 namespace effcore {
           class group_palette extends group_radiobuttons {
 
+  public $title_tag_name = 'label';
+  public $content_tag_name = 'x-group-content';
   public $attributes = ['data-type' => 'palette'];
 
   function build() {
@@ -18,6 +20,35 @@ namespace effcore {
           'title' => translation::get('Color ID = %%_id (value = %%_value)', ['id' => $c_color->id, 'value' => $c_color->value]),
           'style' => ['background: '.$c_color->value]
         ], $c_color->id);
+      }
+    }
+  }
+
+  function render_self() {
+    if ($this->title) {
+      return $this->render_opener().(new markup($this->title_tag_name, ['for' => 'f_opener_'.$this->name_first_get()], [$this->title]))->render();
+    }
+  }
+
+  function render_opener() {
+    $color_id    = $this->value_get();
+    $color_value = $this->color_value_get($color_id);
+    return (new markup_simple('input', [
+      'type' => 'checkbox',
+      'data-opener-type' => 'palette',
+      'title' => translation::get('Select color'),
+      'id' => 'f_opener_'.$this->name_first_get(),
+      'style' => ['background: '.$color_value],
+      'checked' => true
+    ]))->render();
+  }
+
+  function color_value_get($color_id) {
+    foreach (storage::get('files')->select('colors') as $c_colors) {
+      foreach ($c_colors as $c_row_id => $c_color) {
+        if ($c_color->id == $color_id) {
+          return $c_color->value;
+        }
       }
     }
   }
