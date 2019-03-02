175b79ba88ab80f1ab3021e4199ea7e38591a083
Maxim Rysevets <root@effcore.com>
2019-03-02 19:13:15 +0300

    sprint_502_150

--- system/module_page/backend/Events_Form_Decoration.php
+++ system/module_page/backend/Events_Form_Decoration.php
@@ -11,20 +11,29 @@ namespace effcore\modules\page {
 
   static function on_init($form, $items) {
     $settings = storage::get('files')->select('settings');
-    $items['*color_text_id']->value_set($settings['page']->color_text_id);
-    $items['*color_bg_id'  ]->value_set($settings['page']->color_bg_id);
+    $items['*color_text_id'   ]->value_set($settings['page']->color_text_id   );
+    $items['*color_bg_id'     ]->value_set($settings['page']->color_bg_id     );
+    $items['*color_ok_id'     ]->value_set($settings['page']->color_ok_id     );
+    $items['*color_warning_id']->value_set($settings['page']->color_warning_id);
+    $items['*color_error_id'  ]->value_set($settings['page']->color_error_id  );
   }
 
   static function on_submit($form, $items) {
     switch ($form->clicked_button->value_get()) {
       case 'save':
-        storage::get('files')->changes_insert('page', 'update', 'settings/page/color_text_id', $items['*color_text_id']->value_get(), false);
-        storage::get('files')->changes_insert('page', 'update', 'settings/page/color_bg_id',   $items['*color_bg_id'  ]->value_get());
+        storage::get('files')->changes_insert('page', 'update', 'settings/page/color_text_id',    $items['*color_text_id'   ]->value_get(), false);
+        storage::get('files')->changes_insert('page', 'update', 'settings/page/color_bg_id',      $items['*color_bg_id'     ]->value_get(), false);
+        storage::get('files')->changes_insert('page', 'update', 'settings/page/color_ok_id',      $items['*color_ok_id'     ]->value_get(), false);
+        storage::get('files')->changes_insert('page', 'update', 'settings/page/color_warning_id', $items['*color_warning_id']->value_get(), false);
+        storage::get('files')->changes_insert('page', 'update', 'settings/page/color_error_id',   $items['*color_error_id'  ]->value_get());
         message::insert('The changes was saved.');
         break;
       case 'restore':
-        storage::get('files')->changes_delete('page', 'update', 'settings/page/color_text_id', false);
-        storage::get('files')->changes_delete('page', 'update', 'settings/page/color_bg_id');
+        storage::get('files')->changes_delete('page', 'update', 'settings/page/color_text_id',    false);
+        storage::get('files')->changes_delete('page', 'update', 'settings/page/color_bg_id',      false);
+        storage::get('files')->changes_delete('page', 'update', 'settings/page/color_ok_id',      false);
+        storage::get('files')->changes_delete('page', 'update', 'settings/page/color_warning_id', false);
+        storage::get('files')->changes_delete('page', 'update', 'settings/page/color_error_id');
         message::insert('The changes was deleted.');
         static::on_init($form, $items);
         break;
