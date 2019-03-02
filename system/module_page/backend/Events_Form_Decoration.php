341e17bad029bad1fd25eec17dc0219265cd28ac
Maxim Rysevets <root@effcore.com>
2019-03-02 17:33:59 +0300

    sprint_502_149

--- system/module_page/backend/Events_Form_Decoration.php
+++ system/module_page/backend/Events_Form_Decoration.php
@@ -11,19 +11,19 @@ namespace effcore\modules\page {
 
   static function on_init($form, $items) {
     $settings = storage::get('files')->select('settings');
-    $items['*color_id'   ]->value_set($settings['page']->color_id   );
-    $items['*color_bg_id']->value_set($settings['page']->color_bg_id);
+    $items['*color_text_id']->value_set($settings['page']->color_text_id);
+    $items['*color_bg_id'  ]->value_set($settings['page']->color_bg_id);
   }
 
   static function on_submit($form, $items) {
     switch ($form->clicked_button->value_get()) {
       case 'save':
-        storage::get('files')->changes_insert('page', 'update', 'settings/page/color_id',    $items['*color_id'   ]->value_get(), false);
-        storage::get('files')->changes_insert('page', 'update', 'settings/page/color_bg_id', $items['*color_bg_id']->value_get());
+        storage::get('files')->changes_insert('page', 'update', 'settings/page/color_text_id', $items['*color_text_id']->value_get(), false);
+        storage::get('files')->changes_insert('page', 'update', 'settings/page/color_bg_id',   $items['*color_bg_id'  ]->value_get());
         message::insert('The changes was saved.');
         break;
       case 'restore':
-        storage::get('files')->changes_delete('page', 'update', 'settings/page/color_id', false);
+        storage::get('files')->changes_delete('page', 'update', 'settings/page/color_text_id', false);
         storage::get('files')->changes_delete('page', 'update', 'settings/page/color_bg_id');
         message::insert('The changes was deleted.');
         static::on_init($form, $items);
