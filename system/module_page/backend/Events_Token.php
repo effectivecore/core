17dabc6a19d30f0fe8e6809aab5b295d954dcad0
Maxim Rysevets <root@effcore.com>
2019-03-06 15:41:41 +0300

    sprint_502_177

--- system/module_page/backend/Events_Token.php
+++ system/module_page/backend/Events_Token.php
@@ -16,13 +16,13 @@ namespace effcore\modules\page {
       case 'color_page'            : return $colors[$settings['page']->color_page_id            ]->value;
       case 'color_text'            : return $colors[$settings['page']->color_text_id            ]->value;
       case 'color_main'            : return $colors[$settings['page']->color_main_id            ]->value;
+      case 'color_link'            : return $colors[$settings['page']->color_link_id            ]->value;
+      case 'color_link_active'     : return $colors[$settings['page']->color_link_active_id     ]->value;
       case 'color_menu'            : return $colors[$settings['page']->color_menu_id            ]->value;
       case 'color_menu_active'     : return $colors[$settings['page']->color_menu_active_id     ]->value;
       case 'color_menu_text'       : return $colors[$settings['page']->color_menu_text_id       ]->value;
       case 'color_menu_link'       : return $colors[$settings['page']->color_menu_link_id       ]->value;
       case 'color_menu_link_active': return $colors[$settings['page']->color_menu_link_active_id]->value;
-      case 'color_link'            : return $colors[$settings['page']->color_link_id            ]->value;
-      case 'color_link_active'     : return $colors[$settings['page']->color_link_active_id     ]->value;
       case 'color_ok'              : return $colors[$settings['page']->color_ok_id              ]->value;
       case 'color_warning'         : return $colors[$settings['page']->color_warning_id         ]->value;
       case 'color_error'           : return $colors[$settings['page']->color_error_id           ]->value;
