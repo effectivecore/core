75f41459f09fae3103e0b7ac64ad706565d468d3
Maxim Rysevets <root@effcore.com>
2019-03-08 22:03:22 +0300

    sprint_503_5

--- system/module_page/backend/Template_text.php
+++ system/module_page/backend/Template_text.php
@@ -8,18 +8,25 @@ namespace effcore {
           class template_text extends template {
 
   function render() {
-    $rendered = $this->data;
-    $rendered = preg_replace_callback('%(?<spacer>[ ]*)'.
-                                       '(?<prefix>\\%\\%_)'.
-                                       '(?<name>[a-z0-9_]+)'.
-                                       '(?<args>\\{[a-z0-9_,]+\\}|)%S', function($c_match) {
-      return isset($c_match['prefix']) &&
-             isset($c_match['name']) &&
-             isset($this->args[$c_match['name']]) &&
-                   $this->args[$c_match['name']] !== '' ? $c_match['spacer'].
-                   $this->args[$c_match['name']] : '';
-    },     $rendered);
-    return $rendered;
+    if (is_object($this->data) &&
+                  $this->data instanceof text) {
+             $this->data->args = $this->args;
+      return $this->data->render();
+    }
+    if (is_string($this->data)) {
+      $rendered = $this->data;
+      $rendered = preg_replace_callback('%(?<spacer>[ ]*)'.
+                                         '(?<prefix>\\%\\%_)'.
+                                         '(?<name>[a-z0-9_]+)'.
+                                         '(?<args>\\{[a-z0-9_,]+\\}|)%S', function($c_match) {
+        return isset($c_match['prefix']) &&
+               isset($c_match['name']) &&
+               isset($this->args[$c_match['name']]) &&
+                     $this->args[$c_match['name']] !== '' ? $c_match['spacer'].
+                     $this->args[$c_match['name']] : '';
+      },     $rendered);
+      return $rendered;
+    }
   }
 
 }}
