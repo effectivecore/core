18a12f48a875442d46d2c51931526ccf3bfa72d4
Maxim Rysevets <root@effcore.com>
2019-03-09 14:31:26 +0300

    sprint_503_7

--- system/module_page/frontend/form.js
+++ system/module_page/frontend/form.js
@@ -1,31 +1,41 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-  effcore.get_elements('input[type=range]').for_each(function(element){
-    var x_value = element.parentNode.querySelector('x-value');
-    if (x_value) {
-      element.addEventListener('mousemove', function(){
-        x_value.innerText = this.title = this.value;
-      });
-    }
-  });
-
-  effcore.get_elements('select[data-source=uagent-timezone]').for_each(function(element){
-    if (element.value == '') {
-      var timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-      if (timezone) element.value = timezone;
-    }
-  });
-
-  effcore.get_elements('x-group[data-type=palette]').for_each(function(wrapper){
-    var opener = wrapper.querySelector('input[data-opener-type=palette]');
-    if (opener) {
-      effcore.get_elements('x-field input', wrapper).for_each(function(element){
-        element.addEventListener('click', function(){
-          opener.style.backgroundColor = element.style.backgroundColor;
-          opener.value                 = element.value;
+  var ranges = document.querySelectorAll('input[type=range]');
+  if (ranges instanceof NodeList) {
+    ranges.forEach(function(c_range){
+      var x_value = c_range.parentNode.querySelector('x-value');
+      if (x_value) {
+        c_range.addEventListener('mousemove', function(){
+          x_value.innerText = c_range.title = c_range.value;
         });
-      });
-    }
-  });
+      }
+    });
+  }
+
+  var timezones = document.querySelectorAll('select[data-source=uagent-timezone]');
+  if (timezones instanceof NodeList) {
+    timezones.forEach(function(c_timezone){
+      if (c_timezone.value == '') {
+        var uagent_timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+        if (uagent_timezone) c_timezone.value = uagent_timezone;
+      }
+    });
+  }
+
+  var palettes = document.querySelectorAll('x-group[data-type=palette]');
+  if (palettes instanceof NodeList) {
+    palettes.forEach(function(c_palette){
+      var opener = c_palette.querySelector('input[data-opener-type=palette]'),
+          inputs = c_palette.querySelectorAll('x-field input');
+      if (opener && inputs instanceof NodeList) {
+        inputs.forEach(function(c_input){
+          c_input.addEventListener('click', function(){
+            opener.style.backgroundColor = c_input.style.backgroundColor;
+            opener.value                 = c_input.value;
+          });
+        });
+      }
+    });
+  }
 
 });
