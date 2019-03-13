53bca45b9142a1efaddf095d0359990a408e2b27
Maxim Rysevets <root@effcore.com>
2019-03-13 19:52:51 +0300

    sprint_510_23

---
+++ system/module_storage/backend/Events_Page_Manage_instances.php
@@ -0,0 +1,62 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2019 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\storage {
+          use \effcore\block;
+          use \effcore\core;
+          use \effcore\entity;
+          use \effcore\selection;
+          use \effcore\tabs;
+          use \effcore\url;
+          abstract class events_page_manage_instances {
+
+  # function() ←→ url mapping:
+  # ──┬──────────────────────────────────────────────────────────────────────────────────
+  # ? │ /manage/instances/select → /manage/instances/select/%%_entity_name
+  # ? │ /manage/instances/insert → /manage/instances/insert/%%_entity_name
+  # ? │                            /manage/instances/select/%%_entity_name/%%_instance_id
+  # ? │                            /manage/instances/update/%%_entity_name/%%_instance_id
+  # ? │                            /manage/instances/delete/%%_entity_name/%%_instance_id
+  # ──┴──────────────────────────────────────────────────────────────────────────────────
+
+  # ─────────────────────────────────────────────────────────────────────
+  # select multiple instances
+  # ─────────────────────────────────────────────────────────────────────
+
+  static function on_show_block_instance_select_multiple($page) {
+    $entities = entity::all_get(false);
+    $entity_name = $page->args_get('entity_name');
+    core::array_sort_by_title($entities);
+    if (!isset($entities[$entity_name])) url::go($page->args_get('base').'/select/'.reset($entities)->name);
+    foreach ($entities as $c_entity) {
+      tabs::item_insert(             $c_entity->title_plural,
+        'instance_select_'.          $c_entity->name,
+        'instance_select', 'select/'.$c_entity->name, null, ['class' => [
+                           'select-'.$c_entity->name =>
+                           'select-'.$c_entity->name]]
+      );
+    }
+  # create selection
+    $entity = entity::get($entity_name);
+    if ($entity) {
+      $selection = new selection;
+      $selection->is_paged = true;
+      foreach ($entity->fields as $c_name => $c_field) {
+        if (!empty($c_field->show_in_manager)) {
+          $selection->field_entity_insert(null, $entity->name, $c_name);
+        }
+      }
+      $selection->field_checkbox_insert(null, '', 80);
+      $selection->field_action_insert();
+      return new block('', ['class' => [
+        $entity->name =>
+        $entity->name]],
+        $selection
+      );
+    }
+  }
+
+}}
--- system/module_storage/backend/Manage_instances.php
+++ system/module_storage/backend/Manage_instances.php
@@ -7,52 +7,6 @@
 namespace effcore {
           abstract class manage_instances {
 
-  # function() ←→ url mapping:
-  # ────────────────────────────┬──────────────────────────────────────────────────────────────────────────────────
-  # instance_select_multiple()  │ /manage/instances/select → /manage/instances/select/%%_entity_name
-  # instance_insert()           │ /manage/instances/insert → /manage/instances/insert/%%_entity_name
-  # instance_select()           │                            /manage/instances/select/%%_entity_name/%%_instance_id
-  # events_form_instance_update │                            /manage/instances/update/%%_entity_name/%%_instance_id
-  # instance_delete()           │                            /manage/instances/delete/%%_entity_name/%%_instance_id
-  # ────────────────────────────┴──────────────────────────────────────────────────────────────────────────────────
-
-  # ─────────────────────────────────────────────────────────────────────
-  # select multiple instances
-  # ─────────────────────────────────────────────────────────────────────
-
-  static function instance_select_multiple($page) {
-    $entities = entity::all_get(false);
-    $entity_name = $page->args_get('entity_name');  
-    core::array_sort_by_title($entities);
-    if (!isset($entities[$entity_name])) url::go($page->args_get('base').'/select/'.reset($entities)->name);
-    foreach ($entities as $c_entity) {
-      tabs::item_insert(             $c_entity->title_plural,
-        'instance_select_'.          $c_entity->name,
-        'instance_select', 'select/'.$c_entity->name, null, ['class' => [
-                           'select-'.$c_entity->name =>
-                           'select-'.$c_entity->name]]
-      );
-    }
-  # create selection
-    $entity = entity::get($entity_name);
-    if ($entity) {
-      $selection = new selection;
-      $selection->is_paged = true;
-      foreach ($entity->fields as $c_name => $c_field) {
-        if (!empty($c_field->show_in_manager)) {
-          $selection->field_entity_insert(null, $entity->name, $c_name);
-        }
-      }
-      $selection->field_checkbox_insert(null, '', 80);
-      $selection->field_action_insert();
-      return new block('', ['class' => [
-        $entity->name =>
-        $entity->name]],
-        $selection
-      );
-    }
-  }
-
   # ─────────────────────────────────────────────────────────────────────
   # select single instance
   # ─────────────────────────────────────────────────────────────────────
