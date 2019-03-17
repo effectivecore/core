24a88074d1cb31c8147196742cc62461133962ab
Maxim Rysevets <root@effcore.com>
2019-03-17 19:12:16 +0300

    sprint_510_40

--- system/module_develop/module_demo/backend/Demo_Pattern.php
+++ system/module_develop/module_demo/backend/Demo_Pattern.php
@@ -7,4 +7,14 @@
 namespace effcore {
           class demo_pattern {
 
+  public $prop_1 = 'property value 1';
+  public $prop_2 = 'property value 2';
+  public $prop_3 = 'property value 3';
+
+  public $prop_as_array = [
+    'item 1' => 'item value 1',
+    'item 2' => 'item value 2',
+    'item 3' => 'item value 3'
+  ];
+
 }}
