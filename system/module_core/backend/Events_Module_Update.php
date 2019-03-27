3c525098558cd2695bb024cc30b9123bcb49bf4e
Maxim Rysevets <root@effcore.com>
2019-03-27 17:40:12 +0300

    sprint_510_96

--- system/module_core/backend/Events_Module_Update.php
+++ system/module_core/backend/Events_Module_Update.php
@@ -5,10 +5,16 @@
   ##################################################################
 
 namespace effcore\modules\core {
+          use \effcore\entity;
+          use \effcore\message;
+          use \effcore\text;
           abstract class events_module_update {
 
   static function on_update_1000($update) {
-    return true;
+    $entity = entity::get('message');
+    if ($entity->install())
+         {message::insert(new text('Entity %%_name was installed.',     ['name' => $entity->name]));          return true; }
+    else {message::insert(new text('Entity %%_name was not installed!', ['name' => $entity->name]), 'error'); return false;}
   }
 
 }}
