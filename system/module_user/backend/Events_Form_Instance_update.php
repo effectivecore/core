0397d084238befed4053609df8b703ae23faf5c7
Maxim Rysevets <root@effcore.com>
2019-04-06 12:42:18 +0300

    sprint_510_170

--- system/module_user/backend/Events_Form_Instance_update.php
+++ system/module_user/backend/Events_Form_Instance_update.php
@@ -8,9 +8,26 @@ namespace effcore\modules\user {
           use \effcore\entity;
           use \effcore\field_email;
           use \effcore\field_nick;
+          use \effcore\field_password;
+          use \effcore\instance;
           use \effcore\message;
           use \effcore\page;
+          use \effcore\translation;
           abstract class events_form_instance_update {
+
+  static function on_init($form, $items) {
+    $entity_name = page::current_get()->args_get('entity_name');
+    $instance_id = page::current_get()->args_get('instance_id');
+    $entity = entity::get($entity_name);
+    if ($entity_name == 'user') {
+      $field_password_current = new field_password('Current password', '', [], -50);
+      $field_password_current->build();
+      $field_password_current->name_set('password_current');
+      $form->child_select('fields')->child_insert(
+        $field_password_current, 'password_current'
+      );
+    }
+  }
 
   static function on_validate($form, $items) {
     $entity_name = page::current_get()->args_get('entity_name');
@@ -20,6 +37,18 @@ namespace effcore\modules\user {
       case 'update':
         if ($entity_name == 'user') {
           if (!$form->has_error()) {
+            $id_keys   = $entity->real_id_get();
+            $id_values = explode('+', $instance_id);
+          # check security
+            $test_password = (new instance('user',
+              array_combine($id_keys, $id_values))
+            )->select();
+            if (!hash_equals($test_password->password_hash, $items['#password_current']->value_get())) {
+              $items['#password_current']->error_set(
+                'Field "%%_title" contains incorrect value!', ['title' => translation::get($items['#password_current']->title)]
+              );
+              return;
+            }
           # test email
             if (!field_email::validate_uniqueness(
               $items['#email'],
