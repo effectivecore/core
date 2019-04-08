a2345b72c4b40d676af82c5d86b6e75a1c664209
Maxim Rysevets <root@effcore.com>
2019-04-08 23:15:33 +0300

    sprint_510_188

---
+++ system/module_develop/backend/Events_Page_Trees_static.php
@@ -0,0 +1,41 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2019 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\develop {
+          use \effcore\core;
+          use \effcore\tabs;
+          use \effcore\tree;
+          use \effcore\url;
+          abstract class events_page_trees_static {
+
+  static function on_page_init($page) {
+    $trees = tree::all_get();
+    $id = $page->args_get('id');
+    core::array_sort_by_title($trees);
+    if (!isset($trees[$id])) url::go($page->args_get('base').'/select/'.reset($trees)->id);
+    foreach ($trees as $c_tree) {
+      tabs::item_insert(                 $c_tree->title,
+        'trees_static_select_'.          $c_tree->id,
+        'trees_static_select', 'select/'.$c_tree->id, null, ['class' => [
+                  'trees-static-select-'.$c_tree->id =>
+                  'trees-static-select-'.$c_tree->id]]);
+    }
+  }
+
+  static function on_show_block_tree($page) {
+    $id = $page->args_get('id');
+    if ($id) {
+      $tree = clone tree::get($id);
+      $tree->build();
+      $tree = core::deep_clone($tree, ['effcore\\tree_item' => 'effcore\\tree_item_managed']);
+      $tree->attribute_delete('class');
+      $tree->attribute_insert('class', ['managed' => 'managed']);
+      $tree->title_state = 'cutted';
+      return $tree;
+    }
+  }
+
+}}
