d0135ee0fdf30f4f1555b7147b9e924a3c06bd0c
Maxim Rysevets <root@effcore.com>
2019-04-12 13:44:08 +0300

    sprint_510_210

--- system/module_core/backend/Core.php
+++ system/module_core/backend/Core.php
@@ -217,11 +217,11 @@ namespace effcore {
     foreach ((array)$data as $c_name => $c_value) {
       if ($is_xml_style && $c_value === true) $c_value = $c_name;
       switch (gettype($c_value)) {
-        case 'NULL'   :                                                                                                                                                              break;
-        case 'boolean': if ($c_value) $result[] = $key_wrapper.$c_name.$key_wrapper;                                                                                                 break;
-        case 'array'  :               $result[] = $key_wrapper.$c_name.$key_wrapper.'='.$value_wrapper.implode(' ', $c_value).                                       $value_wrapper; break;
-        case 'object' :               $result[] = $key_wrapper.$c_name.$key_wrapper.'='.$value_wrapper.(method_exists($c_value, 'render') ? $c_value->render() : '').$value_wrapper; break;
-        default       :               $result[] = $key_wrapper.$c_name.$key_wrapper.'='.$value_wrapper.$c_value.                                                     $value_wrapper; break;
+        case 'NULL'   :                                                                                                                                                                                          break;
+        case 'boolean': if ($c_value) $result[] = $key_wrapper.$c_name.$key_wrapper;                                                                                                                             break;
+        case 'array'  :               $result[] = $key_wrapper.$c_name.$key_wrapper.'='.$value_wrapper.str_replace('"', '&quot;',                         implode(' ', $c_value)               ).$value_wrapper; break;
+        case 'object' :               $result[] = $key_wrapper.$c_name.$key_wrapper.'='.$value_wrapper.str_replace('"', '&quot;', (method_exists($c_value, 'render') ? $c_value->render() : '')).$value_wrapper; break;
+        default       :               $result[] = $key_wrapper.$c_name.$key_wrapper.'='.$value_wrapper.str_replace('"', '&quot;',                                      $c_value                ).$value_wrapper; break;
       }
     }
     if ($join_part) return implode($join_part, $result);
