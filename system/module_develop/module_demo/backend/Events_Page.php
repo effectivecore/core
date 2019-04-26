a67a442dcdc13624396f855d71b0e032f9611b18
Maxim Rysevets <root@effcore.com>
2019-04-27 00:30:32 +0300

    sprint_510_323

--- system/module_develop/module_demo/backend/Events_Page.php
+++ system/module_develop/module_demo/backend/Events_Page.php
@@ -5,6 +5,7 @@
   ##################################################################
 
 namespace effcore\modules\demo {
+          use \effcore\tree_item;
           abstract class events_page {
 
   static function on_before_render($page, $template) {
@@ -20,6 +21,19 @@ namespace effcore\modules\demo {
   }
 
   static function on_selection_after_build($selection) {
+  }
+
+  static function on_tree_before_build($tree) {
+    if ($tree->id == 'demo_nosql') {
+      tree_item::insert('item #1.2.3 (from code)', 'demo_item_1_2_3','demo_item_1_2', 'demo_nosql', '/develop/demo/embedded/trees/item_1/item_1_2/item_1_2_3', null, ['class' => ['demo-item-1-2-3' => 'demo-item-1-2-3']]);
+      tree_item::insert('item #3 (from code)',     'demo_item_3',     null,           'demo_nosql', '/develop/demo/embedded/trees/item_3',                     null, ['class' => ['demo-item-3'     => 'demo-item-3'    ]]);
+    } elseif ($tree->id == 'demo_sql') {
+      tree_item::insert('item #1.2.3 (from code)', '6',               '3',            'demo_sql',   '/develop/demo/embedded/trees/item_1/item_1_2/item_1_2_3', null, ['class' => ['demo-item-1-2-3' => 'demo-item-1-2-3']]);
+      tree_item::insert('item #3 (from code)',     '9',               null,           'demo_sql',   '/develop/demo/embedded/trees/item_3',                     null, ['class' => ['demo-item-3'     => 'demo-item-3'    ]]);
+    }
+  }
+
+  static function on_tree_after_build($tree) {
   }
 
 }}
