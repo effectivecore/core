de8ad69bb112f2c29099734c05920cff3a518f67
Maxim Rysevets <root@effcore.com>
2019-04-27 14:48:28 +0300

    sprint_510_334

--- system/module_develop/backend/Events_Page_Events.php
+++ system/module_develop/backend/Events_Page_Events.php
@@ -12,43 +12,34 @@ namespace effcore\modules\develop {
           use \effcore\text_simple;
           abstract class events_page_events {
 
-  static function on_show_block_events_registered_types($page) {
-    $title = new markup('h2', [], 'Registered event types');
-    $decorator = new decorator('table');
-    $decorator->id = 'events_registered_types';
-    $decorator->result_attributes = ['class' => ['compact' => 'compact']];
+  static function on_show_block_events_registered($page) {
+    $ret_title = new markup('h2', [], 'Registered event types');
+    $reh_title = new markup('h2', [], 'Registered event handlers');
+    $ret_decorator = new decorator('table');
+    $reh_decorator = new decorator('table');
+    $ret_decorator->id = 'events_registered_types';
+    $reh_decorator->id = 'events_registered_handlers';
+    $ret_decorator->result_attributes = ['class' => ['compact' => 'compact']];
+    $reh_decorator->result_attributes = ['class' => ['compact' => 'compact']];
     $events = event::get_all();
     ksort($events);
     foreach ($events as $c_event_type => $c_events) {
-      $decorator->data[] = [
-        'type' => ['value' => new text_simple($c_event_type), 'title' => 'Type'],
-      ];
-    }
-    return new block('', ['data-id' => 'events_registered_types'], [
-      $title,
-      $decorator
-    ]);
-  }
-
-  static function on_show_block_events_registered_handlers($page) {
-    $title = new markup('h2', [], 'Registered event handlers');
-    $decorator = new decorator('table');
-    $decorator->id = 'events_registered_handlers';
-    $decorator->result_attributes = ['class' => ['compact' => 'compact']];
-    foreach (event::get_all() as $c_event_type => $c_events) {
+      $ret_decorator->data[] = ['type' => ['value' => new text_simple($c_event_type), 'title' => 'Type']];
       foreach ($c_events as $c_event) {
-        $decorator->data[] = [
-          'type'      => ['value' => new text_simple($c_event_type),       'title' => 'Type'     ],
+        $reh_decorator->data[] = [
+          'type'      => ['value' => new text_simple($c_event_type      ), 'title' => 'Type'     ],
           'module_id' => ['value' => new text_simple($c_event->module_id), 'title' => 'Module ID'],
-          'for_id'    => ['value' => new text_simple($c_event->for),       'title' => 'For ID'   ],
-          'handler'   => ['value' => new text_simple($c_event->handler),   'title' => 'Handler'  ],
-          'weight'    => ['value' => new text_simple($c_event->weight),    'title' => 'Weight'   ]
+          'for_id'    => ['value' => new text_simple($c_event->for      ), 'title' => 'For ID'   ],
+          'handler'   => ['value' => new text_simple($c_event->handler  ), 'title' => 'Handler'  ],
+          'weight'    => ['value' => new text_simple($c_event->weight   ), 'title' => 'Weight'   ]
         ];
       }
     }
     return new block('', ['data-id' => 'events_registered_handlers'], [
-      $title,
-      $decorator
+      $ret_title,
+      $ret_decorator,
+      $reh_title,
+      $reh_decorator
     ]);
   }
 
