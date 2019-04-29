70258c23cec44a3986186c8e2df1a4db06d50d72
Maxim Rysevets <root@effcore.com>
2019-04-29 19:09:51 +0300

    sprint_510_347

--- system/module_page/backend/Events_Page_Decoration.php
+++ system/module_page/backend/Events_Page_Decoration.php
@@ -7,19 +7,20 @@
 namespace effcore\modules\page {
           use \effcore\color;
           use \effcore\core;
+          use \effcore\page;
           use \effcore\tabs_item;
           use \effcore\url;
           abstract class events_page_decoration {
 
-  static function on_page_init($page) {
+  static function on_tab_before_build($tab) {
     $presets = color::preset_get_all();
-    $type = $page->args_get('type');
-    $id   = $page->args_get('id');
+    $type = page::get_current()->args_get('type');
+    $id   = page::get_current()->args_get('id');
     core::array_sort_by_title($presets);
-    if ($type == null)      url::go($page->args_get('base').'/colors');
-    if ($type == 'presets') url::go($page->args_get('base').'/presets/'.reset($presets)->id);
+    if ($type == null)      url::go(page::get_current()->args_get('base').'/colors');
+    if ($type == 'presets') url::go(page::get_current()->args_get('base').'/presets/'.reset($presets)->id);
     if (strpos($type, 'presets/') === 0 && !isset($presets[$id])) {
-      url::go($page->args_get('base').'/presets/'.reset($presets)->id);
+      url::go(page::get_current()->args_get('base').'/presets/'.reset($presets)->id);
     }
     foreach ($presets as $c_preset) {
       tabs_item::insert(      $c_preset->title,
