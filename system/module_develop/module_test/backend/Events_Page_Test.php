f5c2c7e8372f3830bfc7ac591b2e0575debe8619
Maxim Rysevets <root@effcore.com>
2019-04-29 19:16:31 +0300

    sprint_510_348

--- system/module_develop/module_test/backend/Events_Page_Test.php
+++ system/module_develop/module_test/backend/Events_Page_Test.php
@@ -6,16 +6,17 @@
 
 namespace effcore\modules\test {
           use \effcore\core;
+          use \effcore\page;
+          use \effcore\tabs_item;
           use \effcore\test;
           use \effcore\url;
-          use \effcore\tabs_item;
           abstract class events_page_test {
 
-  static function on_page_init($page) {
+  static function on_tab_before_build($tab) {
     $tests = test::get_all(false);
-    $id = $page->args_get('id');
+    $id = page::get_current()->args_get('id');
     core::array_sort_by_title($tests);
-    if (!isset($tests[$id])) url::go($page->args_get('base').'/'.reset($tests)->id);
+    if (!isset($tests[$id])) url::go(page::get_current()->args_get('base').'/'.reset($tests)->id);
     foreach ($tests as $c_test) {
       tabs_item::insert($c_test->title,
         'test_execute_'.$c_test->id,
