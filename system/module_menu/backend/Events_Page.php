3533bc6b1c4c516e7fdc898fe94811570000c56f
Maxim Rysevets <root@effcore.com>
2019-04-29 18:23:06 +0300

    sprint_510_345

--- system/module_menu/backend/Events_Page.php
+++ system/module_menu/backend/Events_Page.php
@@ -5,12 +5,23 @@
   ##################################################################
 
 namespace effcore\modules\menu {
+          use \effcore\entity;
+          use \effcore\tabs_item;
           use \effcore\tree;
           abstract class events_page {
 
   static function on_show_block_tree_sql($page, $args) {
     if (!empty($args['id_tree'])) {
       return tree::select($args['id_tree']);
+    }
+  }
+
+  static function on_tab_before_build($tab) {
+    foreach (entity::get('tree')->instances_select() as $c_tree) {
+      tabs_item::insert($c_tree->title,
+        'manage_instances_menu_tree_item_'.$c_tree->id,
+        'manage_instances_menu_tree_item',
+        'manage_instances', 'menu/tree_item/'.$c_tree->id);
     }
   }
 
