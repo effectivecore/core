7e514ac7b977de990625b668423f2bb23f709fe3
Maxim Rysevets <root@effcore.com>
2019-05-02 13:32:53 +0300

    sprint_510_367

--- system/module_core/backend/Events_Page_Modules.php
+++ system/module_core/backend/Events_Page_Modules.php
@@ -8,7 +8,7 @@ namespace effcore\modules\core {
           use \effcore\url;
           abstract class events_page_modules {
 
-  static function on_page_init($page) {
+  static function on_before_build($page) {
     $action = $page->args_get('action');
     if ($action == null) {
       url::go($page->args_get('base').'/install');
