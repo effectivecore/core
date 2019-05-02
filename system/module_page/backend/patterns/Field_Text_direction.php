8b721cadf1127c9a79be8187e57a0f328d8f3dd8
Maxim Rysevets <root@effcore.com>
2019-05-02 18:52:34 +0300

    sprint_510_373

---
+++ system/module_page/backend/patterns/Field_Text_direction.php
@@ -0,0 +1,24 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2019 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore {
+          class field_text_direction extends field_select {
+
+  public $title = 'Text direction';
+  public $attributes = ['data-type' => 'text_direction'];
+  public $element_attributes = [
+    'name'     => 'text_direction',
+    'required' => true
+  ];
+
+  function build() {
+    parent::build();
+    $this->option_insert('- no -', 'not_selected');
+    $this->option_insert('left to right', 'ltr');
+    $this->option_insert('right to left', 'rtl');
+  }
+
+}}
