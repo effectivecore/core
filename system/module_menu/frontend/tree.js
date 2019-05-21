3ebb29209c166895b23dfb4d28f67366275a6b7b
Maxim Rysevets <root@effcore.com>
2019-05-22 00:51:10 +0300

    sprint_510_443

--- system/module_menu/frontend/tree.js
+++ system/module_menu/frontend/tree.js
@@ -10,21 +10,21 @@ document.addEventListener('DOMContentLoaded', function(){
       if (draggable instanceof NodeList) {
         draggable.forEach(function(c_draggable){
           c_draggable.setAttribute('draggable', 'true');
-          c_draggable.addEventListener('dragstart', function(event){event.stopPropagation(); c_tree.setAttribute   ('data-drag-active', 'true'); c_draggable.setAttribute   ('data-drag-active', 'true'); event.dataTransfer.setData('text/plain', c_draggable.getAttribute('data-id'));}, false);
-          c_draggable.addEventListener('dragend',   function(event){event.stopPropagation(); c_tree.removeAttribute('data-drag-active'        ); c_draggable.removeAttribute('data-drag-active'        );                                                                               }, false);
+          c_draggable.addEventListener('dragstart', function(event){event.stopPropagation(); c_tree.setAttribute   ('data-drag-active', 'true'); c_draggable.setAttribute   ('data-drag-out', 'true'); event.dataTransfer.setData('text/plain', c_draggable.getAttribute('data-id'));}, false);
+          c_draggable.addEventListener('dragend',   function(event){event.stopPropagation(); c_tree.removeAttribute('data-drag-active'        ); c_draggable.removeAttribute('data-drag-out'        );                                                                               }, false);
         });
       }
 
       var droppable = c_tree.querySelectorAll('x-drop_area');
       if (droppable instanceof NodeList) {
         droppable.forEach(function(c_droppable){
-          c_droppable.addEventListener('dragover',  function(event){event.stopPropagation(); event.preventDefault(); c_droppable.setAttribute   ('data-drop-hover', 'true');}, false);
-          c_droppable.addEventListener('dragleave', function(event){event.stopPropagation(); event.preventDefault(); c_droppable.removeAttribute('data-drop-hover'        );}, false);
+          c_droppable.addEventListener('dragover',  function(event){event.stopPropagation(); event.preventDefault(); c_droppable.setAttribute   ('data-drop-in', 'true');}, false);
+          c_droppable.addEventListener('dragleave', function(event){event.stopPropagation(); event.preventDefault(); c_droppable.removeAttribute('data-drop-in'        );}, false);
           c_droppable.addEventListener('drop',      function(event){event.stopPropagation(); event.preventDefault();
             var draggable_id = event.dataTransfer.getData('text/plain'),
                 draggable = c_tree.querySelector('[data-id="'+draggable_id+'"]'),
                 droppable_type = c_droppable.getAttribute('data-type');
-            c_droppable.removeAttribute('data-drop-hover');
+            c_droppable.removeAttribute('data-drop-in');
             if (droppable_type == 'in'                                                 ) c_droppable.parentNode.querySelector('ul').appendChild(draggable                                    );
             if (droppable_type == 'before'                                             ) c_droppable.parentNode.parentNode.insertBefore        (draggable, c_droppable.parentNode            );
             if (droppable_type == 'after' && c_droppable.parentNode.nextSibling != null) c_droppable.parentNode.parentNode.insertBefore        (draggable, c_droppable.parentNode.nextSibling);
