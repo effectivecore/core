db3f818468fadecc620d88ba5401da5d3b6fba83
Maxim Rysevets <root@effcore.com>
2019-05-25 16:10:46 +0300

    sprint_510_482

--- system/module_storage/backend/Storage.php
+++ system/module_storage/backend/Storage.php
@@ -14,18 +14,20 @@ namespace effcore {
   }
 
   static function init($name) {
-    storage_nosql_files::init('storages');
-    foreach (storage_nosql_files::$data['storages'] as $c_module_id => $c_module_storages) {
-      foreach ($c_module_storages as $c_row_id => $c_storage) {
-        if ($c_storage->name == $name) {
-          static::$cache[$c_storage->name] = $c_storage;
+    if (!isset(static::$cache[$name])) {
+      storage_nosql_files::init('storages');
+      foreach (storage_nosql_files::$data['storages'] as $c_module_id => $c_module_storages) {
+        foreach ($c_module_storages as $c_row_id => $c_storage) {
+          if ($c_storage->name == $name) {
+            static::$cache[$c_storage->name] = $c_storage;
+          }
         }
       }
     }
   }
 
   static function get($name, $load = true) {
-    if (!isset(static::$cache[$name])) static::init($name);
+    static::init($name);
     if (static::$cache[$name] instanceof external_cache && $load)
         static::$cache[$name] = static::$cache[$name]->external_cache_load();
     return static::$cache[$name];
