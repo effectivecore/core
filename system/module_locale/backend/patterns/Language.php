a8a62146bd1d75756b84e6fd61cbca77f83b3552
Maxim Rysevets <root@effcore.com>
2019-05-25 16:04:02 +0300

    sprint_510_480

--- system/module_locale/backend/patterns/Language.php
+++ system/module_locale/backend/patterns/Language.php
@@ -23,30 +23,32 @@ namespace effcore {
   }
 
   static function init() {
-    foreach (storage::get('files')->select('languages') as $c_module_id => $c_languages) {
-      foreach ($c_languages as $c_row_id => $c_language) {
-        if (isset(static::$cache[$c_language->code])) console::log_insert_about_duplicate('language', $c_language->code, $c_module_id);
-        static::$cache[$c_language->code] = $c_language;
-        static::$cache[$c_language->code]->module_id = $c_module_id;
+    if (static::$cache == null) {
+      foreach (storage::get('files')->select('languages') as $c_module_id => $c_languages) {
+        foreach ($c_languages as $c_row_id => $c_language) {
+          if (isset(static::$cache[$c_language->code])) console::log_insert_about_duplicate('language', $c_language->code, $c_module_id);
+          static::$cache[$c_language->code] = $c_language;
+          static::$cache[$c_language->code]->module_id = $c_module_id;
+        }
       }
-    }
-    foreach (storage::get('files')->select('plurals') as $c_module_id => $c_plurals_by_module) {
-      foreach ($c_plurals_by_module as $c_plurals_by_language) {
-        foreach ($c_plurals_by_language->data as $c_plural_name => $c_plural_info) {
-          if (isset(static::$cache[$c_plurals_by_language->code]))
-                    static::$cache[$c_plurals_by_language->code]->plurals[$c_plural_name] = $c_plural_info;
+      foreach (storage::get('files')->select('plurals') as $c_module_id => $c_plurals_by_module) {
+        foreach ($c_plurals_by_module as $c_plurals_by_language) {
+          foreach ($c_plurals_by_language->data as $c_plural_name => $c_plural_info) {
+            if (isset(static::$cache[$c_plurals_by_language->code]))
+                      static::$cache[$c_plurals_by_language->code]->plurals[$c_plural_name] = $c_plural_info;
+          }
         }
       }
     }
   }
 
   static function get($code) {
-    if    (static::$cache == null) static::init();
+    static::init();
     return static::$cache[$code];
   }
 
   static function get_all() {
-    if    (static::$cache == null) static::init();
+    static::init();
     return static::$cache;
   }
 
