b40475f0598f0e8a2f7d530a702e95f9302efc01
Maxim Rysevets <root@effcore.com>
2019-05-25 15:55:37 +0300

    sprint_510_477

--- system/module_develop/module_test/backend/patterns/Test.php
+++ system/module_develop/module_test/backend/patterns/Test.php
@@ -39,24 +39,27 @@ namespace effcore {
   }
 
   static function init() {
-    foreach (storage::get('files')->select('tests') as $c_module_id => $c_tests) {
-      foreach ($c_tests as $c_row_id => $c_test) {
-        if (isset(static::$cache[$c_test->id])) console::log_insert_about_duplicate('test', $c_test->id, $c_module_id);
-        static::$cache[$c_test->id] = $c_test;
-        static::$cache[$c_test->id]->module_id = $c_module_id;
+    if (static::$cache == null) {
+      foreach (storage::get('files')->select('tests') as $c_module_id => $c_tests) {
+        foreach ($c_tests as $c_row_id => $c_test) {
+          if (isset(static::$cache[$c_test->id])) console::log_insert_about_duplicate('test', $c_test->id, $c_module_id);
+          static::$cache[$c_test->id] = $c_test;
+          static::$cache[$c_test->id]->module_id = $c_module_id;
+        }
       }
     }
   }
 
   static function get($id, $load = true) {
-    if (static::$cache == null) static::init();
+    static::init();
+    if (isset(static::$cache[$id]) == false) return;
     if (static::$cache[$id] instanceof external_cache && $load)
         static::$cache[$id] = static::$cache[$id]->external_cache_load();
     return static::$cache[$id];
   }
 
   static function get_all($load = true) {
-    if (static::$cache == null) static::init();
+    static::init();
     if ($load)
       foreach (static::$cache as &$c_item)
         if ($c_item instanceof external_cache)
