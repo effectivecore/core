4ad5f7c3ca6a71668d5c026f3f66081d2d860a69
Maxim Rysevets <root@effcore.com>
2019-05-25 12:41:21 +0300

    sprint_510_460

---
+++ system/module_menu/backend/Events_Form_Instance_update.php
@@ -0,0 +1,13 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2019 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\menu {
+          abstract class events_form_instance_update {
+
+  static function on_init($form, $items) {
+  }
+
+}}
