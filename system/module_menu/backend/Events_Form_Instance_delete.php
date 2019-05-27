2022474a9ab46b297f098257c679d77f66a45980
Maxim Rysevets <root@effcore.com>
2019-05-27 18:55:26 +0300

    sprint_510_488

--- system/module_menu/backend/Events_Form_Instance_delete.php
+++ system/module_menu/backend/Events_Form_Instance_delete.php
@@ -5,9 +5,15 @@
   ##################################################################
 
 namespace effcore\modules\menu {
-          abstract class events_form_instance_update {
+          use \effcore\page;
+          abstract class events_form_instance_delete {
 
   static function on_init($form, $items) {
+    $entity_name = page::get_current()->args_get('entity_name');
+    $instance_id = page::get_current()->args_get('instance_id');
+    if ($entity_name == 'tree_item') {
+
+    }
   }
 
 }}
