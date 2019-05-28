69c51062d167bd842936b89ce69cbf9e62696ff6
Maxim Rysevets <root@effcore.com>
2019-05-28 17:44:11 +0300

    sprint_510_1000

--- system/module_menu/backend/Events_Form_Instance_delete.php
+++ system/module_menu/backend/Events_Form_Instance_delete.php
@@ -8,7 +8,10 @@ namespace effcore\modules\menu {
           use \effcore\entity;
           use \effcore\instance;
           use \effcore\markup;
+          use \effcore\message;
           use \effcore\page;
+          use \effcore\text;
+          use \effcore\translation;
           use \effcore\tree_item;
           abstract class events_form_instance_delete {
 
@@ -19,12 +22,11 @@ namespace effcore\modules\menu {
       $tree_item = tree_item::select($instance_id, $form->_instance->id_tree);
       $tree_item->url = '';
       $tree_item->build();
-      $form->_ids = [$tree_item->id];
       foreach ($tree_item->children_select_recursive() as $c_child) {
-        $form->_ids[] = $c_child->id;
+        $form->_related[] = $c_child->id;
         $c_child->url = '';
       }
-      if (count($form->_ids) > 1)
+      if (isset($form->_related))
            $question = new markup('p', [], 'Delete all items below?');
       else $question = new markup('p', [], 'Delete item?');
       $items['info']->child_insert($question,                        'question');
@@ -33,17 +35,20 @@ namespace effcore\modules\menu {
   }
 
   static function on_submit($form, $items) {
+    $entity = entity::get('tree_item');
     switch ($form->clicked_button->value_get()) {
       case 'delete':
-        if (isset($form->_ids)) {
+        if (isset($form->_related)) {
           $in_values = [];
-          foreach ($form->_ids as $c_id) $in_values['in_value_'.$c_id.'_!v'] = $c_id;
-          entity::get('tree_item')->instances_delete(['conditions' => [
+          foreach ($form->_related as $c_id) $in_values['in_value_'.$c_id.'_!v'] = $c_id;
+          $result = entity::get('tree_item')->instances_delete(['conditions' => [
             'id_!f'    => 'id',
             'in_begin' => 'in (',
             'in_!,'    => $in_values,
             'in_end'   => ')'
           ]]);
+          if ($result) message::insert_to_storage(new text('Related items of type "%%_name" with id = "%%_id" was deleted.',     ['name' => translation::get($entity->title), 'id' => implode(', ', $form->_related)])         );
+          else         message::insert_to_storage(new text('Related items of type "%%_name" with id = "%%_id" was not deleted!', ['name' => translation::get($entity->title), 'id' => implode(', ', $form->_related)]), 'error');
         }
         break;
     }
