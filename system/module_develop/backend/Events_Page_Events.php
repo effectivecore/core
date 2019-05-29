011906335d8c245a195cdac7d4d00ebad763743e
Maxim Rysevets <root@effcore.com>
2019-05-29 14:36:57 +0300

    sprint_510_1008

--- system/module_develop/backend/Events_Page_Events.php
+++ system/module_develop/backend/Events_Page_Events.php
@@ -14,29 +14,17 @@ namespace effcore\modules\develop {
           abstract class events_page_events {
 
   static function on_show_block_events($page) {
+    $targets = new markup('x-targets');
+    $report = new node();
     $events = event::get_all();
     ksort($events);
-  # ─────────────────────────────────────────────────────────────────────
-  # prepare report for each registered event types
-  # ─────────────────────────────────────────────────────────────────────
-    $ret_decorator = new decorator('table');
-    $ret_decorator->id = 'events_registered_types';
-    $ret_decorator->result_attributes = ['class' => ['compact' => 'compact']];
     foreach ($events as $c_event_type => $c_events) {
-      $ret_decorator->data[] = [
-        'type' => ['value' => new markup('a', ['href' => '#type_'.$c_event_type], $c_event_type), 'title' => 'Registered event types']
-      ];
-    }
-  # ─────────────────────────────────────────────────────────────────────
-  # prepare report for each registered event handler
-  # ─────────────────────────────────────────────────────────────────────
-    $reh_report = new node();
-    foreach ($events as $c_event_type => $c_events) {
+      $targets->child_insert(new markup('a', ['href' => '#type_'.$c_event_type], $c_event_type));
       $c_decorator = new decorator('table');
       $c_decorator->id = 'events_registered_handlers_'.$c_event_type;
       $c_decorator->result_attributes = ['class' => ['compact' => 'compact']];
-      $reh_report->child_insert(new markup('h2', ['id' => 'type_'.$c_event_type], $c_event_type), $c_event_type.'_header'   );
-      $reh_report->child_insert($c_decorator,                                                     $c_event_type.'_decorator');
+      $report->child_insert(new markup('h2', ['id' => 'type_'.$c_event_type], $c_event_type), $c_event_type.'_header'   );
+      $report->child_insert($c_decorator,                                                     $c_event_type.'_decorator');
       foreach ($c_events as $c_event) {
         $c_decorator->data[] = [
           'module_id' => ['value' => new text_simple($c_event->module_id), 'title' => 'Module ID'],
@@ -47,8 +35,8 @@ namespace effcore\modules\develop {
       }
     }
     return new block('', ['data-id' => 'events_registered'], [
-      $ret_decorator,
-      $reh_report
+      $targets,
+      $report
     ]);
   }
 
