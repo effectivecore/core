4d57e844c7a426a97e5477fd5e269f257a05994d
Maxim Rysevets <root@effcore.com>
2019-05-31 14:24:05 +0300

    sprint_510_1030

--- system/module_develop/backend/Events_Module.php
+++ system/module_develop/backend/Events_Module.php
@@ -5,7 +5,9 @@
   ##################################################################
 
 namespace effcore\modules\develop {
+          use \effcore\frontend;
           use \effcore\module;
+          use \effcore\user;
           abstract class events_module {
 
   static function on_enable() {
@@ -16,6 +18,18 @@ namespace effcore\modules\develop {
   static function on_disable() {
     $module = module::get('develop');
     $module->disable();
+  }
+
+  static function on_start() {
+    $user = user::get_current();
+    $settings = module::settings_get('page');
+    if (($settings->console_visibility == 'show_for_admin' && isset($user->roles['admins'])) ||
+        ($settings->console_visibility == 'show_for_everyone')) {
+      frontend::insert('console', null, 'styles', [
+        'file'       => '/system/module_develop/frontend/develop.cssd',
+        'attributes' => ['rel' => 'stylesheet', 'media' => 'all'
+      ]]);
+    }
   }
 
 }}
