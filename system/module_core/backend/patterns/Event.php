fbbbd2b9678b9c7bdbcd154cf9ea327e6415ec54
Maxim Rysevets <root@effcore.com>
2019-06-01 23:30:10 +0300

    sprint_510_1039

--- system/module_core/backend/patterns/Event.php
+++ system/module_core/backend/patterns/Event.php
@@ -80,13 +80,13 @@ namespace effcore {
         if ($for == null          ||
             $for == $c_event->for ||
                     $c_event->for == null) {
+          $c_log = console::log_insert('event', 'call', ltrim($c_event->handler, '\\'));
           timer::tap('event call: '.$type);
           $result[$c_event->handler][] = $c_return = call_user_func_array($c_event->handler,                      $args);
           if ($on_after_step)                        call_user_func_array($on_after_step, ['event' => $c_event] + $args);
           timer::tap('event call: '.$type);
-          console::log_insert('event', 'call', ltrim($c_event->handler, '\\'), $c_return ? 'ok' : '-',
-            timer::period_get('event call: '.$type, -1, -2)
-          );
+          $c_log->time = timer::period_get('event call: '.$type, -1, -2);
+          $c_log->value = $c_return ? 'ok' : '-';
         }
       }
     }
