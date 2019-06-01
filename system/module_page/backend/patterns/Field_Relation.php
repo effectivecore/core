1597a6c037769e6fd41197f87f93c932ad3daae7
Maxim Rysevets <root@effcore.com>
2019-06-02 00:20:45 +0300

    sprint_510_1041

--- system/module_page/backend/patterns/Field_Relation.php
+++ system/module_page/backend/patterns/Field_Relation.php
@@ -14,6 +14,7 @@ namespace effcore {
 # ─────────────────────────────────────────────────────────────────────
   public $entity_name;
   public $entity_field_id_name;
+  public $entity_field_id_parent_name;
   public $entity_field_title_name;
   public $query_params = [];
 
@@ -22,11 +23,32 @@ namespace effcore {
     $this->option_insert('- no -', 'not_selected');
     $entity = entity::get($this->entity_name);
     $instances = $entity->instances_select($this->query_params);
-    foreach ($instances as $c_instance) {
-      $this->option_insert(
-        $c_instance->{$this->entity_field_title_name},
-        $c_instance->{$this->entity_field_id_name   }
-      );
+    if ($this->entity_field_id_parent_name) {
+      $tree_id = 'field_relation-'.$this->name_get();
+      $tree = tree::insert('', $tree_id);
+      foreach ($instances as $c_instance) {
+        $c_tree_item = tree_item::insert(
+          $c_instance->{$this->entity_field_title_name    },           $tree_id.'-'.
+          $c_instance->{$this->entity_field_id_name       },
+          $c_instance->{$this->entity_field_id_parent_name} !== null ? $tree_id.'-'.
+          $c_instance->{$this->entity_field_id_parent_name} :   null,  $tree_id);
+        $c_tree_item->id_real = $c_instance->{$this->entity_field_id_name};
+      }
+      $tree->build();
+      foreach ($tree->children_select_recursive() as $c_npath => $c_element) {
+        $c_depth = count_chars($c_npath, 1)[ord('/')] ?? 0;
+        $this->option_insert(
+          str_repeat('- ', $c_depth + 1).$c_element->title,
+          $c_element->id_real
+        );
+      }
+    } else {
+      foreach ($instances as $c_instance) {
+        $this->option_insert(
+          $c_instance->{$this->entity_field_title_name},
+          $c_instance->{$this->entity_field_id_name   }
+        );
+      }
     }
   }
 
