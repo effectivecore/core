0446b1ab51bb1a663e85cb452942ae1f58bcff59
Maxim Rysevets <root@effcore.com>
2019-06-03 17:46:46 +0300

    sprint_510_1043

--- system/module_menu/backend/Events_Form_Instance_delete.php
+++ system/module_menu/backend/Events_Form_Instance_delete.php
@@ -20,7 +20,9 @@ namespace effcore\modules\menu {
     $instance_id = page::get_current()->args_get('instance_id');
     $entity = entity::get('tree_item');
     if ($entity_name == 'tree_item' && $form->_instance) {
-      $tree_item = tree_item::select($instance_id, $form->_instance->id_tree);
+      $tree_item = tree_item::select(
+        $form->_instance->id,
+        $form->_instance->id_tree);
       $tree_item->url = '';
       $tree_item->build();
       $tree_item_children = $tree_item->children_select_recursive();
