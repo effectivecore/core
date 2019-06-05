0ad0d755c41367e6f4f19d7aaba07368d6f90e45
Maxim Rysevets <root@effcore.com>
2019-06-05 16:45:55 +0300

    sprint_510_2006

---
+++ system/module_develop/backend/Events_Page_Data_NoSQL.php
@@ -0,0 +1,13 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2019 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\develop {
+          abstract class events_page_nosql_data {
+
+  static function on_show_block_nosql_data() {
+  }
+
+}}
