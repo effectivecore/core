5804df635bb157c34738b4de05dea6d6d2299456
Maxim Rysevets <root@effcore.com>
2019-06-05 17:02:02 +0300

    sprint_510_2009

--- system/module_develop/backend/Events_Page_Data_NoSQL.php
+++ system/module_develop/backend/Events_Page_Data_NoSQL.php
@@ -12,6 +12,7 @@ namespace effcore\modules\develop {
           use \effcore\file;
           use \effcore\markup;
           use \effcore\node;
+          use \effcore\template;
           use \effcore\text_simple;
           abstract class events_page_nosql_data {
 
@@ -59,6 +60,23 @@ namespace effcore\modules\develop {
       ];
     }
     return new block('', ['data-id' => 'file_types_registered'], [
+      $decorator
+    ]);
+  }
+
+  static function on_show_block_templates($page) {
+    $decorator = new decorator('table');
+    $decorator->id = 'templates_registered';
+    $templates = template::get_all();
+    ksort($templates);
+    foreach ($templates as $c_template) {
+      $decorator->data[] = [
+        'name'      => ['value' => new text_simple($c_template->name     ), 'title' => 'Name'     ],
+        'type'      => ['value' => new text_simple($c_template->type     ), 'title' => 'Type'     ],
+        'module_id' => ['value' => new text_simple($c_template->module_id), 'title' => 'Module ID'],
+      ];
+    }
+    return new block('', ['data-id' => 'templates_registered'], [
       $decorator
     ]);
   }
