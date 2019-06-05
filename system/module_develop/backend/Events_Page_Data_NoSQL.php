aa1776e7a02c35420f8004cc0608655cd4a901dd
Maxim Rysevets <root@effcore.com>
2019-06-05 17:06:31 +0300

    sprint_510_2010

--- system/module_develop/backend/Events_Page_Data_NoSQL.php
+++ system/module_develop/backend/Events_Page_Data_NoSQL.php
@@ -14,6 +14,7 @@ namespace effcore\modules\develop {
           use \effcore\node;
           use \effcore\template;
           use \effcore\text_simple;
+          use \effcore\token;
           abstract class events_page_nosql_data {
 
   static function on_show_block_nosql_data() {
@@ -77,6 +78,24 @@ namespace effcore\modules\develop {
       ];
     }
     return new block('', ['data-id' => 'templates_registered'], [
+      $decorator
+    ]);
+  }
+
+  static function on_show_block_tokens($page) {
+    $decorator = new decorator('table');
+    $decorator->id = 'tokens_registered';
+    $tokens = token::get_all();
+    ksort($tokens);
+    foreach ($tokens as $c_row_id => $c_token) {
+      $decorator->data[] = [
+        'rowid'     => ['value' => new text_simple($c_row_id          ), 'title' => 'Row ID'   ],
+        'match'     => ['value' => new text_simple($c_token->match    ), 'title' => 'Match'    ],
+        'type'      => ['value' => new text_simple($c_token->type     ), 'title' => 'Type'     ],
+        'module_id' => ['value' => new text_simple($c_token->module_id), 'title' => 'Module ID']
+      ];
+    }
+    return new block('', ['data-id' => 'tokens_registered'], [
       $decorator
     ]);
   }
