442853f740705e87535e917b11beed21732ed148
Maxim Rysevets <root@effcore.com>
2019-06-06 23:45:09 +0300

    sprint_510_2017

---
+++ system/module_page/backend/patterns/Layout.php
@@ -0,0 +1,12 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2019 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore {
+          class layout extends node {
+
+  public $title;
+
+}}
