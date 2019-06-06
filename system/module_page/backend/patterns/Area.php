e5fa06d377766b96ebb394262f90f35f649df4d4
Maxim Rysevets <root@effcore.com>
2019-06-06 16:39:16 +0300

    sprint_510_2016

---
+++ system/module_page/backend/patterns/Area.php
@@ -0,0 +1,12 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2019 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore {
+          class area extends node {
+
+  public $type; # null | table | row | col
+
+}}
