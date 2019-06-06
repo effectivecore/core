7690b30061d93a71fff8e500a4ceee6854600248
Maxim Rysevets <root@effcore.com>
2019-06-06 15:18:40 +0300

    sprint_510_2014

---
+++ system/module_page/backend/Events_Form_Layouts.php
@@ -0,0 +1,22 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2019 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\page {
+          use \effcore\message;
+          abstract class events_form_layouts {
+
+  static function on_init($form, $items) {
+  }
+
+  static function on_submit($form, $items) {
+    switch ($form->clicked_button->value_get()) {
+      case 'save':
+        message::insert('The changes was saved.');
+        break;
+    }
+  }
+
+}}
