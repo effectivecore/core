64f33d806810a3ecc7bc5e723e1290cd03577025
Maxim Rysevets <root@effcore.com>
2019-06-15 13:52:03 +0300

    sprint_510_2073

--- system/module_page/backend/patterns/Group_License_agreement.php
+++ system/module_page/backend/patterns/Group_License_agreement.php
@@ -12,17 +12,20 @@ namespace effcore {
   public $agree_title = 'I accept the terms of the license agreement';
 
   function build() {
-    parent::build();
-    $language = language::get(language::code_get_current());
-    $license = new file($language->license_path ?
-             dir_system.$language->license_path : dir_root.'license.md');
-    $markup_license = new markup('x-document', ['class' => ['license' => 'license']], markdown::markdown_to_markup($license->load()));
-    $switcher_agree = new field_switcher($this->agree_title);
-    $switcher_agree->build();
-    $switcher_agree->name_set('is_agree');
-    $switcher_agree->required_set(true);
-    $this->child_insert($markup_license, 'license' );
-    $this->child_insert($switcher_agree, 'is_agree');
+    if (!$this->is_builded) {
+         $this->is_builded = true;
+      parent::build();
+      $language = language::get(language::code_get_current());
+      $license_file = new file($language->license_path ?
+                    dir_system.$language->license_path : dir_root.'license.md');
+      $markup_license = new markup('x-document', ['class' => ['license' => 'license']], markdown::markdown_to_markup($license_file->load()));
+      $switcher_agree = new field_switcher($this->agree_title);
+      $switcher_agree->build();
+      $switcher_agree->name_set('is_agree');
+      $switcher_agree->required_set(true);
+      $this->child_insert($markup_license, 'license' );
+      $this->child_insert($switcher_agree, 'is_agree');
+    }
   }
 
 }}
