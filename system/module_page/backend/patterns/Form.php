cb3e902e9078afa34078af6aafa7674e9157d932
Maxim Rysevets <root@effcore.com>
2019-06-20 12:48:46 +0300

    sprint_510_2115

--- system/module_page/backend/patterns/Form.php
+++ system/module_page/backend/patterns/Form.php
@@ -142,12 +142,12 @@ namespace effcore {
     $this->items = [];
     $groups      = [];
     foreach ($this->children_select_recursive(null, '', true) as $c_npath => $c_child) {
-      if ($c_child instanceof container)         $this->items[$c_npath                                                  ] = $c_child;
-      if ($c_child instanceof button)            $this->items['~'.$c_child->value_get     ()                            ] = $c_child;
-      if ($c_child instanceof field_hidden)      $this->items['!'.$c_child->name_get      ()                            ] = $c_child;
-      if ($c_child instanceof field)             $groups     ['#'.$c_child->name_get      ()                          ][] = $c_child;
-      if ($c_child instanceof field_radiobutton) $groups     ['#'.$c_child->name_get      ().':'.$c_child->value_get()][] = $c_child;
-      if ($c_child instanceof group_mono)        $groups     ['*'.$c_child->name_get_first()                          ][] = $c_child;
+      if ($c_child instanceof container)                                $this->items[$c_npath                                                  ] = $c_child;
+      if ($c_child instanceof button)                                   $this->items['~'.$c_child->value_get     ()                            ] = $c_child;
+      if ($c_child instanceof field_hidden)                             $this->items['!'.$c_child->name_get      ()                            ] = $c_child;
+      if ($c_child instanceof field)                                    $groups     ['#'.$c_child->name_get      ()                          ][] = $c_child;
+      if ($c_child instanceof field_radiobutton)                        $groups     ['#'.$c_child->name_get      ().':'.$c_child->value_get()][] = $c_child;
+      if ($c_child instanceof group_mono && $c_child->name_get_first()) $groups     ['*'.$c_child->name_get_first()                          ][] = $c_child;
     }
     foreach ($groups as $c_name => $c_group) {
       if (count($c_group) == 1) $this->items[$c_name] = reset($c_group);
