e98673fa0ddf06a683ba8bf634c8c804d4f395fb
Maxim Rysevets <root@effcore.com>
2019-07-02 13:27:38 +0300

    sprint_510_2179

--- system/module_storage/backend/Events_Form_Instance_update.php
+++ system/module_storage/backend/Events_Form_Instance_update.php
@@ -49,7 +49,7 @@ namespace effcore\modules\storage {
           if ($items['fields']->children_select_count() == 0 || $has_enabled_fields == false) $items['~update']->disabled_set();
           if ($items['fields']->children_select_count() == 0) {
             $form->child_update(
-              'fields', new markup('x-no-result', [], 'no editable fields')
+              'fields', new markup('x-no-result', [], 'no fields')
             );
           }
         } else core::send_header_and_exit('page_not_found');
