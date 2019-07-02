0c7b63458fd7124752dc0977b0d88ebc854d57a7
Maxim Rysevets <root@effcore.com>
2019-07-02 23:05:19 +0300

    sprint_510_2182

--- system/module_storage/backend/Events_Form_Instance_insert.php
+++ system/module_storage/backend/Events_Form_Instance_insert.php
@@ -7,9 +7,13 @@
 namespace effcore\modules\storage {
           use \effcore\core;
           use \effcore\entity;
+          use \effcore\field_checkbox;
+          use \effcore\instance;
           use \effcore\markup;
+          use \effcore\message;
           use \effcore\page;
           use \effcore\text;
+          use \effcore\translation;
           use \effcore\url;
           abstract class events_form_instance_insert {
 
@@ -45,16 +49,26 @@ namespace effcore\modules\storage {
   }
 
   static function on_submit($form, $items) {
-    $base        = page::get_current()->args_get('base');
+    $back_insert = page::get_current()->args_get('back_insert');
+    $back_cancel = page::get_current()->args_get('back_cancel');
     $entity_name = page::get_current()->args_get('entity_name');
     $entity = entity::get($entity_name);
     switch ($form->clicked_button->value_get()) {
       case 'insert':
-      # @todo: make functionality
-        break;
-      case 'cancel':
-        url::go(url::back_url_get() ?: '/manage/instances/select/'.core::sanitize_id($entity->group).'/'.$entity->name);
-        break;
+        $form->_instance = new instance($entity_name);
+        foreach ($entity->fields as $c_name => $c_field) {
+          if (isset($c_field->field_class) && isset($items['#'.$c_name])) {
+            if (!empty($c_field->field_value_manual_get_if_empty) && $items['#'.$c_name]->value_get() == '') continue;
+            if (!empty($c_field->field_value_manual_get         )                                          ) continue;
+            if ($items['#'.$c_name] instanceof field_checkbox == true) $form->_instance->{$c_name} = $items['#'.$c_name]->checked_get();
+            if ($items['#'.$c_name] instanceof field_checkbox != true) $form->_instance->{$c_name} = $items['#'.$c_name]->value_get  ();
+          }
+        }
+          if ($form->_instance->insert())
+               message::insert_to_storage(new text('Item of type "%%_name" with id = "%%_id" was inserted.',     ['name' => translation::get($entity->title), 'id' => 'n/a']));
+          else message::insert_to_storage(new text('Item of type "%%_name" with id = "%%_id" was not inserted!', ['name' => translation::get($entity->title), 'id' => 'n/a']), 'warning');
+                     url::go(url::back_url_get() ?: ($back_update ?: '/manage/instances/select/'.core::sanitize_id($entity->group).'/'.$entity->name)); break;
+      case 'cancel': url::go(url::back_url_get() ?: ($back_cancel ?: '/manage/instances/select/'.core::sanitize_id($entity->group).'/'.$entity->name)); break;
     }
   }
 
