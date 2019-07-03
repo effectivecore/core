b4120527b28e38385bd2e44d7e926ca51eaceccf
Maxim Rysevets <root@effcore.com>
2019-07-03 21:32:45 +0300

    sprint_510_2192

--- system/module_page/backend/Events_Form_Instance_update.php
+++ system/module_page/backend/Events_Form_Instance_update.php
@@ -66,37 +66,40 @@ namespace effcore\modules\page {
   }
 
   static function on_submit($form, $items) {
-    $cache = $form->validation_cache_get('page_parts');
-    switch ($form->clicked_button->value_get()) {
-      case 'update':
-        $new_parts = [];
-        foreach ($cache as $c_id_area => $c_new_parts)
-          foreach ($c_new_parts as $c_id_part => $c_part)
-            $new_parts[$c_id_area][$c_id_part] = $c_part;
-        $form->_instance->parts = $new_parts ?: null;
-        break;
-      case 'cancel':
-        break;
-      default:
-        $manage_result = null;
-        $insert_result = null;
-        foreach ($form->_parts_manage as $c_part_manage) {$manage_result = group_page_part_manage::submit($c_part_manage, null, null); if ($manage_result) break;}
-        foreach ($form->_parts_insert as $c_part_insert) {$insert_result = group_page_part_insert::submit($c_part_insert, null, null); if ($insert_result) break;}
-        if ($manage_result) {
-          unset($cache[$manage_result->id_area][$manage_result->id_preset]);
-          $form->validation_data_is_persistent = true;
-          $form->validation_cache_set('page_parts', $cache);
-          message::insert(new text('Part of the page with id = "%%_id_page_part" has been deleted from the area with id = "%%_id_area".', ['id_page_part' => $manage_result->id_preset, 'id_area' => $manage_result->id_area]));
-          static::on_init($form, $items);
-          return;
-        } else if ($insert_result) {
-          $cache[$insert_result->id_area][$insert_result->id_preset] = new page_part_preset_link($insert_result->id_preset);
-          $form->validation_data_is_persistent = true;
-          $form->validation_cache_set('page_parts', $cache);
-          message::insert(new text('Part of the page with id = "%%_id_page_part" has been inserted to the area with id = "%%_id_area".', ['id_page_part' => $insert_result->id_preset, 'id_area' => $insert_result->id_area]));
-          static::on_init($form, $items);
-          return;
-        }
+    $entity_name = page::get_current()->args_get('entity_name');
+    if ($entity_name == 'page' && !empty($form->_instance)) {
+      $cache = $form->validation_cache_get('page_parts');
+      switch ($form->clicked_button->value_get()) {
+        case 'update':
+          $new_parts = [];
+          foreach ($cache as $c_id_area => $c_new_parts)
+            foreach ($c_new_parts as $c_id_part => $c_part)
+              $new_parts[$c_id_area][$c_id_part] = $c_part;
+          $form->_instance->parts = $new_parts ?: null;
+          break;
+        case 'cancel':
+          break;
+        default:
+          $manage_result = null;
+          $insert_result = null;
+          foreach ($form->_parts_manage as $c_part_manage) {$manage_result = group_page_part_manage::submit($c_part_manage, null, null); if ($manage_result) break;}
+          foreach ($form->_parts_insert as $c_part_insert) {$insert_result = group_page_part_insert::submit($c_part_insert, null, null); if ($insert_result) break;}
+          if ($manage_result) {
+            unset($cache[$manage_result->id_area][$manage_result->id_preset]);
+            $form->validation_data_is_persistent = true;
+            $form->validation_cache_set('page_parts', $cache);
+            message::insert(new text('Part of the page with id = "%%_id_page_part" has been deleted from the area with id = "%%_id_area".', ['id_page_part' => $manage_result->id_preset, 'id_area' => $manage_result->id_area]));
+            static::on_init($form, $items);
+            return;
+          } else if ($insert_result) {
+            $cache[$insert_result->id_area][$insert_result->id_preset] = new page_part_preset_link($insert_result->id_preset);
+            $form->validation_data_is_persistent = true;
+            $form->validation_cache_set('page_parts', $cache);
+            message::insert(new text('Part of the page with id = "%%_id_page_part" has been inserted to the area with id = "%%_id_area".', ['id_page_part' => $insert_result->id_preset, 'id_area' => $insert_result->id_area]));
+            static::on_init($form, $items);
+            return;
+          }
+      }
     }
   }
 
