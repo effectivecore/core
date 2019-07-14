74e503e9fcf26fa72dc68f4e65db516ad0113eb3
Maxim Rysevets <root@effcore.com>
2019-07-14 21:42:32 +0300

    sprint_510_2265

--- system/module_core/backend/Events_Form_Install.php
+++ system/module_core/backend/Events_Form_Install.php
@@ -152,8 +152,8 @@ namespace effcore\modules\core {
               event::start('on_module_enable',  $c_module->id);
             # cancel installation if an error occurred
               if (count(storage::get('sql')->errors) == 0)
-                message::insert(new text('Module "%%_title" (%%_id) has been installed.', ['title' => translation::get($c_module->title), 'id' => $c_module->id]));
-              else break;
+                   {message::insert(new text('Module "%%_title" (%%_id) was installed.',     ['title' => translation::get($c_module->title), 'id' => $c_module->id])         );}
+              else {message::insert(new text('Module "%%_title" (%%_id) was not installed!', ['title' => translation::get($c_module->title), 'id' => $c_module->id]), 'error'); break};
             }
           }
           if (count(storage::get('sql')->errors) == 0) {
