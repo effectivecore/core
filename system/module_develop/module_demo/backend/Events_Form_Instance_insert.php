ffa350612064c7aafa241cc6a07fa03295a9f39e
Maxim Rysevets <root@effcore.com>
2019-07-17 13:23:13 +0300

    sprint_510_2274

--- system/module_develop/module_demo/backend/Events_Form_Instance_insert.php
+++ system/module_develop/module_demo/backend/Events_Form_Instance_insert.php
@@ -7,6 +7,8 @@
 namespace effcore\modules\demo {
           use \effcore\entity;
           use \effcore\page;
+          use \effcore\text_multiline;
+          use \effcore\translation;
           abstract class events_form_instance_insert {
 
   static function on_validate($form, $items) {
@@ -14,6 +16,18 @@ namespace effcore\modules\demo {
     $entity = entity::get($entity_name);
     if ($entity) {
       if ($entity->name == 'demo_data_join') {
+        $id_data = $items['#id_data']->value_get();
+        if ($id_data) {
+          $result = $entity->instances_select(['conditions' => [
+            'id_data_!f' => 'id_data', '=',
+            'id_data_!v' => $id_data], 'limit' => 1]);
+          if ($result) {
+            $items['#id_data']->error_set(new text_multiline([
+              'Field "%%_title" contains the previously used value!',
+              'Only unique value is allowed.'], ['title' => translation::get($items['#id_data']->title)]
+            ));
+          }
+        }
       }
     }
   }
