c52bafe3d79d990b58e821ea7e429d3dc5717a27
Maxim Rysevets <root@effcore.com>
2019-07-21 00:11:44 +0300

    sprint_510_2308

--- system/module_develop/module_demo/backend/Events_Form_Instance_update.php
+++ system/module_develop/module_demo/backend/Events_Form_Instance_update.php
@@ -18,18 +18,20 @@ namespace effcore\modules\demo {
         $entity = entity::get($entity_name);
         if ($entity) {
           if ($entity->name == 'demo_data_join') {
-            $id_data_new = $items['#id_data']->value_get();
-            $id_data_old = $items['#id_data']->value_get_initial();
-            if ($id_data_new != $id_data_old) {
-              $result = $entity->instances_select(['conditions' => [
-                'id_data_!f' => 'id_data', 'operator' => '=',
-                'id_data_!v' => $id_data_new],
-                'limit'      => 1]);
-              if ($result) {
-                $items['#id_data']->error_set(new text_multiline([
-                  'Field "%%_title" contains the previously used combination of values!',
-                  'Only unique value is allowed.'], ['title' => translation::get($items['#id_data']->title)]
-                ));
+            if (!$form->has_error()) {
+              $id_data_new = $items['#id_data']->value_get();
+              $id_data_old = $items['#id_data']->value_get_initial();
+              if ($id_data_new != $id_data_old) {
+                $result = $entity->instances_select(['conditions' => [
+                  'id_data_!f' => 'id_data', 'operator' => '=',
+                  'id_data_!v' => $id_data_new],
+                  'limit'      => 1]);
+                if ($result) {
+                  $items['#id_data']->error_set(new text_multiline([
+                    'Field "%%_title" contains the previously used combination of values!',
+                    'Only unique value is allowed.'], ['title' => translation::get($items['#id_data']->title)]
+                  ));
+                }
               }
             }
           }
