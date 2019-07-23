31772875fbe4c5d1b1e2809c1709d032ffde2c56
Maxim Rysevets <root@effcore.com>
2019-07-23 17:52:05 +0300

    sprint_510_2314

--- system/module_develop/module_demo/backend/Events_Page_Demo.php
+++ system/module_develop/module_demo/backend/Events_Page_Demo.php
@@ -40,64 +40,82 @@ namespace effcore\modules\demo {
     tree_item::insert('item #3 (from code)',     '9',               null,           'demo_sql',   '/develop/demo/embedded/trees/item_3',                     null, [], [], -10);
   }
 
-  ################
-  ### messages ###
-  ################
-
-  static function on_show_demo_messages($page) {
-    message::insert( 'credentials',                                            'credentials'                );
-    message::insert( new text('Notice message #%%_number.',  ['number' => 1]), 'notice'                     );
-    message::insert( new text('Notice message #%%_number.',  ['number' => 2]), 'notice'                     );
-    message::insert( new text('Notice message #%%_number.',  ['number' => 3]), 'notice'                     );
-    message::insert( new text_multiline(['Ok message #%%_number.', '(default type)'], ['number' => 1], ' ') );
-    message::insert( new text('Ok message #%%_number.',      ['number' => 2])                               );
-    message::insert( new text('Ok message #%%_number.',      ['number' => 3])                               );
-    message::insert( new text('Warning message #%%_number.', ['number' => 1]), 'warning'                    );
-    message::insert( new text('Warning message #%%_number.', ['number' => 2]), 'warning'                    );
-    message::insert( new text('Warning message #%%_number.', ['number' => 3]), 'warning'                    );
-    message::insert( new text('Error message #%%_number.',   ['number' => 1]), 'error'                      );
-    message::insert( new text('Error message #%%_number.',   ['number' => 2]), 'error'                      );
-    message::insert( new text('Error message #%%_number.',   ['number' => 3]), 'error'                      );
-  }
-
   ##############
-  ### canvas ###
+  ### markup ###
   ##############
 
-  static function on_show_block_demo_canvas($page) {
-    $canvas = new canvas_svg(105, 16, 5);
-    $canvas->glyph_set('-XXX-|X---X|X---X|X---X|X---X|X---X|X---X|X---X|X---X|-XXX-',  5, 3); # 0
-    $canvas->glyph_set('----X|---X-|--X-X|-X--X|X---X|----X|----X|----X|----X|----X', 15, 3); # 1
-    $canvas->glyph_set('XXXX-|----X|----X|----X|----X|---X-|--X--|-X---|X----|XXXXX', 25, 3); # 2
-    $canvas->glyph_set('XXXXX|----X|---X-|--X--|-X---|XXXXX|----X|---X-|--X--|-X---', 35, 3); # 3
-    $canvas->glyph_set('----X|---X-|--X-X|-X--X|X---X|-XXXX|----X|----X|----X|----X', 45, 3); # 4
-    $canvas->glyph_set('-XXXX|X----|X----|X----|X----|-XXXX|----X|---X-|--X--|-X---', 55, 3); # 5
-    $canvas->glyph_set('---X-|--X--|-X---|X----|-XXX-|X---X|X---X|X---X|X---X|-XXX-', 65, 3); # 6
-    $canvas->glyph_set('XXXXX|----X|---X-|--X--|-X---|X----|X----|X----|X----|X----', 75, 3); # 7
-    $canvas->glyph_set('-XXX-|X---X|X---X|X---X|-XXX-|X---X|X---X|X---X|X---X|-XXX-', 85, 3); # 8
-    $canvas->glyph_set('-XXX-|X---X|X---X|X---X|X---X|-XXX-|----X|---X-|--X--|-X---', 95, 3); # 9
-    return new block('Canvas', ['data-id' => 'demo_canvas'], [
-      $canvas
+  static function on_show_block_demo_markup_dynamic($page) {
+  # ─────────────────────────────────────────────────────────────────────
+  # paragraph
+  # ─────────────────────────────────────────────────────────────────────
+    $paragraph_title = new markup('h3', [], 'Paragraph');
+    $paragraph = new markup('p', [], ['content' => rtrim(str_repeat('Paragraph content. ', 16)).'&#10;', 'link_view_more' => new markup('a', ['href' => '/'], 'View more')]);
+  # ─────────────────────────────────────────────────────────────────────
+  # unordered list
+  # ─────────────────────────────────────────────────────────────────────
+    $unordered_list_title = new markup('h3', [], 'Unordered list');
+    $unordered_list = new markup('ul', [], [
+      'li_1'       => new markup('li', [], ['content' => new text('item #%%_number', ['number' => 1])]),
+      'li_2'       => new markup('li', [], ['content' => new text('item #%%_number', ['number' => 2]),
+        'li_2_ul'  => new markup('ul', [], [
+          'li_2_1' => new markup('li', [], new text('item #%%_number', ['number' => 2.1])),
+          'li_2_2' => new markup('li', [], new text('item #%%_number', ['number' => 2.2])),
+          'li_2_3' => new markup('li', [], new text('item #%%_number', ['number' => 2.3]))])]),
+      'li_3'       => new markup('li', [], ['content' => new text('item #%%_number', ['number' => 3])])
     ]);
-  }
-
-  ################
-  ### diagrams ###
-  ################
-
-  static function on_show_block_demo_diagrams($page) {
-    $diagram_linear = new diagram('Title', 'linear');
-    $diagram_linear->slice_add('Parameter 1', 70, '0.07 sec.');
-    $diagram_linear->slice_add('Parameter 2', 20, '0.02 sec.');
-    $diagram_linear->slice_add('Parameter 3', 10, '0.01 sec.');
-    $diagram_radial = new diagram('Title', 'radial');
-    $diagram_radial->slice_add('Parameter 1', 40, '0.04 sec.', '#216ce4');
-    $diagram_radial->slice_add('Parameter 2', 30, '0.03 sec.', '#30c432');
-    $diagram_radial->slice_add('Parameter 3', 20, '0.02 sec.', '#fc5740');
-    $diagram_radial->slice_add('Parameter 4', 10, '0.01 sec.', '#fd9a1e');
-    return new node([], [
-      new block('Linear diagram', ['data-id' => 'demo_diagram_linear'], $diagram_linear),
-      new block('Radial diagram', ['data-id' => 'demo_diagram_radial'], $diagram_radial)
+  # ─────────────────────────────────────────────────────────────────────
+  # ordered list
+  # ─────────────────────────────────────────────────────────────────────
+    $ordered_list_title = new markup('h3', [], 'Ordered list');
+    $ordered_list = new markup('ol', [], [
+      'li_1'       => new markup('li', [], ['content' => new text('item #%%_number', ['number' => 1])]),
+      'li_2'       => new markup('li', [], ['content' => new text('item #%%_number', ['number' => 2]),
+        'li_2_ol'  => new markup('ol', [], [
+          'li_2_1' => new markup('li', [], new text('item #%%_number', ['number' => 2.1])),
+          'li_2_2' => new markup('li', [], new text('item #%%_number', ['number' => 2.2])),
+          'li_2_3' => new markup('li', [], new text('item #%%_number', ['number' => 2.3]))])]),
+      'li_3'       => new markup('li', [], ['content' => new text('item #%%_number', ['number' => 3])])
+    ]);
+  # ─────────────────────────────────────────────────────────────────────
+  # table (combinations of arrays and table_body_row and table_body_row_cell)
+  # ─────────────────────────────────────────────────────────────────────
+    $table_thead = [[
+      'th_1' => new text('head cell #%%_number', ['number' => 1]),
+      'th_2' => new text('head cell #%%_number', ['number' => 2]),
+      'th_3' => new text('head cell #%%_number', ['number' => 3])
+    ]];
+    $table_tbody = [
+      ['td_1' =>                             new text('cell #%%_number', ['number' => 1.1]),
+       'td_2' =>                             new text('cell #%%_number', ['number' => 1.2]),
+       'td_3' =>                             new text('cell #%%_number', ['number' => 1.3])],
+      ['td_1' =>                             new text('cell #%%_number', ['number' => 2.1]),
+       'td_2' =>                             new text('cell #%%_number', ['number' => 2.2]),
+       'td_3' => new table_body_row_cell([], new text('cell #%%_number', ['number' => 2.3]))],
+      new table_body_row([], [
+       'td_1' =>                             new text('cell #%%_number', ['number' => 3.1]),
+       'td_2' =>                             new text('cell #%%_number', ['number' => 3.2]),
+       'td_3' => new table_body_row_cell([], new text('cell #%%_number', ['number' => 3.3]))]),
+      new table_body_row([], [
+       'td_1' => new table_body_row_cell(['colspan' => 3], new text(''))
+      ])
+    ];
+    $table_title = new markup('h3', [], 'Table');
+    $table = new table(['class' => ['table' => 'table']],
+      $table_tbody,
+      $table_thead
+    );
+  # ─────────────────────────────────────────────────────────────────────
+  # result block
+  # ─────────────────────────────────────────────────────────────────────
+    return new block('Markup dynamic', ['data-id' => 'demo_markup_dynamic'], [
+      $paragraph_title,
+      $paragraph,
+      $unordered_list_title,
+      $unordered_list,
+      $ordered_list_title,
+      $ordered_list,
+      $table_title,
+      $table
     ]);
   }
 
@@ -182,82 +200,74 @@ namespace effcore\modules\demo {
     ]);
   }
 
+  ################
+  ### messages ###
+  ################
+
+  static function on_show_demo_messages($page) {
+    message::insert( 'credentials',                                            'credentials'                );
+    message::insert( new text('Notice message #%%_number.',  ['number' => 1]), 'notice'                     );
+    message::insert( new text('Notice message #%%_number.',  ['number' => 2]), 'notice'                     );
+    message::insert( new text('Notice message #%%_number.',  ['number' => 3]), 'notice'                     );
+    message::insert( new text_multiline(['Ok message #%%_number.', '(default type)'], ['number' => 1], ' ') );
+    message::insert( new text('Ok message #%%_number.',      ['number' => 2])                               );
+    message::insert( new text('Ok message #%%_number.',      ['number' => 3])                               );
+    message::insert( new text('Warning message #%%_number.', ['number' => 1]), 'warning'                    );
+    message::insert( new text('Warning message #%%_number.', ['number' => 2]), 'warning'                    );
+    message::insert( new text('Warning message #%%_number.', ['number' => 3]), 'warning'                    );
+    message::insert( new text('Error message #%%_number.',   ['number' => 1]), 'error'                      );
+    message::insert( new text('Error message #%%_number.',   ['number' => 2]), 'error'                      );
+    message::insert( new text('Error message #%%_number.',   ['number' => 3]), 'error'                      );
+  }
+
   ##############
-  ### markup ###
+  ### canvas ###
   ##############
 
-  static function on_show_block_demo_markup_dynamic($page) {
-  # ─────────────────────────────────────────────────────────────────────
-  # paragraph
-  # ─────────────────────────────────────────────────────────────────────
-    $paragraph_title = new markup('h3', [], 'Paragraph');
-    $paragraph = new markup('p', [], ['content' => rtrim(str_repeat('Paragraph content. ', 16)).'&#10;', 'link_view_more' => new markup('a', ['href' => '/'], 'View more')]);
-  # ─────────────────────────────────────────────────────────────────────
-  # unordered list
-  # ─────────────────────────────────────────────────────────────────────
-    $unordered_list_title = new markup('h3', [], 'Unordered list');
-    $unordered_list = new markup('ul', [], [
-      'li_1'       => new markup('li', [], ['content' => new text('item #%%_number', ['number' => 1])]),
-      'li_2'       => new markup('li', [], ['content' => new text('item #%%_number', ['number' => 2]),
-        'li_2_ul'  => new markup('ul', [], [
-          'li_2_1' => new markup('li', [], new text('item #%%_number', ['number' => 2.1])),
-          'li_2_2' => new markup('li', [], new text('item #%%_number', ['number' => 2.2])),
-          'li_2_3' => new markup('li', [], new text('item #%%_number', ['number' => 2.3]))])]),
-      'li_3'       => new markup('li', [], ['content' => new text('item #%%_number', ['number' => 3])])
+  static function on_show_block_demo_canvas($page) {
+    $canvas = new canvas_svg(105, 16, 5);
+    $canvas->glyph_set('-XXX-|X---X|X---X|X---X|X---X|X---X|X---X|X---X|X---X|-XXX-',  5, 3); # 0
+    $canvas->glyph_set('----X|---X-|--X-X|-X--X|X---X|----X|----X|----X|----X|----X', 15, 3); # 1
+    $canvas->glyph_set('XXXX-|----X|----X|----X|----X|---X-|--X--|-X---|X----|XXXXX', 25, 3); # 2
+    $canvas->glyph_set('XXXXX|----X|---X-|--X--|-X---|XXXXX|----X|---X-|--X--|-X---', 35, 3); # 3
+    $canvas->glyph_set('----X|---X-|--X-X|-X--X|X---X|-XXXX|----X|----X|----X|----X', 45, 3); # 4
+    $canvas->glyph_set('-XXXX|X----|X----|X----|X----|-XXXX|----X|---X-|--X--|-X---', 55, 3); # 5
+    $canvas->glyph_set('---X-|--X--|-X---|X----|-XXX-|X---X|X---X|X---X|X---X|-XXX-', 65, 3); # 6
+    $canvas->glyph_set('XXXXX|----X|---X-|--X--|-X---|X----|X----|X----|X----|X----', 75, 3); # 7
+    $canvas->glyph_set('-XXX-|X---X|X---X|X---X|-XXX-|X---X|X---X|X---X|X---X|-XXX-', 85, 3); # 8
+    $canvas->glyph_set('-XXX-|X---X|X---X|X---X|X---X|-XXX-|----X|---X-|--X--|-X---', 95, 3); # 9
+    return new block('Canvas', ['data-id' => 'demo_canvas'], [
+      $canvas
     ]);
-  # ─────────────────────────────────────────────────────────────────────
-  # ordered list
-  # ─────────────────────────────────────────────────────────────────────
-    $ordered_list_title = new markup('h3', [], 'Ordered list');
-    $ordered_list = new markup('ol', [], [
-      'li_1'       => new markup('li', [], ['content' => new text('item #%%_number', ['number' => 1])]),
-      'li_2'       => new markup('li', [], ['content' => new text('item #%%_number', ['number' => 2]),
-        'li_2_ol'  => new markup('ol', [], [
-          'li_2_1' => new markup('li', [], new text('item #%%_number', ['number' => 2.1])),
-          'li_2_2' => new markup('li', [], new text('item #%%_number', ['number' => 2.2])),
-          'li_2_3' => new markup('li', [], new text('item #%%_number', ['number' => 2.3]))])]),
-      'li_3'       => new markup('li', [], ['content' => new text('item #%%_number', ['number' => 3])])
+  }
+
+  ################
+  ### diagrams ###
+  ################
+
+  static function on_show_block_demo_diagrams($page) {
+    $diagram_linear = new diagram('Title', 'linear');
+    $diagram_linear->slice_add('Parameter 1', 70, '0.07 sec.');
+    $diagram_linear->slice_add('Parameter 2', 20, '0.02 sec.');
+    $diagram_linear->slice_add('Parameter 3', 10, '0.01 sec.');
+    $diagram_radial = new diagram('Title', 'radial');
+    $diagram_radial->slice_add('Parameter 1', 40, '0.04 sec.', '#216ce4');
+    $diagram_radial->slice_add('Parameter 2', 30, '0.03 sec.', '#30c432');
+    $diagram_radial->slice_add('Parameter 3', 20, '0.02 sec.', '#fc5740');
+    $diagram_radial->slice_add('Parameter 4', 10, '0.01 sec.', '#fd9a1e');
+    return new node([], [
+      new block('Linear diagram', ['data-id' => 'demo_diagram_linear'], $diagram_linear),
+      new block('Radial diagram', ['data-id' => 'demo_diagram_radial'], $diagram_radial)
     ]);
-  # ─────────────────────────────────────────────────────────────────────
-  # table (combinations of arrays and table_body_row and table_body_row_cell)
-  # ─────────────────────────────────────────────────────────────────────
-    $table_thead = [[
-      'th_1' => new text('head cell #%%_number', ['number' => 1]),
-      'th_2' => new text('head cell #%%_number', ['number' => 2]),
-      'th_3' => new text('head cell #%%_number', ['number' => 3])
-    ]];
-    $table_tbody = [
-      ['td_1' =>                             new text('cell #%%_number', ['number' => 1.1]),
-       'td_2' =>                             new text('cell #%%_number', ['number' => 1.2]),
-       'td_3' =>                             new text('cell #%%_number', ['number' => 1.3])],
-      ['td_1' =>                             new text('cell #%%_number', ['number' => 2.1]),
-       'td_2' =>                             new text('cell #%%_number', ['number' => 2.2]),
-       'td_3' => new table_body_row_cell([], new text('cell #%%_number', ['number' => 2.3]))],
-      new table_body_row([], [
-       'td_1' =>                             new text('cell #%%_number', ['number' => 3.1]),
-       'td_2' =>                             new text('cell #%%_number', ['number' => 3.2]),
-       'td_3' => new table_body_row_cell([], new text('cell #%%_number', ['number' => 3.3]))]),
-      new table_body_row([], [
-       'td_1' => new table_body_row_cell(['colspan' => 3], new text(''))
-      ])
-    ];
-    $table_title = new markup('h3', [], 'Table');
-    $table = new table(['class' => ['table' => 'table']],
-      $table_tbody,
-      $table_thead
-    );
-  # ─────────────────────────────────────────────────────────────────────
-  # result block
-  # ─────────────────────────────────────────────────────────────────────
-    return new block('Markup dynamic', ['data-id' => 'demo_markup_dynamic'], [
-      $paragraph_title,
-      $paragraph,
-      $unordered_list_title,
-      $unordered_list,
-      $ordered_list_title,
-      $ordered_list,
-      $table_title,
-      $table
+  }
+
+  ##############
+  ### colors ###
+  ##############
+
+  static function on_show_block_demo_colors($page) {
+    return new block('Colors', ['data-id' => 'demo_markup_colors'], [
+      new text('UNDER CONSTRUCTION')
     ]);
   }
 
