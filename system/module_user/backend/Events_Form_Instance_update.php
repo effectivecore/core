2400d7c1ca2311bfb99531cb80ace766a6dbdc43
Maxim Rysevets <root@effcore.com>
2019-07-23 12:28:33 +0300

    sprint_510_2310

--- system/module_user/backend/Events_Form_Instance_update.php
+++ system/module_user/backend/Events_Form_Instance_update.php
@@ -64,8 +64,8 @@ namespace effcore\modules\user {
       # field 'user' + field 'role'
         if ($entity->name == 'relation_role_ws_user') {
           if (!$form->has_error()) {
-            $id_user     = $items['#id_user']->value_get();
-            $id_role_new = $items['#id_role']->value_get();
+            $id_user     = $items['#id_user']->value_get        ();
+            $id_role_new = $items['#id_role']->value_get        ();
             $id_role_old = $items['#id_role']->value_get_initial();
             if ($id_role_new != $id_role_old) {
               $result = $entity->instances_select(['conditions' => [
@@ -76,6 +76,27 @@ namespace effcore\modules\user {
                 $items['#id_role']->error_set(new text_multiline([
                   'Field "%%_title" contains incorrect value!',
                   'This combination of values is already in use!'], ['title' => translation::get($items['#id_role']->title)]
+                ));
+              }
+            }
+          }
+        }
+      # field 'role' + field 'permission'
+        if ($entity->name == 'relation_role_ws_permission') {
+          if (!$form->has_error()) {
+            $id_role           = $items['#id_role'      ]->value_get        ();
+            $id_permission_new = $items['#id_permission']->value_get        ();
+            $id_permission_old = $items['#id_permission']->value_get_initial();
+            if ($id_permission_new != $id_permission_old) {
+              $result = $entity->instances_select(['conditions' => [
+                'id_role_!f'       => 'id_role',       'id_role_operator'       => '=', 'id_role_!v'       => $id_role, 'and',
+                'id_permission_!f' => 'id_permission', 'id_permission_operator' => '=', 'id_permission_!v' => $id_permission_new],
+                'limit'            => 1]);
+              if ($result) {
+                $items['#id_role'      ]->error_set();
+                $items['#id_permission']->error_set(new text_multiline([
+                  'Field "%%_title" contains incorrect value!',
+                  'This combination of values is already in use!'], ['title' => translation::get($items['#id_permission']->title)]
                 ));
               }
             }
