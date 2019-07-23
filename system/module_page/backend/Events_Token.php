b90466dfba8d02788a8705ad0f6eedeeb8c6aabd
Maxim Rysevets <root@effcore.com>
2019-07-23 18:08:54 +0300

    sprint_510_2316

--- system/module_page/backend/Events_Token.php
+++ system/module_page/backend/Events_Token.php
@@ -10,33 +10,35 @@ namespace effcore\modules\page {
           abstract class events_token {
 
   static function on_replace($name, $args = []) {
+    $result   = null;
     $settings = module::settings_get('page');
     $colors   = color::get_all();
     switch ($name) {
-      case 'color_page'            : return $colors[$settings->color_page_id            ]->value;
-      case 'color_text'            : return $colors[$settings->color_text_id            ]->value;
-      case 'color_main'            : return $colors[$settings->color_main_id            ]->value;
-      case 'color_link'            : return $colors[$settings->color_link_id            ]->value;
-      case 'color_link_active'     : return $colors[$settings->color_link_active_id     ]->value;
-      case 'color_table_row_odd'   : return $colors[$settings->color_table_row_odd_id   ]->value;
-      case 'color_table_row_even'  : return $colors[$settings->color_table_row_even_id  ]->value;
-      case 'color_relation'        : return $colors[$settings->color_relation_id        ]->value;
-      case 'color_menu'            : return $colors[$settings->color_menu_id            ]->value;
-      case 'color_menu_active'     : return $colors[$settings->color_menu_active_id     ]->value;
-      case 'color_menu_text'       : return $colors[$settings->color_menu_text_id       ]->value;
-      case 'color_menu_link'       : return $colors[$settings->color_menu_link_id       ]->value;
-      case 'color_menu_link_active': return $colors[$settings->color_menu_link_active_id]->value;
-      case 'color_ok'              : return $colors[$settings->color_ok_id              ]->value;
-      case 'color_warning'         : return $colors[$settings->color_warning_id         ]->value;
-      case 'color_error'           : return $colors[$settings->color_error_id           ]->value;
-      case 'color_fieldset'        : return $colors[$settings->color_fieldset_id        ]->value;
-      case 'color_fieldset_nested' : return $colors[$settings->color_fieldset_nested_id ]->value;
-      case 'color_field'           : return $colors[$settings->color_field_id           ]->value;
-      case 'color_field_text'      : return $colors[$settings->color_field_text_id      ]->value;
-      case 'color_button'          : return $colors[$settings->color_button_id          ]->value;
-      case 'color_button_active'   : return $colors[$settings->color_button_active_id   ]->value;
-      case 'color_button_text'     : return $colors[$settings->color_button_text_id     ]->value;
-    }
+      case 'color_page'            : $result = $colors[$settings->color_page_id            ]; break;
+      case 'color_text'            : $result = $colors[$settings->color_text_id            ]; break;
+      case 'color_main'            : $result = $colors[$settings->color_main_id            ]; break;
+      case 'color_link'            : $result = $colors[$settings->color_link_id            ]; break;
+      case 'color_link_active'     : $result = $colors[$settings->color_link_active_id     ]; break;
+      case 'color_table_row_odd'   : $result = $colors[$settings->color_table_row_odd_id   ]; break;
+      case 'color_table_row_even'  : $result = $colors[$settings->color_table_row_even_id  ]; break;
+      case 'color_relation'        : $result = $colors[$settings->color_relation_id        ]; break;
+      case 'color_menu'            : $result = $colors[$settings->color_menu_id            ]; break;
+      case 'color_menu_active'     : $result = $colors[$settings->color_menu_active_id     ]; break;
+      case 'color_menu_text'       : $result = $colors[$settings->color_menu_text_id       ]; break;
+      case 'color_menu_link'       : $result = $colors[$settings->color_menu_link_id       ]; break;
+      case 'color_menu_link_active': $result = $colors[$settings->color_menu_link_active_id]; break;
+      case 'color_ok'              : $result = $colors[$settings->color_ok_id              ]; break;
+      case 'color_warning'         : $result = $colors[$settings->color_warning_id         ]; break;
+      case 'color_error'           : $result = $colors[$settings->color_error_id           ]; break;
+      case 'color_fieldset'        : $result = $colors[$settings->color_fieldset_id        ]; break;
+      case 'color_fieldset_nested' : $result = $colors[$settings->color_fieldset_nested_id ]; break;
+      case 'color_field'           : $result = $colors[$settings->color_field_id           ]; break;
+      case 'color_field_text'      : $result = $colors[$settings->color_field_text_id      ]; break;
+      case 'color_button'          : $result = $colors[$settings->color_button_id          ]; break;
+      case 'color_button_active'   : $result = $colors[$settings->color_button_active_id   ]; break;
+      case 'color_button_text'     : $result = $colors[$settings->color_button_text_id     ]; break;}
+    if (!empty($result->value))
+        return $result->value;
   }
 
 }}
