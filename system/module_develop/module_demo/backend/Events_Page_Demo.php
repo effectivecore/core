b2e2d4b12915633e0ace445399278ede53dc2aed
Maxim Rysevets <root@effcore.com>
2019-07-31 22:00:54 +0300

    sprint_513_9

--- system/module_develop/module_demo/backend/Events_Page_Demo.php
+++ system/module_develop/module_demo/backend/Events_Page_Demo.php
@@ -30,12 +30,12 @@ namespace effcore\modules\demo {
     }
   }
 
-  static function on_tree_before_build_demo_nosql($event, $tree) {
+  static function on_tree_build_before_demo_nosql($event, $tree) {
     tree_item::insert('item #1.2.3 (from code)', 'demo_item_1_2_3','demo_item_1_2', 'demo_nosql', '/develop/demo/embedded/trees/item_1/item_1_2/item_1_2_3', null, [], [], -10);
     tree_item::insert('item #3 (from code)',     'demo_item_3',     null,           'demo_nosql', '/develop/demo/embedded/trees/item_3',                     null, [], [], -10);
   }
 
-  static function on_tree_before_build_demo_sql($event, $tree) {
+  static function on_tree_build_before_demo_sql($event, $tree) {
     tree_item::insert('item #1.2.3 (from code)', '6',               '3',            'demo_sql',   '/develop/demo/embedded/trees/item_1/item_1_2/item_1_2_3', null, [], [], -10);
     tree_item::insert('item #3 (from code)',     '9',               null,           'demo_sql',   '/develop/demo/embedded/trees/item_3',                     null, [], [], -10);
   }
