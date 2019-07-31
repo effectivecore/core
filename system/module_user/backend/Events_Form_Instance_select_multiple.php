653fb32b54faff46eb50d03a42f5abe1cfe9562b
Maxim Rysevets <root@effcore.com>
2019-07-31 23:27:05 +0300

    sprint_514

--- system/module_user/backend/Events_Form_Instance_select_multiple.php
+++ system/module_user/backend/Events_Form_Instance_select_multiple.php
@@ -5,7 +5,6 @@
   ##################################################################
 
 namespace effcore\modules\user {
-          use const \effcore\dir_root;
           use \effcore\entity;
           use \effcore\page;
           abstract class events_form_instance_select_multiple {
@@ -16,14 +15,6 @@ namespace effcore\modules\user {
     if ($entity) {
       switch ($form->clicked_button->value_get()) {
         case 'apply':
-        # delete selected users avatar
-          if ($entity->name == 'user' && !empty($form->_selected_instances)) {
-            foreach ($form->_selected_instances as $c_instance) {
-              if (!empty($c_instance->avatar_path)) {
-                @unlink(dir_root.$c_instance->avatar_path);
-              }
-            }
-          }
           break;
       }
     }
