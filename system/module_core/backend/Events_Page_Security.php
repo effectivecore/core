8f42486a5b8285497a592809dc856ddc5a3cd081
Maxim Rysevets <root@effcore.com>
2019-08-02 18:42:57 +0300

    sprint_520

---
+++ system/module_core/backend/Events_Page_Security.php
@@ -0,0 +1,18 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2019 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\core {
+          use \effcore\block;
+          use \effcore\text;
+          abstract class events_page_security {
+
+  static function on_show_block_security($page) {
+    return new block('', ['data-id' => 'security'], [
+      new text('UNDER CONSTRUCTION')
+    ]);
+  }
+
+}}
