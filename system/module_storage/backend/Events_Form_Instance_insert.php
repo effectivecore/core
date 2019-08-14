9c841918f5cd85f7be03a975e966844e8c28510e
Maxim Rysevets <root@effcore.com>
2019-08-14 14:15:35 +0300

    sprint_529_7

--- system/module_storage/backend/Events_Form_Instance_insert.php
+++ system/module_storage/backend/Events_Form_Instance_insert.php
@@ -72,8 +72,8 @@ namespace effcore\modules\storage {
           if ($form->_instance->insert())
                message::insert(new text('Item of type "%%_name" with id = "%%_id" was inserted.',     ['name' => translation::get($entity->title), 'id' => implode('+', $form->_instance->values_id_get()) ])           );
           else message::insert(new text('Item of type "%%_name" with id = "%%_id" was not inserted!', ['name' => translation::get($entity->title), 'id' => 'n/a'                                           ]), 'warning');
-                     url::go(url::back_url_get() ?: ($back_update ?: '/manage/instances/select/'.core::sanitize_id($entity->group).'/'.$entity->name)); break;
-      case 'cancel': url::go(url::back_url_get() ?: ($back_cancel ?: '/manage/instances/select/'.core::sanitize_id($entity->group).'/'.$entity->name)); break;
+                     url::go(url::back_url_get() ?: ($back_update ?: '/manage/instances/select/'.$entity->group_managing_get_id().'/'.$entity->name)); break;
+      case 'cancel': url::go(url::back_url_get() ?: ($back_cancel ?: '/manage/instances/select/'.$entity->group_managing_get_id().'/'.$entity->name)); break;
     }
   }
 
