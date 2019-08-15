5b8b12503edde95aa872c2f60196e8c4c5dfbc88
Maxim Rysevets <root@effcore.com>
2019-08-15 12:46:54 +0300

    sprint_529_15

--- system/module_page/backend/patterns/Group_Page_part_Insert.php
+++ system/module_page/backend/patterns/Group_Page_part_Insert.php
@@ -29,8 +29,7 @@ namespace effcore {
       $select_preset->build();
       $select_preset->name_set('insert_to_'.$this->id_area);
       $select_preset->required_set(false);
-      $button_insert = new button;
-      $button_insert->title = '';
+      $button_insert = new button('');
       $button_insert->build();
       $button_insert->value_set('button_insert_to_'.$this->id_area);
       $this->child_insert($select_preset, 'select');
