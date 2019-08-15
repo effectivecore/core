577ce889942b2312249373e9e9709853bbcca312
Maxim Rysevets <root@effcore.com>
2019-08-15 07:22:38 +0300

    sprint_529_14

---
+++ system/module_page/backend/Events_Form_Instance_update__Selection.php
@@ -0,0 +1,45 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2019 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\page {
+          use \effcore\core;
+          use \effcore\entity;
+          use \effcore\field_number;
+          use \effcore\fieldset;
+          use \effcore\page;
+          abstract class events_form_instance_update_selection {
+
+  static function on_init($event, $form, $items) {
+    $entity_name = page::get_current()->args_get('entity_name');
+    $entity = entity::get($entity_name);
+    if ($entity) {
+      if ($entity->name == 'selection' && !empty($form->_instance)) {
+        $fields           = new fieldset('Fields');
+        $query_params     = new fieldset('Query parameters');
+        $decorator_params = new fieldset('Decorator parameters');
+        $conditions       = new fieldset('Conditions');
+        $order            = new fieldset('Order');
+        $limit = new field_number('Limit');
+        $limit->build();
+        $limit->name_set('limit');
+        $limit->value_set(1);
+        $limit->min_set(1);
+        $limit->max_set(10000);
+
+        $query_params                ->child_insert($conditions,       'conditions'      );
+        $query_params                ->child_insert($order,            'order'           );
+        $query_params                ->child_insert($limit,            'limit'           );
+        $form->child_select('fields')->child_insert($fields,           'fields'          );
+        $form->child_select('fields')->child_insert($query_params,     'query_params'    );
+        $form->child_select('fields')->child_insert($decorator_params, 'decorator_params');
+      }
+    }
+  }
+
+  static function on_submit($event, $form, $items) {
+  }
+
+}}
