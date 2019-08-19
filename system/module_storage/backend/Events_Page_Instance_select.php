8b7354d4257f9a33e7ca70ef5fd96982b4da4230
Maxim Rysevets <root@effcore.com>
2019-08-19 12:42:52 +0300

    sprint_529_51

--- system/module_storage/backend/Events_Page_Instance_select.php
+++ system/module_storage/backend/Events_Page_Instance_select.php
@@ -5,12 +5,9 @@
   ##################################################################
 
 namespace effcore\modules\storage {
-          use \effcore\block;
           use \effcore\core;
           use \effcore\entity;
           use \effcore\instance;
-          use \effcore\markup;
-          use \effcore\selection;
           abstract class events_page_instance_select {
 
   static function on_build_before($event, $page) {
@@ -28,47 +25,6 @@ namespace effcore\modules\storage {
         } else core::send_header_and_exit('page_not_found');
       }   else core::send_header_and_exit('page_not_found');
     }     else core::send_header_and_exit('page_not_found');
-  }
-
-  static function on_show_block_instance_select($page) {
-    $entity_name = $page->args_get('entity_name');
-    $instance_id = $page->args_get('instance_id');
-    $entity = entity::get($entity_name);
-    if ($entity) {
-      $id_keys   = $entity->id_get_real();
-      $id_values = explode('+', $instance_id);
-      if (count($id_keys) ==
-          count($id_values)) {
-        $conditions = array_combine($id_keys, $id_values);
-        $instance = new instance($entity_name, $conditions);
-        if ($instance->select()) {
-        # create selection
-          $selection = new selection;
-          $selection->id = 'instance_manage';
-          foreach ($entity->selection_params as $c_key => $c_value)
-            $selection                       ->{$c_key} = $c_value;
-            $selection->decorator_params['view_type'] = $entity->view_type_single;
-            $selection->query_params['conditions']    = $entity->storage_get()->attributes_prepare($conditions);
-          $has_visible_fields = false;
-          foreach ($entity->fields as $c_name => $c_field) {
-            if (!empty($c_field->field_is_visible_on_select)) {
-              $has_visible_fields = true;
-              $selection->field_insert_entity(null, $entity->name, $c_name);
-            }
-          }
-          if (!$has_visible_fields) {
-            return new block('', ['data-id' => 'instance_select', 'data-main-entity' => $entity->name],
-              new markup('x-no-result', [], 'no visible fields')
-            );
-          } else {
-            $selection->field_insert_action(null, 'Action', ['delete', 'update']);
-            return new block('', ['data-id' => 'instance_select', 'data-main-entity' => $entity->name],
-              $selection
-            );
-          }
-        }
-      }
-    }
   }
 
 }}
