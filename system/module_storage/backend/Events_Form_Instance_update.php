ccf13e0b55b084d70a94a6a5bc324ad64d1a38e4
Maxim Rysevets <root@effcore.com>
2019-08-20 16:00:30 +0300

    sprint_529_59

--- system/module_storage/backend/Events_Form_Instance_update.php
+++ system/module_storage/backend/Events_Form_Instance_update.php
@@ -8,10 +8,12 @@ namespace effcore\modules\storage {
           use \effcore\core;
           use \effcore\entity;
           use \effcore\field_checkbox;
+          use \effcore\field_hidden;
           use \effcore\instance;
           use \effcore\markup;
           use \effcore\message;
           use \effcore\page;
+          use \effcore\text_multiline;
           use \effcore\text;
           use \effcore\translation;
           use \effcore\url;
@@ -28,6 +30,15 @@ namespace effcore\modules\storage {
           count($id_values)) {
         $form->_instance = new instance($entity->name, array_combine($id_keys, $id_values));
         if ($form->_instance->select()) {
+        # fixation of 'updated' value for prevent parallel update (not secure: only for organizational methods)
+          if ($entity->ws_updated &&
+              $entity->ws_updated_parallel_checking) {
+            $hidden_updated = new field_hidden('updated');
+            $hidden_updated->value_set($hidden_updated->value_request_get() ?
+               core::sanitize_datetime($hidden_updated->value_request_get()) : $form->_instance->updated);
+            $form->child_insert($hidden_updated, 'hidden_updated');
+          }
+        # make fields for managing
           $has_enabled_fields = false;
           foreach ($entity->fields as $c_name => $c_field) {
             if (!empty($c_field->managing_is_on_update) && isset($c_field->managing_class)) {
@@ -62,6 +73,33 @@ namespace effcore\modules\storage {
     }     else core::send_header_and_exit('page_not_found');
   }
 
+  static function on_validate($event, $form, $items) {
+    $entity_name = page::get_current()->args_get('entity_name');
+    $entity = entity::get($entity_name);
+    if ($entity) {
+      switch ($form->clicked_button->value_get()) {
+        case 'update':
+          if (!empty($form->_instance)) {
+          # prevent parallel update
+            if ($entity->ws_updated &&
+                $entity->ws_updated_parallel_checking) {
+              $hidden_updated = $form->child_select('hidden_updated');
+              $hidden_updated->value_get();
+              $fresh_instance = clone $form->_instance;
+              $fresh_instance->select();
+              if ($fresh_instance->updated != $hidden_updated->value_get()) {
+                $form->error_set(new text_multiline([
+                  'While editing this form, someone made changes in parallel and saved them!',
+                  'Reload this page and make changes again to prevent inconsistency.']));
+                return;
+              }
+            }
+          }
+          break;
+      }
+    }
+  }
+
   static function on_submit($event, $form, $items) {
     $back_update = page::get_current()->args_get('back_update');
     $back_return = page::get_current()->args_get('back_return');
@@ -70,6 +108,7 @@ namespace effcore\modules\storage {
     switch ($form->clicked_button->value_get()) {
       case 'update':
         if (!empty($form->_instance)) {
+        # transfer new values to instance
           foreach ($entity->fields as $c_name => $c_field) {
             if (isset($c_field->managing_class) && isset($items['#'.$c_name])) {
               if (!empty($c_field->managing_value_manual_get_if_empty) && $items['#'.$c_name]->value_get() == '') continue;
@@ -78,6 +117,7 @@ namespace effcore\modules\storage {
               if ($items['#'.$c_name] instanceof field_checkbox != true) $form->_instance->{$c_name} = $items['#'.$c_name]->value_get  ();
             }
           }
+        # update values
           if ($form->_instance->update())
                message::insert(new text('Item of type "%%_name" with id = "%%_id" was updated.',     ['name' => translation::get($entity->title), 'id' => implode('+', $form->_instance->values_id_get()) ])           );
           else message::insert(new text('Item of type "%%_name" with id = "%%_id" was not updated!', ['name' => translation::get($entity->title), 'id' => implode('+', $form->_instance->values_id_get()) ]), 'warning');
