19db57f937ffea7b272459693442173c4f2cf6ef
Maxim Rysevets <root@effcore.com>
2019-08-22 13:15:02 +0300

    sprint_529_83

--- system/module_page/backend/Events_Token.php
+++ system/module_page/backend/Events_Token.php
@@ -40,19 +40,16 @@ namespace effcore\modules\page {
       case 'color_button_active'   : $color_result = $colors[$settings->color_button_active_id   ]; break;
       case 'color_button_text'     : $color_result = $colors[$settings->color_button_text_id     ]; break;}
     if ($color_result) {
-      if (!empty($color_result->value) && count($args) == 0)   return $color_result->value;
-      if (!empty($color_result->value) && count($args) == 3 && !empty($color_result->value_hex)) {
-        $color = ltrim($color_result->value_hex, '#');
-        $color_parts = [];
-        if (strlen($color) == 6) {$color_parts = str_split($color, 2);                                                                                                         }
-        if (strlen($color) == 3) {$color_parts = str_split($color, 1); $color_parts[0].= $color_parts[0]; $color_parts[1].= $color_parts[1]; $color_parts[2].= $color_parts[2];}
-        if (!empty($color_parts)) {
-          $r = max(min((int)hexdec($color_parts[0]) + (int)$args[0], 255), 0);
-          $g = max(min((int)hexdec($color_parts[1]) + (int)$args[1], 255), 0);
-          $b = max(min((int)hexdec($color_parts[2]) + (int)$args[2], 255), 0);
-          return '#'.str_pad(dechex($r), 2, '0', STR_PAD_LEFT).
-                     str_pad(dechex($g), 2, '0', STR_PAD_LEFT).
-                     str_pad(dechex($b), 2, '0', STR_PAD_LEFT);
+      if (!empty($color_result->value) && count($args) == 0) return $color_result->value;
+      if (!empty($color_result->value) && count($args) == 3) {
+        $rgb = $color_result->rgb_get();
+        if ($rgb) {
+          $new_r = max(min($rgb['r'] + (int)$args[0], 255), 0);
+          $new_g = max(min($rgb['g'] + (int)$args[1], 255), 0);
+          $new_b = max(min($rgb['b'] + (int)$args[2], 255), 0);
+          return '#'.str_pad(dechex($new_r), 2, '0', STR_PAD_LEFT).
+                     str_pad(dechex($new_g), 2, '0', STR_PAD_LEFT).
+                     str_pad(dechex($new_b), 2, '0', STR_PAD_LEFT);
         }
       }
     }
