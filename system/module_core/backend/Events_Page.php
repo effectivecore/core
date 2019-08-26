cf45a4b82ab264f589f272ccaadf31f16b88b5c7
Maxim Rysevets <root@effcore.com>
2019-08-26 17:54:00 +0300

    sprint_529_111

--- system/module_core/backend/Events_Page.php
+++ system/module_core/backend/Events_Page.php
@@ -5,9 +5,47 @@
   ##################################################################
 
 namespace effcore\modules\core {
+          use \effcore\markup;
+          use \effcore\text;
+          use \effcore\tree_item;
           abstract class events_page {
 
   static function on_breadcrumbs_build($event, $breadcrumbs) {
+  # find all active menu items
+    $branches = [];
+    foreach (tree_item::select_all_by_id_tree('main') as $c_item) {
+      if ($c_item->is_active      () ||
+          $c_item->is_active_trail()) {
+        $branches[][$c_item->id] = $c_item;
+      }
+    }
+  # find all parents (resolve all branches)
+    foreach ($branches as &$c_branch) {
+      $counter = 0;
+      while (true) {
+        if ($counter++ >= 15) break;
+        $c_parent_id = end($c_branch)->id_parent;
+        if ($c_parent_id) {
+            $c_parent = tree_item::select($c_parent_id, 'main');
+            $c_branch[$c_parent->id] = $c_parent;}
+        else break;
+      }
+    }
+  # find the longest branch
+    $longest = [];
+    foreach ($branches as $c_branch) {
+      if (count($c_branch) > count($longest)) {
+        $longest = $c_branch;
+      }
+    }
+  # make result
+    foreach (array_reverse($longest) as $c_item) {
+      $breadcrumbs->child_insert(
+        new markup('a', ['href' => $c_item->href_get() ?: false],
+          new text($c_item->title, [], true, true)
+        )
+      );
+    }
   }
 
 }}
--- system/module_menu/backend/Events_Page.php
+++ system/module_menu/backend/Events_Page.php
@@ -5,9 +5,6 @@
   ##################################################################
 
 namespace effcore\modules\menu {
-          use \effcore\markup;
-          use \effcore\text;
-          use \effcore\tree_item;
           use \effcore\tree;
           abstract class events_page {
 
@@ -15,45 +12,6 @@ namespace effcore\modules\menu {
     if (!empty($args['id_tree'])) {
       return tree::select($args['id_tree']);
     }
-  }
-
-  static function on_show_block_breadcrumbs_main($page, $args) {
-    $branches = [];
-    foreach (tree_item::select_all_by_id_tree('main') as $c_item) {
-      if ($c_item->is_active      () ||
-          $c_item->is_active_trail()) {
-        $branches[][$c_item->id] = $c_item;
-      }
-    }
-  # resolve all branches (find all parents)
-    foreach ($branches as &$c_branch) {
-      $counter = 0;
-      while (true) {
-        if ($counter++ >= 15) break;
-        $c_parent_id = end($c_branch)->id_parent;
-        if ($c_parent_id) {
-            $c_parent = tree_item::select($c_parent_id, 'main');
-            $c_branch[$c_parent->id] = $c_parent;}
-        else break;
-      }
-    }
-  # find the longest branch
-    $found = [];
-    foreach ($branches as $c_branch) {
-      if (count($c_branch) > count($found)) {
-        $found = $c_branch;
-      }
-    }
-  # make markup
-    $result = new markup('x-breadcrumbs');
-    foreach (array_reverse($found) as $c_item) {
-      $result->child_insert(
-        new markup('a', ['href' => $c_item->href_get() ?: false],
-          new text($c_item->title, [], true, true)
-        ), $c_item->id
-      );
-    }
-    return $result;
   }
 
 }}
