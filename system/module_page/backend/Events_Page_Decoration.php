65d699214ea2dead1c73bf5555e127b347940dbd
Maxim Rysevets <root@effcore.com>
2019-08-26 14:25:50 +0300

    sprint_529_108

--- system/module_page/backend/Events_Page_Decoration.php
+++ system/module_page/backend/Events_Page_Decoration.php
@@ -7,26 +7,39 @@
 namespace effcore\modules\page {
           use \effcore\color;
           use \effcore\core;
+          use \effcore\layout;
           use \effcore\page;
           use \effcore\tabs_item;
           use \effcore\url;
           abstract class events_page_decoration {
 
   static function on_tab_build_before($event, $tab) {
-    $presets = color::preset_get_all();
     $type = page::get_current()->args_get('type');
-    $id   = page::get_current()->args_get('id');
-    core::array_sort_by_text_property($presets);
-    if ($type == null)      url::go(page::get_current()->args_get('base').'/colors');
-    if ($type == 'presets') url::go(page::get_current()->args_get('base').'/presets/'.reset($presets)->id);
-    if (strpos($type, 'presets/') === 0 && !isset($presets[$id])) {
-      url::go(page::get_current()->args_get('base').'/presets/'.reset($presets)->id);
+    $id   = page::get_current()->args_get('id'  );
+    if ($type == null) url::go(page::get_current()->args_get('base').'/colors');
+  # colors presets
+    if ($type == 'colors_presets') {
+      $presets = color::preset_get_all();
+      core::array_sort_by_text_property($presets);
+      if (empty($presets[$id])) url::go(page::get_current()->args_get('base').'/colors_presets/'.reset($presets)->id);
+      foreach ($presets as $c_preset) {
+        tabs_item::insert(                                             $c_preset->title,
+          'decoration_colors_presets_'.                                $c_preset->id,
+          'decoration_colors_presets', 'decoration', 'colors_presets/'.$c_preset->id
+        );
+      }
     }
-    foreach ($presets as $c_preset) {
-      tabs_item::insert(      $c_preset->title,
-        'decoration_presets_'.$c_preset->id,
-        'decoration_presets', 'decoration', 'presets/'.$c_preset->id
-      );
+  # layouts
+    if ($type == 'layouts') {
+      $layouts = layout::select_all();
+      core::array_sort_by_text_property($layouts);
+      if (empty($layouts[$id])) url::go(page::get_current()->args_get('base').'/layouts/'.reset($layouts)->id);
+      foreach ($layouts as $c_layout) {
+        tabs_item::insert(                               $c_layout->title,
+          'decoration_layouts_'.                         $c_layout->id,
+          'decoration_layouts', 'decoration', 'layouts/'.$c_layout->id
+        );
+      }
     }
   }
 
