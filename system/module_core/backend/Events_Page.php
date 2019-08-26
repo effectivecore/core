16f546abfd74641f2fc1dca36e3bf7b3ef30ccf8
Maxim Rysevets <root@effcore.com>
2019-08-26 16:33:53 +0300

    sprint_529_110

---
+++ system/module_core/backend/Events_Page.php
@@ -0,0 +1,13 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2019 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\core {
+          abstract class events_page {
+
+  static function on_breadcrumbs_build($event, $breadcrumbs) {
+  }
+
+}}
