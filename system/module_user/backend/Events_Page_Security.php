9c06c9b954ac99e12142b5844cda115384f0556e
Maxim Rysevets <root@effcore.com>
2019-08-26 12:10:47 +0300

    sprint_529_104

--- system/module_user/backend/Events_Page_Security.php
+++ system/module_user/backend/Events_Page_Security.php
@@ -4,7 +4,7 @@
   ### Copyright © 2017—2019 Maxim Rysevets. All rights reserved. ###
   ##################################################################
 
-namespace effcore\modules\core {
+namespace effcore\modules\user {
           use \effcore\block;
           use \effcore\text;
           abstract class events_page_security {
