2b48c7c349da33978383ed6328ac52c054d93609
Maxim Rysevets <root@effcore.com>
2019-08-27 15:30:45 +0300

    sprint_529_116

--- system/module_develop/module_demo/backend/Events_Examples.php
+++ system/module_develop/module_demo/backend/Events_Examples.php
@@ -7,26 +7,27 @@
 namespace effcore\modules\demo {
           abstract class events_examples {
 
-  static function on_breadcrumbs_build     ($event, $breadcrumbs) {}
-  static function on_decorator_build_before($event, $decorator) {}
-  static function on_decorator_build_after ($event, $decorator) {}
-  static function on_email_send_before     ($event, &$to, &$subject, &$body, &$from, &$encoding, &$form, &$items) {}
-  static function on_instance_delete_before($event, $instance) {}
-  static function on_instance_insert_before($event, $instance) {}
-  static function on_instance_select_before($event, $instance) {}
-  static function on_instance_update_before($event, $instance) {}
-  static function on_instance_delete_after ($event, $instance, $result) {}
-  static function on_instance_insert_after ($event, $instance, $result) {}
-  static function on_instance_select_after ($event, $instance, $result) {}
-  static function on_instance_update_after ($event, $instance, $result) {}
-  static function on_page_build_before     ($event, $page) {}
-  static function on_page_build_after      ($event, $page) {}
-  static function on_page_render_before    ($event, $page, $template) {}
-  static function on_selection_build_before($event, $selection) {}
-  static function on_selection_build_after ($event, $selection) {}
-  static function on_tab_build_before      ($event, $tab) {}
-  static function on_tab_build_after       ($event, $tab) {}
-  static function on_tree_build_before     ($event, $tree) {}
-  static function on_tree_build_after      ($event, $tree) {}
+  static function on_breadcrumbs_build_before($event, $breadcrumbs) {}
+  static function on_breadcrumbs_build_after ($event, $breadcrumbs) {}
+  static function on_decorator_build_before  ($event, $decorator) {}
+  static function on_decorator_build_after   ($event, $decorator) {}
+  static function on_email_send_before       ($event, &$to, &$subject, &$body, &$from, &$encoding, &$form, &$items) {}
+  static function on_instance_delete_before  ($event, $instance) {}
+  static function on_instance_insert_before  ($event, $instance) {}
+  static function on_instance_select_before  ($event, $instance) {}
+  static function on_instance_update_before  ($event, $instance) {}
+  static function on_instance_delete_after   ($event, $instance, $result) {}
+  static function on_instance_insert_after   ($event, $instance, $result) {}
+  static function on_instance_select_after   ($event, $instance, $result) {}
+  static function on_instance_update_after   ($event, $instance, $result) {}
+  static function on_page_build_before       ($event, $page) {}
+  static function on_page_build_after        ($event, $page) {}
+  static function on_page_render_before      ($event, $page, $template) {}
+  static function on_selection_build_before  ($event, $selection) {}
+  static function on_selection_build_after   ($event, $selection) {}
+  static function on_tab_build_before        ($event, $tab) {}
+  static function on_tab_build_after         ($event, $tab) {}
+  static function on_tree_build_before       ($event, $tree) {}
+  static function on_tree_build_after        ($event, $tree) {}
 
 }}
