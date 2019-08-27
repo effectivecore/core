c1880522969732483897cc7cd1fd9093c43a8b2f
Maxim Rysevets <root@effcore.com>
2019-08-27 17:34:08 +0300

    sprint_529_119

--- system/module_menu/backend/Events_Page.php
+++ system/module_menu/backend/Events_Page.php
@@ -5,12 +5,25 @@
   ##################################################################
 
 namespace effcore\modules\menu {
+          use \effcore\page;
+          use \effcore\translation;
           use \effcore\tree;
           abstract class events_page {
 
   static function on_show_block_tree_sql($page, $args) {
     if (!empty($args['id_tree'])) {
       return tree::select($args['id_tree']);
+    }
+  }
+
+  static function on_breadcrumbs_build_before($event, $breadcrumbs) {
+    $entity_name = page::get_current()->args_get('entity_name');
+    $category_id = page::get_current()->args_get('category_id');
+    if (page::get_current()->id == 'instance_insert' && $entity_name == 'tree_item') {
+      $trees = tree::select_all('sql');
+      if (!empty($trees[$category_id]))
+           $breadcrumbs->link_update('entity', translation::get('Items for: %%_title', ['title' => translation::get($trees[$category_id]->title)]), '/manage/data/menu/tree_item///'.$category_id);
+      else $breadcrumbs->link_delete('entity');
     }
   }
 
