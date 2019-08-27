28e92e219bba1c978bf70b81a5310f3b2d261ada
Maxim Rysevets <root@effcore.com>
2019-08-27 19:06:44 +0300

    sprint_529_121

--- system/module_menu/backend/Events_Page.php
+++ system/module_menu/backend/Events_Page.php
@@ -8,6 +8,7 @@ namespace effcore\modules\menu {
           use \effcore\page;
           use \effcore\translation;
           use \effcore\tree;
+          use \effcore\url;
           abstract class events_page {
 
   static function on_show_block_tree_sql($page, $args) {
@@ -17,12 +18,14 @@ namespace effcore\modules\menu {
   }
 
   static function on_breadcrumbs_build_before($event, $breadcrumbs) {
-    $entity_name = page::get_current()->args_get('entity_name');
-    $category_id = page::get_current()->args_get('category_id');
+    $entity_name   = page::get_current()->args_get('entity_name'  );
+    $category_id   = page::get_current()->args_get('category_id'  );
+    $back_return_0 = page::get_current()->args_get('back_return_0');
+    $back_return_n = page::get_current()->args_get('back_return_n');
     if (page::get_current()->id == 'instance_insert' && $entity_name == 'tree_item') {
       $trees = tree::select_all('sql');
       if (!empty($trees[$category_id]))
-           $breadcrumbs->link_update('entity', translation::get('Items for: %%_title', ['title' => translation::get($trees[$category_id]->title)]), '/manage/data/menu/tree_item///'.$category_id);
+           $breadcrumbs->link_update('entity', translation::get('Items for: %%_title', ['title' => translation::get($trees[$category_id]->title)]), $back_return_0 ?: (url::back_url_get() ?: ($back_return_n ?: '/manage/data/menu/tree_item///'.$category_id)));
       else $breadcrumbs->link_delete('entity');
     }
   }
--- system/module_storage/backend/Events_Page.php
+++ system/module_storage/backend/Events_Page.php
@@ -5,18 +5,21 @@
   ##################################################################
 
 namespace effcore\modules\storage {
-          use \effcore\page;
           use \effcore\entity;
+          use \effcore\page;
+          use \effcore\url;
           abstract class events_page {
 
   static function on_breadcrumbs_build_before($event, $breadcrumbs) {
     $managing_group_id = page::get_current()->args_get('managing_group_id');
     $entity_name       = page::get_current()->args_get('entity_name'      );
+    $back_return_0     = page::get_current()->args_get('back_return_0'    );
+    $back_return_n     = page::get_current()->args_get('back_return_n'    );
     if (page::get_current()->id == 'instance_insert') {
       $groups = entity::groups_managing_get();
       $entity = entity::get($entity_name);
-      $breadcrumbs->link_insert('entity_group', $groups[$managing_group_id], '/manage/data/'.$managing_group_id                  );
-      $breadcrumbs->link_insert('entity',       $entity->title,              '/manage/data/'.$managing_group_id.'/'.$entity->name);
+      $breadcrumbs->link_insert('entity_group', $groups[$managing_group_id],                                                              '/manage/data/'.$managing_group_id                    );
+      $breadcrumbs->link_insert('entity',       $entity->title,              $back_return_0 ?: (url::back_url_get() ?: ($back_return_n ?: '/manage/data/'.$managing_group_id.'/'.$entity->name)));
     }
   }
 
