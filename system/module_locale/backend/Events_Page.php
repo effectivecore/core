7c0545a0b5cb5183f6e1c33736f948c68c5f3917
Maxim Rysevets <root@effcore.com>
2019-08-29 20:58:56 +0300

    sprint_529_136

--- system/module_locale/backend/Events_Page.php
+++ system/module_locale/backend/Events_Page.php
@@ -8,18 +8,21 @@ namespace effcore\modules\locales {
           use \effcore\language;
           use \effcore\markup;
           use \effcore\translation;
+          use \effcore\url;
           abstract class events_page {
 
   static function on_show_block_menu_languages($page) {
     $menu = new markup('x-languages');
     foreach (language::get_all() as $c_language) {
-      $title = $c_language->code == 'en' ?
+      $c_title = $c_language->code == 'en' ?
         $c_language->title->en :
-        $c_language->title->en.' ('.$c_language->title->native.')';
-      $href = $page->args_get('base').'/'.$c_language->code;
-      $link = new markup('a', ['href' => $href, 'title' => translation::get('go to %%_title language', ['title' => $c_language->title->en], 'en')], $title);
-      $link_wrapper = new markup('x-language', ['data-code' => $c_language->code], $link);
-      $menu->child_insert($link_wrapper, $c_language->code);
+        $c_language->title->en.   ' ('.
+        $c_language->title->native.')';
+      $c_href = $page->args_get('base').'/'.$c_language->code;
+      if (url::is_active($c_href))
+           $c_link = new markup('a', ['href' => $c_href, 'title' => translation::get('go to %%_title language', ['title' => $c_language->title->en], 'en'), 'aria-selected' => 'true'], $c_title);
+      else $c_link = new markup('a', ['href' => $c_href, 'title' => translation::get('go to %%_title language', ['title' => $c_language->title->en], 'en')                           ], $c_title);
+      $menu->child_insert(new markup('x-language', ['data-code' => $c_language->code], $c_link), $c_language->code);
     }
     return $menu;
   }
