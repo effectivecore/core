9e52e6210f2e3783cba83b1527cd618dab244df8
Maxim Rysevets <root@effcore.com>
2019-09-01 16:49:22 +0300

    sprint_529_149

--- system/module_core/backend/Events_Page_Info.php
+++ system/module_core/backend/Events_Page_Info.php
@@ -21,7 +21,7 @@ namespace effcore\modules\core {
 
   static function on_show_block_system_info($page) {
     $logo      = new markup('x-logo',      [], new markup_simple('img', ['src' => '/'.module::get('page')->path.'frontend/images/logo-system.svg', 'alt' => new text('system logotype'), 'width' => '300']));
-    $copyright = new markup('x-copyright', [], '© 2017—2020 Maxim Rysevets. All rights reserved.');
+    $copyright = new markup('x-copyright', [], 'Copyright © 2017—2020 Maxim Rysevets. All rights reserved.');
     $build     = new markup('x-build',     [], [
       new markup('x-title', [], 'Build number'),
       new markup('x-value', [], storage::get('files')->select('bundle/system/build'))]);
