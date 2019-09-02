71979132b1bf21b877db245c911aea16005058ae
Maxim Rysevets <root@effcore.com>
2019-09-02 18:49:26 +0300

    sprint_529_167

--- system/module_storage/backend/Events_Storage.php
+++ system/module_storage/backend/Events_Storage.php
@@ -5,52 +5,9 @@
   ##################################################################
 
 namespace effcore\modules\storage {
-          use const \effcore\br;
           use const \effcore\dir_root;
-          use \effcore\console;
-          use \effcore\core;
           use \effcore\field_file;
-          use \effcore\timer;
           abstract class events_storage {
-
-  static function on_init_before($event, $storage) {
-    timer::tap('storage init');
-  }
-
-  static function on_init_after($event, $storage) {
-    timer::tap('storage init');
-    console::log_insert('storage', 'init.', 'storage %%_name was initialized', 'ok',
-      timer::period_get('storage init', -1, -2), ['name' => $storage->name]
-    );
-  }
-
-  static function on_query_before($event, $storage, $query) {
-    $query_hash = core::hash_get_data($query);
-    timer::tap('storage query with hash: '.$query_hash);
-  }
-
-  static function on_query_after($event, $storage, $query, $result, $errors) {
-    $args_trimmed = [];
-    foreach ($storage->args as $c_arg) {
-      $args_trimmed[] = mb_strimwidth($c_arg, 0, 40, '…', 'UTF-8');
-    }
-    $query_hash = core::hash_get_data($query);
-    $query_prepared = $query;
-    $storage->query_prepare($query_prepared, true);
-    $query_flat = core::array_values_select_recursive($query_prepared);
-    $query_flat_string = implode(' ', $query_flat).';';
-    $query_flat_string_beautiful = str_replace([' ,', '( ', ' )'], [',', '(', ')'], $query_flat_string);
-    $query_args_beautiful = '\''.implode('\', \'', $args_trimmed).'\'';
-    timer::tap('storage query with hash: '.$query_hash);
-    console::log_insert('storage', 'query',
-      count($storage->args) ? 'sql query = "%%_query"'.($errors[0] == '00000' ? br : '; ').'arguments = [%%_args]' :
-                              'sql query = "%%_query"',
-      $errors[0] == '00000' ? 'ok' : 'error',
-      timer::period_get('storage query with hash: '.$query_hash, -1, -2),
-      [ 'query' => $query_flat_string_beautiful,
-         'args' => $query_args_beautiful ]
-    );
-  }
 
   static function on_instance_delete_before($event, $instance) {
     $entity = $instance->entity_get();
