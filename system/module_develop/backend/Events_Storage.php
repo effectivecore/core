4b63372dd482f0222ceebe72161389d0f7a32cc5
Maxim Rysevets <root@effcore.com>
2019-09-11 21:54:15 +0300

    sprint_529_230

--- system/module_develop/backend/Events_Storage.php
+++ system/module_develop/backend/Events_Storage.php
@@ -28,26 +28,27 @@ namespace effcore\modules\develop {
   }
 
   static function on_query_after($event, $storage, $query, $result, $errors) {
-    $args_trimmed = [];
-    foreach ($storage->args as $c_arg) {
-      $args_trimmed[] = mb_strimwidth($c_arg, 0, 40, '…', 'UTF-8');
+    if ($errors[0] == '00000') {
+      timer::tap('storage query with hash: '.$query_hash);
+      $args_trimmed = [];
+      foreach ($storage->args as $c_arg)
+        $args_trimmed[] = mb_strimwidth($c_arg, 0, 40, '…', 'UTF-8');
+      $query_hash = core::hash_get_data($query);
+      $query_prepared = $query;
+      $storage->query_prepare($query_prepared, true);
+      $query_flat = core::array_values_select_recursive($query_prepared);
+      $query_flat_string = implode(' ', $query_flat).';';
+      $query_beautiful = str_replace([' ,', '( ', ' )'], [',', '(', ')'], $query_flat_string);
+      $query_beautiful_args = '\''.implode('\', \'', $args_trimmed).'\'';
+      $query_time = timer::period_get('storage query with hash: '.$query_hash, -1, -2);
+      console::log_insert('storage', 'query', count($storage->args) ?
+        'sql query = "%%_query"'.br.'arguments = [%%_args]' :
+        'sql query = "%%_query"',
+        'ok',      $query_time, [
+        'query' => $query_beautiful,
+        'args'  => $query_beautiful_args
+      ]);
     }
-    $query_hash = core::hash_get_data($query);
-    $query_prepared = $query;
-    $storage->query_prepare($query_prepared, true);
-    $query_flat = core::array_values_select_recursive($query_prepared);
-    $query_flat_string = implode(' ', $query_flat).';';
-    $query_beautiful = str_replace([' ,', '( ', ' )'], [',', '(', ')'], $query_flat_string);
-    $query_beautiful_args = '\''.implode('\', \'', $args_trimmed).'\'';
-    timer::tap('storage query with hash: '.$query_hash);
-    console::log_insert('storage', 'query', count($storage->args) ?
-      'sql query = "%%_query"'.($errors[0] == '00000' ? br : '; ').'arguments = [%%_args]' :
-      'sql query = "%%_query"',
-      $errors[0] == '00000' ? 'ok' : 'error',
-      timer::period_get('storage query with hash: '.$query_hash, -1, -2), [
-      'query' => $query_beautiful,
-      'args'  => $query_beautiful_args
-    ]);
   }
 
 }}
