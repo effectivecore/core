e16daa6309153cfd0466cc16c5f911c99b4feb3c
Maxim Rysevets <root@effcore.com>
2019-09-20 11:12:05 +0300

    sprint_529_261

--- system/module_storage/backend/patterns/Group_Selection_field_Manage.php
+++ system/module_storage/backend/patterns/Group_Selection_field_Manage.php
@@ -21,11 +21,34 @@ namespace effcore {
         if ($entity_field) {
           $button_delete = new button('', ['data-style' => 'narrow-delete']);
           $button_delete->build();
-          $button_delete->value_set('button_delete_field_'.$this->entity_field_name);
+          $button_delete->value_set('button_field_delete_'.$this->entity_field_name);
           $this->child_insert($button_delete, 'button');
-          $this->child_insert(new markup('x-title', [], $entity->title ?? 'LOST PART'            ), 'title');
-          $this->child_insert(new markup('x-id',    [], new text_simple($this->entity_field_name)), 'id'   );
+          $this->child_insert(new markup('x-title', [], [$entity->title, ': ', $entity_field->title]), 'title');
+          $this->child_insert(new markup('x-id',    [], [
+            new text_simple($this->entity_name), '.',
+            new text_simple($this->entity_field_name)]), 'id');
           $this->is_builded = true;
+        }
+      }
+    }
+  }
+
+  ###########################
+  ### static declarations ###
+  ###########################
+
+  static function submit(&$group, $form, $npath) {
+    $button = $group->child_select('button');
+    if ($button->is_clicked()) {
+      $fields = $form->validation_cache_get('fields');
+      foreach ($fields as $c_id => $c_field) {
+        if ($c_field->entity_name       == $group->entity_name &&
+            $c_field->entity_field_name == $group->entity_field_name) {
+          unset($fields[$c_id]);
+          $form->validation_cache_set('fields', $fields);
+          message::insert(new text('Field from entity "%%_name" with id = "%%_id" was deleted.', ['name' => $group->entity_name, 'id' => $group->entity_field_name]));
+          message::insert(new text('Click the button "%%_name" to save your changes!', ['name' => translation::get('update')]), 'warning');
+          return true;
         }
       }
     }
