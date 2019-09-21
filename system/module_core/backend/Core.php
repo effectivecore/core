68479921fd2a90ad087624439b18b39186793e60
Maxim Rysevets <root@effcore.com>
2019-09-21 10:22:56 +0300

    sprint_529_270

--- system/module_core/backend/Core.php
+++ system/module_core/backend/Core.php
@@ -840,6 +840,32 @@ namespace effcore {
                   $matches['full_name_unknown'];
   }
 
+  #############
+  ### image ###
+  #############
+
+  static function thumbnail_create($src_path, $dst_path, $dst_w = 100) {
+    $type = @exif_imagetype($src_path);
+    if ($type !== false) {
+      if ($type == IMAGETYPE_GIF  && function_exists('imagecreatefromgif' )) $src_resource = @imagecreatefromgif ($src_path);
+      if ($type == IMAGETYPE_JPEG && function_exists('imagecreatefromjpeg')) $src_resource = @imagecreatefromjpeg($src_path);
+      if ($type == IMAGETYPE_PNG  && function_exists('imagecreatefrompng' )) $src_resource = @imagecreatefrompng ($src_path);
+      if (isset($src_resource) &&
+                $src_resource) {
+        $src_w = imagesx($src_resource);
+        $src_h = imagesy($src_resource);
+        $ratio = $src_w / $src_h;
+        $dst_h = $dst_w * $ratio;
+        $dst_resource = @imagecreatetruecolor($dst_w, $dst_h);
+        if ($dst_resource) {
+          @imagecopyresampled($dst_resource, $src_resource, 0, 0, 0, 0, $dst_w, $dst_h, $src_w, $src_h);
+          $result = @imagepng($dst_resource, $dst_path);
+          return $result;
+        }
+      }
+    }
+  }
+
   ########################
   ### shared functions ###
   ########################
