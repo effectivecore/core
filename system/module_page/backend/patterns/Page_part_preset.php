c9e0012623f282a2412829c588a95d12147cc2b1
Maxim Rysevets <root@effcore.com>
2019-09-25 17:42:45 +0300

    sprint_529_288

--- system/module_page/backend/patterns/Page_part_preset.php
+++ system/module_page/backend/patterns/Page_part_preset.php
@@ -12,6 +12,17 @@ namespace effcore {
   public $managing_title;
   public $in_areas;
 
+  function __construct($id = null, $managing_group = null, $managing_title = null, $in_areas = null, $display = null, $type = null, $source = null, $weight = 0) {
+    if ($id)             $this->id             = $id;
+    if ($managing_group) $this->managing_group = $managing_group;
+    if ($managing_title) $this->managing_title = $managing_title;
+    if ($in_areas)       $this->in_areas       = $in_areas;
+    if ($display)        $this->display        = $display;
+    if ($type)           $this->type           = $type;
+    if ($source)         $this->source         = $source;
+    parent::__construct($weight);
+  }
+
   function page_part_make() {
     $preset = static::select($this->id);
     if ($preset) {
@@ -28,13 +39,18 @@ namespace effcore {
   ###########################
 
   static protected $cache;
+  static protected $is_init_nosql   = false;
+  static protected $is_init_dynamic = false;
 
   static function cache_cleaning() {
     static::$cache = null;
+    static::$is_init_nosql   = false;
+    static::$is_init_dynamic = false;
   }
 
   static function init() {
-    if (static::$cache == null) {
+    if (!static::$is_init_nosql) {
+         static::$is_init_nosql = true;
       foreach (storage::get('files')->select('page_part_presets') as $c_module_id => $c_presets) {
         foreach ($c_presets as $c_preset) {
           if (isset(static::$cache[$c_preset->id])) console::log_insert_about_duplicate('page_part_preset', $c_preset->id, $c_module_id);
@@ -45,8 +61,16 @@ namespace effcore {
     }
   }
 
-  static function select_all($id_area = null) {
-    static::init();
+  static function init_dynamic() {
+    if (!static::$is_init_dynamic) {
+         static::$is_init_dynamic = true;
+      event::start('on_page_parts_init_dynamic');
+    }
+  }
+
+  static function select_all($id_area = null, $with_dynamic = true) {
+                       static::init        ();
+    if ($with_dynamic) static::init_dynamic();
     $result = static::$cache;
     if ($id_area)
       foreach ($result as $c_id => $c_preset)
@@ -59,6 +83,14 @@ namespace effcore {
   static function select($id) {
     static::init();
     return static::$cache[$id] ?? null;
+  }
+
+  static function insert($id, $managing_group = null, $managing_title, $in_areas = null, $display = null, $type = null, $source = null, $weight = 0, $module_id = null) {
+    static::init();
+    $new_part = new static($id, $managing_group, $managing_title, $in_areas, $display, $type, $source, $weight);
+           static::$cache[$id] = $new_part;
+           static::$cache[$id]->module_id = $module_id;
+    return static::$cache[$id];
   }
 
 }}
