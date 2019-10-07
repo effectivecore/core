b98d1bd1373b18b0aff5fa91799acd95be15a0cf
Maxim Rysevets <root@effcore.com>
2019-10-07 21:33:31 +0300

    sprint_529_355

---
+++ system/module_locale/module_translation_ru/backend/Events_Module.php
@@ -0,0 +1,32 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2020 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\translation {
+          use \effcore\language;
+          use \effcore\message;
+          use \effcore\module;
+          use \effcore\storage;
+          use \effcore\text;
+          abstract class events_module {
+
+  static function on_enable() {
+    $module = module::get('translation');
+    $module->enable();
+    message::insert(new text('Translations for language %%_name was inserted.', ['name' => language::get('ru')->title->en]));
+    message::insert(new text('Language %%_name was inserted.',                  ['name' => language::get('ru')->title->en]));
+    message::insert(new text('Language %%_name was enabled.',                   ['name' => language::get('ru')->title->en]));
+    storage::get('files')->changes_insert('locales', 'update', 'settings/locales/lang_code', 'ru');
+    language::code_set_current('ru');
+  }
+
+  static function on_disable() {
+    $module = module::get('translation');
+    $module->disable();
+    storage::get('files')->changes_insert('locales', 'update', 'settings/locales/lang_code', 'en');
+    language::code_set_current('en');
+  }
+
+}}
