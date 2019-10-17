1b9c8dfac44571184e61c684eddb9dbdfff4e721
Maxim Rysevets <root@effcore.com>
2019-10-17 23:16:51 +0300

    sprint_529_457

--- system/module_locale/module_translation_ru/backend/Events_Module.php
+++ system/module_locale/module_translation_ru/backend/Events_Module.php
@@ -24,8 +24,10 @@ namespace effcore\modules\translation_ru {
   static function on_disable() {
     $module = module::get('translation_ru');
     $module->disable();
-    storage::get('files')->changes_insert('locales', 'update', 'settings/locales/lang_code', 'en');
-    language::code_set_current('en');
+    if (language::code_get_current() == 'ru') {
+      storage::get('files')->changes_insert('locales', 'update', 'settings/locales/lang_code', 'en');
+      language::code_set_current('en');
+    }
   }
 
 }}
