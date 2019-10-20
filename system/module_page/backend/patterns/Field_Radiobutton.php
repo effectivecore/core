a56a4cd3ce10d588fd4b02dd656118148c3b1d14
Maxim Rysevets <root@effcore.com>
2019-10-20 16:49:21 +0300

    sprint_529_480

--- system/module_page/backend/patterns/Field_Radiobutton.php
+++ system/module_page/backend/patterns/Field_Radiobutton.php
@@ -43,7 +43,7 @@ namespace effcore {
   static function validate_required($field, $form, $element, &$new_values) {
     if ($field->required_get() && !core::in_array_string_compare($field->value_get(), $new_values)) {
       $field->error_set(
-        'Field "%%_title" must be checked!', ['title' => translation::get($field->title)]
+        'Field "%%_title" must be enabled!', ['title' => translation::get($field->title)]
       );
     } else {
       return true;
