4bfe906218ad6770f6fc627e50224381d880599c
Maxim Rysevets <root@effcore.com>
2019-10-22 15:46:07 +0300

    sprint_529_1000

--- system/module_core/backend/Core.php
+++ system/module_core/backend/Core.php
@@ -364,8 +364,8 @@ namespace effcore {
     $result = [];
     foreach ($array as $c_key => &$c_value) {
       $c_dpath = $dpath ? $dpath.'/'.$c_key : $c_key;
-      if (is_array($c_value)) $result += static::array_values_select_recursive($c_value, $all, $c_dpath);
-      if (is_array($c_value) == false || $all) $result[$c_dpath] = &$c_value;
+      if (is_array($c_value)                 ) $result += static::array_values_select_recursive($c_value, $all, $c_dpath);
+      if (is_array($c_value) == false || $all) $result[$c_dpath] =                             &$c_value;
     }
     return $result;
   }
@@ -411,8 +411,8 @@ namespace effcore {
     $result = [];
     foreach ($data as $c_key => &$c_value) {
       $c_dpath = $dpath ? $dpath.'/'.$c_key : $c_key;
-      if ((is_array($c_value) || is_object($c_value))) $result += static::arrobj_select_values_recursive($c_value, $all, $c_dpath);
-      if ((is_array($c_value) || is_object($c_value)) == false || $all) $result[$c_dpath] = &$c_value;
+      if ((is_array($c_value) || is_object($c_value))                 ) $result += static::arrobj_select_values_recursive($c_value, $all, $c_dpath);
+      if ((is_array($c_value) || is_object($c_value)) == false || $all) $result[$c_dpath] =                              &$c_value;
     }
     return $result;
   }
