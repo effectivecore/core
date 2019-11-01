72e6b837c4eda4c8337c30250bd68a5fd8c07fc9
Maxim Rysevets <root@effcore.com>
2019-11-01 12:03:40 +0300

    sprint_1008_1

--- system/module_page/backend/Events_Page.php
+++ system/module_page/backend/Events_Page.php
@@ -7,12 +7,22 @@
 namespace effcore\modules\page {
           use \effcore\markup;
           use \effcore\text;
+          use \effcore\url;
+          use \effcore\user;
           abstract class events_page {
 
   static function on_show_title($page) {
     return new markup('h1', ['id' => 'title'],
       new text($page->title, [], true, true)
     );
+  }
+
+  static function on_show_page_actions($page) {
+    if ($page->origin == 'sql' && isset(user::get_current()->roles['admins'])) {
+      return new markup('x-page-actions', [],
+        new markup('a', ['data-id' => 'update', 'href' => '/manage/data/content/page/'.$page->id.'/update?'.url::back_part_make()], 'update this page')
+      );
+    }
   }
 
 }}
