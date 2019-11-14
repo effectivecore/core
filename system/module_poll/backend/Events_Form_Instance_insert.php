7e2a4427c62c395b1f1c6a1baddfd300ef3cd51e
Maxim Rysevets <root@effcore.com>
2019-11-14 16:44:08 +0300

    sprint_1012_19

---
+++ system/module_poll/backend/Events_Form_Instance_insert.php
@@ -0,0 +1,16 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2020 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\poll {
+          abstract class events_form_instance_insert {
+
+  static function on_init($event, $form, $items) {
+  }
+
+  static function on_submit($event, $form, $items) {
+  }
+
+}}
