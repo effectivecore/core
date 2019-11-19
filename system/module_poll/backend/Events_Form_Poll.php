49d26a9996251ab131bfad89f76d37d6e3b9ab2b
Maxim Rysevets <root@effcore.com>
2019-11-19 23:16:42 +0300

    sprint_1012_52

--- system/module_poll/backend/Events_Form_Poll.php
+++ system/module_poll/backend/Events_Form_Poll.php
@@ -5,10 +5,37 @@
   ##################################################################
 
 namespace effcore\modules\polls {
+          use \effcore\core;
+          use \effcore\instance;
           use \effcore\message;
           abstract class events_form_poll {
 
   static function on_init($event, $form, $items) {
+    $poll = new instance('poll', ['id' => 1]);
+    if ($poll->select()) {
+      $items['fields']->title = $poll->question;
+      if ($poll->expired > core::datetime_get()) {
+        foreach ($poll->data['answers'] as $c_id => $c_text) {
+          $items['*answers']->field_insert(
+            $c_text, null, ['value' => $c_id], $c_id
+          );
+        }
+      } else {
+      }
+    }
+  }
+
+  static function on_validate($event, $form, $items) {
+    switch ($form->clicked_button->value_get()) {
+      case 'vote':
+        if (!$items['*answers']->value_get()) {
+          message::insert('No one item was selected!', 'warning');
+          foreach ($items['#answers'] as $c_item) {
+            $c_item->error_set();
+          }
+        }
+        break;
+    }
   }
 
   static function on_submit($event, $form, $items) {
