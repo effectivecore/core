f38f7d4f2a99fbe3beaa1824a5464e87f34118cf
Maxim Rysevets <root@effcore.com>
2019-11-21 21:53:17 +0300

    sprint_1012_68

---
+++ system/module_poll/backend/Events_Page.php
@@ -0,0 +1,13 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2020 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\polls {
+          abstract class events_page {
+
+  static function on_page_parts_dynamic_build($event, $id = null) {
+  }
+
+}}
