8885e609c827ad5159c1eae35faf413b5033202e
Maxim Rysevets <root@effcore.com>
2019-11-22 17:01:42 +0300

    sprint_1012_72

--- system/module_poll/backend/Events_Page.php
+++ system/module_poll/backend/Events_Page.php
@@ -5,9 +5,25 @@
   ##################################################################
 
 namespace effcore\modules\polls {
+          use \effcore\entity;
+          use \effcore\instance;
+          use \effcore\page_part_preset;
+          use \effcore\translation;
           abstract class events_page {
 
   static function on_page_parts_dynamic_build($event, $id = null) {
+    if ($id === null) {
+      foreach (entity::get('poll')->instances_select() as $c_poll) {
+        page_part_preset::insert('poll_form_'.$c_poll->id, translation::get('Poll').' (SQL)', $c_poll->question, ['content' => 'content'], null, 'link', 'forms/polls/poll', ['_id_poll' => $c_poll->id], [], 0, 'polls');
+      }
+    }
+    if ($id !== null && strpos($id, 'poll_form_') === 0) {
+      $id_poll = substr($id, strlen('poll_form_'));
+      $c_poll = (new instance('poll', ['id' => $id_poll]))->select();
+      if ($c_poll) {
+        page_part_preset::insert('poll_form_'.$c_poll->id, translation::get('Poll').' (SQL)', $c_poll->question, ['content' => 'content'], null, 'link', 'forms/polls/poll', ['_id_poll' => $c_poll->id], [], 0, 'polls');
+      }
+    }
   }
 
 }}
