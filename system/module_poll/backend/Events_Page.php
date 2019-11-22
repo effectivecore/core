ab429981c58943b1d13d99f73a9c9e119a07d203
Maxim Rysevets <root@effcore.com>
2019-11-22 18:37:01 +0300

    sprint_1012_73

--- system/module_poll/backend/Events_Page.php
+++ system/module_poll/backend/Events_Page.php
@@ -14,14 +14,14 @@ namespace effcore\modules\polls {
   static function on_page_parts_dynamic_build($event, $id = null) {
     if ($id === null) {
       foreach (entity::get('poll')->instances_select() as $c_poll) {
-        page_part_preset::insert('poll_form_'.$c_poll->id, translation::get('Poll').' (SQL)', $c_poll->question, ['content' => 'content'], null, 'link', 'forms/polls/poll', ['_id_poll' => $c_poll->id], [], 0, 'polls');
+        page_part_preset::insert('poll_form_'.$c_poll->id, translation::get('Poll').' (SQL)', $c_poll->question, ['content' => 'content'], null, 'copy', 'forms/polls/poll', ['_id_poll' => $c_poll->id], [], 0, 'polls');
       }
     }
     if ($id !== null && strpos($id, 'poll_form_') === 0) {
       $id_poll = substr($id, strlen('poll_form_'));
       $c_poll = (new instance('poll', ['id' => $id_poll]))->select();
       if ($c_poll) {
-        page_part_preset::insert('poll_form_'.$c_poll->id, translation::get('Poll').' (SQL)', $c_poll->question, ['content' => 'content'], null, 'link', 'forms/polls/poll', ['_id_poll' => $c_poll->id], [], 0, 'polls');
+        page_part_preset::insert('poll_form_'.$c_poll->id, translation::get('Poll').' (SQL)', $c_poll->question, ['content' => 'content'], null, 'copy', 'forms/polls/poll', ['_id_poll' => $c_poll->id], [], 0, 'polls');
       }
     }
   }
