eb661d886bfd820f8c642cfbdd1859b2991d9b9e
Maxim Rysevets <root@effcore.com>
2019-11-26 18:46:03 +0300

    sprint_1014_6

--- system/module_storage/backend/patterns/Entity.php
+++ system/module_storage/backend/patterns/Entity.php
@@ -38,7 +38,7 @@ namespace effcore {
       $this->fields['is_embed']->not_null = true;
       $this->fields['is_embed']->default = 0;
       $this->fields['is_embed']->managing_on_select_is_enabled = true;
-      $this->fields['is_embed']->managing_form_class = '\\effcore\\field_switcher';
+      $this->fields['is_embed']->managing_field_class = '\\effcore\\field_switcher';
       $this->fields['is_embed']->managing_field_properties['weight'] = 95;
       $this->fields['is_embed']->managing_form_element_attributes['disabled'] = true;
       $this->fields['is_embed']->managing_selection_params['is_apply_translation'] = true;
