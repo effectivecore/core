f34fc721e97e50585db106333d50f170c217b436
Maxim Rysevets <root@effcore.com>
2019-11-28 22:42:50 +0300

    sprint_1015_1

--- system/module_page/frontend/page.js
+++ system/module_page/frontend/page.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
 
 /* this code activate hover state on ios devices */
+
   document.addEventListener('touchstart', function(){}, false);
 
 });
