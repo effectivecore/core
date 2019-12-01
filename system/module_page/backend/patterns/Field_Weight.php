c8c50e688c99110e9e26e7e98ad93fa792a28973
Maxim Rysevets <root@effcore.com>
2019-12-01 19:19:00 +0300

    sprint_1015_16

--- system/module_page/backend/patterns/Field_Weight.php
+++ system/module_page/backend/patterns/Field_Weight.php
@@ -7,14 +7,17 @@
 namespace effcore {
           class field_weight extends field_number {
 
+  const input_min_weight = -1000;
+  const input_max_weight = +1000;
+
   public $title = 'Weight';
   public $attributes = ['data-type' => 'weight'];
   public $element_attributes = [
     'type'     => 'number',
     'name'     => 'weight',
     'required' => true,
-    'min'      => -1000,
-    'max'      => +1000,
+    'min'      => self::input_min_weight,
+    'max'      => self::input_max_weight,
     'step'     => 1,
     'value'    => 0
   ];
