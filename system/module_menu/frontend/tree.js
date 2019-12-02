92be102593c9a3adfbc6eb28ff1dfc56dc4aa968
Maxim Rysevets <root@effcore.com>
2019-12-02 16:18:35 +0300

    sprint_1016_3

--- system/module_menu/frontend/tree.js
+++ system/module_menu/frontend/tree.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
 
+/* rearrangeable */
+
+  document._select_all('x-tree[data-visualization-mode="simple-rearrangeable"]').forEach(function(c_has_rearrangeable){
+    c_has_rearrangeable.setAttribute('data-js-is-processed', 'true');
+
+  });
+
 });
