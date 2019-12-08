ecdc2abf7fafec7ecdf80a1bf3038622336a0d08
Maxim Rysevets <root@effcore.com>
2019-12-08 15:45:52 +0300

    sprint_1017_14

--- system/module_storage/backend/patterns/Widget_Selection_fields.php
+++ system/module_storage/backend/patterns/Widget_Selection_fields.php
@@ -18,13 +18,15 @@ namespace effcore {
 
   function build() {
     if (!$this->is_builded) {
+      $widgets_group = new markup('x-widgets-group', ['data-has-rearrangeable' => 'true']);
       foreach ($this->fields as $c_id => $c_info) {
         $c_widget_manage = new widget_selection_field_manage($c_info->entity_name, $c_info->entity_field_name);
         $c_widget_manage->build();
-        $this->child_insert($c_widget_manage, $c_id);}
+        $widgets_group->child_insert($c_widget_manage, $c_id);}
       $widget_insert = new widget_selection_field_insert;
       $widget_insert->build();
-      $this->child_insert($widget_insert, 'field_insert');
+      $this->child_insert($widgets_group, 'manage');
+      $this->child_insert($widget_insert, 'insert');
       $this->is_builded = true;
     }
   }
