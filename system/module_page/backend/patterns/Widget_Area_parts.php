83b9dd24526bca8e42a502d119b9659a290a4a5f
Maxim Rysevets <root@effcore.com>
2019-12-10 15:13:50 +0300

    sprint_1017_21

--- system/module_page/backend/patterns/Widget_Area_parts.php
+++ system/module_page/backend/patterns/Widget_Area_parts.php
@@ -20,11 +20,13 @@ namespace effcore {
 
   function build() {
     if (!$this->is_builded) {
+      $c_weight = 0;
       $widgets_group = new markup('x-widgets-group', ['data-has-rearrangeable' => 'true']);
       foreach ($this->presets as $c_id_preset) {
-        $c_widget_manage = new widget_area_part_manage($this->id_area, $c_id_preset);
+        $c_widget_manage = new widget_area_part_manage($this->id_area, $c_id_preset, [], $c_weight);
         $c_widget_manage->build();
-        $widgets_group->child_insert($c_widget_manage, $c_id_preset);}
+        $widgets_group->child_insert($c_widget_manage, $c_id_preset);
+        $c_weight -= 5;}
       $widget_insert = new widget_area_part_insert($this->id_area);
       $widget_insert->build();
       $this->child_insert($widgets_group, 'widgets_group');
