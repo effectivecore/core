37cecafd9078bd8f7514eeb142be42423c3a8819
Maxim Rysevets <root@effcore.com>
2019-12-12 21:24:38 +0300

    sprint_1017_35

--- system/module_storage/backend/patterns/Widget_Selection_fields.php
+++ system/module_storage/backend/patterns/Widget_Selection_fields.php
@@ -9,10 +9,8 @@ namespace effcore {
 
   public $tag_name = 'x-widget';
   public $attributes = ['data-type' => 'selection_fields'];
-  public $fields = [];
 
-  function __construct($fields, $attributes = [], $weight = 0) {
-    $this->fields = $fields;
+  function __construct($attributes = [], $weight = 0) {
     parent::__construct(null, null, null, $attributes, [], $weight);
   }
 
@@ -20,7 +18,7 @@ namespace effcore {
     if (!$this->is_builded) {
       $c_weight = 0;
       $widgets_manage_group = new markup('x-widgets-group', ['data-has-rearrangeable' => 'true']);
-      foreach ($this->fields as $c_id => $c_info) {
+      foreach ($this->cform->validation_cache_get('fields') as $c_id => $c_info) {
         $c_widget_manage = new widget_selection_field_manage($c_info->entity_name, $c_info->entity_field_name, [], $c_weight);
         $c_widget_manage->build();
         $c_widget_manage->on_click_delete_handler = function ($group, $form, $npath) {$this->on_click_delete($group, $form, $npath);};
@@ -44,6 +42,9 @@ namespace effcore {
       'entity_field_name' => $entity_info[1]];
     $form->validation_cache_is_persistent = true;
     $form->validation_cache_set('fields', $fields);
+    $this->is_builded = false;
+    $this->build();
+  # report
     $entity = entity::get(             $entity_info[0]);
     $entity_field = $entity->field_get($entity_info[1]);
     message::insert(new text('Field "%%_title" (%%_id) was inserted.', ['title' => translation::get($entity->title).': '.translation::get($entity_field->title), 'id' => $entity_info[0].'.'.$entity_info[1]]));
@@ -60,6 +61,9 @@ namespace effcore {
         unset($fields[$c_row_id]);
         $form->validation_cache_is_persistent = true;
         $form->validation_cache_set('fields', $fields);
+        $this->is_builded = false;
+        $this->build();
+      # report
         $entity = entity::get($group->entity_name);
         $entity_field = $entity ? $entity->field_get($group->entity_field_name) : null;
         message::insert(new text('Field "%%_title" (%%_id) was deleted.', ['title' => isset($entity_field->title) ? translation::get($entity->title).': '.translation::get($entity_field->title) : 'LOST PART', 'id' => $group->entity_name.'.'.$group->entity_field_name]));
