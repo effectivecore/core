77092bf07d087297654ee23b243bc1dfc7c9f38c
Maxim Rysevets <root@effcore.com>
2019-12-12 17:23:29 +0300

    sprint_1017_32

--- system/module_core/backend/Cache.php
+++ system/module_core/backend/Cache.php
@@ -32,7 +32,7 @@ namespace effcore {
 
   static function update($name, $data, $sub_dirs = '', $info = null) {
     if (parent::update($name, $data, $sub_dirs, $info)) {
-      console::log_insert('storage', 'cache', 'cache for '.$name.' was rebuilded', 'ok', 0);
+      console::log_insert('storage', 'cache', 'cache for '.$name.' was rebuild', 'ok');
     }
   }
 
