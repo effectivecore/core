04c79c706eb39d6f276c5e29a73d4fc2149580dd
Maxim Rysevets <root@effcore.com>
2019-12-13 17:55:25 +0300

    sprint_1017_43

--- system/module_page/backend/patterns/Widget_Area_part_manage.php
+++ system/module_page/backend/patterns/Widget_Area_part_manage.php
@@ -21,19 +21,19 @@ namespace effcore {
 
   function build() {
     if (!$this->is_builded) {
-      $group_id = $this->id_preset.'_'.$this->id_area;
+      $field_name_suffix = $this->id_preset.'_'.$this->id_area;
       $preset = page_part_preset::select($this->id_preset);
-      $this->weight = (int)(field::request_value_get('weight_'.$group_id) !== '' ?
-                            field::request_value_get('weight_'.$group_id) : $this->weight);
+      $this->weight = (int)(field::request_value_get('weight_'.$field_name_suffix) !== '' ?
+                            field::request_value_get('weight_'.$field_name_suffix) : $this->weight);
       $field_weight = new field_weight();
       $field_weight->description_state = 'hidden';
       $field_weight->build();
-      $field_weight->name_set('weight_'.$group_id);
+      $field_weight->name_set('weight_'.$field_name_suffix);
       $field_weight->required_set(false);
       $field_weight->value_set($this->weight);
       $button_delete = new button('', ['data-style' => 'narrow-delete', 'title' => new text('Delete')]);
       $button_delete->build();
-      $button_delete->value_set('button_delete_'.$group_id);
+      $button_delete->value_set('button_delete_'.$field_name_suffix);
       $this->child_insert($field_weight,  'field_weight' );
       $this->child_insert($button_delete, 'button_delete');
       $this->child_insert(new markup('x-info', [], [
