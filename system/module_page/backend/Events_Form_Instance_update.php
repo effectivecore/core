c6732208c4646154dc5302923fefce50bf26af1c
Maxim Rysevets <root@effcore.com>
2019-12-15 18:28:39 +0300

    sprint_1017_52

--- system/module_page/backend/Events_Form_Instance_update.php
+++ system/module_page/backend/Events_Form_Instance_update.php
@@ -25,8 +25,6 @@ namespace effcore\modules\page {
           $items['#url']->disabled_set(true);
         }
       # build layout
-        if ($form->validation_cache_get('parts') === null)
-            $form->validation_cache_set('parts', $form->_instance->parts ?: []);
         $layout = core::deep_clone(layout::select($form->_instance->id_layout));
         foreach ($layout->children_select_recursive() as $c_area) {
           if ($c_area instanceof area && $c_area->id) {
@@ -35,8 +33,10 @@ namespace effcore\modules\page {
             $c_area->build();
             $c_widget_area_parts = new widget_area_parts($c_area->id);
             $c_widget_area_parts->form_current_set($form);
+            $c_widget_area_parts->items_set($form->_instance->parts[$c_area->id] ?? []);
             $c_widget_area_parts->build();
             $c_area->child_insert($c_widget_area_parts, 'widget_area_parts');
+            $form->_widgets_area[$c_area->id] = $c_widget_area_parts;
           }
         }
         $form->child_select('fields')->child_insert(
@@ -52,7 +52,14 @@ namespace effcore\modules\page {
     if ($entity) {
       if ($entity->name == 'page' && !empty($form->_instance)) {
         if ($form->clicked_button->value_get() == 'update') {
-          $form->_instance->parts = $form->validation_cache_get('parts') ?: null;
+          $all_parts = [];
+          foreach ($form->_widgets_area as $c_id_area => $c_widget) {
+            $c_parts = $c_widget->items_get();
+            if ($c_parts) {
+              $all_parts[$c_id_area] = $c_parts;
+            }
+          }
+          $form->_instance->parts = count($all_parts) ? $all_parts : null;
         }
       }
     }
