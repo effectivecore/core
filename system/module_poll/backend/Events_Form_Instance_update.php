d03fbcd454e7a11e4761851928ec1b2eaccea036
Maxim Rysevets <root@effcore.com>
2019-12-17 20:14:10 +0300

    sprint_1018_10

--- system/module_poll/backend/Events_Form_Instance_update.php
+++ system/module_poll/backend/Events_Form_Instance_update.php
@@ -19,34 +19,33 @@ namespace effcore\modules\polls {
     $entity = entity::get($entity_name);
     if ($entity) {
       if ($entity->name == 'poll') {
+        $c_weight = 0;
         $fieldset_answers = new fieldset('Answers', null, ['data-has-rearrangeable' => 'true']);
         $form->child_select('fields')->child_insert($fieldset_answers, 'answers');
-        $answers = array_chunk($form->_instance->data['answers'] ?? [], 1, true);
-        $used_ids = core::array_kmap(range(1, 10));
-        for ($i = 0; $i < 10; $i++) {
+        $answers_rows = entity::get('poll_answer')->instances_select(['conditions' => [
+          'id_poll_!f'       => 'id_poll',
+          'id_poll_operator' => '=',
+          'id_poll_!v'       => $form->_instance->id]]);
+        foreach ($answers_rows as $c_answer) {
         # field for answer text
-          $c_answer_id   = isset($answers[$i]) ? key    ($answers[$i]) : '';
-          $c_answer_text = isset($answers[$i]) ? current($answers[$i]) : '';
-          if ($c_answer_id == '') $c_answer_id = current($used_ids);
-          unset($used_ids[$c_answer_id]);
           $c_field_answer_text = new field_text('Text');
           $c_field_answer_text->description_state = 'hidden';
           $c_field_answer_text->build();
-          $c_field_answer_text->name_set('answer_text_'.$c_answer_id);
-          $c_field_answer_text->value_set($c_answer_text);
-          $c_field_answer_text->required_set($i == 0);
+          $c_field_answer_text->name_set('answer_text_'.$c_answer->id);
+          $c_field_answer_text->value_set($c_answer->answer);
         # field for answer weight
           $c_field_answer_weight = new field_weight();
           $c_field_answer_weight->description_state = 'hidden';
           $c_field_answer_weight->build();
-          $c_field_answer_weight->name_set('answer_weight_'.$c_answer_id);
+          $c_field_answer_weight->name_set('answer_weight_'.$c_answer->id);
           $c_field_answer_weight->required_set(false);
-          $c_field_answer_weight->value_set(0 - ($i * 5));
+          $c_field_answer_weight->value_set( -($c_weight * 5) );
+          $c_weight++;
         # group fields to box
           $c_box_answer = new markup('x-widget', ['data-type' => 'poll_answer-manage', 'data-rearrangeable' => 'true', 'data-fields-is-inline' => 'true']);
-          $c_box_answer    ->child_insert($c_field_answer_weight, 'answer_weight');
-          $c_box_answer    ->child_insert($c_field_answer_text,   'answer_text'  );
-          $fieldset_answers->child_insert($c_box_answer,          'answer_'.   $i);
+          $c_box_answer    ->child_insert($c_field_answer_weight, 'answer_weight'        );
+          $c_box_answer    ->child_insert($c_field_answer_text,   'answer_text'          );
+          $fieldset_answers->child_insert($c_box_answer,          'answer_'.$c_answer->id);
         }
       }
     }
