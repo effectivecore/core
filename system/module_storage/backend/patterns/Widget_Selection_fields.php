60c552f16d1c0d28c6ee51b62fdf0c4724307f23
Maxim Rysevets <root@effcore.com>
2019-12-18 11:48:46 +0300

    sprint_1019_6

--- system/module_storage/backend/patterns/Widget_Selection_fields.php
+++ system/module_storage/backend/patterns/Widget_Selection_fields.php
@@ -75,8 +75,12 @@ namespace effcore {
   # report
     $entity = entity::get(             $entity_info[0]);
     $entity_field = $entity->field_get($entity_info[1]);
-    message::insert(new text('Field "%%_title" (%%_id) was inserted.', ['title' => translation::get($entity->title).': '.translation::get($entity_field->title), 'id' => $entity_info[0].'.'.$entity_info[1]]));
-    message::insert(new text('Click the button "%%_name" to save your changes!', ['name' => translation::get('update')]), 'warning');
+    message::insert(new text_multiline([
+      'Field "%%_title" (%%_id) was inserted.',
+      'Click the button "%%_name" to save your changes!'], [
+      'title' => translation::get($entity->title).': '.translation::get($entity_field->title),
+      'id'    => $entity_info[0].'.'.$entity_info[1],
+      'name'  => translation::get('update')]));
     return true;
   }
 
@@ -94,8 +98,12 @@ namespace effcore {
       # report
         $entity = entity::get($group->entity_name);
         $entity_field = $entity ? $entity->field_get($group->entity_field_name) : null;
-        message::insert(new text('Field "%%_title" (%%_id) was deleted.', ['title' => isset($entity_field->title) ? translation::get($entity->title).': '.translation::get($entity_field->title) : 'LOST PART', 'id' => $group->entity_name.'.'.$group->entity_field_name]));
-        message::insert(new text('Click the button "%%_name" to save your changes!', ['name' => translation::get('update')]), 'warning');
+        message::insert(new text_multiline([
+          'Field "%%_title" (%%_id) was deleted.',
+          'Click the button "%%_name" to save your changes!'], [
+          'title' => isset($entity_field->title) ? translation::get($entity->title).': '.translation::get($entity_field->title) : 'LOST PART',
+          'id'    => $group->entity_name.'.'.$group->entity_field_name,
+          'name'  => translation::get('update')]));
         return true;
       }
     }
