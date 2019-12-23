03a6ba4bece496653e928e6c4de32ff94f23347d
Maxim Rysevets <root@effcore.com>
2019-12-23 14:44:49 +0300

    sprint_1020_54

--- system/module_poll/backend/Events_Form_Instance_update.php
+++ system/module_poll/backend/Events_Form_Instance_update.php
@@ -43,6 +43,22 @@ namespace effcore\modules\polls {
     }
   }
 
+  static function on_validate($event, $form, $items) {
+    $entity_name = page::get_current()->args_get('entity_name');
+    $entity = entity::get($entity_name);
+    if ($entity) {
+      switch ($form->clicked_button->value_get()) {
+        case 'update':
+          if ($entity->name == 'poll') {
+            if (count($form->_widget_answers->items_get()) < 2) {
+              $form->error_set('The poll must contain a minimum %%_number responses!', ['number' => 2]);
+            }
+          }
+          break;
+      }
+    }
+  }
+
   static function on_submit($event, $form, $items) {
     $entity_name = page::get_current()->args_get('entity_name');
     $entity = entity::get($entity_name);
