3cea44841021e787c69162e4b5e0f735584a0aec
Maxim Rysevets <root@effcore.com>
2019-12-27 20:21:13 +0300

    sprint_1022_6

--- system/module_page/backend/patterns/Field_Date.php
+++ system/module_page/backend/patterns/Field_Date.php
@@ -49,6 +49,17 @@ namespace effcore {
   ### static declarations ###
   ###########################
 
+  static function on_request_value_set($field, $form, $npath) {
+    $name = $field->name_get();
+    $type = $field->type_get();
+    if ($name && $type) {
+      if ($field->disabled_get()) return true;
+      if ($field->readonly_get()) return true;
+      $new_value = static::request_value_get($name, static::current_number_get($name), $form->source_get());
+      $field->value_set($new_value);
+    }
+  }
+
   static function on_validate($field, $form, $npath) {
     $element = $field->child_select('element');
     $name = $field->name_get();
@@ -58,16 +69,14 @@ namespace effcore {
       if ($field->readonly_get()) return true;
       $new_value = static::request_value_get($name, static::current_number_get($name), $form->source_get());
       $old_value = $field->value_get_initial();
-      $result = static::validate_required  ($field, $form, $element, $new_value) &&
-                static::validate_minlength ($field, $form, $element, $new_value) &&
-                static::validate_maxlength ($field, $form, $element, $new_value) &&
-                static::validate_value     ($field, $form, $element, $new_value) &&
-                static::validate_min       ($field, $form, $element, $new_value) &&
-                static::validate_max       ($field, $form, $element, $new_value) &&
-                static::validate_pattern   ($field, $form, $element, $new_value) && (!empty($field->is_validate_uniqueness) ?
-                static::validate_uniqueness($field, $new_value,      $old_value) : true);
-      $field->value_set($new_value);
-      return $result;
+      return static::validate_required  ($field, $form, $element, $new_value) &&
+             static::validate_minlength ($field, $form, $element, $new_value) &&
+             static::validate_maxlength ($field, $form, $element, $new_value) &&
+             static::validate_value     ($field, $form, $element, $new_value) &&
+             static::validate_min       ($field, $form, $element, $new_value) &&
+             static::validate_max       ($field, $form, $element, $new_value) &&
+             static::validate_pattern   ($field, $form, $element, $new_value) && (!empty($field->is_validate_uniqueness) ?
+             static::validate_uniqueness($field, $new_value,      $old_value) : true);
     }
   }
 
