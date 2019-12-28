661d3c951fe0a7787c46e416260e32a9a1bfac89
Maxim Rysevets <root@effcore.com>
2019-12-28 11:35:13 +0300

    sprint_1022_8

--- system/module_user/backend/patterns/Field_Email.php
+++ system/module_user/backend/patterns/Field_Email.php
@@ -29,7 +29,6 @@ namespace effcore {
       if ($field->disabled_get()) return true;
       if ($field->readonly_get()) return true;
       $new_value = static::request_value_get($name, static::current_number_get($name), $form->source_get());
-      $new_value = strtolower($new_value);
       $field->value_set($new_value);
     }
   }
