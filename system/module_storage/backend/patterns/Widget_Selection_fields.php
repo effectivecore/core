cff87c612988625f4f3bb2c357abb5362d49f839
Maxim Rysevets <root@effcore.com>
2019-12-29 19:06:21 +0300

    sprint_1025_6

--- system/module_storage/backend/patterns/Widget_Selection_fields.php
+++ system/module_storage/backend/patterns/Widget_Selection_fields.php
@@ -11,6 +11,9 @@ namespace effcore {
   public $item_title = 'Field';
 
   function widget_manage_get($item, $c_row_id, $prefix, $entity_name = 'demo_data', $entity_field_name = 'id') {
+    $widget_manage = new markup('x-widget', [
+      'data-rearrangeable'         => 'true',
+      'data-fields-is-inline-full' => 'true'], [], $item->weight);
   # field for weight
     $field_weight = new field_weight;
     $field_weight->description_state = 'hidden';
@@ -30,17 +33,44 @@ namespace effcore {
     $button_delete = new button(null, ['data-style' => 'narrow-delete', 'title' => new text('delete')]);
     $button_delete->break_on_validate = true;
     $button_delete->build();
-    $button_delete->value_set($prefix.'button_delete'.$c_row_id);
+    $button_delete->value_set($prefix.'delete'.$c_row_id);
     $button_delete->_type = 'delete';
     $button_delete->_id = $c_row_id;
   # group the fields in widget 'manage'
-    $widget_manage = new markup('x-widget', [
-      'data-rearrangeable'         => 'true',
-      'data-fields-is-inline-full' => 'true'], [], $item->weight);
     $widget_manage->child_insert($field_weight,  'weight'       );
     $widget_manage->child_insert($data_markup,   'data'         );
     $widget_manage->child_insert($button_delete, 'button_delete');
     return $widget_manage;
+  }
+
+  function widget_insert_get() {
+    $entities = entity::get_all();
+    core::array_sort_by_text_property($entities);
+    $options = ['not_selected' => '- no -'];
+    foreach ($entities as $c_entity) {
+      if (!empty($c_entity->managing_is_enabled)) {
+        foreach ($c_entity->fields as $c_field_name => $c_field_info) {
+          if (!empty($c_field_info->managing_on_select_is_enabled)) {
+            if (!isset($options[$c_entity->name])) {
+                       $options[$c_entity->name] = new \stdClass;
+                       $options[$c_entity->name]->title = $c_entity->title;}
+            $options[$c_entity->name]->values[$c_entity->name.'.'.$c_field_name] = new text_multiline([
+              'title' => $c_field_info->title, 'id' => '('.$c_entity->name.'.'.$c_field_name.')'], [], ' '
+            );
+          }
+        }
+      }
+    }
+    $select = new field_select('Insert field');
+    $select->values = $options;
+    $select->build();
+    $select->name_set($this->unique_prefix.'insert');
+    $select->required_set(false);
+    $button_insert = new button('insert', ['title' => new text('insert')]);
+    $button_insert->break_on_validate = true;
+    $button_insert->build();
+    $button_insert->value_set($this->unique_prefix.'insert');
+    return new node([], [$select, $button_insert]);
   }
 
 }}
