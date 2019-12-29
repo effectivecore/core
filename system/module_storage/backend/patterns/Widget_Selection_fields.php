88ef85d87bb3768cd8240804b1fb72b33515a6ea
Maxim Rysevets <root@effcore.com>
2019-12-29 16:58:27 +0300

    sprint_1025_3

---
+++ system/module_storage/backend/patterns/Widget_Selection_fields.php
@@ -0,0 +1,13 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2020 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore {
+          class widget_selection_fields extends widget_fields {
+
+  public $attributes = ['data-type' => 'selection_fields'];
+  public $item_title = 'Field';
+
+}}
