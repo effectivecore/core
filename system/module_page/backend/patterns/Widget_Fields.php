7903e8fcec7e48ba255fb67f7dfcd8e8a1cf49f2
Maxim Rysevets <root@effcore.com>
2020-01-14 14:40:18 +0300

    sprint_1025_34

--- system/module_page/backend/patterns/Widget_Fields.php
+++ system/module_page/backend/patterns/Widget_Fields.php
@@ -131,8 +131,8 @@ namespace effcore {
   function on_cache_update($form, $npath) {
     $items = $this->items_get();
     foreach ($items as $c_row_id => $c_item) {
-      $c_item->weight = (int)(field::request_value_get($this->unique_prefix.'weight'.$c_row_id));
-      $c_item->text   =       field::request_value_get($this->unique_prefix.'text'.  $c_row_id);}
+      $c_item->weight = (int)$this->_fields['weight'.$c_row_id]->value_get();
+      $c_item->text   =      $this->_fields['text'.  $c_row_id]->value_get();}
     $this->items_set($items);
   }
 
