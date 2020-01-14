5d8042b5659d37483589bfe67795dbbde3ac65e6
Maxim Rysevets <root@effcore.com>
2020-01-14 20:54:18 +0300

    sprint_1026_2

--- system/module_page/backend/Events_Form_Instance_update.php
+++ system/module_page/backend/Events_Form_Instance_update.php
@@ -12,7 +12,8 @@ namespace effcore\modules\page {
           use \effcore\markup;
           use \effcore\page_part_preset_link;
           use \effcore\page;
-          use \effcore\widget_area_parts_old;
+          use \effcore\widget_area_parts;
+/* rem */ use \effcore\widget_area_parts_old;
           abstract class events_form_instance_update {
 
   static function on_init($event, $form, $items) {
@@ -31,12 +32,18 @@ namespace effcore\modules\page {
             $c_area->managing_is_enabled = true;
             $c_area->tag_name = 'div';
             $c_area->build();
-            $c_widget_area_parts_old = new widget_area_parts_old($c_area->id);
-            $c_widget_area_parts_old->form_current_set($form);
-            $c_widget_area_parts_old->items_set_once($form->_instance->parts[$c_area->id] ?? null);
-            $c_widget_area_parts_old->build();
-            $c_area->child_insert($c_widget_area_parts_old, 'widget_area_parts_old');
-            $form->_widgets_area[$c_area->id] = $c_widget_area_parts_old;
+            $c_widget_area_parts = new widget_area_parts('parts_'.$c_area->id.'_');
+            $c_widget_area_parts->form_current_set($form);
+            $c_widget_area_parts->build();
+            $c_widget_area_parts->items_set_once($form->_instance->parts[$c_area->id]);
+            $c_area->child_insert($c_widget_area_parts, 'widget_area_parts');
+            $form->_widgets_area[$c_area->id] = $c_widget_area_parts;
+/* rem */   $c_widget_area_parts_old = new widget_area_parts_old($c_area->id);
+/* rem */   $c_widget_area_parts_old->form_current_set($form);
+/* rem */   $c_widget_area_parts_old->items_set_once($form->_instance->parts[$c_area->id] ?? null);
+/* rem */   $c_widget_area_parts_old->build();
+/* rem */   $c_area->child_insert($c_widget_area_parts_old, 'widget_area_parts_old');
+/* rem */   $form->_widgets_area_old[$c_area->id] = $c_widget_area_parts_old;
           }
         }
         $form->child_select('fields')->child_insert(
@@ -54,7 +61,7 @@ namespace effcore\modules\page {
         case 'update':
           if ($entity->name == 'page' && !empty($form->_instance)) {
             $all_parts = [];
-            foreach ($form->_widgets_area as $c_id_area => $c_widget) {
+            foreach ($form->_widgets_area_old as $c_id_area => $c_widget) {
               $c_parts = $c_widget->items_get_sorted();
               if ($c_parts) {
                 $all_parts[$c_id_area] = $c_parts;
