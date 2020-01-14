205eb190e4b3bfe2d42b2947bb0e925193cbb8a3
Maxim Rysevets <root@effcore.com>
2020-01-14 20:54:18 +0300

    sprint_1026_2

---
+++ system/module_page/backend/patterns/Widget_Area_parts.php
@@ -0,0 +1,13 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2020 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore {
+          class widget_area_parts extends widget_fields {
+
+  public $attributes = ['data-type' => 'area_parts'];
+  public $item_title = 'Part';
+
+}}
