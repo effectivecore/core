90ab70fcdcc0cddaf68b5d8ee05be6a9006ab0cf
Maxim Rysevets <root@effcore.com>
2020-01-15 12:52:41 +0300

    sprint_1026_3

--- system/module_page/backend/Events_Form_Instance_update.php
+++ system/module_page/backend/Events_Form_Instance_update.php
@@ -13,7 +13,6 @@ namespace effcore\modules\page {
           use \effcore\page_part_preset_link;
           use \effcore\page;
           use \effcore\widget_area_parts;
-/* rem */ use \effcore\widget_area_parts_old;
           abstract class events_form_instance_update {
 
   static function on_init($event, $form, $items) {
@@ -32,18 +31,12 @@ namespace effcore\modules\page {
             $c_area->managing_is_enabled = true;
             $c_area->tag_name = 'div';
             $c_area->build();
-            $c_widget_area_parts = new widget_area_parts('parts_'.$c_area->id.'_');
+            $c_widget_area_parts = new widget_area_parts('parts_'.$c_area->id.'_', $c_area->id);
             $c_widget_area_parts->form_current_set($form);
             $c_widget_area_parts->build();
-            $c_widget_area_parts->items_set_once($form->_instance->parts[$c_area->id]);
+            $c_widget_area_parts->items_set_once($form->_instance->parts[$c_area->id] ?? null);
             $c_area->child_insert($c_widget_area_parts, 'widget_area_parts');
             $form->_widgets_area[$c_area->id] = $c_widget_area_parts;
-/* rem */   $c_widget_area_parts_old = new widget_area_parts_old($c_area->id);
-/* rem */   $c_widget_area_parts_old->form_current_set($form);
-/* rem */   $c_widget_area_parts_old->items_set_once($form->_instance->parts[$c_area->id] ?? null);
-/* rem */   $c_widget_area_parts_old->build();
-/* rem */   $c_area->child_insert($c_widget_area_parts_old, 'widget_area_parts_old');
-/* rem */   $form->_widgets_area_old[$c_area->id] = $c_widget_area_parts_old;
           }
         }
         $form->child_select('fields')->child_insert(
@@ -60,16 +53,16 @@ namespace effcore\modules\page {
       switch ($form->clicked_button->value_get()) {
         case 'update':
           if ($entity->name == 'page' && !empty($form->_instance)) {
-            $all_parts = [];
-            foreach ($form->_widgets_area_old as $c_id_area => $c_widget) {
-              $c_parts = $c_widget->items_get_sorted();
-              if ($c_parts) {
-                $all_parts[$c_id_area] = $c_parts;
-              }
-            }
-            if (count($all_parts))
-                 $form->_instance->parts = $all_parts;
-            else $form->_instance->parts = null;
+            //$all_parts = [];
+            //foreach ($form->_widgets_area as $c_id_area => $c_widget) {
+            //  $c_parts = $c_widget->items_get_sorted();
+            //  if ($c_parts) {
+            //    $all_parts[$c_id_area] = $c_parts;
+            //  }
+            //}
+            //if (count($all_parts))
+            //     $form->_instance->parts = $all_parts;
+            //else $form->_instance->parts = null;
           }
           break;
       }
--- system/module_page/backend/patterns/Widget_Area_parts.php
+++ system/module_page/backend/patterns/Widget_Area_parts.php
@@ -9,5 +9,82 @@ namespace effcore {
 
   public $attributes = ['data-type' => 'area_parts'];
   public $item_title = 'Part';
+  public $id_area;
+
+  function __construct($unique_prefix, $id_area, $attributes = [], $weight = 0) {
+    $this->unique_prefix = $unique_prefix;
+    $this->id_area       = $id_area;
+    parent::__construct($unique_prefix, $attributes, $weight);
+  }
+
+  function widget_manage_get($item, $c_row_id, $prefix) {
+    $widget = new markup('x-widget', [
+      'data-rearrangeable'         => 'true',
+      'data-fields-is-inline-full' => 'true'], [], $item->weight);
+  # field for weight
+    $field_weight = new field_weight;
+    $field_weight->description_state = 'hidden';
+    $field_weight->build();
+    $field_weight->name_set($prefix.'weight'.$c_row_id);
+    $field_weight->required_set(false);
+    $field_weight->value_set($item->weight);
+    $this->_fields['weight'.$c_row_id] = $field_weight;
+  # data markup
+    $preset = page_part_preset::select($item->id);
+    $data_markup = new markup('x-info',  [], [
+        'title' => new markup('x-title', [], $preset ? [$preset->managing_group, ': ', $preset->managing_title] : 'LOST PART'),
+        'id'    => new markup('x-id',    [], new text_simple($preset->id)) ]);
+  # button for deletion of the old item
+    $button_delete = new button(null, ['data-style' => 'narrow-delete', 'title' => new text('delete')]);
+    $button_delete->break_on_validate = true;
+    $button_delete->build();
+    $button_delete->value_set($prefix.'delete'.$c_row_id);
+    $button_delete->_type = 'delete';
+    $button_delete->_id = $c_row_id;
+    $this->_buttons['delete'.$c_row_id] = $button_delete;
+  # group the previous elements in widget 'manage'
+    $widget->child_insert($field_weight,  'weight'       );
+    $widget->child_insert($data_markup,   'data'         );
+    $widget->child_insert($button_delete, 'button_delete');
+    return $widget;
+  }
+
+  function widget_insert_get() {
+    $widget = new markup('x-widget', [
+      'data-type' => 'insert']);
+  # field for selection of the type of new item
+    $presets = page_part_preset::select_all($this->id_area);
+    core::array_sort_by_text_property($presets, 'managing_group');
+    $options = ['not_selected' => '- no -'];
+    foreach ($presets as $c_preset) {
+      $c_group_id = core::sanitize_id($c_preset->managing_group);
+      if (!isset($options[$c_group_id])) {
+                 $options[$c_group_id] = new \stdClass;
+                 $options[$c_group_id]->title = $c_preset->managing_group;}
+      $options[$c_group_id]->values[$c_preset->id] = translation::get($c_preset->managing_title).' ('.$c_preset->id.')';
+    }
+    foreach ($options as $c_group) {
+      if ($c_group instanceof \stdClass) {
+        core::array_sort_text($c_group->values);
+      }
+    }
+    $select = new field_select('Insert field');
+    $select->values = $options;
+    $select->build();
+    $select->name_set($this->unique_prefix.'insert');
+    $select->required_set(false);
+    $this->_fields['insert'] = $select;
+  # button for insertion of the new item
+    $button = new button(null, ['data-style' => 'narrow-insert', 'title' => new text('insert')]);
+    $button->break_on_validate = true;
+    $button->build();
+    $button->value_set($this->unique_prefix.'insert');
+    $button->_type = 'insert';
+    $this->_buttons['insert'] = $button;
+  # group the previous elements in widget 'insert'
+    $widget->child_insert($select, 'select');
+    $widget->child_insert($button, 'button');
+    return $widget;
+  }
 
 }}
