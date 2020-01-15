c832c61f5bc267348100f7498cdff86af0977fa7
Maxim Rysevets <root@effcore.com>
2020-01-15 16:36:09 +0300

    sprint_1027_5

--- system/module_storage/backend/patterns/Widget_Selection_fields.php
+++ system/module_storage/backend/patterns/Widget_Selection_fields.php
@@ -11,17 +11,7 @@ namespace effcore {
   public $item_title = 'Field';
 
   function widget_manage_get($item, $c_row_id, $prefix) {
-    $widget = new markup('x-widget', [
-      'data-rearrangeable'         => 'true',
-      'data-fields-is-inline-full' => 'true'], [], $item->weight);
-  # field for weight
-    $field_weight = new field_weight;
-    $field_weight->description_state = 'hidden';
-    $field_weight->build();
-    $field_weight->name_set($prefix.'weight'.$c_row_id);
-    $field_weight->required_set(false);
-    $field_weight->value_set($item->weight);
-    $this->_fields['weight'.$c_row_id] = $field_weight;
+    $widget = parent::widget_manage_get($item, $c_row_id, $prefix);
   # data markup
     $entity = entity::get($item->entity_name);
     $entity_field = $entity ? $entity->field_get($item->entity_field_name) : null;
@@ -30,18 +20,8 @@ namespace effcore {
         'id'    => new markup('x-id',    [], [
                    new text_simple($item->entity_name      ), '.',
                    new text_simple($item->entity_field_name)]) ]);
-  # button for deletion of the old item
-    $button_delete = new button(null, ['data-style' => 'narrow-delete', 'title' => new text('delete')]);
-    $button_delete->break_on_validate = true;
-    $button_delete->build();
-    $button_delete->value_set($prefix.'delete'.$c_row_id);
-    $button_delete->_type = 'delete';
-    $button_delete->_id = $c_row_id;
-    $this->_buttons['delete'.$c_row_id] = $button_delete;
   # group the previous elements in widget 'manage'
-    $widget->child_insert($field_weight,  'weight'       );
-    $widget->child_insert($data_markup,   'data'         );
-    $widget->child_insert($button_delete, 'button_delete');
+    $widget->child_insert($data_markup, 'data');
     return $widget;
   }
 
