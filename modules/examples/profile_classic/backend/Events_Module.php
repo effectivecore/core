19de98500f7def6dfbfa9097152978ecb11bd7e5
Maxim Rysevets <root@effcore.com>
2020-01-17 23:25:35 +0300

    sprint_1034_3

--- modules/examples/profile_classic/backend/Events_Module.php
+++ modules/examples/profile_classic/backend/Events_Module.php
@@ -4,17 +4,17 @@
   ### Copyright © 20NN—20NN Author/Rightholder. All rights reserved. ###
   ######################################################################
 
-namespace effcore\modules\example_decoration {
+namespace effcore\modules\profile_classic {
           use \effcore\module;
           abstract class events_module {
 
   static function on_enable($event) {
-    $module = module::get('example_decoration');
+    $module = module::get('profile_classic');
     $module->enable();
   }
 
   static function on_disable($event) {
-    $module = module::get('example_decoration');
+    $module = module::get('profile_classic');
     $module->disable();
   }
 
