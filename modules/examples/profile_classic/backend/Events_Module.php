0d1d27ac38ddcb4339b1917194bb89de15cdf167
Maxim Rysevets <root@effcore.com>
2020-01-18 13:43:43 +0300

    sprint_1034_6

--- modules/examples/profile_classic/backend/Events_Module.php
+++ modules/examples/profile_classic/backend/Events_Module.php
@@ -8,6 +8,16 @@ namespace effcore\modules\profile_classic {
           use \effcore\module;
           abstract class events_module {
 
+  static function on_install($event) {
+    $module = module::get('profile_classic');
+    $module->install();
+  }
+
+  static function on_uninstall($event) {
+    $module = module::get('profile_classic');
+    $module->uninstall();
+  }
+
   static function on_enable($event) {
     $module = module::get('profile_classic');
     $module->enable();
