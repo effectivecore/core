1cb5df5f612d272e3235ba0c16b807bf6ba3344c
Maxim Rysevets <root@effcore.com>
2020-01-22 16:55:43 +0300

    sprint_1034_41

--- system/profiles/profile_default/backend/Events_Module.php
+++ system/profiles/profile_default/backend/Events_Module.php
@@ -11,6 +11,8 @@ namespace effcore\modules\profile_default {
           use \effcore\text_multiline;
           abstract class events_module {
 
+  static $conflict_has_been = false;
+
   static function on_install($event) {
     $page_ids = [];
     if (page::get('front'       )) $page_ids[] = 'front';
@@ -18,14 +20,15 @@ namespace effcore\modules\profile_default {
     if (page::get('logout'      )) $page_ids[] = 'logout';
     if (page::get('recovery'    )) $page_ids[] = 'recovery';
     if (page::get('registration')) $page_ids[] = 'registration';
-    if (count($page_ids)) {
+    if (!count($page_ids)) {
+      $module = module::get('profile_default');
+      $module->install();
+    } else {
+      static::$conflict_has_been = true;
       message::insert(new text_multiline([
         'This profile cannot be installed because the system already has Pages with the following IDs: %%_ids',
         'Uninstall the existing profile first.'], ['ids' => implode(', ', $page_ids)]), 'warning'
       );
-    } else {
-      $module = module::get('profile_default');
-      $module->install();
     }
   }
 
@@ -35,8 +38,10 @@ namespace effcore\modules\profile_default {
   }
 
   static function on_enable($event) {
-    $module = module::get('profile_default');
-    $module->enable();
+    if (!static::$conflict_has_been) {
+      $module = module::get('profile_default');
+      $module->enable();
+    }
   }
 
   static function on_disable($event) {
