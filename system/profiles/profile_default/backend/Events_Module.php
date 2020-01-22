6ae5c3ed272a2b28184e04a4277e48e15f0285c8
Maxim Rysevets <root@effcore.com>
2020-01-22 16:23:19 +0300

    sprint_1034_40

--- system/profiles/profile_default/backend/Events_Module.php
+++ system/profiles/profile_default/backend/Events_Module.php
@@ -5,12 +5,28 @@
   ######################################################################
 
 namespace effcore\modules\profile_default {
+          use \effcore\message;
           use \effcore\module;
+          use \effcore\page;
+          use \effcore\text_multiline;
           abstract class events_module {
 
   static function on_install($event) {
-    $module = module::get('profile_default');
-    $module->install();
+    $page_ids = [];
+    if (page::get('front'       )) $page_ids[] = 'front';
+    if (page::get('login'       )) $page_ids[] = 'login';
+    if (page::get('logout'      )) $page_ids[] = 'logout';
+    if (page::get('recovery'    )) $page_ids[] = 'recovery';
+    if (page::get('registration')) $page_ids[] = 'registration';
+    if (count($page_ids)) {
+      message::insert(new text_multiline([
+        'This profile cannot be installed because the system already has Pages with the following IDs: %%_ids',
+        'Uninstall the existing profile first.'], ['ids' => implode(', ', $page_ids)]), 'warning'
+      );
+    } else {
+      $module = module::get('profile_default');
+      $module->install();
+    }
   }
 
   static function on_uninstall($event) {
