4e9f89311b4bf902028b42c0fa312b5ff0da1407
Maxim Rysevets <root@effcore.com>
2020-02-01 22:12:05 +0300

    sprint_1034_72

--- system/module_page/backend/patterns/Area.php
+++ system/module_page/backend/patterns/Area.php
@@ -11,12 +11,15 @@ namespace effcore {
   public $title;
   public $type; # null | table | row | column
   public $managing_is_enabled = false;
+  public $tag_name_real;
 
   function build() {
     if (!$this->is_builded) {
       if ($this->type) $this->attribute_insert('data-area-type', $this->type);
       if ($this->id  ) $this->attribute_insert('data-area-id',   $this->id  );
-      if ($this->id && $this->managing_is_enabled) $this->child_insert(new markup('x-area-id', [], ['simple' => new text_simple($this->id)]), 'id');
+      if ($this->id && $this->managing_is_enabled) $this->child_insert(new markup('x-area-info', [], [
+        'id'       => new markup('x-area-id',       [], new text_simple($this->id)),
+        'tag_name' => new markup('x-area-tag-name', [], new text_simple($this->tag_name_real)) ]), 'id');
       $this->is_builded = true;
     }
   }
@@ -24,6 +27,12 @@ namespace effcore {
   function render() {
     $this->build();
     return parent::render();
+  }
+
+  function managing_enable($tag_name = 'div') {
+    $this->managing_is_enabled = true;
+    $this->tag_name_real = $this->tag_name;
+    $this->tag_name      = $tag_name;
   }
 
 }}
