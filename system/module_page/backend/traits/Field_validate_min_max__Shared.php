20f27f98bdff7e850202cc6174e851109ffa91e2
Maxim Rysevets <root@effcore.com>
2020-02-09 14:22:16 +0300

    sprint_1034_107

--- system/module_page/backend/traits/Field_validate_min_max__Shared.php
+++ system/module_page/backend/traits/Field_validate_min_max__Shared.php
@@ -9,7 +9,7 @@ namespace effcore {
 
   static function validate_min($field, $form, $element, &$new_value) {
     $min = $field->min_get();
-    if (strlen($new_value) && $min && $new_value < $min) {
+    if (strlen($new_value) && strlen($min) && $new_value < $min) {
       $field->error_set(new text_multiline([
         'Field "%%_title" contains incorrect value!',
         'Field value is less than %%_number.'], ['title' => translation::get($field->title), 'number' => $min]
@@ -21,7 +21,7 @@ namespace effcore {
 
   static function validate_max($field, $form, $element, &$new_value) {
     $max = $field->max_get();
-    if (strlen($new_value) && $max && $new_value > $max) {
+    if (strlen($new_value) && strlen($max) && $new_value > $max) {
       $field->error_set(new text_multiline([
         'Field "%%_title" contains incorrect value!',
         'Field value is more than %%_number.'], ['title' => translation::get($field->title), 'number' => $max]
