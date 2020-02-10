d4fc17e981ced76a2eca2c1a59123ea1262ecddd
Maxim Rysevets <root@effcore.com>
2020-02-10 15:50:47 +0300

    sprint_1034_112

--- system/module_page/backend/Events_Page.php
+++ system/module_page/backend/Events_Page.php
@@ -6,11 +6,13 @@
 
 namespace effcore\modules\page {
           use \effcore\access;
+          use \effcore\entity;
+          use \effcore\instance;
           use \effcore\markup;
           use \effcore\message;
+          use \effcore\part_preset;
           use \effcore\text;
           use \effcore\url;
-          use \effcore\user;
           abstract class events_page {
 
   static function block_messages($page) {
@@ -28,6 +30,21 @@ namespace effcore\modules\page {
       return new markup('x-page-actions', [],
         new markup('a', ['data-id' => 'update', 'href' => '/manage/data/content/page/'.$page->id.'/update?'.url::back_part_make()], 'update this page')
       );
+    }
+  }
+
+  static function on_part_presets_dynamic_build($event, $id = null) {
+    if ($id === null                                  ) {foreach (entity::get('text')->instances_select() as $c_text)                                                                                             part_preset::insert('text_sql_'.$c_text->id, 'Text', $c_text->description ?: 'NO TITLE', [], null, 'code', '\\effcore\\modules\\page\\events_page::block_text_sql', [], ['id' => $c_text->id], 0, 'page');}
+    if ($id !== null && strpos($id, 'text_sql_') === 0) {                                                    $c_text = (new instance('text', ['id' => substr($id, strlen('text_sql_'))]))->select(); if ($c_text) part_preset::insert('text_sql_'.$c_text->id, 'Text', $c_text->description ?: 'NO TITLE', [], null, 'code', '\\effcore\\modules\\page\\events_page::block_text_sql', [], ['id' => $c_text->id], 0, 'page');}
+  }
+
+  static function block_text_sql($page, $args) {
+    if (!empty($args['id'])) {
+      $text = (new instance('text', [
+        'id' => $args['id']
+      ]))->select();
+      return $text ?
+             $text->text : null;
     }
   }
 
