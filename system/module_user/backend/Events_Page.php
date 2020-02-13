4df91bb7030f1e5c72ed10d89b7fa7475fa4ec36
Maxim Rysevets <root@effcore.com>
2020-02-13 16:19:12 +0300

    sprint_1034_132

--- system/module_user/backend/Events_Page.php
+++ system/module_user/backend/Events_Page.php
@@ -9,12 +9,15 @@ namespace effcore\modules\user {
           abstract class events_page {
 
   static function on_tree_build_before($event, $tree) {
-    if (!frontend::select('tree_system')) frontend::insert('tree_system', null, 'styles', ['path' => 'frontend/tree.cssd', 'attributes' => ['rel' => 'stylesheet', 'media' => 'all']], 'tree_style', 'menu');
-    if (!frontend::select('tree_user'  )) frontend::insert('tree_user',   null, 'styles', ['path' => 'frontend/tree.cssd', 'attributes' => ['rel' => 'stylesheet', 'media' => 'all']], 'tree_style', 'user');
+    if (!frontend::select('tree_menu')) frontend::insert('tree_menu', null, 'styles', ['path' => 'frontend/tree.cssd', 'attributes' => ['rel' => 'stylesheet', 'media' => 'all']], 'tree_style', 'menu');
+    if (!frontend::select('tree_user')) frontend::insert('tree_user', null, 'styles', ['path' => 'frontend/tree.cssd', 'attributes' => ['rel' => 'stylesheet', 'media' => 'all']], 'tree_style', 'user');
   }
 
   static function on_tree_build_after($event, $tree) {
-    if ($tree->visualization_mode == 'decorated-rearrangeable') {
+    if ($tree->id == 'system' && !frontend::select('tree_system')) {
+      frontend::insert('tree_system', null, 'styles', ['path' => 'frontend/tree-system.cssd', 'attributes' => ['rel' => 'stylesheet', 'media' => 'all']], 'tree_style', 'menu');
+    }
+    if ($tree->visualization_mode == 'decorated-rearrangeable' && !frontend::select('tree_rearrangeable')) {
       frontend::insert('tree_rearrangeable', null, 'scripts', ['path'  => 'frontend/tree-rearrangeable.js',   'attributes' => ['defer' => true]],                         'tree_script_rearrangeable', 'menu');
       frontend::insert('tree_rearrangeable', null, 'styles',  ['path'  => 'frontend/tree-rearrangeable.cssd', 'attributes' => ['rel' => 'stylesheet', 'media' => 'all']], 'tree_style_rearrangeable',  'menu');
     }
