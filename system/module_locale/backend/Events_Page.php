3f32bae2bd2a7ec6aef319435949af0d186cca1e
Maxim Rysevets <root@effcore.com>
2020-02-21 12:56:43 +0300

    sprint_1034_173

--- system/module_locale/backend/Events_Page.php
+++ system/module_locale/backend/Events_Page.php
@@ -14,19 +14,21 @@ namespace effcore\modules\locales {
           abstract class events_page {
 
   static function block_menu_languages($page) {
-    $menu = new markup('x-languages');
     $languages = language::get_all();
     core::array_sort_by_text_property($languages, 'title_en', 'd', false);
     $languages = ['en' => $languages['en']] + $languages;
+    $menu = new markup('nav', ['aria-label' => 'languages'], ['container' => new markup('ul')]);
     foreach ($languages as $c_language) {
       $c_title = $c_language->code == 'en' ?
-        $c_language->title_en :
-        $c_language->title_en.' ('.$c_language->title_native.')';
+                 $c_language->title_en :
+                 $c_language->title_en.' ('.$c_language->title_native.')';
       $c_href = $page->args_get('base').'/'.$c_language->code;
       if (url::is_active($c_href))
-           $c_link = new markup('a', ['href' => $c_href, 'title' => translation::get('go to %%_language language', ['language' => $c_language->title_en], 'en'), 'aria-selected' => 'true'], new text_simple($c_title));
-      else $c_link = new markup('a', ['href' => $c_href, 'title' => translation::get('go to %%_language language', ['language' => $c_language->title_en], 'en')                           ], new text_simple($c_title));
-      $menu->child_insert(new markup('x-language', ['data-code' => $c_language->code], $c_link), $c_language->code);
+           $c_link = new markup('a', ['href' => $c_href, 'title' => translation::get('go to %%_language language', ['language' => $c_language->title_en], 'en'), 'aria-current' => 'true'], new text_simple($c_title));
+      else $c_link = new markup('a', ['href' => $c_href, 'title' => translation::get('go to %%_language language', ['language' => $c_language->title_en], 'en')                          ], new text_simple($c_title));
+      $menu->child_select('container')->child_insert(
+        new markup('li', ['data-code' => $c_language->code], $c_link), $c_language->code
+      );
     }
     return $menu;
   }
