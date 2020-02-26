2eb6838bf079d5e35095e06649e6e06430c6da7e
Maxim Rysevets <root@effcore.com>
2020-02-26 22:55:16 +0300

    sprint_1034_195

--- system/profiles/profile_default/backend/Events_Module.php
+++ system/profiles/profile_default/backend/Events_Module.php
@@ -5,6 +5,7 @@
   ######################################################################
 
 namespace effcore\modules\profile_default {
+          use \effcore\field;
           use \effcore\message;
           use \effcore\module;
           use \effcore\page;
@@ -15,20 +16,24 @@ namespace effcore\modules\profile_default {
 
   static function on_install($event) {
     $module = module::get('profile_default');
-    $page_ids = [];
-    if (page::get('front'       )) $page_ids[] = 'front';
-    if (page::get('login'       )) $page_ids[] = 'login';
-    if (page::get('logout'      )) $page_ids[] = 'logout';
-    if (page::get('recovery'    )) $page_ids[] = 'recovery';
-    if (page::get('registration')) $page_ids[] = 'registration';
-    if (!count($page_ids)) {
-      $module->install();
-    } else {
-      static::$conflict_has_been = true;
-      message::insert(new text_multiline([
-        'Unable to install the profile "%%_profile" because the system already has Pages with the following IDs: %%_ids',
-        'Uninstall the existing profile first.'], ['profile' => $module->title, 'ids' => implode(', ', $page_ids)]), 'warning'
-      );
+    if (page::get_current()->args_get('base') == '/install' && field::request_value_get('profile') != $module->id) {static::$conflict_has_been = true; return;}
+    if (page::get_current()->args_get('base') == '/install' && field::request_value_get('profile') == $module->id) {$module->install();}
+    if (page::get_current()->args_get('base') != '/install') {
+      $page_ids = [];
+      if (page::get('front'       )) $page_ids[] = 'front';
+      if (page::get('login'       )) $page_ids[] = 'login';
+      if (page::get('logout'      )) $page_ids[] = 'logout';
+      if (page::get('recovery'    )) $page_ids[] = 'recovery';
+      if (page::get('registration')) $page_ids[] = 'registration';
+      if (!count($page_ids)) {
+        $module->install();
+      } else {
+        static::$conflict_has_been = true;
+        message::insert(new text_multiline([
+          'Unable to install the profile "%%_profile" because the system already has Pages with the following IDs: %%_ids',
+          'Uninstall the existing profile first.'], ['profile' => $module->title, 'ids' => implode(', ', $page_ids)]), 'warning'
+        );
+      }
     }
   }
 
