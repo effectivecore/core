4b111bdd22616076abf6f6cb7739d8809acafadb
Maxim Rysevets <root@effcore.com>
2020-02-29 18:13:54 +0300

    sprint_1035_17

--- system/module_poll/backend/Events_Form_Instance_insert.php
+++ system/module_poll/backend/Events_Form_Instance_insert.php
@@ -15,8 +15,7 @@ namespace effcore\modules\polls {
           abstract class events_form_instance_insert {
 
   static function on_init($event, $form, $items) {
-    $entity_name = page::get_current()->args_get('entity_name');
-    $entity = entity::get($entity_name);
+    $entity = entity::get($form->entity_name);
     if ($entity) {
       if ($entity->name == 'poll') {
         page::get_current()->args_set('back_insert_is_canceled', true);
@@ -37,8 +36,7 @@ namespace effcore\modules\polls {
   }
 
   static function on_validate($event, $form, $items) {
-    $entity_name = page::get_current()->args_get('entity_name');
-    $entity = entity::get($entity_name);
+    $entity = entity::get($form->entity_name);
     if ($entity) {
       switch ($form->clicked_button->value_get()) {
         case 'insert':
@@ -53,8 +51,7 @@ namespace effcore\modules\polls {
   }
 
   static function on_submit($event, $form, $items) {
-    $entity_name = page::get_current()->args_get('entity_name');
-    $entity = entity::get($entity_name);
+    $entity = entity::get($form->entity_name);
     if ($entity) {
       if ($entity->name == 'poll' && !empty($form->_instance)) {
         switch ($form->clicked_button->value_get()) {
--- system/module_user/backend/Events_Form_Instance_insert.php
+++ system/module_user/backend/Events_Form_Instance_insert.php
@@ -13,8 +13,7 @@ namespace effcore\modules\user {
           abstract class events_form_instance_insert {
 
   static function on_init($event, $form, $items) {
-    $entity_name = page::get_current()->args_get('entity_name');
-    $entity = entity::get($entity_name);
+    $entity = entity::get($form->entity_name);
     if ($entity) {
     # group 'access'
       if (!empty($entity->ws_access)) {
@@ -36,8 +35,7 @@ namespace effcore\modules\user {
   }
 
   static function on_validate($event, $form, $items) {
-    $entity_name = page::get_current()->args_get('entity_name');
-    $entity = entity::get($entity_name);
+    $entity = entity::get($form->entity_name);
     if ($entity) {
       switch ($form->clicked_button->value_get()) {
         case 'insert':
@@ -77,8 +75,7 @@ namespace effcore\modules\user {
   }
 
   static function on_submit($event, $form, $items) {
-    $entity_name = page::get_current()->args_get('entity_name');
-    $entity = entity::get($entity_name);
+    $entity = entity::get($form->entity_name);
     if ($entity) {
       switch ($form->clicked_button->value_get()) {
         case 'insert':
