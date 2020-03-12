e55818c48229fd01dc46a2eec38f2e5d70064804
Maxim Rysevets <root@effcore.com>
2020-03-12 19:17:33 +0300

    sprint_1037_2

--- system/module_user/backend/patterns/Widget_License_agreement.php
+++ system/module_user/backend/patterns/Widget_License_agreement.php
@@ -5,25 +5,30 @@
   ##################################################################
 
 namespace effcore {
-          class widget_license_agreement extends fieldset {
+          class widget_license_agreement extends control {
 
-  public $title = 'License agreement';
+  public $tag_name = 'x-widget';
   public $attributes = ['data-type' => 'license_agreement'];
-  public $agree_title = 'I accept the terms of the license agreement.';
+  public $main_title = 'License agreement';
+  public $text_agree = 'I accept the terms of the license agreement.';
 
   function build() {
     if (!$this->is_builded) {
-      parent::build();
-      $this->state = 'closed';
+      $wrapper = new fieldset($this->title);
+      $wrapper->title = $this->main_title;
+      $wrapper->state = 'closed';
+      $this->child_insert($wrapper, 'wrapper');
+    # text of license agreement
       $language = language::get(language::code_get_current());
       $license_file = new file($language->license_path ?: dir_root.'license.md');
-      $markup_license = new markup('x-document', ['data-type' => 'license'], markdown::markdown_to_markup($license_file->load()));
-      $switcher_agree = new field_switcher($this->agree_title);
+      $license_markup = new markup('x-document', ['data-type' => 'license'], markdown::markdown_to_markup($license_file->load()));
+      $wrapper->child_insert($license_markup, 'license');
+    # switcher 'agree to license agreement'
+      $switcher_agree = new field_switcher($this->text_agree);
       $switcher_agree->build();
       $switcher_agree->name_set('is_agree');
       $switcher_agree->required_set(true);
-      $this->child_insert($markup_license, 'license' );
-      $this->child_insert($switcher_agree, 'is_agree');
+      $wrapper->child_insert($switcher_agree, 'is_agree');
       $this->is_builded = true;
     }
   }
