d2331e78cc1697a880fd0fd6289763b40098cdaf
Maxim Rysevets <root@effcore.com>
2020-04-08 14:57:36 +0300

    sprint_1041_46

--- system/module_page/backend/patterns/Widget_Pictures.php
+++ system/module_page/backend/patterns/Widget_Pictures.php
@@ -14,29 +14,24 @@ namespace effcore {
 
   # ─────────────────────────────────────────────────────────────────────
 
-  function widget_manage_get($item, $c_row_id) {
+  function widget_insert_get() {
     $widget = new markup('x-widget', [
-      'data-rearrangeable'         => 'true',
-      'data-fields-is-inline-full' => 'true'], [], $item->weight);
-  # field for weight
-    $field_weight = new field_weight;
-    $field_weight->description_state = 'hidden';
-    $field_weight->build();
-    $field_weight->name_set($this->name_complex.'__weight__'.$c_row_id);
-    $field_weight->required_set(false);
-    $field_weight->value_set($item->weight);
-    $this->_fields['weight__'.$c_row_id] = $field_weight;
-  # button for deletion of the old item
-    $button_delete = new button(null, ['data-style' => 'narrow-delete', 'title' => new text('delete')]);
-    $button_delete->break_on_validate = true;
-    $button_delete->build();
-    $button_delete->value_set($this->name_complex.'__delete__'.$c_row_id);
-    $button_delete->_type = 'delete';
-    $button_delete->_id = $c_row_id;
-    $this->_buttons['delete__'.$c_row_id] = $button_delete;
-  # group the previous elements in widget 'manage'
-    $widget->child_insert($field_weight,  'weight'       );
-    $widget->child_insert($button_delete, 'button_delete');
+      'data-type' => 'insert']);
+  # field for upload new picture
+    $field_picture = new field_picture;
+    $field_picture->build();
+    $field_picture->name_set($this->name_complex.'__picture');
+    $this->_fields['picture'] = $field_picture;
+  # button for insertion of the new item
+    $button = new button(null, ['data-style' => 'narrow-insert', 'title' => new text('insert')]);
+    $button->break_on_validate = true;
+    $button->build();
+    $button->value_set($this->name_complex.'__insert');
+    $button->_type = 'insert';
+    $this->_buttons['insert'] = $button;
+  # group the previous elements in widget 'insert'
+    $widget->child_insert($field_picture, 'picture');
+    $widget->child_insert($button,        'button');
     return $widget;
   }
 
