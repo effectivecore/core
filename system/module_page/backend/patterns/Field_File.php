ff71204b124325a25ec797eae0d79df4d0e4e64d
Maxim Rysevets <root@effcore.com>
2020-04-29 15:48:47 +0300

    sprint_1041_99

--- system/module_page/backend/patterns/Field_File.php
+++ system/module_page/backend/patterns/Field_File.php
@@ -53,22 +53,22 @@ namespace effcore {
     'type' => 'file',
     'name' => 'file'];
 # ─────────────────────────────────────────────────────────────────────
-  public $max_file_size = '10K';
+  public $max_file_size = '5K';
   public $min_files_number = 0;
   public $max_files_number = 1;
   public $max_length_name = 227; # = 255 - strlen('-TimestmpRandom_v.') - max_length_type
   public $max_length_type =  10;
-  public $allowed_types = [];
+  public $allowed_types = ['txt' => 'txt'];
   public $allowed_characters = 'a-zA-Z0-9_\\-\\.';
   public $allowed_characters_title = '"a-z", "A-Z", "0-9", "_", "-", "."';
-  public $upload_dir = '';
   public $fixed_name;
   public $fixed_type;
+  public $upload_dir = '';
   public $has_validate_phase_3 = true;
 # ─────────────────────────────────────────────────────────────────────
+  public $pool_result = null;
   public $pool_fin = [];
   public $pool_pre = [];
-  public $pool_result = null;
 
   function value_get() {
     $values = $this->values_get();
