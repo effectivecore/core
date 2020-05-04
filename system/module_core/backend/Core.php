18ab45fd170ae6ef2b73b695ff916cd6e52a51a2
Maxim Rysevets <root@effcore.com>
2020-05-04 14:13:54 +0300

    sprint_1041_117

--- system/module_core/backend/Core.php
+++ system/module_core/backend/Core.php
@@ -741,7 +741,7 @@ namespace effcore {
     else return $bytes.'B';
   }
 
-  static function upload_max_filesize_php_get() {
+  static function upload_max_filesize_bytes_get() {
     $value = ini_get('upload_max_filesize');
     if (static::is_abbreviated_bytes($value))
          return static::abbreviated_to_bytes($value);
