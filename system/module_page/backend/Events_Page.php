05e8df69407a72b94a9badf20555cb5d49fb589c
Maxim Rysevets <root@effcore.com>
2020-05-04 18:19:53 +0300

    sprint_1041_119

--- system/module_page/backend/Events_Page.php
+++ system/module_page/backend/Events_Page.php
@@ -35,40 +35,24 @@ namespace effcore\modules\page {
   }
 
   static function on_part_presets_dynamic_build($event, $id = null) {
-    if ($id === null                                  ) {foreach (entity::get('text')->instances_select() as $c_text)                                                                                             part_preset::insert('text_sql_'.$c_text->id, 'Texts', $c_text->description ?: 'NO TITLE', [], null, 'code', '\\effcore\\modules\\page\\events_page::block_text_sql', [], ['id' => $c_text->id], 0, 'page');}
-    if ($id !== null && strpos($id, 'text_sql_') === 0) {                                                    $c_text = (new instance('text', ['id' => substr($id, strlen('text_sql_'))]))->select(); if ($c_text) part_preset::insert('text_sql_'.$c_text->id, 'Texts', $c_text->description ?: 'NO TITLE', [], null, 'code', '\\effcore\\modules\\page\\events_page::block_text_sql', [], ['id' => $c_text->id], 0, 'page');}
-    if ($id === null                                     ) {foreach (entity::get('picture')->instances_select() as $c_picture)                                                                                                      part_preset::insert('picture_sql_'.$c_picture->id, 'Pictures', $c_picture->title ?: 'NO TITLE', [], null, 'code', '\\effcore\\modules\\page\\events_page::block_picture_sql', [], ['id' => $c_picture->id], 0, 'page');}
-    if ($id !== null && strpos($id, 'picture_sql_') === 0) {                                                       $c_picture = (new instance('picture', ['id' => substr($id, strlen('picture_sql_'))]))->select(); if ($c_picture) part_preset::insert('picture_sql_'.$c_picture->id, 'Pictures', $c_picture->title ?: 'NO TITLE', [], null, 'code', '\\effcore\\modules\\page\\events_page::block_picture_sql', [], ['id' => $c_picture->id], 0, 'page');}
+    if ($id === null                                     ) {foreach (entity::get('text'   )->instances_select() as $c_item)                                                                                                   part_preset::insert('text_sql_'.   $c_item->id, 'Texts',    $c_item->description ?: 'NO TITLE', [], null, 'code', '\\effcore\\modules\\page\\events_page::block_instance', [], ['instance_id' => $c_item->id, 'entity_name' => 'text'   ], 0, 'page');}
+    if ($id !== null && strpos($id, 'text_sql_'   ) === 0) {                                                       $c_item = (new instance('text',    ['id' => substr($id, strlen('text_sql_'   ))]))->select(); if ($c_item) part_preset::insert('text_sql_'.   $c_item->id, 'Texts',    $c_item->description ?: 'NO TITLE', [], null, 'code', '\\effcore\\modules\\page\\events_page::block_instance', [], ['instance_id' => $c_item->id, 'entity_name' => 'text'   ], 0, 'page');}
+    if ($id === null                                     ) {foreach (entity::get('picture')->instances_select() as $c_item)                                                                                                   part_preset::insert('picture_sql_'.$c_item->id, 'Pictures', $c_item->title       ?: 'NO TITLE', [], null, 'code', '\\effcore\\modules\\page\\events_page::block_instance', [], ['instance_id' => $c_item->id, 'entity_name' => 'picture'], 0, 'page');}
+    if ($id !== null && strpos($id, 'picture_sql_') === 0) {                                                       $c_item = (new instance('picture', ['id' => substr($id, strlen('picture_sql_'))]))->select(); if ($c_item) part_preset::insert('picture_sql_'.$c_item->id, 'Pictures', $c_item->title       ?: 'NO TITLE', [], null, 'code', '\\effcore\\modules\\page\\events_page::block_instance', [], ['instance_id' => $c_item->id, 'entity_name' => 'picture'], 0, 'page');}
+    if ($id === null                                     ) {foreach (entity::get('video'  )->instances_select() as $c_item)                                                                                                   part_preset::insert('video_sql_'.  $c_item->id, 'Videos',   $c_item->title       ?: 'NO TITLE', [], null, 'code', '\\effcore\\modules\\page\\events_page::block_instance', [], ['instance_id' => $c_item->id, 'entity_name' => 'video'  ], 0, 'page');}
+    if ($id !== null && strpos($id, 'video_sql_'  ) === 0) {                                                       $c_item = (new instance('video',   ['id' => substr($id, strlen('video_sql_'  ))]))->select(); if ($c_item) part_preset::insert('video_sql_'.  $c_item->id, 'Videos',   $c_item->title       ?: 'NO TITLE', [], null, 'code', '\\effcore\\modules\\page\\events_page::block_instance', [], ['instance_id' => $c_item->id, 'entity_name' => 'video'  ], 0, 'page');}
+    if ($id === null                                     ) {foreach (entity::get('audio'  )->instances_select() as $c_item)                                                                                                   part_preset::insert('audio_sql_'.  $c_item->id, 'Audios',   $c_item->title       ?: 'NO TITLE', [], null, 'code', '\\effcore\\modules\\page\\events_page::block_instance', [], ['instance_id' => $c_item->id, 'entity_name' => 'audio'  ], 0, 'page');}
+    if ($id !== null && strpos($id, 'audio_sql_'  ) === 0) {                                                       $c_item = (new instance('audio',   ['id' => substr($id, strlen('audio_sql_'  ))]))->select(); if ($c_item) part_preset::insert('audio_sql_'.  $c_item->id, 'Audios',   $c_item->title       ?: 'NO TITLE', [], null, 'code', '\\effcore\\modules\\page\\events_page::block_instance', [], ['instance_id' => $c_item->id, 'entity_name' => 'audio'  ], 0, 'page');}
   }
 
-  static function block_text_sql($page, $args) {
-    if (!empty($args['id'])) {
-      $entity = entity::get('text');
+  static function block_instance($page, $args) {
+    if (!empty($args['instance_id'])) {
+      $entity = entity::get($args['entity_name']);
       $selection = new selection;
-      $selection->id = 'text_'.$args['id'];
+      $selection->id = $args['entity_name'].'_'.$args['instance_id'];
       $selection->template = 'content';
-      $selection->decorator_params = $entity->decorator_params;
-      $selection->query_params['conditions'] = ['id_!f' => '~text.id', 'operator' => '=', 'id_!v' => $args['id']];
-      foreach ($entity->fields as $c_name => $c_field) {
-        if (!empty($c_field->managing_on_select_is_enabled)) {
-          $selection->field_insert_entity(null,
-            $entity->name, $c_name, $c_field->managing_selection_params ?? []
-          );
-        }
-      }
-      $selection->build();
-      return $selection;
-    }
-  }
-
-  static function block_picture_sql($page, $args) {
-    if (!empty($args['id'])) {
-      $entity = entity::get('picture');
-      $selection = new selection;
-      $selection->id = 'picture_'.$args['id'];
-      $selection->template = 'content';
-      $selection->decorator_params = $entity->decorator_params;
-      $selection->query_params['conditions'] = ['id_!f' => '~picture.id', 'operator' => '=', 'id_!v' => $args['id']];
+      $selection->decorator_params = $entity->decorator_params ?? null;
+      $selection->query_params['conditions'] = ['id_!f' => '~'.$args['entity_name'].'.id', 'operator' => '=', 'id_!v' => $args['instance_id']];
       foreach ($entity->fields as $c_name => $c_field) {
         if (!empty($c_field->managing_on_select_is_enabled)) {
           $selection->field_insert_entity(null,
