e5f71717be6715500bfee6efe38a069372bdcc19
Maxim Rysevets <root@effcore.com>
2020-05-25 21:30:40 +0300

    sprint_1041_180

---
+++ system/module_page/backend/patterns/Widget_Gallery.php
@@ -0,0 +1,13 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2020 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore {
+          class widget_gallery extends widget_files {
+
+  public $upload_dir = 'galleries/';
+  public $fixed_name = 'gallery-%%_instance_id_context-%%_item_id_context';
+
+}}
