798b6fd69b85b0b1111af4ed512b3ff07a92cdcf
Maxim Rysevets <root@effcore.com>
2020-06-02 20:21:15 +0300

    sprint_1041_234

--- system/module_storage/backend/Events_Storage.php
+++ system/module_storage/backend/Events_Storage.php
@@ -7,6 +7,8 @@
 namespace effcore\modules\storage {
           use const \effcore\dir_root;
           use \effcore\field_file;
+          use \effcore\file_uploaded;
+          use \effcore\widget_files;
           abstract class events_storage {
 
   static function on_instance_delete_before($event, $instance) {
@@ -14,9 +16,19 @@ namespace effcore\modules\storage {
     foreach ($entity->fields as $c_name => $c_field) {
       if (!empty($c_field->managing_control_class)) {
         $c_reflection = new \ReflectionClass($c_field->managing_control_class);
-        if ($c_reflection->newInstanceWithoutConstructor() instanceof field_file) {
+        $c_reflection_instance = $c_reflection->newInstanceWithoutConstructor();
+        if ($c_reflection_instance instanceof field_file) {
           if (!empty($instance->{$c_name})) {
             @unlink(dir_root.$instance->{$c_name});
+          }
+        }
+        if ($c_reflection_instance instanceof widget_files) {
+          if (!empty($instance->{$c_name})) {
+            foreach ($instance->{$c_name} as $c_item) {
+              if ($c_item->object instanceof file_uploaded) {
+                @unlink($c_item->object->get_current_path());
+              }
+            }
           }
         }
       }
