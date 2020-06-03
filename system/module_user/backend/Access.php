779e9dc21b1515c2e8798fd8dd1f01dcfc47f113
Maxim Rysevets <root@effcore.com>
2020-06-03 17:20:05 +0300

    sprint_1041_244

--- system/module_user/backend/Access.php
+++ system/module_user/backend/Access.php
@@ -10,8 +10,18 @@ namespace effcore {
   static function check($access, $user = null) {
     if ($access === null) return true;
     if (  $user === null) $user = user::get_current();
-    if (isset($access->roles)) foreach ($user->roles as $c_role) if (isset($access->roles[ $c_role ])) return true;
-    if (isset($access->users))                                   if (isset($access->users[$user->id])) return true;
+    if (isset($access->roles      )) foreach ($user->roles as $c_role) if (isset($access->roles[ $c_role ])) return true;
+    if (isset($access->users      ))                                   if (isset($access->users[$user->id])) return true;
+    if (isset($access->permissions)) {
+      foreach ($access->permissions as $c_id_permission) {
+        $c_permission_roles = permission::get_roles_by_permission($c_id_permission);
+        foreach ($user->roles as $c_role) {
+          if (isset($c_permission_roles[$c_role])) {
+            return true;
+          }
+        }
+      }
+    }
   }
 
 }}
