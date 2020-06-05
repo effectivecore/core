503417f9e84a70799ef53355a05705ee026700c1
Maxim Rysevets <root@effcore.com>
2020-06-05 16:39:36 +0300

    sprint_1041_252

---
+++ system/module_storage/backend/Events_Page_Instance_delete.php
@@ -0,0 +1,23 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2020 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\storage {
+          use \effcore\access;
+          use \effcore\core;
+          use \effcore\entity;
+          abstract class events_page_instance_delete {
+
+  static function on_build_before($event, $page) {
+    $entity_name = $page->args_get('entity_name');
+    $entity = entity::get($entity_name);
+    if ($entity) {
+      if (isset($entity->access_delete) && !access::check(
+                $entity->access_delete))
+           core::send_header_and_exit('access_forbidden');
+    } else core::send_header_and_exit('page_not_found');
+  }
+
+}}
---
+++ system/module_storage/backend/Events_Page_Instance_update.php
@@ -0,0 +1,23 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2020 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\storage {
+          use \effcore\access;
+          use \effcore\core;
+          use \effcore\entity;
+          abstract class events_page_instance_update {
+
+  static function on_build_before($event, $page) {
+    $entity_name = $page->args_get('entity_name');
+    $entity = entity::get($entity_name);
+    if ($entity) {
+      if (isset($entity->access_update) && !access::check(
+                $entity->access_update))
+           core::send_header_and_exit('access_forbidden');
+    } else core::send_header_and_exit('page_not_found');
+  }
+
+}}
