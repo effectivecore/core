ac6eadee893359a674f2a052de84d44ad55ec869
Maxim Rysevets <root@effcore.com>
2020-06-11 12:41:34 +0300

    sprint_1041_264

--- system/module_core/backend/Events_Module_Update.php
+++ system/module_core/backend/Events_Module_Update.php
@@ -17,14 +17,15 @@ namespace effcore\modules\core {
     $bundle = module::bundle_get($bundle_id);
     if ($bundle) {
       $repo_path = realpath(dir_root.$bundle->path.'/..');
+      $stderr_to_stdout = '2>&1';
       if ($repo_path !== false) {
         $result = [];
         $commands = [
-          'whoami                             2>&1',
-          'git --version                      2>&1',
-          'git -C '.$repo_path.' clean  -f -d 2>&1',
-          'git -C '.$repo_path.' reset --hard 2>&1',
-          'git -C '.$repo_path.' pull         2>&1'];
+          'whoami                             '.$stderr_to_stdout,
+          'git --version                      '.$stderr_to_stdout,
+          'git -C '.$repo_path.' clean  -f -d '.$stderr_to_stdout,
+          'git -C '.$repo_path.' reset --hard '.$stderr_to_stdout,
+          'git -C '.$repo_path.' pull         '.$stderr_to_stdout];
         foreach ($commands as $c_num => $c_command) {
           $return_var = null;
           $result['command-'.$c_num] = '$ '.$c_command;
