56af9203f425268ec4bd5121625ed6fe3151d2a6
Maxim Rysevets <root@effcore.com>
2020-06-11 14:52:21 +0300

    sprint_1041_265

--- system/module_develop/module_demo/backend/Events_Module_Update.php
+++ system/module_develop/module_demo/backend/Events_Module_Update.php
@@ -6,30 +6,31 @@
 
 namespace effcore\modules\demo {
           use \effcore\message;
+          use \effcore\text;
           abstract class events_module_update {
 
-  static function on_update_data_before($event, $update) {
-    message::insert('Call '.$event->handler.' for update #'.$update->number);
+  static function on_update_data_before($event, $update) {    
+    message::insert(new text('Call "%%_call" for #%%_number', ['call' => $event->handler, 'number' => $update->number]));
   }
 
   static function on_update_data_after($event, $update) {
-    message::insert('Call '.$event->handler.' for update #'.$update->number);
+    message::insert(new text('Call "%%_call" for #%%_number', ['call' => $event->handler, 'number' => $update->number]));
   }
 
   # ─────────────────────────────────────────────────────────────────────
 
   static function on_update_data_1000($update) {
-    message::insert('Call '.$update->handler);
+    message::insert(new text('Call "%%_call"', ['call' => $update->handler]));
     return true;
   }
 
   static function on_update_data_1001($update) {
-    message::insert('Call '.$update->handler);
+    message::insert(new text('Call "%%_call"', ['call' => $update->handler]));
     return true;
   }
 
   static function on_update_data_1002($update) {
-    message::insert('Call '.$update->handler);
+    message::insert(new text('Call "%%_call"', ['call' => $update->handler]));
     return true;
   }
 
