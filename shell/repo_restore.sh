37c76cd5b2bcdd5cd795d432130396ea26b86e87
Maxim Rysevets <root@effcore.com>
2020-06-12 17:47:59 +0300

    sprint_1041_279

--- shell/repo_restore.sh
+++ shell/repo_restore.sh
@@ -3,8 +3,8 @@
 repo_origin=$(grep 'repo_origin: ' ../system/bundle.data | sed 's/  repo_origin: //g')
 repo_branch=$(grep 'repo_branch: ' ../system/bundle.data | sed 's/  repo_branch: //g')
 
-rm -rf ../.git
 rm -rf ../dynamic/tmp/repo_system
 git clone --branch=$repo_branch $repo_origin ../dynamic/tmp/repo_system
+rm -rf ../.git
 mv ../dynamic/tmp/repo_system/.git ../
 rm -rf ../dynamic/tmp/repo_system
