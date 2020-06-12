8b5d8fb9b49725869726a8ba1c9388a7a46dd8f4
Maxim Rysevets <root@effcore.com>
2020-06-12 11:57:36 +0300

    sprint_1041_273

--- shell/cache_clear.sh
+++ shell/cache_clear.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-cd ../
-
-find dynamic/cache -not -name 'readme.md' -not -path 'dynamic/cache' -delete
+find ../dynamic/cache -not -name 'readme.md' -not -path '../dynamic/cache' -delete
