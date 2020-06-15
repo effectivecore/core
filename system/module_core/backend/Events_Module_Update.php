05c838b267c488c8c7cf31231a25bf3bca59d9e8
Maxim Rysevets <root@effcore.com>
2020-06-15 15:29:40 +0300

    sprint_1041_293

--- system/module_core/backend/Events_Module_Update.php
+++ system/module_core/backend/Events_Module_Update.php
@@ -54,14 +54,14 @@ namespace effcore\modules\core {
           $commands = [
             'whoami '                                                                                 .$stderr_to_stdout,
             'git --version '                                                                          .$stderr_to_stdout,
-            'del /f /s /q '.$repo_path_tmp.                                                        ' '.$stderr_to_stdout,
-            'rmdir  /s /q '.$repo_path_tmp.                                                        ' '.$stderr_to_stdout,
+            'del /f /s /q '.$repo_path_tmp.                                                        '   & exit 0',
+            'rmdir  /s /q '.$repo_path_tmp.                                                        '   & exit 0',
             'git clone --branch='.$bundle->repo_branch.' '.$bundle->repo_origin.' '.$repo_path_tmp.' '.$stderr_to_stdout,
-            'del /f /s /q '.$repo_path_cur.'\\.git '                                                  .$stderr_to_stdout,
-            'rmdir  /s /q '.$repo_path_cur.'\\.git '                                                  .$stderr_to_stdout,
+            'del /f /s /q '.$repo_path_cur.'\\.git '.                                              '   & exit 0',
+            'rmdir  /s /q '.$repo_path_cur.'\\.git '.                                              '   & exit 0',
             'xcopy  /e /i '.$repo_path_tmp.'\\.git '.$repo_path_cur.'\\.git'.                      ' '.$stderr_to_stdout,
-            'del /f /s /q '.$repo_path_tmp.                                                        ' '.$stderr_to_stdout,
-            'rmdir  /s /q '.$repo_path_tmp.                                                        ' '.$stderr_to_stdout
+            'del /f /s /q '.$repo_path_tmp.                                                        '   & exit 0',
+            'rmdir  /s /q '.$repo_path_tmp.                                                        '   & exit 0'
           ];
         } else {
           $commands = [
