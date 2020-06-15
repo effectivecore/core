2db5e02119beb7a3a65522687a53bbdd00907ac5
Maxim Rysevets <root@effcore.com>
2020-06-15 15:13:42 +0300

    sprint_1041_292

--- system/module_core/backend/Events_Module_Update.php
+++ system/module_core/backend/Events_Module_Update.php
@@ -59,7 +59,7 @@ namespace effcore\modules\core {
             'git clone --branch='.$bundle->repo_branch.' '.$bundle->repo_origin.' '.$repo_path_tmp.' '.$stderr_to_stdout,
             'del /f /s /q '.$repo_path_cur.'\\.git '                                                  .$stderr_to_stdout,
             'rmdir  /s /q '.$repo_path_cur.'\\.git '                                                  .$stderr_to_stdout,
-            'xcopy  /e /i '.$repo_path_tmp.'\\.git '.$repo_path_cur.                               ' '.$stderr_to_stdout,
+            'xcopy  /e /i '.$repo_path_tmp.'\\.git '.$repo_path_cur.'\\.git'.                      ' '.$stderr_to_stdout,
             'del /f /s /q '.$repo_path_tmp.                                                        ' '.$stderr_to_stdout,
             'rmdir  /s /q '.$repo_path_tmp.                                                        ' '.$stderr_to_stdout
           ];
