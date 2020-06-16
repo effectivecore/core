066f65ba1336b7268c6be5d7424e5e869bb85bce
Maxim Rysevets <root@effcore.com>
2020-06-16 14:21:23 +0300

    sprint_1041_301

--- system/module_develop/module_demo/backend/Events_Module_Update.php
+++ system/module_develop/module_demo/backend/Events_Module_Update.php
@@ -6,8 +6,29 @@
 
 namespace effcore\modules\demo {
           use \effcore\message;
+          use \effcore\modules\core\events_module_update as core_events_module_update;
           use \effcore\text;
           abstract class events_module_update {
+
+  # to activate the files update through the repository:
+  # ══════════════════════════════════════════════════════════════════════════════════════════
+  # - create "bundle.data" with repository information
+  # - bind the new bundle with its modules - set "id_bundle" in each "module.data"
+  # - create "events.data" if it does not exist
+  # - in "events.data" register a new event "on_update_files"
+  # - create "Events_Module_Update.php" in the main bundle module
+  # - in "Events_Module_Update.php" create a new event handler "on_update_files"
+  # ──────────────────────────────────────────────────────────────────────────────────────────
+
+  static function on_update_files($event, $bundle_id) {
+    return core_events_module_update::on_update_files($event, $bundle_id); # ← unregistered event (for demonstration only)
+  }
+
+  static function on_restore_repo($event, $bundle_id) {
+    return core_events_module_update::on_restore_repo($event, $bundle_id); # ← unregistered event (for demonstration only)
+  }
+
+  # ─────────────────────────────────────────────────────────────────────
 
   static function on_update_data_before($event, $update) {    
     message::insert(new text('Call "%%_call" for #%%_number', ['call' => $event->handler, 'number' => $update->number]));
