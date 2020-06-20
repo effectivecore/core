f900f492ffb642b518db21f95b5c9f4807a2e930
Maxim Rysevets <root@effcore.com>
2020-06-20 21:32:24 +0300

    sprint_1041_336

--- system/module_user/backend/Events_Selection.php
+++ system/module_user/backend/Events_Selection.php
@@ -8,15 +8,18 @@ namespace effcore\modules\user {
           abstract class events_selection {
 
   static function on_selection_build_before($event, $selection) {
-    $selection->field_insert_entity('user.nickname', 'user', 'nickname', ['type' => 'join_field', 'weight' => 390]);
- /* $selection->fields['relation_role_ws_user.id_user']->title = 'User ID'; */
-    $selection->query_params['join_script']['with_user'] = (object)[
-      'type'                 => 'left outer join',
-      'entity_name'          => 'user',
-      'entity_field_name'    => 'id',
-      'on_entity_name'       => 'relation_role_ws_user',
-      'on_entity_field_name' => 'id_user'
-    ];
+    if ($selection->id === 'instance_select_multiple-relation_role_ws_user' ||
+        $selection->id === 'instance_select-relation_role_ws_user') {
+      $selection->field_insert_entity('user.nickname', 'user', 'nickname', ['type' => 'join_field', 'weight' => 390]);
+   /* $selection->fields['relation_role_ws_user.id_user']->title = 'User ID'; */
+      $selection->query_params['join_script']['with_user'] = (object)[
+        'type'                 => 'left outer join',
+        'entity_name'          => 'user',
+        'entity_field_name'    => 'id',
+        'on_entity_name'       => 'relation_role_ws_user',
+        'on_entity_field_name' => 'id_user'
+      ];
+    }
   }
 
 }}
