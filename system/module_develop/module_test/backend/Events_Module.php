0a81dc95f4bdd091f5dadebf28a25e3cffd94ee3
Maxim Rysevets <root@effcore.com>
2020-07-01 21:08:22 +0300

    sprint_1041_379

--- system/module_develop/module_test/backend/Events_Module.php
+++ system/module_develop/module_test/backend/Events_Module.php
@@ -8,6 +8,16 @@ namespace effcore\modules\test {
           use \effcore\module;
           abstract class events_module {
 
+  static function on_install($event) {
+    $module = module::get('test');
+    $module->install();
+  }
+
+  static function on_uninstall($event) {
+    $module = module::get('test');
+    $module->uninstall();
+  }
+
   static function on_enable($event) {
     $module = module::get('test');
     $module->enable();
