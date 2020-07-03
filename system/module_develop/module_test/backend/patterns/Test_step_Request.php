39695f72ebf583818cae738baa00b41197d72bb7
Maxim Rysevets <root@effcore.com>
2020-07-03 10:59:17 +0300

    sprint_1041_395

--- system/module_develop/module_test/backend/patterns/Test_step_Request.php
+++ system/module_develop/module_test/backend/patterns/Test_step_Request.php
@@ -14,15 +14,17 @@ namespace effcore {
   public $post     = [];
 
   function run(&$test, &$c_scenario, &$c_step, &$c_results) {
-    if (isset($c_results['response'])) static::$history_responses[] = $c_results['response'];
-    $prepared_post = $this->prepared_get_post();
-    $reports[] = translation::apply('make request to "%%_url"', ['url' => $this->prepared_get_url()]);
+    if (isset($c_results['response'])) static::$history[] = $c_results['response'];
+    $prepared_url     = $this->prepared_url_get    ();
+    $prepared_headers = $this->prepared_headers_get();
+    $prepared_post    = $this->prepared_post_get   ();
+    $reports[] = translation::apply('make request to "%%_url"', ['url' => $this->prepared_url_get()]);
     foreach ($prepared_post as $c_key => $c_value)
       $reports[] = translation::apply('&ndash; request post param "%%_name" = "%%_value"', ['name' => $c_key, 'value' => $c_value]);
   # make request
     $response = static::request(
-      $this->prepared_get_url    (),
-      $this->prepared_get_headers(),
+      $prepared_url,
+      $prepared_headers,
       $prepared_post,
       $this->proxy);
     $reports[] = translation::apply('&ndash; response param "%%_name" = "%%_value"', ['name' => 'http_code', 'value' => $response['info']['http_code']]);
@@ -30,23 +32,23 @@ namespace effcore {
     $c_results['response'] = $response;
   }
 
-  function prepared_get_url() {
+  function prepared_url_get() {
     $is_https = $this->is_https instanceof param_from_form ?
                 $this->is_https->get() :
                 $this->is_https;
     return ($is_https ? 'https' : 'http').'://'.url::get_current()->domain.$this->url;
   }
 
-  function prepared_get_headers() {
+  function prepared_headers_get() {
     return $this->headers;
   }
 
-  function prepared_get_post() {
+  function prepared_post_get() {
     $result = [];
     foreach ($this->post as $c_key => $c_value) {
-      if ($c_value == '%%_nickname_random') $c_value = $this->random_get_nickname();
-      if ($c_value == '%%_password_random') $c_value = $this->random_get_password();
-      if ($c_value == '%%_email_random'   ) $c_value = $this->random_get_email   ();
+      if ($c_value == '%%_nickname_random') $c_value = $this->random_nickname_get();
+      if ($c_value == '%%_password_random') $c_value = $this->random_password_get();
+      if ($c_value == '%%_email_random'   ) $c_value = $this->random_email_get   ();
       if ($c_value == '%%_captcha'        ) $c_value = $this->captcha_code_get   ();
       if ($c_value == '%%_validation_id'  ) $c_value = $this->validation_id_get  ();
       $result[$c_key] = $c_value;
@@ -54,21 +56,21 @@ namespace effcore {
     return $result;
   }
 
-  function random_get_nickname() {
+  function random_nickname_get() {
     return 'test_'.core::hash_get_mini(random_int(0, 0x7fffffff));
   }
 
-  function random_get_email() {
+  function random_email_get() {
     return 'test_'.core::hash_get_mini(random_int(0, 0x7fffffff)).'@example.com';
   }
 
-  function random_get_password() {
+  function random_password_get() {
     return core::password_generate();
   }
 
   function captcha_code_get() {
     if (module::is_enabled('captcha')) {
-      $last_responce = end(static::$history_responses);
+      $last_responce = end(static::$history);
       if ($last_responce) {
         return field_captcha::get_code_by_id(
           core::ip_to_hex($last_responce['info']['primary_ip'])
@@ -78,7 +80,7 @@ namespace effcore {
   }
 
   function validation_id_get() {
-    $last_responce = end(static::$history_responses);
+    $last_responce = end(static::$history);
     if ($last_responce) {
       $form_id            = $this->post   ['form_id']                                    ?? '';
       $prev_validation_id = $last_responce['headers']['X-Form-Validation-Id--'.$form_id] ?? '';
@@ -90,7 +92,7 @@ namespace effcore {
   ### static declarations ###
   ###########################
 
-  static $history_responses = [];
+  static $history = [];
   static $curlopt_timeout = 5;
   static $curlopt_sslversion = CURL_SSLVERSION_TLSv1_2;
   static $curlopt_ssl_verifyhost = false;
@@ -110,6 +112,11 @@ namespace effcore {
     curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, static::$curlopt_ssl_verifyhost);
     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, static::$curlopt_ssl_verifypeer);
     if ($proxy) curl_setopt($curl, CURLOPT_PROXY, $proxy);
+  # prepare post query
+    if ($post) {
+      curl_setopt($curl, CURLOPT_POST,        true);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $post);
+    }
   # prepare headers
     curl_setopt($curl, CURLOPT_HEADERFUNCTION, function ($curl, $c_header) use (&$result) {
       $c_matches = [];
@@ -117,11 +124,6 @@ namespace effcore {
       if ($c_matches) $result['headers'][$c_matches['name']] = trim($c_matches['value'], "\r\n\"");
       return strlen($c_header);
     });
-  # prepare post query
-    if ($post) {
-      curl_setopt($curl, CURLOPT_POST,        true);
-      curl_setopt($curl, CURLOPT_POSTFIELDS, $post);
-    }
   # prepare return
     $data = curl_exec($curl);
     $result['error_message'] = curl_error($curl);
