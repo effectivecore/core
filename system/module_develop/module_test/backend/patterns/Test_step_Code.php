d5592a3f6f5aabb307d465f1d901ef89f4cdbcd6
Maxim Rysevets <root@effcore.com>
2020-07-13 16:02:51 +0300

    sprint_1041_419

--- system/module_develop/module_test/backend/patterns/Test_step_Code.php
+++ system/module_develop/module_test/backend/patterns/Test_step_Code.php
@@ -8,7 +8,7 @@ namespace effcore {
           class step_code {
 
   public $handler;
-  public $args;
+  public $args = [];
   public $is_apply_tokens = true;
 
   function run(&$test, &$c_scenario, &$c_step, &$c_results) {
