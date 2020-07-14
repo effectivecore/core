bce092f4d40f3a16a879b70c89bbd95005d001fd
Maxim Rysevets <root@effcore.com>
2020-07-14 11:33:29 +0300

    sprint_1041_421

--- system/module_develop/module_test/backend/patterns/Test_step_Title.php
+++ system/module_develop/module_test/backend/patterns/Test_step_Title.php
@@ -12,10 +12,12 @@ namespace effcore {
   public $is_apply_tokens = true;
 
   function run(&$test, &$c_scenario, &$c_step, &$c_results) {
-    if ($this->is_apply_tokens)
-      foreach ($this->args as &$c_arg)
-        $c_arg = token::apply($c_arg);
-    $c_results['reports'][] = new text($this->title, $this->args);
+    $args = [];
+    foreach ($this->args as $c_key => $c_value)
+      if ($this->is_apply_tokens && is_string($c_value))
+           $args[$c_key] = token::apply($c_value);
+      else $args[$c_key] =              $c_value;
+    $c_results['reports'][] = new text($this->title, $args);
   }
 
 }}
