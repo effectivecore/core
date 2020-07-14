002edcc8a71b462af01546bc5381e497c57a987c
Maxim Rysevets <root@effcore.com>
2020-07-14 13:58:38 +0300

    sprint_1041_425

---
+++ system/module_user/backend/Events_Form_Security_settings.php
@@ -0,0 +1,29 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2020 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\user {
+          use \effcore\message;
+          abstract class events_form_security_settings {
+
+  static function on_init($event, $form, $items) {
+  }
+
+  static function on_validate($event, $form, $items) {
+    switch ($form->clicked_button->value_get()) {
+      case 'save':
+        break;
+    }
+  }
+
+  static function on_submit($event, $form, $items) {
+    switch ($form->clicked_button->value_get()) {
+      case 'save':
+        message::insert('The changes was saved.');
+        break;
+    }
+  }
+
+}}
--- system/module_user/backend/Events_Page_Security.php
+++ system/module_user/backend/Events_Page_Security.php
@@ -5,20 +5,12 @@
   ##################################################################
 
 namespace effcore\modules\user {
-          use \effcore\block;
-          use \effcore\text;
           use \effcore\url;
           abstract class events_page_security {
 
   static function on_redirect($event, $page) {
     $type = $page->args_get('type');
     if ($type === null) {url::go($page->args_get('base').'/settings');}
-  }
-
-  static function block_security($page) {
-    return new block('Security', ['data-id' => 'security', 'data-title-is-hidden' => true], [
-      new text('UNDER CONSTRUCTION')
-    ]);
   }
 
 }}
