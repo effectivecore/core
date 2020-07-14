d853e2dce7d423fae4140e25f7f6f3f356f6fe27
Maxim Rysevets <root@effcore.com>
2020-07-14 16:40:02 +0300

    sprint_1041_428

--- system/module_captcha/backend/Events_Form_Security_settings_captcha.php
+++ system/module_captcha/backend/Events_Form_Security_settings_captcha.php
@@ -6,9 +6,13 @@
 
 namespace effcore\modules\captcha {
           use \effcore\message;
+          use \effcore\module;
+          use \effcore\storage;
           abstract class events_form_security_settings_captcha {
 
   static function on_init($event, $form, $items) {
+    $settings = module::settings_get('captcha');
+    $items['#length']->value_set($settings->captcha_length);
   }
 
   static function on_validate($event, $form, $items) {
@@ -21,6 +25,7 @@ namespace effcore\modules\captcha {
   static function on_submit($event, $form, $items) {
     switch ($form->clicked_button->value_get()) {
       case 'save':
+        storage::get('files')->changes_insert('captcha', 'update', 'settings/captcha/captcha_length', $items['#length']->value_get());
         message::insert('The changes was saved.');
         break;
     }
