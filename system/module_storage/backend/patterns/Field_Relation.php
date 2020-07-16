bc4cabcefceaa67413b672d3771a4168b4b92154
Maxim Rysevets <root@effcore.com>
2020-07-16 18:07:06 +0300

    sprint_1041_446

--- system/module_storage/backend/patterns/Field_Relation.php
+++ system/module_storage/backend/patterns/Field_Relation.php
@@ -8,6 +8,7 @@ namespace effcore {
           class field_relation extends field_select {
 
   public $title = 'Relation';
+  public $title_ws_id = true;
   public $attributes = ['data-type' => 'relation'];
   public $element_attributes = [
     'name'     => 'relation',
@@ -41,17 +42,13 @@ namespace effcore {
         $tree->build();
         foreach ($tree->children_select_recursive(null, '', false, true) as $c_npath => $c_child) {
           $c_depth = count_chars($c_npath, 1)[ord('/')] ?? 0;
-          $this->option_insert(str_repeat('- ', $c_depth + 1).
-            $c_child->title,
-            $c_child->id_real
-          );
+          if ($this->title_ws_id === true) $this->option_insert(str_repeat('- ', $c_depth + 1).(new text($c_child->title))->render().' ('.$c_child->id_real.')', $c_child->id_real);
+          if ($this->title_ws_id !== true) $this->option_insert(str_repeat('- ', $c_depth + 1).(new text($c_child->title))->render(),                            $c_child->id_real);
         }
       } else {
         foreach ($instances as $c_instance) {
-          $this->option_insert(
-            $c_instance->{$this->related_entity_field_title_name},
-            $c_instance->{$this->related_entity_field_id_name   }
-          );
+          if ($this->title_ws_id === true) $this->option_insert( (new text($c_instance->{$this->related_entity_field_title_name}))->render().' ('.$c_instance->{$this->related_entity_field_id_name}.')', $c_instance->{$this->related_entity_field_id_name});
+          if ($this->title_ws_id !== true) $this->option_insert(           $c_instance->{$this->related_entity_field_title_name},                                                                         $c_instance->{$this->related_entity_field_id_name});
         }
       }
       $this->is_builded = true;
