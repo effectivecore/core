24bfc3f52b247960a503d541d7039bb1d1005515
Maxim Rysevets <root@effcore.com>
2020-07-17 12:56:22 +0300

    sprint_1041_448

--- system/module_develop/module_test/backend/patterns/Test_step_Repeat.php
+++ system/module_develop/module_test/backend/patterns/Test_step_Repeat.php
@@ -7,6 +7,7 @@
 namespace effcore {
           class step_repeat {
 
+  public $id;
   public $quantity = 1;
   public $actions = [];
 
@@ -17,6 +18,7 @@ namespace effcore {
     for ($i = 1; $i <= $quantity; $i++) {
       $c_results['reports'][] = new text('repeat %%_cur from %%_max', ['cur' => $i, 'max' => $quantity]);
       foreach ($this->actions as $c_step) {
+        if ($this->id) token::insert('test_step_repeat_i_'.$this->id, '%%_test_step_repeat_i_'.$this->id, 'text', $i, null, 'test');
         $c_step->run($test, $this->actions, $c_step, $c_results);
         if (array_key_exists('return', $c_results)) {
           return;
