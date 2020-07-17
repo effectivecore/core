0510725bf378dec0412cd5c8b94ba905f7805815
Maxim Rysevets <root@effcore.com>
2020-07-17 17:23:25 +0300

    sprint_1041_452

---
+++ system/module_develop/module_test/backend/patterns/Test_step_Actions.php
@@ -0,0 +1,19 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2020 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore {
+          class step_actions {
+
+  function run(&$test, &$c_scenario, &$c_step, &$c_results) {
+    foreach ($this->actions as $c_step) {
+      $c_step->run($test, $this->actions, $c_step, $c_results);
+      if (array_key_exists('return', $c_results)) {
+        return;
+      }
+    }
+  }
+
+}}
