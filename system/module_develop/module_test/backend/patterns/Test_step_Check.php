4499cad1301c8059b0740269bf6c4b937656b3a1
Maxim Rysevets <root@effcore.com>
2020-07-18 13:16:27 +0300

    sprint_1041_460

--- system/module_develop/module_test/backend/patterns/Test_step_Check.php
+++ system/module_develop/module_test/backend/patterns/Test_step_Check.php
@@ -8,13 +8,15 @@ namespace effcore {
           class step_check {
 
   public $where;
+  public $which;
   public $match;
   public $on_success;
   public $on_failure;
 
   function run(&$test, &$c_scenario, &$c_step, &$c_results) {
     $result = ($this->where === 'http_code' && array_key_exists('http_code',                  $c_results['response']['info'   ]) && $this->match === (int)$c_results['response']['info'   ]['http_code'                 ]) ||
-              ($this->where === 'subm_errs' && array_key_exists('X-Form-Submit-Errors-Count', $c_results['response']['headers']) && $this->match === (int)$c_results['response']['headers']['X-Form-Submit-Errors-Count']);
+              ($this->where === 'subm_errs' && array_key_exists('X-Form-Submit-Errors-Count', $c_results['response']['headers']) && $this->match === (int)$c_results['response']['headers']['X-Form-Submit-Errors-Count']) ||
+              ($this->where === 'token'     && (string)token::apply($this->which) === (string)$this->match);
     if ($result === true) $c_results['reports'][] = new text_multiline(['checking on "%%_name" = "%%_value"', 'result of checking is = "%%_result"'], ['name' => $this->where, 'value' => $this->match, 'result' => (new text('success'))->render() ]);
     if ($result !== true) $c_results['reports'][] = new text_multiline(['checking on "%%_name" = "%%_value"', 'result of checking is = "%%_result"'], ['name' => $this->where, 'value' => $this->match, 'result' => (new text('failure'))->render() ]);
     if ($result === true && isset($this->on_success)) foreach ($this->on_success as $c_step) { $c_step->run($test, $this->on_success, $c_step, $c_results); if (array_key_exists('return', $c_results)) return; }
