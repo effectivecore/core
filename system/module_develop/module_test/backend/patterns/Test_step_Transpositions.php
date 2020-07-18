adb3fdb918688b5471f2e1af67643f3b9085bfde
Maxim Rysevets <root@effcore.com>
2020-07-18 12:26:30 +0300

    sprint_1041_458

--- system/module_develop/module_test/backend/patterns/Test_step_Transpositions.php
+++ system/module_develop/module_test/backend/patterns/Test_step_Transpositions.php
@@ -18,6 +18,12 @@ namespace effcore {
     for ($i = 1; $i < $max; $i++) {
       $c_results['reports'][] = new text('=================================');
       $c_results['reports'][] = new text('transposition %%_cur from %%_max (%%_bits)', ['cur' => $i + 1, 'max' => $max, 'bits' => str_pad(decbin($i), $actions_count, '0', STR_PAD_LEFT)]);
+      for ($j = $actions_count - 1; $j >= 0; $j--) {
+        $c_rowid = $rowids[$actions_count - 1 - $j];
+        $c_state = $i >> $j & 1;
+        token::insert('test_step_transpositions_is_active_'.$c_rowid,
+                   '%%_test_step_transpositions_is_active_'.$c_rowid, 'text', $c_state, null, 'test');
+      }
       if ($this->action_before) {
         $c_results['reports'][] = new text('action "%%_name" will be started', ['name' => 'action_before']);
         $this->action_before->run($test, $this->action_before, $c_step, $c_results);
