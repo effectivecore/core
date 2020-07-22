a8a32cc05b9282ddd3be1ca2a0a342b425b53619
Maxim Rysevets <root@effcore.com>
2020-07-22 18:57:44 +0300

    sprint_1041_480

--- system/module_develop/module_test/backend/Events_Form_Test.php
+++ system/module_develop/module_test/backend/Events_Form_Test.php
@@ -52,9 +52,10 @@ namespace effcore\modules\test {
           if (!empty($test_result['reports'])) {
             $items['report']->child_select('document')->children_delete();
             foreach ($test_result['reports'] as $c_dpath => $c_part) {
+              $c_depth = count_chars($c_dpath, 1)[ord('/')] ?? 0;
               if (is_array($c_part)) foreach ($c_part as &$c_line) $c_line = core::return_rendered($c_line);
-              if (is_array($c_part)) $items['report']->child_select('document')->child_insert(new markup('p', [], new text_multiline($c_part) ));
-              else                   $items['report']->child_select('document')->child_insert(new markup('p', [],                    $c_part  ));
+              if (is_array($c_part)) $items['report']->child_select('document')->child_insert(new markup('p', ['data-depth' => $c_depth], new text_multiline($c_part) ));
+              else                   $items['report']->child_select('document')->child_insert(new markup('p', ['data-depth' => $c_depth],                    $c_part  ));
             }
           }
         }
