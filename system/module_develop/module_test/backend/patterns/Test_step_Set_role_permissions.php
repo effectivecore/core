9e4b51d60ddf5297efcd9262439d5fb0c5d5a7cb
Maxim Rysevets <root@effcore.com>
2020-07-23 12:58:37 +0300

    sprint_1041_482

--- system/module_develop/module_test/backend/patterns/Test_step_Set_role_permissions.php
+++ system/module_develop/module_test/backend/patterns/Test_step_Set_role_permissions.php
@@ -12,16 +12,15 @@ namespace effcore {
   public $is_reset = false;
 
   function run(&$test, $dpath, &$c_results) {
-    if (true           ) $reports[] = new text('changing permissions for role = "%%_role"', ['role' => $this->id_role]);
-    if ($this->is_reset) $reports[] = new text('there will be try to delete all permissions');
-    if (true           ) $reports[] = new text('there will be try to insert permissions: %%_permissions', ['permissions' => implode(', ', $this->permissions) ?: 'n/a']);
+    if (true           ) $c_results['reports'][$dpath][] = new text('changing permissions for role = "%%_role"', ['role' => $this->id_role]);
+    if ($this->is_reset) $c_results['reports'][$dpath][] = new text('there will be try to delete all permissions');
+    if (true           ) $c_results['reports'][$dpath][] = new text('there will be try to insert permissions: %%_permissions', ['permissions' => implode(', ', $this->permissions) ?: 'n/a']);
     $old_permissions =   role::relation_permission_select    ($this->id_role);
     if ($this->is_reset) role::relation_permission_delete_all($this->id_role);
                          role::relation_permission_insert    ($this->id_role, $this->permissions, 'test');
     $new_permissions =   role::relation_permission_select    ($this->id_role);
-    $reports[] = new text('permissions before insertion: %%_permissions', ['permissions' => implode(', ', $old_permissions) ?: 'n/a']);
-    $reports[] = new text('permissions after insertion: %%_permissions',  ['permissions' => implode(', ', $new_permissions) ?: 'n/a']);
-    $c_results['reports'][$dpath] = $reports;
+    $c_results['reports'][$dpath][] = new text('permissions before insertion: %%_permissions', ['permissions' => implode(', ', $old_permissions) ?: 'n/a']);
+    $c_results['reports'][$dpath][] = new text('permissions after insertion: %%_permissions',  ['permissions' => implode(', ', $new_permissions) ?: 'n/a']);
   }
 
 }}
