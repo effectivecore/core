81e620e502e468e6e9752806f67ab2a58e98bb97
Maxim Rysevets <root@effcore.com>
2020-07-27 22:43:08 +0300

    sprint_1041_530

--- system/module_storage/backend/Events_Page_Instance_select_multiple.php
+++ system/module_storage/backend/Events_Page_Instance_select_multiple.php
@@ -31,13 +31,20 @@ namespace effcore\modules\storage {
   # collect manageable entities
     foreach ($entities as $c_entity) {
       if ($c_entity->managing_is_enabled) {
-        $entities_by_groups[$c_entity->managing_group_id]
-                           [$c_entity->name             ] = $c_entity->title_plural;
+        if (access::check($c_entity->access_select)) {
+          $entities_by_groups[$c_entity->managing_group_id]
+                             [$c_entity->name             ] = $c_entity->title_plural;
+        }
       }
     }
-  # redirect if required
-    if (empty($entities_by_groups[$managing_group_id][$entity_name])) {
-      url::go($page->args_get('base').'/content/page');
+  # redirect if required (weight of tab items is not consider)
+    if (count($entities_by_groups) && empty($entities_by_groups[$managing_group_id][$entity_name])) {
+      $first_managing_group = key(      $entities_by_groups );
+      $first_entity_name    = key(reset($entities_by_groups));
+      url::go($page->args_get('base').'/'.$first_managing_group.'/'.$first_entity_name);
+    }
+    if (count($entities_by_groups) === 0) {
+      core::send_header_and_exit('access_forbidden');
     }
   }
 
