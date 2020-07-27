5aa8e83f4ee65d198686378cf335c441a2311f66
Maxim Rysevets <root@effcore.com>
2020-07-27 14:07:16 +0300

    sprint_1041_527

--- system/module_develop/module_test/backend/patterns/Test_step_Check.php
+++ system/module_develop/module_test/backend/patterns/Test_step_Check.php
@@ -7,16 +7,16 @@
 namespace effcore {
           class step_check {
 
+  public $check;
   public $where;
-  public $which;
   public $match;
   public $on_success;
   public $on_failure;
 
   function run(&$test, $dpath, &$c_results) {
-    if ($this->where === 'token') $result = (string)token::apply($this->which) === (string)$this->match;
-    if ($result === true) $c_results['reports'][$dpath.'/on_success_title'] = new text_multiline(['checking on "%%_name" = "%%_value"', 'result of checking is = "%%_result"'], ['name' => $this->where.($this->which ? ': '.$this->which : ''), 'value' => $this->match, 'result' => (new text('success'))->render() ]);
-    if ($result !== true) $c_results['reports'][$dpath.'/on_failure_title'] = new text_multiline(['checking on "%%_name" = "%%_value"', 'result of checking is = "%%_result"'], ['name' => $this->where.($this->which ? ': '.$this->which : ''), 'value' => $this->match, 'result' => (new text('failure'))->render() ]);
+    if ($this->check === 'token') $result = (string)token::apply($this->where) === (string)$this->match;
+    if ($result === true) $c_results['reports'][$dpath.'/on_success_title'] = new text_multiline(['checking on "%%_name" = "%%_value"', 'result of checking is = "%%_result"'], ['name' => $this->check.($this->where ? ': '.$this->where : ''), 'value' => $this->match, 'result' => (new text('success'))->render() ]);
+    if ($result !== true) $c_results['reports'][$dpath.'/on_failure_title'] = new text_multiline(['checking on "%%_name" = "%%_value"', 'result of checking is = "%%_result"'], ['name' => $this->check.($this->where ? ': '.$this->where : ''), 'value' => $this->match, 'result' => (new text('failure'))->render() ]);
     if ($result === true && isset($this->on_success)) foreach ($this->on_success as $c_step) { $c_step->run($test, $dpath.'/on_success', $c_results); if (array_key_exists('return', $c_results)) return; }
     if ($result !== true && isset($this->on_failure)) foreach ($this->on_failure as $c_step) { $c_step->run($test, $dpath.'/on_failure', $c_results); if (array_key_exists('return', $c_results)) return; }
   }
