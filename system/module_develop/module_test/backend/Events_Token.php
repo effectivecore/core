0f326237ba5b6bd663b20c9f7ecb58318f1f3341
Maxim Rysevets <root@effcore.com>
2020-09-01 12:27:27 +0300

    sprint_1041_559

--- system/module_develop/module_test/backend/Events_Token.php
+++ system/module_develop/module_test/backend/Events_Token.php
@@ -20,6 +20,16 @@ namespace effcore\modules\test {
     if ($name === 'test_email_random'    && count($args) === 1) {if (!isset( static::$cache['test_email_random'   ][$args[0]] )) static::$cache['test_email_random'   ][$args[0]] = 'test_'.core::hash_get_mini(random_int(0, 0x7fffffff)).'@example.com'; return static::$cache['test_email_random'   ][$args[0]];}
     if ($name === 'test_nickname_random' && count($args) === 1) {if (!isset( static::$cache['test_nickname_random'][$args[0]] )) static::$cache['test_nickname_random'][$args[0]] = 'test_'.core::hash_get_mini(random_int(0, 0x7fffffff));                return static::$cache['test_nickname_random'][$args[0]];}
     if ($name === 'test_password_random' && count($args) === 1) {if (!isset( static::$cache['test_password_random'][$args[0]] )) static::$cache['test_password_random'][$args[0]] =         core::password_generate();                                     return static::$cache['test_password_random'][$args[0]];}
+    if ($name === 'test_cookies') {
+      $result = [];
+      foreach (step_request::$history as $c_responce) {
+        if ( isset($c_responce['headers']['Set-Cookie']) ) {
+          foreach ($c_responce['headers']['Set-Cookie'] as $c_cookie) {
+            $c_key   = core::array_key_first($c_cookie['parsed']);
+            $c_value =                 reset($c_cookie['parsed']);
+            $result[$c_key] = $c_value; }}}
+      return core::data_to_attr($result, false, '; ', '', '');
+    }
     if ($name === 'test_captcha') {
       if (module::is_enabled('captcha')) {
         $last_responce = end(step_request::$history);
@@ -33,19 +43,8 @@ namespace effcore\modules\test {
     if ($name === 'test_form_validation_id' && count($args) === 1) {
       $last_responce = end(step_request::$history);
       if ($last_responce) {
-        $form_id = $args[0];
-        return $last_responce['headers']['X-Form-Validation-Id--'.$form_id] ?? '';
+        return $last_responce['headers']['X-Form-Validation-Id--'.$args[0]] ?? '';
       }
-    }
-    if ($name === 'test_cookies') {
-      $result = [];
-      foreach (step_request::$history as $c_responce) {
-        if ( isset($c_responce['headers']['Set-Cookie']) ) {
-          foreach ($c_responce['headers']['Set-Cookie'] as $c_cookie) {
-            $c_key   = core::array_key_first($c_cookie['parsed']);
-            $c_value =                 reset($c_cookie['parsed']);
-            $result[$c_key] = $c_value; }}}
-      return core::data_to_attr($result, false, '; ', '', '');
     }
     if ($name === 'test_responce_http_code') {
       $last_response = end(step_request::$history);
