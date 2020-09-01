be800df03526afec3ecf46b5e8076057c4cb2720
Maxim Rysevets <root@effcore.com>
2020-09-01 12:34:06 +0300

    sprint_1041_560

--- system/module_develop/module_test/backend/Events_Token.php
+++ system/module_develop/module_test/backend/Events_Token.php
@@ -22,9 +22,9 @@ namespace effcore\modules\test {
     if ($name === 'test_password_random' && count($args) === 1) {if (!isset( static::$cache['test_password_random'][$args[0]] )) static::$cache['test_password_random'][$args[0]] =         core::password_generate();                                     return static::$cache['test_password_random'][$args[0]];}
     if ($name === 'test_cookies') {
       $result = [];
-      foreach (step_request::$history as $c_responce) {
-        if ( isset($c_responce['headers']['Set-Cookie']) ) {
-          foreach ($c_responce['headers']['Set-Cookie'] as $c_cookie) {
+      foreach (step_request::$history as $c_response) {
+        if ( isset($c_response['headers']['Set-Cookie']) ) {
+          foreach ($c_response['headers']['Set-Cookie'] as $c_cookie) {
             $c_key   = core::array_key_first($c_cookie['parsed']);
             $c_value =                 reset($c_cookie['parsed']);
             $result[$c_key] = $c_value; }}}
@@ -32,27 +32,27 @@ namespace effcore\modules\test {
     }
     if ($name === 'test_captcha') {
       if (module::is_enabled('captcha')) {
-        $last_responce = end(step_request::$history);
-        if ($last_responce) {
+        $last_response = end(step_request::$history);
+        if ($last_response) {
           return field_captcha::get_code_by_id(
-            core::ip_to_hex($last_responce['info']['primary_ip'])
+            core::ip_to_hex($last_response['info']['primary_ip'])
           );
         }
       }
     }
     if ($name === 'test_form_validation_id' && count($args) === 1) {
-      $last_responce = end(step_request::$history);
-      if ($last_responce) {
-        return $last_responce['headers']['X-Form-Validation-Id--'.$args[0]] ?? '';
+      $last_response = end(step_request::$history);
+      if ($last_response) {
+        return $last_response['headers']['X-Form-Validation-Id--'.$args[0]] ?? '';
       }
     }
-    if ($name === 'test_responce_http_code') {
+    if ($name === 'test_response_http_code') {
       $last_response = end(step_request::$history);
       if ($last_response && isset($last_response['info']) && array_key_exists('http_code', $last_response['info'])) {
         return (int)$last_response['info']['http_code'];
       }
     }
-    if ($name === 'test_responce_submit_error') {
+    if ($name === 'test_response_submit_error') {
       $last_response = end(step_request::$history);
       if ($last_response && isset($last_response['headers']) && array_key_exists('X-Form-Submit-Errors-Count', $last_response['headers'])) {
         return (int)$last_response['headers']['X-Form-Submit-Errors-Count'];
