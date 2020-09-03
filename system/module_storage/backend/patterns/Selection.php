4f19ca9d52eaa39857be94136e2f47edb362a5bf
Maxim Rysevets <root@effcore.com>
2020-09-03 11:27:39 +0300

    sprint_1041_566

--- system/module_storage/backend/patterns/Selection.php
+++ system/module_storage/backend/patterns/Selection.php
@@ -138,15 +138,15 @@ namespace effcore {
               case 'field':
               case 'join_field':
                 $c_entity     = entity::get($c_field->entity_name, false);
-                $c_title      = $c_entity->fields[$c_field->entity_field_name]->title;
-                $c_value_type = $c_entity->fields[$c_field->entity_field_name]->type;
-                $c_value      = $c_instance->    {$c_field->entity_field_name};
-                if ($c_value !== null && $c_value_type == 'real'    ) $c_value = locale::format_number  ($c_value, 10);
-                if ($c_value !== null && $c_value_type == 'integer' ) $c_value = locale::format_number  ($c_value    );
-                if ($c_value !== null && $c_value_type == 'date'    ) $c_value = locale::format_date    ($c_value    );
-                if ($c_value !== null && $c_value_type == 'time'    ) $c_value = locale::format_time    ($c_value    );
-                if ($c_value !== null && $c_value_type == 'datetime') $c_value = locale::format_datetime($c_value    );
-                if ($c_value !== null && $c_value_type == 'boolean' ) $c_value = locale::format_logic   ($c_value    );
+                $c_title      = $c_field->title ?? $c_entity->fields[$c_field->entity_field_name]->title;
+                $c_value_type =                    $c_entity->fields[$c_field->entity_field_name]->type;
+                $c_value      =                    $c_instance->    {$c_field->entity_field_name};
+                if ($c_value !== null && $c_value_type === 'real'    ) $c_value = locale::format_number  ($c_value, 10);
+                if ($c_value !== null && $c_value_type === 'integer' ) $c_value = locale::format_number  ($c_value    );
+                if ($c_value !== null && $c_value_type === 'date'    ) $c_value = locale::format_date    ($c_value    );
+                if ($c_value !== null && $c_value_type === 'time'    ) $c_value = locale::format_time    ($c_value    );
+                if ($c_value !== null && $c_value_type === 'datetime') $c_value = locale::format_datetime($c_value    );
+                if ($c_value !== null && $c_value_type === 'boolean' ) $c_value = locale::format_logic   ($c_value    );
                 $c_row[$c_row_id] = [
                   'title'  => $c_title,
                   'value'  => $c_value,
