716938787472e0df0fd84251235bff4acdb3136a
Maxim Rysevets <root@effcore.com>
2020-09-21 12:27:08 +0300

    sprint_1041_589

--- system/module_develop/module_demo/backend/Events_File.php
+++ system/module_develop/module_demo/backend/Events_File.php
@@ -15,25 +15,27 @@ namespace effcore\modules\demo {
           abstract class events_file {
 
   static function on_load_virtual($event, $type_info, &$file_info, &$path) {
-    if (access::check((object)['roles' => ['admins' => 'admins']])) {
-      $user = user::get_current();
-      $data = 'dirs: '.$file_info->dirs.nl;
-      $data.= 'name: '.$file_info->name.nl;
-      $data.= 'type: '.$file_info->type.nl;
-      $data.= 'current user: '.$user->nickname.nl;
-      $data.= 'current user roles: '.implode(', ', $user->roles);
-      header('Content-Length: '.strlen($data));
-      header('Content-Type: application/octet-stream');
-      header('Content-Disposition: attachment; filename='.$file_info->name.'.txt');
-      header('Cache-Control: private, no-cache, no-store, must-revalidate');
-      header('Expires: 0');
-      print $data;
-      console::log_store();
-      exit();
-    } else {
-      core::send_header_and_exit('access_forbidden', null, new text_multiline([
-        'go to <a href="/">front page</a>'
-      ], [], br.br));
+    if ($type_info->type === 'demotype') {
+      if (access::check((object)['roles' => ['admins' => 'admins']])) {
+        $user = user::get_current();
+        $data = 'dirs: '.$file_info->dirs.nl;
+        $data.= 'name: '.$file_info->name.nl;
+        $data.= 'type: '.$file_info->type.nl;
+        $data.= 'current user: '.$user->nickname.nl;
+        $data.= 'current user roles: '.implode(', ', $user->roles);
+        header('Content-Length: '.strlen($data));
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename='.$file_info->name.'.txt');
+        header('Cache-Control: private, no-cache, no-store, must-revalidate');
+        header('Expires: 0');
+        print $data;
+        console::log_store();
+        exit();
+      } else {
+        core::send_header_and_exit('access_forbidden', null, new text_multiline([
+          'go to <a href="/">front page</a>'
+        ], [], br.br));
+      }
     }
   }
 
