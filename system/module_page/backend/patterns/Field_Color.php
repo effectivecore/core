9981a1a7ac966cd5bc7e64e953c74e43b5284d0c
Maxim Rysevets <root@effcore.com>
2020-10-09 22:47:13 +0300

    sprint_1041_1047

--- system/module_page/backend/patterns/Field_Color.php
+++ system/module_page/backend/patterns/Field_Color.php
@@ -8,7 +8,7 @@ namespace effcore {
           class field_color extends field_text {
 
   public $title = 'Color';
-  public $description = 'The color should be in the format "#abcdef", where "ab" is the value of the red component, "cd" - green and "ef" - blue.';
+  public $description = 'The color should be in the format "#abcdef", where "ab" is the value of the red component, "cd" — green and "ef" — blue.';
   public $attributes = ['data-type' => 'color'];
   public $element_attributes = [
     'type'     => 'color',
