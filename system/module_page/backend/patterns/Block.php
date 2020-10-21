e96d865d94d0c20026a491a98a83b5416582132c
Maxim Rysevets <root@effcore.com>
2020-10-21 18:22:18 +0300

    sprint_1041_1077

--- system/module_page/backend/patterns/Block.php
+++ system/module_page/backend/patterns/Block.php
@@ -18,20 +18,25 @@ namespace effcore {
   }
 
   function build($page = null) {
-    if (!isset($this->display) ||
-        (isset($this->display) && $this->display->check === 'page_args' && preg_match($this->display->match,         $page->args_get($this->display->where))) ||
-        (isset($this->display) && $this->display->check === 'user'      &&            $this->display->where === 'role' && preg_match($this->display->match.'m', implode(nl, user::get_current()->roles)))) {
-      switch ($this->type) {
-        case 'copy':
-        case 'link': if ($this->type === 'copy') $result = core::deep_clone(storage::get('files')->select($this->source, true));
-                     if ($this->type === 'link') $result =                  storage::get('files')->select($this->source, true);
-                     foreach ($this->properties as $c_key => $c_value)
-                       core::arrobj_insert_value($result, $c_key, $c_value);
-                     $this->child_insert($result);                                                                       return;
-        case 'code': $this->child_insert(call_user_func_array($this->source, ['page' => $page, 'args' => $this->args])); return;
-        case 'text': $this->child_insert(new text($this->source));                                                       return;
-        default    : $this->child_insert(         $this->source );                                                       return;
+    if (!$this->is_builded) {
+      $result = null;
+      if (!isset($this->display) ||
+          (isset($this->display) && $this->display->check === 'page_args' && preg_match($this->display->match,         $page->args_get($this->display->where))) ||
+          (isset($this->display) && $this->display->check === 'user'      &&            $this->display->where === 'role' && preg_match($this->display->match.'m', implode(nl, user::get_current()->roles)))) {
+        switch ($this->type) {
+          case 'copy':
+          case 'link': if ($this->type === 'copy') $result = core::deep_clone(storage::get('files')->select($this->source, true));
+                       if ($this->type === 'link') $result =                  storage::get('files')->select($this->source, true);
+                       foreach ($this->properties     as    $c_key => $c_value)
+                         core::arrobj_insert_value($result, $c_key,   $c_value);
+                       break;
+          case 'code': $result = @call_user_func_array($this->source, ['page' => $page, 'args' => $this->args]); break;
+          case 'text': $result = new text($this->source); break;
+          default    : $result =          $this->source;
+        }
       }
+      if ($result) $this->child_insert($result);
+      $this->is_builded = true;
     }
   }
 
