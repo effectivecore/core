9b72085b1b197dd81e9daf928ec0410952330e9e
Maxim Rysevets <root@effcore.com>
2020-10-21 22:53:40 +0300

    sprint_1041_1080

--- system/module_page/backend/patterns/Block.php
+++ system/module_page/backend/patterns/Block.php
@@ -7,14 +7,23 @@
 namespace effcore {
           class block extends markup {
 
-  public $tag_name = null;
-  public $template = null;
+  public $tag_name = 'section';
+  public $template = 'block';
 # ─────────────────────────────────────────────────────────────────────
+  public $title;
+  public $title_tag_name = 'h2';
+  public $title_attributes = ['data-section-title' => true];
+  public $content_tag_name = 'x-section-content';
   public $display;
   public $type; # code | link | text | …
   public $source;
   public $properties = [];
   public $args       = [];
+
+  function __construct($title = null, $attributes = [], $children = [], $weight = 0) {
+    if ($title) $this->title = $title;
+    parent::__construct(null, $attributes, $children, $weight);
+  }
 
   function build($page = null) {
     if (!$this->is_builded) {
@@ -36,6 +45,30 @@ namespace effcore {
       }
       if ($result) $this->child_insert($result, 'result');
       $this->is_builded = true;
+    }
+  }
+
+  function render() {
+    if ($this->template) {
+      return (template::make_new($this->template, [
+        'tag_name'   => $this->tag_name,
+        'attributes' => $this->render_attributes(),
+        'self'       => $this->render_self(),
+        'children'   => $this->content_tag_name ? (new markup($this->content_tag_name, ['data-section-content' => true],
+                        $this->render_children($this->children_select(true)) ))->render() :
+                        $this->render_children($this->children_select(true))
+      ]))->render();
+    } else {
+      return $this->render_self().
+             $this->render_children($this->children_select(true));
+    }
+  }
+
+  function render_self() {
+    if ($this->title) {
+      return (new markup($this->title_tag_name, $this->title_attributes, [
+        $this->title
+      ]))->render();
     }
   }
 
