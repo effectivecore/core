227b44d394775faa3707aaace3ce601e53bb57ad
Maxim Rysevets <root@effcore.com>
2020-12-04 17:58:42 +0300

    sprint_1041_1220

--- system/module_storage/backend/Events_Storage.php
+++ system/module_storage/backend/Events_Storage.php
@@ -7,8 +7,10 @@
 namespace effcore\modules\storage {
           use const \effcore\dir_root;
           use \effcore\field_file;
+          use \effcore\file;
           use \effcore\file_uploaded;
           use \effcore\widget_files;
+          use \effcore\widget_files_pictures;
           abstract class events_storage {
 
   static function on_instance_delete_before($event, $instance) {
@@ -17,16 +19,23 @@ namespace effcore\modules\storage {
       if (!empty($c_field->managing_control_class)) {
         $c_reflection = new \ReflectionClass($c_field->managing_control_class);
         $c_reflection_instance = $c_reflection->newInstanceWithoutConstructor();
+      # deleting the file associated with field_file
         if ($c_reflection_instance instanceof field_file) {
           if (!empty($instance->{$c_name})) {
             @unlink(dir_root.$instance->{$c_name});
           }
         }
+      # deleting files associated with widget_files, widget_files_pictures
         if ($c_reflection_instance instanceof widget_files) {
           if (!empty($instance->{$c_name})) {
             foreach ($instance->{$c_name} as $c_item) {
               if ($c_item->object instanceof file_uploaded) {
                 @unlink($c_item->object->get_current_path());
+                if ($c_reflection_instance instanceof widget_files_pictures) {
+                  $thumbnail = new file($c_item->object->get_current_path());
+                  $thumbnail->name_set($thumbnail->name_get().'.thumb');
+                  @unlink($thumbnail->path_get());
+                }
               }
             }
           }
