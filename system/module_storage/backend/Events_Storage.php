858d77e55ae81713f4ec1e400798f60d0c78fd0a
Maxim Rysevets <root@effcore.com>
2020-12-09 15:03:13 +0300

    sprint_1041_1234

--- system/module_storage/backend/Events_Storage.php
+++ system/module_storage/backend/Events_Storage.php
@@ -6,10 +6,12 @@
 
 namespace effcore\modules\storage {
           use const \effcore\dir_root;
+          use \effcore\dynamic;
           use \effcore\field_file;
           use \effcore\file;
           use \effcore\file_uploaded;
           use \effcore\media;
+          use \effcore\token;
           use \effcore\widget_files;
           use \effcore\widget_files_pictures;
           abstract class events_storage {
@@ -18,18 +20,16 @@ namespace effcore\modules\storage {
     $entity = $instance->entity_get();
     foreach ($entity->fields as $c_name => $c_field) {
       if (!empty($c_field->managing_control_class)) {
-        $c_reflection = new \ReflectionClass($c_field->managing_control_class);
-        $c_reflection_instance = $c_reflection->newInstanceWithoutConstructor();
+        $c_control = (new \ReflectionClass($c_field->managing_control_class))->newInstanceWithoutConstructor();
+        foreach ($c_field->managing_control_properties           ?? [] as $c_prop_name => $c_prop_value) $c_control->{$c_prop_name} = $c_prop_value;
+        foreach ($c_field->managing_control_properties_on_update ?? [] as $c_prop_name => $c_prop_value) $c_control->{$c_prop_name} = $c_prop_value;
       # deleting thumbnails associated with widget_files_pictures
-        if ($c_reflection_instance instanceof widget_files_pictures) {
-          if (!empty($instance->{$c_name})) {
-            foreach ($instance->{$c_name} as $c_item) {
-              if ($c_item->object instanceof file_uploaded) {
-                $thumbnail = new file($c_item->object->get_current_path());
-                media::picture_thumbnails_cleaning($thumbnail->dirs_get(), $thumbnail->name_get());
-              }
-            }
-          }
+        if ($c_control instanceof widget_files_pictures) {
+          token::insert('item_id_context', '%%_instance_id_context', 'text', $instance->id);
+          token::insert('item_id_context', '%%_item_id_context',     'text', '');
+          $dirs = dynamic::dir_files.$c_control->upload_dir;
+          $name =       token::apply($c_control->fixed_name);
+          media::picture_thumbnails_cleaning($dirs, $name);
         }
       }
     }
