91e9582b3d2ca5d04ed7c770c6fa2face65ab29c
Maxim Rysevets <root@effcore.com>
2020-12-09 13:33:39 +0300

    sprint_1041_1232

--- system/module_storage/backend/Events_Storage.php
+++ system/module_storage/backend/Events_Storage.php
@@ -14,6 +14,27 @@ namespace effcore\modules\storage {
           use \effcore\widget_files_pictures;
           abstract class events_storage {
 
+  static function on_instance_update_before($event, $instance) {
+    $entity = $instance->entity_get();
+    foreach ($entity->fields as $c_name => $c_field) {
+      if (!empty($c_field->managing_control_class)) {
+        $c_reflection = new \ReflectionClass($c_field->managing_control_class);
+        $c_reflection_instance = $c_reflection->newInstanceWithoutConstructor();
+      # deleting thumbnails associated with widget_files_pictures
+        if ($c_reflection_instance instanceof widget_files_pictures) {
+          if (!empty($instance->{$c_name})) {
+            foreach ($instance->{$c_name} as $c_item) {
+              if ($c_item->object instanceof file_uploaded) {
+                $thumbnail = new file($c_item->object->get_current_path());
+                media::picture_thumbnails_cleaning($thumbnail->dirs_get(), $thumbnail->name_get());
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
   static function on_instance_delete_before($event, $instance) {
     $entity = $instance->entity_get();
     foreach ($entity->fields as $c_name => $c_field) {
@@ -26,16 +47,23 @@ namespace effcore\modules\storage {
             @unlink(dir_root.$instance->{$c_name});
           }
         }
-      # deleting files associated with widget_files, widget_files_pictures
+      # deleting files associated with widget_files
         if ($c_reflection_instance instanceof widget_files) {
           if (!empty($instance->{$c_name})) {
             foreach ($instance->{$c_name} as $c_item) {
               if ($c_item->object instanceof file_uploaded) {
                 @unlink($c_item->object->get_current_path());
-                if ($c_reflection_instance instanceof widget_files_pictures) {
-                  $thumbnail = new file($c_item->object->get_current_path());
-                  media::picture_thumbnails_cleaning($thumbnail->dirs_get(), $thumbnail->name_get());
-                }
+              }
+            }
+          }
+        }
+      # deleting thumbnails associated with widget_files_pictures
+        if ($c_reflection_instance instanceof widget_files_pictures) {
+          if (!empty($instance->{$c_name})) {
+            foreach ($instance->{$c_name} as $c_item) {
+              if ($c_item->object instanceof file_uploaded) {
+                $thumbnail = new file($c_item->object->get_current_path());
+                media::picture_thumbnails_cleaning($thumbnail->dirs_get(), $thumbnail->name_get());
               }
             }
           }
