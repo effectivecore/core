0ca9f6421fcdec6dd40278c01f4863271f49100c
Maxim Rysevets <root@effcore.com>
2020-12-19 13:12:36 +0300

    sprint_1041_1254

--- system/module_page/backend/patterns/Widget_Files_pictures.php
+++ system/module_page/backend/patterns/Widget_Files_pictures.php
@@ -122,8 +122,8 @@ namespace effcore {
         $c_file = new file($c_item->object->get_current_path());
         $c_item_type = 'picture';
         $c_item_markup = $c_file->type === 'picture' ?
-          new markup('a', ['title' => new text('click to open in new window'), 'target' => 'widget_files-pictures-items', 'href' => '/'.$c_file->path_get_relative().'?thumb=big'], new markup_simple('img', ['src' => '/'.$c_file->path_get_relative().'?thumb=middle', 'alt' => new text('thumbnail')])) :
-          new markup('a', ['title' => new text('click to open in new window'), 'target' => 'widget_files-pictures-items', 'href' => '/'.$c_file->path_get_relative()             ], new markup_simple('img', ['src' => '/'.$c_file->path_get_relative(),                 'alt' => new text('thumbnail')]));
+          new markup('a', ['data-type' => 'picture-wrapper', 'title' => new text('click to open in new window'), 'target' => 'widget_files-pictures-items', 'href' => '/'.$c_file->path_get_relative().'?thumb=big'], new markup_simple('img', ['src' => '/'.$c_file->path_get_relative().'?thumb=middle', 'alt' => new text('thumbnail')])) :
+          new markup('a', ['data-type' => 'picture-wrapper', 'title' => new text('click to open in new window'), 'target' => 'widget_files-pictures-items', 'href' => '/'.$c_file->path_get_relative()             ], new markup_simple('img', ['src' => '/'.$c_file->path_get_relative(),                 'alt' => new text('thumbnail')]));
         $decorator->data[$c_item_num] = [
           'type'     => ['value' => $c_item_type  ],
           'num'      => ['value' => $c_item_num   ],
