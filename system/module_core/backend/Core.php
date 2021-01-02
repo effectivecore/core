9af27b2c9229ae714bf1071122bd5fedc8319494
Maxim Rysevets <root@effcore.com>
2021-01-02 18:12:25 +0300

    sprint_1041_1278

--- system/module_core/backend/Core.php
+++ system/module_core/backend/Core.php
@@ -215,7 +215,7 @@ namespace effcore {
 
   static function data_to_string($data) {
     switch (gettype($data)) {
-      case 'string' : return '\''.addcslashes($data, nl.'\'\\').'\'';
+      case 'string' : return '\''.addcslashes($data, "\0..\37'\\").'\'';
       case 'boolean': return $data ? 'true' : 'false';
       case 'NULL'   : return 'null';
       case 'object' :
@@ -259,7 +259,7 @@ namespace effcore {
                          $array_defaults[$c_key] === $c_value) continue;
             $result.= static::data_to_code($c_value, $prefix.(is_int($c_key) ?
                                                                  '['.$c_key.']' :
-                                                   '[\''.addcslashes($c_key, nl.'\'\\').'\']'));
+                                                   '[\''.addcslashes($c_key, "\0..\37'\\").'\']'));
           }
         } else {
           $result.= $prefix.' = [];'.nl;
