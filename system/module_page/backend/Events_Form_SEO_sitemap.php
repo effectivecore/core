59227f1b400113209258e5209aaa7de6615cc84b
Maxim Rysevets <root@effcore.com>
2021-01-07 23:38:59 +0300

    sprint_1041_1299

--- system/module_page/backend/Events_Form_SEO_sitemap.php
+++ system/module_page/backend/Events_Form_SEO_sitemap.php
@@ -5,16 +5,27 @@
   ##################################################################
 
 namespace effcore\modules\page {
+          use \effcore\data;
+          use \effcore\file;
           use \effcore\message;
           abstract class events_form_seo_sitemap {
 
   static function on_init($event, $form, $items) {
+    $file = new file(data::directory.'sitemap.xml');
+    if ($file->is_exist()) {
+      $items['#content']->value_set(
+        $file->load()
+      );
+    }
   }
 
   static function on_submit($event, $form, $items) {
     switch ($form->clicked_button->value_get()) {
       case 'save':
-        message::insert('The changes was saved.');
+        $file = new file(data::directory.'sitemap.xml');
+        $file->data_set($items['#content']->value_get());
+        if ($file->save()) message::insert('The changes was saved.');
+        else               message::insert('The changes was not saved!', 'error');
         break;
     }
   }
