0ea7724787cc4d45cd00569ef6540029e2f8611e
Maxim Rysevets <root@effcore.com>
2021-01-07 21:23:59 +0300

    sprint_1041_1296

--- system/module_page/backend/Events_Page_SEO.php
+++ system/module_page/backend/Events_Page_SEO.php
@@ -4,13 +4,13 @@
   ### Copyright © 2017—2021 Maxim Rysevets. All rights reserved. ###
   ##################################################################
 
-namespace effcore\modules\user {
+namespace effcore\modules\page {
           use \effcore\url;
-          abstract class events_page_security {
+          abstract class events_page_seo {
 
   static function on_redirect($event, $page) {
     $type = $page->args_get('type');
-    if ($type === null) url::go($page->args_get('base').'/settings');
+    if ($type === null) url::go($page->args_get('base').'/meta');
   }
 
 }}
