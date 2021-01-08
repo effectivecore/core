ca8aa7586657112eb378969d8399911c96e1828c
Maxim Rysevets <root@effcore.com>
2021-01-08 12:19:12 +0300

    sprint_1041_1301

--- system/module_core/backend/Console.php
+++ system/module_core/backend/Console.php
@@ -78,7 +78,7 @@ namespace effcore {
                                     $c_log->object.' | '.
                                     $c_log->action.' | '.$c_info.nl)) {
           message::insert(new text_multiline([
-            'Cannot insert or update file "%%_file" in the directory "%%_directory"!',
+            'File "%%_file" cannot be inserted or updated in the directory "%%_directory"!',
             'Check file (if exists) and directory permissions.'], [
             'file'      => $file->file_get(),
             'directory' => $file->dirs_get_relative()]), 'error'
