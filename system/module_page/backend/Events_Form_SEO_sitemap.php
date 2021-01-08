be27749c5454e070e96902f7759759aaed4cb7d7
Maxim Rysevets <root@effcore.com>
2021-01-08 13:57:04 +0300

    sprint_1041_1306

--- system/module_page/backend/Events_Form_SEO_sitemap.php
+++ system/module_page/backend/Events_Form_SEO_sitemap.php
@@ -8,6 +8,7 @@ namespace effcore\modules\page {
           use \effcore\data;
           use \effcore\file;
           use \effcore\message;
+          use \effcore\text_multiline;
           abstract class events_form_seo_sitemap {
 
   static function on_init($event, $form, $items) {
@@ -25,7 +26,7 @@ namespace effcore\modules\page {
         $file = new file(data::directory.'sitemap.xml');
         $file->data_set($items['#content']->value_get());
         if ($file->save()) message::insert('The changes was saved.');
-        else               message::insert('The changes was not saved!', 'error');
+        else message::insert(new text_multiline(['The changes was not saved!', 'File "%%_file" cannot be written to disc!', 'File permissions (if the file exists) and directory permissions should be checked.'], ['file' => $file->path_get_relative()]), 'error');
         break;
     }
   }
