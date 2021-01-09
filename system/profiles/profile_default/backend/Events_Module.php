c7efa6b4d13d3e12bcdb770e09aa8984c585c7b3
Maxim Rysevets <root@effcore.com>
2021-01-09 22:16:47 +0300

    sprint_1041_1326

--- system/profiles/profile_default/backend/Events_Module.php
+++ system/profiles/profile_default/backend/Events_Module.php
@@ -12,12 +12,11 @@ namespace effcore\modules\profile_default {
           use \effcore\text_multiline;
           abstract class events_module {
 
-  static $conflict_has_been = false;
+  static $is_failed_installation = false;
 
   static function on_install($event) {
     $module = module::get('profile_default');
-    if (page::get_current()->id === 'install' && field::request_value_get('profile') !== $module->id) {static::$conflict_has_been = true; return;}
-    if (page::get_current()->id === 'install' && field::request_value_get('profile') === $module->id) {$module->install();}
+    if (page::get_current()->id === 'install') $module->install();
     if (page::get_current()->id !== 'install') {
       $page_ids = [];
       if (page::get('front'       )) $page_ids[] = 'front';
@@ -28,7 +27,7 @@ namespace effcore\modules\profile_default {
       if (!count($page_ids)) {
         $module->install();
       } else {
-        static::$conflict_has_been = true;
+        static::$is_failed_installation = true;
         message::insert(new text_multiline([
           'Unable to install the profile "%%_profile" because the system already has Pages with the following IDs: %%_ids',
           'Uninstall the existing profile first.'], ['profile' => $module->title, 'ids' => implode(', ', $page_ids)]), 'warning'
@@ -43,7 +42,7 @@ namespace effcore\modules\profile_default {
   }
 
   static function on_enable($event) {
-    if (!static::$conflict_has_been) {
+    if (!static::$is_failed_installation) {
       $module = module::get('profile_default');
       $module->enable();
     }
