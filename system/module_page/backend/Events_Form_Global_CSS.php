5386b0192b9f63644d7d92a0b70bd9af2ce43a40
Maxim Rysevets <root@effcore.com>
2021-01-10 17:15:06 +0300

    sprint_1041_1328

---
+++ system/module_page/backend/Events_Form_Global_CSS.php
@@ -0,0 +1,20 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2021 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\page {
+          use \effcore\data;
+          use \effcore\file;
+          use \effcore\message;
+          use \effcore\text_multiline;
+          abstract class events_form_global_css {
+
+  static function on_init($event, $form, $items) {
+  }
+
+  static function on_submit($event, $form, $items) {
+  }
+
+}}
