7b140af74be64b3b19e69490ca43176a1b49e80b
Maxim Rysevets <root@effcore.com>
2021-01-27 19:13:16 +0300

    sprint_1041_1428

--- system/module_page/backend/patterns/Field_File_audio.php
+++ system/module_page/backend/patterns/Field_File_audio.php
@@ -14,11 +14,23 @@ namespace effcore {
     'name' => 'audio'];
   public $max_file_size = '10M';
   public $types_allowed = [
-    'mp3' => 'mp3'
-  ];
+    'mp3' => 'mp3'];
+  public $player_is_visible = true;
+  public $player_controls = true;
+  public $player_preload = 'metadata';
+  public $player_name = 'default';
+  public $player_timeline_is_visible = 'true';
 
   protected function pool_manager_action_insert_get_field_text($item, $id, $type) {
-    return new text('delete audio "%%_audio"', ['audio' => $item->file]);
+    if ($this->player_is_visible) {
+      $file = new file($item->get_current_path());
+      $player_markup = new markup('audio', ['src' => '/'.$file->path_get_relative(),
+             'controls'                        => $this->player_controls,
+             'preload'                         => $this->player_preload,
+             'data-player-name'                => $this->player_name,
+             'data-player-timeline-is-visible' => $this->player_timeline_is_visible], [], +450);
+           return new node([], [$player_markup, new text('delete audio "%%_audio"', ['audio' => $item->file])]);
+    } else return new node([], [                new text('delete audio "%%_audio"', ['audio' => $item->file])]);
   }
 
 }}
