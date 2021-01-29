075a7a5f7289a67d15917457e4bb2d52f91a46f5
Maxim Rysevets <root@effcore.com>
2021-01-29 16:06:07 +0300

    sprint_1041_1434

---
+++ system/module_page/backend/patterns/Widget_Files_multimedia.php
@@ -0,0 +1,45 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2021 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore {
+          class widget_files_media extends widget_files {
+
+  ###########################
+  ### static declarations ###
+  ###########################
+
+  static function complex_value_to_markup($complex) {  
+    $decorator = new decorator;
+    $decorator->id = 'widget_files-pictures-items';
+    $decorator->view_type = 'template';
+    $decorator->template = 'content';
+    $decorator->template_row = 'gallery_row';
+    $decorator->template_row_mapping = core::array_kmap(['num', 'type', 'children']);
+    if ($complex) {
+      core::array_sort_by_weight($complex);
+      foreach ($complex as $c_row_id => $c_item) {
+        $c_file = new file($c_item->object->get_current_path());
+        switch ($c_item->object->type) {
+          case 'picture':
+            $c_item_type = 'picture';
+            $c_item_markup = new markup('a', ['data-type' => 'picture-wrapper', 'title' => new text('click to open in new window'), 'target' => 'widget_files-pictures-items', 'href' => '/'.$c_file->path_get_relative().'?thumb=big'], new markup_simple('img', ['src' => '/'.$c_file->path_get_relative().'?thumb=middle', 'alt' => new text('thumbnail')]));
+            break;
+          case 'mp3':
+            $c_item_type = 'audio';
+            $c_item_markup = new markup('audio', ['data-id' => $c_row_id, 'src' => '/'.$c_file->path_get_relative(), 'controls' => true, 'preload' => 'metadata', 'data-player-name' => 'default', 'data-player-timeline-is-visible' => 'true'], [], +450);
+            break;
+        }
+        $decorator->data[$c_row_id] = [
+          'type'     => ['value' => $c_item_type  ],
+          'num'      => ['value' => $c_row_id     ],
+          'children' => ['value' => $c_item_markup]
+        ];
+      }
+    }
+    return $decorator;
+  }
+
+}}
