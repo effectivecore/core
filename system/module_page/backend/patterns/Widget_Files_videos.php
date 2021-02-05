a278b63f0f77abefc0a1439ee2ac9acb285f1c70
Maxim Rysevets <root@effcore.com>
2021-02-05 18:41:14 +0300

    sprint_1041_1464

--- system/module_page/backend/patterns/Widget_Files_videos.php
+++ system/module_page/backend/patterns/Widget_Files_videos.php
@@ -19,4 +19,59 @@ namespace effcore {
     'mp4' => 'mp4'
   ];
 
+  function widget_insert_get() {
+    $widget = new markup('x-widget', [
+      'data-type' => 'insert']);
+  # control for upload new file
+    $field_file_video = new field_file_video;
+    $field_file_video->title = 'File';
+    $field_file_video->max_file_size    = $this->max_file_size;
+    $field_file_video->types_allowed    = $this->types_allowed;
+    $field_file_video->cform            = $this->cform;
+    $field_file_video->min_files_number = null;
+    $field_file_video->max_files_number = null;
+    $field_file_video->has_on_validate  = false;
+    $field_file_video->build();
+    $field_file_video->multiple_set();
+    $field_file_video->name_set($this->name_get_complex().'__file[]');
+    $this->controls['#file'] = $field_file_video;
+  # button for insertion of the new item
+    $button = new button(null, ['data-style' => 'narrow-insert', 'title' => new text('insert')]);
+    $button->break_on_validate = true;
+    $button->build();
+    $button->value_set($this->name_get_complex().'__insert');
+    $button->_type = 'insert';
+    $this->controls['~insert'] = $button;
+  # grouping of previous elements in widget 'insert'
+    $widget->child_insert($field_file_video, 'file');
+    $widget->child_insert($button, 'button');
+    return $widget;
+  }
+
+  ###########################
+  ### static declarations ###
+  ###########################
+
+  static function complex_value_to_markup($complex) {  
+    $decorator = new decorator;
+    $decorator->id = 'widget_files-videos-items';
+    $decorator->view_type = 'template';
+    $decorator->template = 'content';
+    $decorator->template_row = 'gallery_row';
+    $decorator->template_row_mapping = core::array_kmap(['num', 'type', 'children']);
+    if ($complex) {
+      core::array_sort_by_weight($complex);
+      foreach ($complex as $c_row_id => $c_item) {
+        if ($c_item->object->type === 'mp4') {
+          $decorator->data[$c_row_id] = [
+            'type'     => ['value' => 'video'  ],
+            'num'      => ['value' => $c_row_id],
+            'children' => ['value' => new markup_simple('video', ['src' => '/'.$c_item->object->get_current_path(true)])]
+          ];
+        }
+      }
+    }
+    return $decorator;
+  }
+
 }}
