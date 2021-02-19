4c332a00e8f154c81cd27fb508d326bcbf347bcb
Maxim Rysevets <root@effcore.com>
2021-02-19 13:45:45 +0300

    sprint_1041_1509

--- system/module_develop/backend/Events_Form_Palette.php
+++ system/module_develop/backend/Events_Form_Palette.php
@@ -10,21 +10,24 @@ namespace effcore\modules\develop {
           use \effcore\message;
           use \effcore\module;
           use \effcore\storage;
-          use \effcore\text_multiline;
+          use \effcore\storage_nosql_files;
+          use \effcore\text;
           abstract class events_form_palette {
 
   static function on_init($event, $form, $items) {
     $settings = module::settings_get('develop');
-    $base_hex = $settings->test_color;
-    $items['#color']->value_set($base_hex);
+    $base_color = new color(null, $settings->test_color, $settings->test_color);
     $palette_markup = [];
     $palette_colors = [];
     for ($i = 0; $i < 21; $i++) {
-      $palette_markup[] = (new markup('x-color'));
-      $palette_colors[] = (new color(null, $base_hex, $base_hex))->filter_shift(($i - 10) * 5, ($i - 10) * 5, ($i - 10) * 5, 1, color::return_hex);
+      $c_color_hex = $base_color->filter_shift(($i - 10) * 5, ($i - 10) * 5, ($i - 10) * 5, 1, color::return_hex);
+      $c_color_key = 'custom_'.ltrim($c_color_hex, '#');
+      $palette_colors[$c_color_key] = new color(null, $c_color_hex, $c_color_hex, 'custom');
+      $palette_markup[] = new markup('x-color');
     }
+    $items['#color']->value_set($settings->test_color);
     $items['palette/result']->child_select('palette')->child_insert(new markup('x-palette', [], $palette_markup), 'palette');
-    $items['palette/result']->child_select('data'   )->child_insert(new text_multiline($palette_colors),          'data');
+    $items['palette/result']->child_select('data'   )->child_insert(new text(storage_nosql_files::data_to_text($palette_colors, 'colors')), 'data');
   }
 
   static function on_submit($event, $form, $items) {
