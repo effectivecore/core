e94f26a793a2bf1c56e1e3bd5da005872969b543
Maxim Rysevets <root@effcore.com>
2021-02-19 13:10:45 +0300

    sprint_1041_1508

--- system/module_develop/backend/Events_Form_Palette.php
+++ system/module_develop/backend/Events_Form_Palette.php
@@ -5,20 +5,26 @@
   ##################################################################
 
 namespace effcore\modules\develop {
+          use \effcore\color;
           use \effcore\markup;
           use \effcore\message;
           use \effcore\module;
           use \effcore\storage;
+          use \effcore\text_multiline;
           abstract class events_form_palette {
 
   static function on_init($event, $form, $items) {
     $settings = module::settings_get('develop');
-    $items['#color']->value_set($settings->test_color);
-    $colors_markup = [];
-    for ($i = 0; $i < 21; $i++) $colors_markup[] = new markup('x-color');
-    $items['palette/result']->child_insert(
-      new markup('x-colors-group', [], $colors_markup), 'colors'
-    );
+    $base_hex = $settings->test_color;
+    $items['#color']->value_set($base_hex);
+    $palette_markup = [];
+    $palette_colors = [];
+    for ($i = 0; $i < 21; $i++) {
+      $palette_markup[] = (new markup('x-color'));
+      $palette_colors[] = (new color(null, $base_hex, $base_hex))->filter_shift(($i - 10) * 5, ($i - 10) * 5, ($i - 10) * 5, 1, color::return_hex);
+    }
+    $items['palette/result']->child_select('palette')->child_insert(new markup('x-palette', [], $palette_markup), 'palette');
+    $items['palette/result']->child_select('data'   )->child_insert(new text_multiline($palette_colors),          'data');
   }
 
   static function on_submit($event, $form, $items) {
@@ -26,6 +32,7 @@ namespace effcore\modules\develop {
       case 'generate':
         storage::get('files')->changes_insert('develop', 'update', 'settings/develop/test_color', $items['#color']->value_get());
         message::insert('Generation done.');
+        static::on_init(null, $form, $items);
         break;
     }
   }
