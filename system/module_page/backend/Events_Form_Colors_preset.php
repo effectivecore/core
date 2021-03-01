eabea6321c6437d22fd9d962e6cf86749b4ecdaa
Maxim Rysevets <root@effcore.com>
2021-03-01 12:43:13 +0300

    sprint_1041_1562

--- system/module_page/backend/Events_Form_Colors_preset.php
+++ system/module_page/backend/Events_Form_Colors_preset.php
@@ -6,6 +6,7 @@
 
 namespace effcore\modules\page {
           use \effcore\color_preset;
+          use \effcore\field_checkbox_color;
           use \effcore\message;
           use \effcore\page;
           abstract class events_form_colors_preset {
@@ -14,70 +15,34 @@ namespace effcore\modules\page {
     $id = page::get_current()->args_get('id');
     $preset = color_preset::get($id);
     if ($preset) {
-      $items['#color__page_id'                  ]->color_set($preset->colors->color__page_id                  );
-      $items['#color__text_id'                  ]->color_set($preset->colors->color__text_id                  );
-      $items['#color__main_id'                  ]->color_set($preset->colors->color__main_id                  );
-      $items['#color__link_id'                  ]->color_set($preset->colors->color__link_id                  );
-      $items['#color__link_active_id'           ]->color_set($preset->colors->color__link_active_id           );
-      $items['#color__table_row_odd_id'         ]->color_set($preset->colors->color__table_row_odd_id         );
-      $items['#color__table_row_even_id'        ]->color_set($preset->colors->color__table_row_even_id        );
-      $items['#color__relation_id'              ]->color_set($preset->colors->color__relation_id              );
-      $items['#color__menu_id'                  ]->color_set($preset->colors->color__menu_id                  );
-      $items['#color__menu_active_id'           ]->color_set($preset->colors->color__menu_active_id           );
-      $items['#color__menu_text_id'             ]->color_set($preset->colors->color__menu_text_id             );
-      $items['#color__menu_link_id'             ]->color_set($preset->colors->color__menu_link_id             );
-      $items['#color__menu_link_active_id'      ]->color_set($preset->colors->color__menu_link_active_id      );
-      $items['#color__tabs_id'                  ]->color_set($preset->colors->color__tabs_id                  );
-      $items['#color__tabs_link_id'             ]->color_set($preset->colors->color__tabs_link_id             );
-      $items['#color__tabs_link_active_id'      ]->color_set($preset->colors->color__tabs_link_active_id      );
-      $items['#color__tabs_link_active_no_bg_id']->color_set($preset->colors->color__tabs_link_active_no_bg_id);
-      $items['#color__ok_id'                    ]->color_set($preset->colors->color__ok_id                    );
-      $items['#color__warning_id'               ]->color_set($preset->colors->color__warning_id               );
-      $items['#color__error_id'                 ]->color_set($preset->colors->color__error_id                 );
-      $items['#color__fieldset_id'              ]->color_set($preset->colors->color__fieldset_id              );
-      $items['#color__fieldset_nested_id'       ]->color_set($preset->colors->color__fieldset_nested_id       );
-      $items['#color__field_id'                 ]->color_set($preset->colors->color__field_id                 );
-      $items['#color__field_text_id'            ]->color_set($preset->colors->color__field_text_id            );
-      $items['#color__button_id'                ]->color_set($preset->colors->color__button_id                );
-      $items['#color__button_active_id'         ]->color_set($preset->colors->color__button_active_id         );
-      $items['#color__button_text_id'           ]->color_set($preset->colors->color__button_text_id           );
+      foreach ($items as $c_npath => $c_item) {
+        if ($c_item instanceof field_checkbox_color) {
+          if (substr($c_item->name_get(), 0, 7) === 'color__') {
+            $c_item->color_set(
+              $preset->colors->{$c_item->name_get()}
+            );
+          }
+        }
+      }
     } else $items['~apply']->disabled_set(true);
   }
 
   static function on_submit($event, $form, $items) {
     switch ($form->clicked_button->value_get()) {
       case 'apply':
+        $changes = [];
         $id = page::get_current()->args_get('id');
         $preset = color_preset::get($id);
         if ($preset) {
-          $changes = [];
-          if ($items['#color__page_id'                  ]->checked_get()) $changes['color__page_id'                  ] = true;
-          if ($items['#color__text_id'                  ]->checked_get()) $changes['color__text_id'                  ] = true;
-          if ($items['#color__main_id'                  ]->checked_get()) $changes['color__main_id'                  ] = true;
-          if ($items['#color__link_id'                  ]->checked_get()) $changes['color__link_id'                  ] = true;
-          if ($items['#color__link_active_id'           ]->checked_get()) $changes['color__link_active_id'           ] = true;
-          if ($items['#color__table_row_odd_id'         ]->checked_get()) $changes['color__table_row_odd_id'         ] = true;
-          if ($items['#color__table_row_even_id'        ]->checked_get()) $changes['color__table_row_even_id'        ] = true;
-          if ($items['#color__relation_id'              ]->checked_get()) $changes['color__relation_id'              ] = true;
-          if ($items['#color__menu_id'                  ]->checked_get()) $changes['color__menu_id'                  ] = true;
-          if ($items['#color__menu_active_id'           ]->checked_get()) $changes['color__menu_active_id'           ] = true;
-          if ($items['#color__menu_text_id'             ]->checked_get()) $changes['color__menu_text_id'             ] = true;
-          if ($items['#color__menu_link_id'             ]->checked_get()) $changes['color__menu_link_id'             ] = true;
-          if ($items['#color__menu_link_active_id'      ]->checked_get()) $changes['color__menu_link_active_id'      ] = true;
-          if ($items['#color__tabs_id'                  ]->checked_get()) $changes['color__tabs_id'                  ] = true;
-          if ($items['#color__tabs_link_id'             ]->checked_get()) $changes['color__tabs_link_id'             ] = true;
-          if ($items['#color__tabs_link_active_id'      ]->checked_get()) $changes['color__tabs_link_active_id'      ] = true;
-          if ($items['#color__tabs_link_active_no_bg_id']->checked_get()) $changes['color__tabs_link_active_no_bg_id'] = true;
-          if ($items['#color__ok_id'                    ]->checked_get()) $changes['color__ok_id'                    ] = true;
-          if ($items['#color__warning_id'               ]->checked_get()) $changes['color__warning_id'               ] = true;
-          if ($items['#color__error_id'                 ]->checked_get()) $changes['color__error_id'                 ] = true;
-          if ($items['#color__fieldset_id'              ]->checked_get()) $changes['color__fieldset_id'              ] = true;
-          if ($items['#color__fieldset_nested_id'       ]->checked_get()) $changes['color__fieldset_nested_id'       ] = true;
-          if ($items['#color__field_id'                 ]->checked_get()) $changes['color__field_id'                 ] = true;
-          if ($items['#color__field_text_id'            ]->checked_get()) $changes['color__field_text_id'            ] = true;
-          if ($items['#color__button_id'                ]->checked_get()) $changes['color__button_id'                ] = true;
-          if ($items['#color__button_active_id'         ]->checked_get()) $changes['color__button_active_id'         ] = true;
-          if ($items['#color__button_text_id'           ]->checked_get()) $changes['color__button_text_id'           ] = true;
+          foreach ($items as $c_npath => $c_item) {
+            if ($c_item instanceof field_checkbox_color) {
+              if (substr($c_item->name_get(), 0, 7) === 'color__') {
+                if ($c_item->checked_get()) {
+                  $changes[$c_item->name_get()] = true;
+                }
+              }
+            }
+          }
           if (!count($changes)) {
             message::insert('No one item was selected!', 'warning');
           } else {
