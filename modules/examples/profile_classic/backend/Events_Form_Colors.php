73bcc827d6b6a7ea3ede90690952c3e4137d5089
Maxim Rysevets <root@effcore.com>
2021-03-03 15:02:15 +0300

    sprint_1041_1579

---
+++ modules/examples/profile_classic/backend/Events_Form_Colors.php
@@ -0,0 +1,41 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2021 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\profile_classic {
+          use \effcore\form_part;
+          use \effcore\module;
+          use \effcore\storage;
+          abstract class events_form_colors {
+
+  static function on_build($event, $form) {
+    $form->child_insert(
+      form_part::get('form_colors__profile_classic'), 'profile_classic'
+    );
+  }
+
+  static function on_init($event, $form, $items) {
+    $settings = module::settings_get('profile_classic');
+    $items['*color_custom__head_id']->value_set($settings->color_custom__head_id);
+    $items['*color_custom__foot_id']->value_set($settings->color_custom__foot_id);
+  }
+
+  static function on_submit($event, $form, $items) {
+    switch ($form->clicked_button->value_get()) {
+      case 'save':
+        $storage = storage::get('files');
+        $storage->changes_insert('profile_classic', 'update', 'settings/profile_classic/color_custom__head_id', $items['*color_custom__head_id']->value_get(), false);
+        $storage->changes_insert('profile_classic', 'update', 'settings/profile_classic/color_custom__foot_id', $items['*color_custom__foot_id']->value_get()       );
+        break;
+      case 'reset':
+        $storage = storage::get('files');
+        $storage->changes_delete('profile_classic', 'update', 'settings/profile_classic/color_custom__head_id', false);
+        $storage->changes_delete('profile_classic', 'update', 'settings/profile_classic/color_custom__foot_id'       );
+        static::on_init(null, $form, $items);
+        break;
+    }
+  }
+
+}}
---
+++ modules/examples/profile_classic/backend/Events_Token.php
@@ -0,0 +1,24 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2021 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\profile_classic {
+          use \effcore\color;
+          use \effcore\module;
+          abstract class events_token {
+
+  static function on_apply($name, $args = []) {
+    $settings = module::settings_get('profile_classic');
+    $colors = color::get_all();
+    if ($name === 'color_custom__head' || $name === 'color_custom__foot') {
+      if ($name === 'color_custom__head') $color = $colors[$settings->color_custom__head_id] ?? $colors['white'];
+      if ($name === 'color_custom__foot') $color = $colors[$settings->color_custom__foot_id] ?? $colors['white'];
+      if (!empty($color->value) && count($args) === 0) return $color->value;
+      if (!empty($color->value) && count($args) === 3) return $color->filter_shift($args[0], $args[1], $args[2], 1, color::return_hex);
+      if (!empty($color->value) && count($args) === 4) return $color->filter_shift($args[0], $args[1], $args[2], $args[3], color::return_rgba);
+    }
+  }
+
+}}
