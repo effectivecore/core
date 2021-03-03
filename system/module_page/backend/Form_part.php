f7e7898d2ca269bd978ede8398e8b5adf303b259
Maxim Rysevets <root@effcore.com>
2021-03-03 13:45:54 +0300

    sprint_1041_1578

---
+++ system/module_page/backend/Form_part.php
@@ -0,0 +1,38 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2021 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore {
+          abstract class form_part {
+
+  static protected $cache;
+
+  static function cache_cleaning() {
+    static::$cache = null;
+  }
+
+  static function init() {
+    if (static::$cache === null) {
+      foreach (storage::get('files')->select('form_parts') as $c_module_id => $c_form_parts) {
+        foreach ($c_form_parts as $c_row_id => $c_form_part) {
+          if (isset(static::$cache[$c_row_id])) console::report_about_duplicate('form_part', $c_row_id, $c_module_id);
+                    static::$cache[$c_row_id] = $c_form_part;
+                    static::$cache[$c_row_id]->module_id = $c_module_id;
+        }
+      }
+    }
+  }
+
+  static function get($row_id) {
+    static::init();
+    return static::$cache[$row_id] ?? null;
+  }
+
+  static function get_all() {
+    static::init();
+    return static::$cache;
+  }
+
+}}
