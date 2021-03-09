add9ed6776f9a78c009c2a1a856b00aeb34f17a2
Maxim Rysevets <root@effcore.com>
2021-03-09 09:42:23 +0300

    sprint_1041_1597

--- system/module_page/backend/patterns/Field_File_picture.php
+++ system/module_page/backend/patterns/Field_File_picture.php
@@ -19,22 +19,22 @@ namespace effcore {
     'jpeg' => 'jpeg',
     'png'  => 'png',
     'gif'  => 'gif'];
-  public $thumbnails_is_visible = true;
-  public $thumbnails_allowed = [];
+  public $thumbnails_is_allowed = true;
+  public $thumbnails = [];
 
   protected function items_set($id, $items) {
-    if (count($this->thumbnails_allowed))
+    if (count($this->thumbnails))
       if (debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)[1]['function'] === 'on_values_pre_insert')
         foreach ($items as $c_item)
           if (media::media_class_get($c_item->type) === 'picture')
             if (media::is_type_for_thumbnail($c_item->type))
               if ($c_item->get_current_state() === 'pre')
-                  $c_item->container_picture_make($this->thumbnails_allowed);
+                  $c_item->container_picture_make($this->thumbnails);
     parent::items_set($id, $items);
   }
 
   protected function pool_manager_action_insert_get_field_text($item, $id, $type) {
-    if ($this->thumbnails_is_visible) {
+    if ($this->thumbnails_is_allowed) {
       $thumbnail_markup = new markup_simple('img', ['src' => '/'.$item->get_current_path(true).'?thumb=small', 'alt' => new text('thumbnail'), 'width' => '44', 'height' => '44', 'data-type' => 'thumbnail'], +450);
            return new node([], [$thumbnail_markup, new text('delete picture "%%_picture"', ['picture' => $item->file])]);
     } else return new node([], [                   new text('delete picture "%%_picture"', ['picture' => $item->file])]);
