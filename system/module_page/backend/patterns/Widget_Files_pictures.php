8cf64a951d4d35d5c97e5be2d4fe4d85d7861b5f
Maxim Rysevets <root@effcore.com>
2021-03-16 21:34:48 +0300

    sprint_1041_1635

--- system/module_page/backend/patterns/Widget_Files_pictures.php
+++ system/module_page/backend/patterns/Widget_Files_pictures.php
@@ -23,15 +23,24 @@ namespace effcore {
 # ─────────────────────────────────────────────────────────────────────
   public $thumbnails_is_allowed = true;
   public $thumbnails = [];
+  public $picture_default_settings = [
+    'title'  => 'click to open in new window',
+    'alt'    => 'thumbnail',
+    'target' => 'widget_files-pictures-items'
+  ];
 
   function items_set($items, $once = false) {
-    if ($this->thumbnails_is_allowed)
-      if (debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)[1]['function'] === 'on_button_click_insert')
-        foreach ($items as $c_item)
-          if (media::media_class_get($c_item->object->type) === 'picture')
-            if (media::is_type_for_thumbnail($c_item->object->type))
-              if ($c_item->object->get_current_state() === 'pre')
-                  $c_item->object->container_picture_make($this->thumbnails);
+    if (debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)[1]['function'] === 'on_button_click_insert') {
+      foreach ($items as $c_item) {
+        if ($c_item->object->get_current_state() === 'pre') {
+          $c_item->settings = $this->picture_default_settings;
+          $c_item->settings['data-thumbnails-is-embedded'] = false;
+          if ($this->thumbnails_is_allowed) {
+            if (media::media_class_get($c_item->object->type) === 'picture') {
+              if (media::is_type_for_thumbnail($c_item->object->type)) {
+                if ($c_item->object->container_picture_make($this->thumbnails))
+                  $c_item->settings['data-thumbnails-is-embedded'] = true;
+                }}}}}}
     parent::items_set($items, $once);
   }
 
@@ -41,7 +50,7 @@ namespace effcore {
     $widget = parent::widget_manage_get($item, $c_row_id);
     $widget->attribute_insert('data-is-new', $item->object->get_current_state() === 'pre' ? 'true' : 'false');
     if (media::media_class_get($item->object->type) === 'picture') {
-      if ($this->thumbnails_is_allowed) {
+      if ($item->settings['data-thumbnails-is-embedded']) {
         $thumbnail_markup = new markup_simple('img', ['src' => '/'.$item->object->get_current_path(true).'?thumb=small', 'alt' => new text('thumbnail'), 'width' => '44', 'height' => '44', 'data-type' => 'thumbnail'], +450);
         $widget->child_insert($thumbnail_markup, 'thumbnail');
       }
@@ -104,9 +113,9 @@ namespace effcore {
     return $decorator;
   }
 
-  static function item_markup_get($item, $row_id, $title = 'click to open in new window', $target = 'widget_files-pictures-items', $alt = 'thumbnail') {
-    return new markup('a', ['data-type' => 'picture-wrapper', 'title' => new text($title), 'target' => $target, 'href' => '/'.$item->object->get_current_path(true).'?thumb=big'],
-      new markup_simple('img', ['src' => '/'.$item->object->get_current_path(true).'?thumb=middle', 'alt' => new text($alt)])
+  static function item_markup_get($item, $row_id) {
+    return new markup('a', ['data-type' => 'picture-wrapper', 'title' => new text($item->settings['title']), 'target' => $item->settings['target'], 'href' => '/'.$item->object->get_current_path(true).'?thumb=big'],
+      new markup_simple('img', ['src' => '/'.$item->object->get_current_path(true).'?thumb=middle', 'alt' => new text($item->settings['alt'])])
     );
   }
 
