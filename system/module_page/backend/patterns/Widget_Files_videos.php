e2a5c15f6df2d0add9b3ed52cbf6cc4ac029fecc
Maxim Rysevets <root@effcore.com>
2021-03-18 12:11:18 +0300

    sprint_1041_1637

--- system/module_page/backend/patterns/Widget_Files_videos.php
+++ system/module_page/backend/patterns/Widget_Files_videos.php
@@ -103,7 +103,8 @@ namespace effcore {
 
   function on_button_click_insert($form, $npath, $button) {
     $values = $this->on_values_validate($form, $npath, $button);
-    if (count($values['file'])) {
+    if (!$this->controls['#file']->has_error() &&                                             count($values['file']) === 0) $this->controls['#file']->error_set('Field "%%_title" cannot be blank!', ['title' => (new text($this->controls['#file']->title))->render() ]);
+    if (!$this->controls['#file']->has_error() && !$this->controls['#poster']->has_error() && count($values['file']) !== 0) {
       $items = $this->items_get();
       foreach ($values['file'] as $c_value) {
         $min_weight = 0;
@@ -143,10 +144,6 @@ namespace effcore {
       }
       message::insert('Do not forget to save the changes!');
       return true;
-    } elseif (!$this->controls['#file']->has_error()) {
-      $this->controls['#file']->error_set(
-        'Field "%%_title" cannot be blank!', ['title' => (new text($this->controls['#file']->title))->render() ]
-      );
     }
   }
 
