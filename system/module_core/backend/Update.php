2b36c4ec75fd2fd58b37e4841183c3e5ba58602c
Maxim Rysevets <root@effcore.com>
2021-03-24 17:54:19 +0300

    sprint_1041_1675

--- system/module_core/backend/Update.php
+++ system/module_core/backend/Update.php
@@ -7,6 +7,18 @@
 namespace effcore {
           abstract class update {
 
+  static function is_required() {
+    foreach (module::get_all() as $c_module) {
+      $c_updates            = static::select_all        ($c_module->id);
+      $c_update_last_number = static::select_last_number($c_module->id);
+      foreach ($c_updates as $c_update) {
+        if ($c_update->number > $c_update_last_number) {
+          return true;
+        }
+      }
+    }
+  }
+
   static function select_all($module_id, $from_number = 0) {
     $updates = [];
     foreach (storage::get('files')->select('modules_update_data', false, false) ?? [] as $c_module_id => $c_updates)
@@ -17,25 +29,15 @@ namespace effcore {
     return $updates;
   }
 
-  static function insert_last_number($module_id, $last_number) {
-    $info = new instance('update', ['module_id' => $module_id]);
-    if ($info->select()) return (new instance('update', ['module_id' => $module_id, 'version_cur' => $last_number]))->update();
-    else                 return (new instance('update', ['module_id' => $module_id, 'version_cur' => $last_number]))->insert();
-  }
-
   static function select_last_number($module_id) {
     $info = new instance('update', ['module_id' => $module_id]);
     return $info->select() ? (int)$info->version_cur : 0;
   }
 
-  static function is_required() {
-    foreach (module::get_all() as $c_module) {
-      $c_updates            = static::select_all        ($c_module->id);
-      $c_update_last_number = static::select_last_number($c_module->id);
-      foreach ($c_updates as $c_update) {
-        if ($c_update->number > $c_update_last_number) return true;
-      }
-    }
+  static function insert_last_number($module_id, $last_number) {
+    $info = new instance('update', ['module_id' => $module_id]);
+    if ($info->select()) return (new instance('update', ['module_id' => $module_id, 'version_cur' => $last_number]))->update();
+    else                 return (new instance('update', ['module_id' => $module_id, 'version_cur' => $last_number]))->insert();
   }
 
 }}
