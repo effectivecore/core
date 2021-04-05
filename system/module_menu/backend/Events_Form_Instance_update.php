8bee1dc575334081489b5fdb4fb9ca6589f98e6d
Maxim Rysevets <root@effcore.com>
2021-04-05 12:12:07 +0300

    sprint_1041_1698

--- system/module_menu/backend/Events_Form_Instance_update.php
+++ system/module_menu/backend/Events_Form_Instance_update.php
@@ -23,7 +23,6 @@ namespace effcore\modules\menu {
           foreach ($tree_item->children_select_recursive() as $c_child) $items['#id_parent']->disabled[$c_child->id] = $c_child->id;
           $items['#id_parent']->disabled[$form->_instance->id] = $form->_instance->id;
           $items['#id_parent']->is_builded = false;
-          $items['#id_parent']->title__not_selected = '- root -';
           $items['#id_parent']->query_params['conditions'] = ['id_tree_!f' => 'id_tree', 'operator' => '=', 'id_tree_!v' => $form->_instance->id_tree];
           $items['#id_parent']->build();
           $items['#id_parent']->value_set(
