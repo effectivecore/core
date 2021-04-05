7715f2cb4598d11d63f3614ebb21be6fa2abd1c2
Maxim Rysevets <root@effcore.com>
2021-04-05 19:39:43 +0300

    sprint_1041_1704

--- modules/examples/profile_classic/backend/Events_Module_Update.php
+++ modules/examples/profile_classic/backend/Events_Module_Update.php
@@ -9,11 +9,11 @@ namespace effcore\modules\profile_classic {
           abstract class events_module_update {
 
   static function on_update_files($event, $bundle_id) {
-    return core_events_module_update::on_update_files($event, $bundle_id);
+    return core_events_module_update::on_update_files__git($event, $bundle_id);
   }
 
   static function on_repo_restore($event, $bundle_id) {
-    return core_events_module_update::on_repo_restore($event, $bundle_id);
+    return core_events_module_update::on_repo_restore__git($event, $bundle_id);
   }
 
 }}
