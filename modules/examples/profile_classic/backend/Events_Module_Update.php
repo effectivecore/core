c981c9e2f563a1c1f860249f8416e88b4d6da082
Maxim Rysevets <root@effcore.com>
2021-04-05 17:23:23 +0300

    sprint_1041_1702

---
+++ modules/examples/profile_classic/backend/Events_Module_Update.php
@@ -0,0 +1,19 @@
+<?php
+
+  ######################################################################
+  ### Copyright © 20NN—20NN Author/Rightholder. All rights reserved. ###
+  ######################################################################
+
+namespace effcore\modules\profile_classic {
+          use \effcore\modules\core\events_module_update as core_events_module_update;
+          abstract class events_module_update {
+
+  static function on_update_files($event, $bundle_id) {
+    return core_events_module_update::on_update_files($event, $bundle_id);
+  }
+
+  static function on_repo_restore($event, $bundle_id) {
+    return core_events_module_update::on_repo_restore($event, $bundle_id);
+  }
+
+}}
