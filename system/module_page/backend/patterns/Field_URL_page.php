637358a762094d8e4dc00c2dc7ffb7e90005b13e
Maxim Rysevets <root@effcore.com>
2021-04-10 21:56:23 +0300

    sprint_1041_1738

--- system/module_page/backend/patterns/Field_URL_page.php
+++ system/module_page/backend/patterns/Field_URL_page.php
@@ -24,9 +24,14 @@ namespace effcore {
 
   function render_description() {
     $this->render_prepare_description();
-    $this->description[] = new markup('p', ['data-id' => 'url-page-0'], new text('Field value should be start with "%%_value".', ['value' => '/'       ]));
-    $this->description[] = new markup('p', ['data-id' => 'url-page-1'], new text('Field value cannot be start with "%%_value".', ['value' => '/manage/']));
-    $this->description[] = new markup('p', ['data-id' => 'url-page-2'], new text('Field value cannot be start with "%%_value".', ['value' => '/user/'  ]));
+    $this->description[] = new markup('p', ['data-id' => 'url-page-root'   ], new text('Field value should be start with "%%_value".', ['value' => '/'        ]));
+    $this->description[] = new markup('p', ['data-id' => 'url-page-manage' ], new text('Field value cannot be start with "%%_value".', ['value' => '/manage/' ]));
+    $this->description[] = new markup('p', ['data-id' => 'url-page-user'   ], new text('Field value cannot be start with "%%_value".', ['value' => '/user/'   ]));
+    $this->description[] = new markup('p', ['data-id' => 'url-page-dynamic'], new text('Field value cannot be start with "%%_value".', ['value' => '/dynamic/']));
+    $this->description[] = new markup('p', ['data-id' => 'url-page-modules'], new text('Field value cannot be start with "%%_value".', ['value' => '/modules/']));
+    $this->description[] = new markup('p', ['data-id' => 'url-page-readme' ], new text('Field value cannot be start with "%%_value".', ['value' => '/readme/' ]));
+    $this->description[] = new markup('p', ['data-id' => 'url-page-shell'  ], new text('Field value cannot be start with "%%_value".', ['value' => '/shell/'  ]));
+    $this->description[] = new markup('p', ['data-id' => 'url-page-system' ], new text('Field value cannot be start with "%%_value".', ['value' => '/system/' ]));
     return parent::render_description();
   }
 
@@ -36,9 +41,14 @@ namespace effcore {
 
   static function validate_value($field, $form, $element, &$new_value) {
     if (  parent::validate_value($field, $form, $element,  $new_value) === true  ) {
-      if (strlen($new_value) && preg_match('%^[^/].*$%',               $new_value)) {$field->error_set(new text_multiline(['Field "%%_title" contains an error!', 'Field value should be start with "%%_value".'], ['title' => (new text($field->title))->render(), 'value' => '/'       ])); return;}
-      if (strlen($new_value) && preg_match('%^/user$|^/user/.*$%',     $new_value)) {$field->error_set(new text_multiline(['Field "%%_title" contains an error!', 'Field value cannot be start with "%%_value".'], ['title' => (new text($field->title))->render(), 'value' => '/user/'  ])); return;}
-      if (strlen($new_value) && preg_match('%^/manage$|^/manage/.*$%', $new_value)) {$field->error_set(new text_multiline(['Field "%%_title" contains an error!', 'Field value cannot be start with "%%_value".'], ['title' => (new text($field->title))->render(), 'value' => '/manage/'])); return;}
+      if (strlen($new_value) && preg_match('%^[^/].*$%',                 $new_value)) {$field->error_set(new text_multiline(['Field "%%_title" contains an error!', 'Field value should be start with "%%_value".'], ['title' => (new text($field->title))->render(), 'value' => '/'        ])); return;}
+      if (strlen($new_value) && preg_match('%^/manage$|^/manage/.*$%',   $new_value)) {$field->error_set(new text_multiline(['Field "%%_title" contains an error!', 'Field value cannot be start with "%%_value".'], ['title' => (new text($field->title))->render(), 'value' => '/manage/' ])); return;}
+      if (strlen($new_value) && preg_match('%^/user$|^/user/.*$%',       $new_value)) {$field->error_set(new text_multiline(['Field "%%_title" contains an error!', 'Field value cannot be start with "%%_value".'], ['title' => (new text($field->title))->render(), 'value' => '/user/'   ])); return;}
+      if (strlen($new_value) && preg_match('%^/dynamic$|^/dynamic/.*$%', $new_value)) {$field->error_set(new text_multiline(['Field "%%_title" contains an error!', 'Field value cannot be start with "%%_value".'], ['title' => (new text($field->title))->render(), 'value' => '/dynamic/'])); return;}
+      if (strlen($new_value) && preg_match('%^/modules$|^/modules/.*$%', $new_value)) {$field->error_set(new text_multiline(['Field "%%_title" contains an error!', 'Field value cannot be start with "%%_value".'], ['title' => (new text($field->title))->render(), 'value' => '/modules/'])); return;}
+      if (strlen($new_value) && preg_match('%^/readme$|^/readme/.*$%',   $new_value)) {$field->error_set(new text_multiline(['Field "%%_title" contains an error!', 'Field value cannot be start with "%%_value".'], ['title' => (new text($field->title))->render(), 'value' => '/readme/' ])); return;}
+      if (strlen($new_value) && preg_match('%^/shell$|^/shell/.*$%',     $new_value)) {$field->error_set(new text_multiline(['Field "%%_title" contains an error!', 'Field value cannot be start with "%%_value".'], ['title' => (new text($field->title))->render(), 'value' => '/shell/'  ])); return;}
+      if (strlen($new_value) && preg_match('%^/system$|^/system/.*$%',   $new_value)) {$field->error_set(new text_multiline(['Field "%%_title" contains an error!', 'Field value cannot be start with "%%_value".'], ['title' => (new text($field->title))->render(), 'value' => '/system/' ])); return;}
       return true;
     }
   }
