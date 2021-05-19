1634bd9f8e00f5cd67c6f334716c3d3bf7ff0d18
Maxim Rysevets <root@effcore.com>
2021-05-19 15:33:22 +0300

    sprint_1041_1948

--- system/module_user/backend/Events_Token.php
+++ system/module_user/backend/Events_Token.php
@@ -7,7 +7,6 @@
 namespace effcore\modules\user {
           use \effcore\access;
           use \effcore\page;
-          use \effcore\url;
           use \effcore\user;
           abstract class events_token {
 
@@ -26,19 +25,6 @@ namespace effcore\modules\user {
       }
     }
     switch ($name) {
-      case 'return_if_url_arg':
-        if (count($args) > 2) {
-          $arg_name_expected  = $args[0];
-          $arg_value_expected = $args[1];
-          $arg_value_real     = url::get_current()->query_arg_select($arg_name_expected);
-          $value_if_true      = $args[2] ?? '';
-          $value_if_false     = $args[3] ?? '';
-          return $arg_value_real ===
-                 $arg_value_expected ?
-                 $value_if_true :
-                 $value_if_false;
-        }
-        break;
       case 'page_id_context':
         return page::get_current()->id;
     }
