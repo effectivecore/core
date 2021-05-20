fa8cf791bd80d4d8ee6397db254e7efe683429c1
Maxim Rysevets <root@effcore.com>
2021-05-20 11:31:41 +0300

    sprint_1041_1955

--- system/module_storage/backend/Events_Token.php
+++ system/module_storage/backend/Events_Token.php
@@ -12,18 +12,11 @@ namespace effcore\modules\storage {
 
   static function on_apply($name, $args = []) {
     switch ($name) {
-      case 'instance_id_context' : return page::get_current()->args_get('instance_id');
-      case 'entity_name_context' : return page::get_current()->args_get('entity_name');
-      case 'entity_title_context':
-        $entity_name = page::get_current()->args_get('entity_name');
-        $entity = entity::get($entity_name, false);
-        return $entity ? translation::apply($entity->title) : '';
-      case 'entity_title_plural_context':
-        $entity_name = page::get_current()->args_get('entity_name');
-        $entity = entity::get($entity_name, false);
-        return $entity ? translation::apply($entity->title_plural) : '';
+      case 'instance_id_context'        : if (page::get_current()) {return         page::get_current()->args_get('instance_id');} break;
+      case 'entity_name_context'        : if (page::get_current()) {return         page::get_current()->args_get('entity_name');} break;
+      case 'entity_title_context'       : if (page::get_current()) {$entity_name = page::get_current()->args_get('entity_name'); if ($entity_name) {$entity = entity::get($entity_name, false); if ($entity) return translation::apply($entity->title       );}} break;
+      case 'entity_title_plural_context': if (page::get_current()) {$entity_name = page::get_current()->args_get('entity_name'); if ($entity_name) {$entity = entity::get($entity_name, false); if ($entity) return translation::apply($entity->title_plural);}} break;
     };
-    return '';
   }
 
 }}
