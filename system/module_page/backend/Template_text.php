bd327c66673f2c3a4f0fffa4235cfe12e6c7b11d
Maxim Rysevets <root@effcore.com>
2021-05-20 09:23:59 +0300

    sprint_1041_1954

--- system/module_page/backend/Template_text.php
+++ system/module_page/backend/Template_text.php
@@ -15,14 +15,12 @@ namespace effcore {
     }
     if (is_string($this->data)) {
       $rendered = $this->data;
-      $rendered = preg_replace_callback('%(?<spacer>[ ]*)'.
-                                         '(?<prefix>\\%\\%_)'.
-                                         '(?<name>[a-z0-9_]+)'.
-                                         '(?<args>\\{[a-z0-9_,]+\\}|)%S', function ($c_match) {
-        return isset(            $c_match['prefix']) &&
-               isset(            $c_match['name'])   &&
-               isset($this->args[$c_match['name']])  &&
-                     $this->args[$c_match['name']] !== '' ? $c_match['spacer'].
+      $rendered = preg_replace_callback('%(?<spaces>[ ]{0,})'.'\\%\\%_'.
+                                         '(?<name>[a-z0-9_]{1,64})'.
+                                '(?:\\{'.'(?<args>[^\\}\\n]{1,1024})'.'\\}|)%S', function ($c_match) {
+        return isset(            $c_match['name'])  &&
+               isset($this->args[$c_match['name']]) &&
+                     $this->args[$c_match['name']] !== '' ? $c_match['spaces'].
                      $this->args[$c_match['name']] : '';
       },     $rendered);
       return $rendered;
