779e082193e387f24ad781b9d62b5f992ce6c492
Maxim Rysevets <root@effcore.com>
2021-05-20 11:58:55 +0300

    sprint_1041_1957

--- system/module_core/backend/Events_Token.php
+++ system/module_core/backend/Events_Token.php
@@ -11,17 +11,8 @@ namespace effcore\modules\core {
   static function on_apply($name, $args = []) {
     switch ($name) {
       case 'return_if_url_arg':
-        if (count($args) > 2) {
-          $arg_name_expected  = $args[0];
-          $arg_value_expected = $args[1];
-          $arg_value_real     = url::get_current()->query_arg_select($arg_name_expected);
-          $value_if_true      = $args[2] ?? '';
-          $value_if_false     = $args[3] ?? '';
-          return $arg_value_real ===
-                 $arg_value_expected ?
-                 $value_if_true :
-                 $value_if_false;
-        }
+        if (count($args) === 3) return url::get_current()->query_arg_select($args[0]) === $args[1] ? $args[2] : '';
+        if (count($args) === 4) return url::get_current()->query_arg_select($args[0]) === $args[1] ? $args[2] : $args[3];
         break;
     }
   }
