9b1b64ba4f4b85b6c638f5d01f5d4a9f0505ef99
Maxim Rysevets <root@effcore.com>
2021-05-20 12:12:04 +0300

    sprint_1041_1958

--- system/module_core/backend/Events_Token.php
+++ system/module_core/backend/Events_Token.php
@@ -6,10 +6,16 @@
 
 namespace effcore\modules\core {
           use \effcore\url;
+          use \effcore\token;
           abstract class events_token {
 
   static function on_apply($name, $args = []) {
     switch ($name) {
+      case 'return_if_token':
+        if (!empty($args[0]) && $args[0] === 'return_if_token') return '/* an attempt to loop has been eliminated */';
+        if (count($args) === 3) return token::apply('%%_'.$args[0]) === $args[1] ? $args[2] : '';
+        if (count($args) === 4) return token::apply('%%_'.$args[0]) === $args[1] ? $args[2] : $args[3];
+        break;
       case 'return_if_url_arg':
         if (count($args) === 3) return url::get_current()->query_arg_select($args[0]) === $args[1] ? $args[2] : '';
         if (count($args) === 4) return url::get_current()->query_arg_select($args[0]) === $args[1] ? $args[2] : $args[3];
