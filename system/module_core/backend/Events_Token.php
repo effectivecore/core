6980572b15361da385248b9dae595bb38b34fe6a
Maxim Rysevets <root@effcore.com>
2021-05-22 13:18:19 +0300

    sprint_1041_1969

--- system/module_core/backend/Events_Token.php
+++ system/module_core/backend/Events_Token.php
@@ -12,9 +12,12 @@ namespace effcore\modules\core {
   static function on_apply($name, $args = []) {
     switch ($name) {
       case 'return_if_token':
-        if (!empty($args[0]) && $args[0] === 'return_if_token') return '/* an attempt to loop has been eliminated */';
-        if (count($args) === 3) return token::apply('%%_'.$args[0]) === $args[1] ? $args[2] : '';
-        if (count($args) === 4) return token::apply('%%_'.$args[0]) === $args[1] ? $args[2] : $args[3];
+        if (count($args) > 2) {
+          $text = token::text_decode($args[0]);
+          if (strpos($text, 'return_if_token') === 0) return '/* "return_if_token" inside "return_if_token" is meaningless */';
+          if (count($args) === 3) return token::apply('%%_'.$text) === $args[1] ? $args[2] : '';
+          if (count($args) === 4) return token::apply('%%_'.$text) === $args[1] ? $args[2] : $args[3];
+        }
         break;
       case 'return_if_url_arg':
         if (count($args) === 3) return url::get_current()->query_arg_select($args[0]) === $args[1] ? $args[2] : '';
