350c7c2b4c7f806d43a1b7ca0d18af3b78068aab
Maxim Rysevets <root@effcore.com>
2021-05-25 17:46:01 +0300

    sprint_1041_1974

--- system/module_page/backend/traits/Widget_files_videos__Shared.php
+++ system/module_page/backend/traits/Widget_files_videos__Shared.php
@@ -19,15 +19,23 @@ namespace effcore {
     'jpeg' => 'jpeg'];
 # ─────────────────────────────────────────────────────────────────────
   public $video_player_default_settings = [
-    'autoplay'    => false,
+    'autoplay'    => null,
     'buffered'    => null,
     'controls'    => true,
     'crossorigin' => null,
-    'loop'        => false,
-    'muted'       => false,
+    'loop'        => null,
+    'muted'       => null,
     'played'      => null,
     'preload'     => 'metadata'
   ];
+
+  function widget_manage_video_item_make(&$widget, &$item, $c_row_id) {
+    if (media::media_class_get($item->object->type) === 'video') {
+      if (!empty($item->settings['data-poster-is-embedded'])) {
+        $widget->child_insert(new markup_simple('img', ['src' => '/'.$item->object->get_current_path(true).'?poster=small', 'alt' => new text('thumbnail'), 'width' => '44', 'height' => '44', 'data-type' => 'thumbnail'], +450), 'thumbnail');
+      }
+    }
+  }
 
   function on_values_validate_poster($form, $npath, $button) {
     return field_file::on_manual_validate_and_return_value($this->controls['#poster'], $form, $npath);
