86fd8284230bfc560489a39402ce17ce84d500d8
Maxim Rysevets <root@effcore.com>
2021-06-10 19:14:30 +0300

    sprint_1041_2037

--- system/module_page/frontend/page.js
+++ system/module_page/frontend/page.js
@@ -111,7 +111,7 @@ document.addEventListener('DOMContentLoaded', function(){
           break;
         case 'video':
           var c_video = c_item.getElementsByTagName('video')[0];
-          var c_thumbnail_img_src = '/system/module_page/frontend/pictures/icons-gallery_player-video.svg';
+          var c_thumbnail_img_src = c_video.getAttribute('data-poster-thumbnail') ? c_video.getAttribute('data-poster-thumbnail') : '/system/module_page/frontend/pictures/icons-gallery_player-video.svg';
           var c_thumbnail_img = document.createElement__withAttributes('img', {'src' : c_thumbnail_img_src});
           c_thumbnail.setAttribute('data-preview-area-content', JSON.stringify(c_video.outerHTML).replace(/^"/, '').replace(/"$/, ''));
           c_thumbnail.append(c_thumbnail_img);
@@ -119,7 +119,7 @@ document.addEventListener('DOMContentLoaded', function(){
           break;
         case 'audio':
           var c_audio = c_item.getElementsByTagName('audio')[0];
-          var c_thumbnail_img_src = c_audio.getAttribute('data-cover-src') ? c_audio.getAttribute('data-cover-src') : '/system/module_page/frontend/pictures/icons-gallery_player-audio.svg';
+          var c_thumbnail_img_src = c_audio.getAttribute('data-cover-thumbnail') ? c_audio.getAttribute('data-cover-thumbnail') : '/system/module_page/frontend/pictures/icons-gallery_player-audio.svg';
           var c_thumbnail_img = document.createElement__withAttributes('img', {'src' : c_thumbnail_img_src});
           c_thumbnail.setAttribute('data-preview-area-content', JSON.stringify(c_audio.outerHTML).replace(/^"/, '').replace(/"$/, ''));
           c_thumbnail.append(c_thumbnail_img);
