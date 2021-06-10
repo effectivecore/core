2e515c5224feb9b4e425dd3b7fd6ead189f681be
Maxim Rysevets <root@effcore.com>
2021-06-10 18:14:48 +0300

    sprint_1041_2034

--- system/module_page/frontend/page.js
+++ system/module_page/frontend/page.js
@@ -101,7 +101,7 @@ document.addEventListener('DOMContentLoaded', function(){
       switch (c_item.getAttribute('data-type')) {
         case 'audio':
           var c_audio = c_item.getElementsByTagName('audio')[0];
-          var c_thumbnail_img_src = '/system/module_page/frontend/pictures/icons-gallery_player-audio.svg';
+          var c_thumbnail_img_src = c_audio.getAttribute('data-cover-src') ? c_audio.getAttribute('data-cover-src') : '/system/module_page/frontend/pictures/icons-gallery_player-audio.svg';
           var c_thumbnail_img = document.createElement__withAttributes('img', {'src' : c_thumbnail_img_src});
           c_thumbnail.setAttribute('data-preview-area-content', JSON.stringify(c_audio.outerHTML).replace(/^"/, '').replace(/"$/, ''));
           c_thumbnail.append(c_thumbnail_img);
