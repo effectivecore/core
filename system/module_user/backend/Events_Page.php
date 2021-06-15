02b9043a1eca22238da4ba95c638424aa9516390
Maxim Rysevets <root@effcore.com>
2021-06-15 14:11:13 +0300

    sprint_1042_6

--- system/module_user/backend/Events_Page.php
+++ system/module_user/backend/Events_Page.php
@@ -11,15 +11,14 @@ namespace effcore\modules\user {
           abstract class events_page {
 
   static function on_tree_build_after($event, $tree) {
-    if ($tree->module_id === 'user') {
+    if (strpos($tree->id, 'user') === 0)
       if (!frontend::select('tree_user__user'))
            frontend::insert('tree_user__user', null, 'styles', ['path' => 'frontend/tree-user.cssd', 'attributes' => ['rel' => 'stylesheet', 'media' => 'all'], 'weight' => -100], 'tree_style', 'user');
-      if ($tree->id === 'user_registered') {
-        $user = user::get_current();
-        if (access::check((object)['roles' => ['registered' => 'registered']])) {
-          if ($user->avatar_path) {
-            $tree->attribute_insert('data-has-avatar', true);
-          }
+    if (strpos($tree->id, 'user_registered') === 0) {
+      $user = user::get_current();
+      if (access::check((object)['roles' => ['registered' => 'registered']])) {
+        if ($user->avatar_path) {
+          $tree->attribute_insert('data-has-avatar', true);
         }
       }
     }
