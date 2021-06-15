c7f488bc732ea0e66527cf22b629262dce8f475a
Maxim Rysevets <root@effcore.com>
2021-06-15 13:26:59 +0300

    sprint_1042_4

--- system/module_user/backend/Events_Module.php
+++ system/module_user/backend/Events_Module.php
@@ -5,15 +5,12 @@
   ##################################################################
 
 namespace effcore\modules\user {
-          use \effcore\access;
           use \effcore\core;
           use \effcore\instance;
           use \effcore\module;
           use \effcore\request;
           use \effcore\session;
           use \effcore\storage;
-          use \effcore\tree_item;
-          use \effcore\user;
           abstract class events_module {
 
   static function on_install($event) {
@@ -23,8 +20,8 @@ namespace effcore\modules\user {
       $admin = new instance('user', ['nickname' => 'Admin']);
       if ($admin->select()) {
         $admin->password_hash = core::password_get_hash(request::value_get('password'));
-        $admin->email    = request::value_get('email'   );
-        $admin->timezone = request::value_get('timezone');
+        $admin->email         =                         request::value_get('email'   );
+        $admin->timezone      =                         request::value_get('timezone');
         $admin->update();
       }
     }
@@ -33,16 +30,6 @@ namespace effcore\modules\user {
   static function on_enable($event) {
     $module = module::get('user');
     $module->enable();
-  }
-
-  static function on_start($event) {
-    $user = user::get_current();
-    if (access::check((object)['roles' => ['registered' => 'registered']]) && $user->avatar_path) {
-      $tree_item = tree_item::select('registered', 'user_registered');
-      if ($tree_item) {
-        $tree_item->attribute_insert('data-has-avatar', 'true');
-      }
-    }
   }
 
   static function on_cron_run($event) {
