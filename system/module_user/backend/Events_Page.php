2f9929fd312660dc30079045963153be8536c946
Maxim Rysevets <root@effcore.com>
2021-06-15 13:12:10 +0300

    sprint_1042_3

--- system/module_user/backend/Events_Page.php
+++ system/module_user/backend/Events_Page.php
@@ -9,7 +9,7 @@ namespace effcore\modules\user {
           abstract class events_page {
 
   static function on_tree_build_after($event, $tree) {
-    if ($tree->id === 'user_registered' || $tree->id === 'user_anonymous') {
+    if ($tree->module_id === 'user') {
       if (!frontend::select('tree_user__user'))
            frontend::insert('tree_user__user', null, 'styles', ['path' => 'frontend/tree-user.cssd', 'attributes' => ['rel' => 'stylesheet', 'media' => 'all'], 'weight' => -100], 'tree_style', 'user');
     }
