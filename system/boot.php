83bb2994cdce2fcc9e0a30f913adc2aa9cfa705b
Maxim Rysevets <root@effcore.com>
2021-07-11 13:03:40 +0300

    sprint_1042_96

--- system/boot.php
+++ system/boot.php
@@ -168,9 +168,7 @@ namespace effcore {
   }
   if (module::is_enabled('test')) {
     header('X-PHP-Memory-usage: '.memory_get_usage(true));
-    header('X-Time-total: '.locale::format_msecond(
-      timer::period_get('total', 0, 1)
-    ));
+    header('X-Time-total: '.timer::period_get('total', 0, 1));
   }
   header('Cache-Control: private, no-cache');
   header('Content-Length: '.strlen($result));
