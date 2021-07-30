fb467e32255fd9a07fac7b7c47f358363665baed
Maxim Rysevets <root@effcore.com>
2021-07-30 16:22:36 +0300

    sprint_1044_1

---
+++ system/module_develop/module_project/backend/Events_Form_Instance_insert.php
@@ -0,0 +1,39 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2021 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\project {
+          use \effcore\entity;
+          use \effcore\text_multiline;
+          use \effcore\text;
+          abstract class events_form_instance_insert {
+
+  static function on_validate($event, $form, $items) {
+    $entity = entity::get($form->entity_name);
+    if ($entity) {
+      switch ($form->clicked_button->value_get()) {
+        case 'insert':
+        case 'insert_and_update':
+        # field 'id_project' + field 'build'
+          if ($entity->name === 'release' && !$form->has_error()) {
+            $id_project = $items['#id_project']->value_get();
+            $build      = $items['#build'     ]->value_get();
+            $result = $entity->instances_select(['conditions' => [
+              'id_project_!f' => 'id_project', 'id_project_operator' => '=', 'id_project_!v' => $id_project, 'conjunction' => 'and',
+              'build_!f'      => 'build',      'build_operator'      => '=', 'build!v'       => $build],     'limit'       => 1]);
+            if ($result) {
+              $items['#id_project']->error_set();
+              $items['#build']->error_set(new text_multiline([
+                'Field "%%_title" contains an error!',
+                'This combination of values is already in use!'], ['title' => (new text($items['#build']->title))->render() ]
+              ));
+            }
+          }
+          break;
+      }
+    }
+  }
+
+}}
---
+++ system/module_develop/module_project/backend/Events_Module.php
@@ -0,0 +1,31 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2021 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\project {
+          use \effcore\module;
+          abstract class events_module {
+
+  static function on_install($event) {
+    $module = module::get('project');
+    $module->install();
+  }
+
+  static function on_uninstall($event) {
+    $module = module::get('project');
+    $module->uninstall();
+  }
+
+  static function on_enable($event) {
+    $module = module::get('project');
+    $module->enable();
+  }
+
+  static function on_disable($event) {
+    $module = module::get('project');
+    $module->disable();
+  }
+
+}}
