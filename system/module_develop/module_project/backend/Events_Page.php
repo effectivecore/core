0b9409e1c5203e80a4e5e00cc96207b3df15dda3
Maxim Rysevets <root@effcore.com>
2021-08-08 11:30:11 +0300

    sprint_1044_33

--- system/module_develop/module_project/backend/Events_Page.php
+++ system/module_develop/module_project/backend/Events_Page.php
@@ -13,11 +13,23 @@ namespace effcore\modules\project {
           abstract class events_page {
 
   static function on_block_presets_dynamic_build($event, $id = null) {
-    if ($id === null                                             ) {foreach (entity::get('project')->instances_select() as $c_item) block_preset::insert('block__release_sql__'.$c_item->id, 'Releases', 'id_project = '.$c_item->id, [ /* all areas */ ], ['title' => 'Releases', 'title_is_visible' => false, 'type' => 'code', 'source' => '\\effcore\\modules\\project\\events_page::block_markup__selection_make', 'args' => ['id_project' => $c_item->id], 'has_admin_menu' => false, 'attributes' => ['data-block' => true, 'data-id' => 'block__release_sql__'.$c_item->id]], 0, 'project');}
-    if ($id !== null && strpos($id, 'block__release_sql__') === 0) {$c_item__id = substr($id, strlen('block__release_sql__'));      block_preset::insert('block__release_sql__'.$c_item__id, 'Releases', 'id_project = '.$c_item__id, [ /* all areas */ ], ['title' => 'Releases', 'title_is_visible' => false, 'type' => 'code', 'source' => '\\effcore\\modules\\project\\events_page::block_markup__selection_make', 'args' => ['id_project' => $c_item__id], 'has_admin_menu' => false, 'attributes' => ['data-block' => true, 'data-id' => 'block__release_sql__'.$c_item__id]], 0, 'project');}
+    if ($id === null                                                  ) {foreach (entity::get('project')->instances_select() as $c_item) block_preset::insert('block__release_last_sql__'.$c_item->id, 'Release (last)', 'id_project = '.$c_item->id, [ /* all areas */ ], ['title' => 'Release (last)', 'title_is_visible' => false, 'type' => 'code', 'source' => '\\effcore\\modules\\project\\events_page::block_markup__selection_release_last_make', 'args' => ['id_project' => $c_item->id], 'has_admin_menu' => false, 'attributes' => ['data-block' => true, 'data-id' => 'block__release_last_sql__'.$c_item->id]], 0, 'project');}
+    if ($id === null                                                  ) {foreach (entity::get('project')->instances_select() as $c_item) block_preset::insert('block__releases_sql__'.    $c_item->id, 'Releases',       'id_project = '.$c_item->id, [ /* all areas */ ], ['title' => 'Releases',       'title_is_visible' => false, 'type' => 'code', 'source' => '\\effcore\\modules\\project\\events_page::block_markup__selection_releases_make',     'args' => ['id_project' => $c_item->id], 'has_admin_menu' => false, 'attributes' => ['data-block' => true, 'data-id' => 'block__releases_sql__'.    $c_item->id]], 0, 'project');}
+    if ($id !== null && strpos($id, 'block__release_last_sql__') === 0) {$c_item__id = substr($id, strlen('block__release_last_sql__')); block_preset::insert('block__release_last_sql__'.$c_item__id, 'Release (last)', 'id_project = '.$c_item__id, [ /* all areas */ ], ['title' => 'Release (last)', 'title_is_visible' => false, 'type' => 'code', 'source' => '\\effcore\\modules\\project\\events_page::block_markup__selection_release_last_make', 'args' => ['id_project' => $c_item__id], 'has_admin_menu' => false, 'attributes' => ['data-block' => true, 'data-id' => 'block__release_last_sql__'.$c_item__id]], 0, 'project');}
+    if ($id !== null && strpos($id, 'block__releases_'. 'sql__') === 0) {$c_item__id = substr($id, strlen('block__releases_'. 'sql__')); block_preset::insert('block__releases_sql__'.    $c_item__id, 'Releases',       'id_project = '.$c_item__id, [ /* all areas */ ], ['title' => 'Releases',       'title_is_visible' => false, 'type' => 'code', 'source' => '\\effcore\\modules\\project\\events_page::block_markup__selection_releases_make',     'args' => ['id_project' => $c_item__id], 'has_admin_menu' => false, 'attributes' => ['data-block' => true, 'data-id' => 'block__releases_sql__'.    $c_item__id]], 0, 'project');}
   }
 
-  static function block_markup__selection_make($page, $args = []) {
+  static function block_markup__selection_release_last_make($page, $args = []) {
+    if (!empty($args['id_project'])) {
+      token::insert('id_project_context', 'text', $args['id_project']);
+      $selection = core::deep_clone(selection::get('release_last'));
+      $selection->title = $selection->title->render();
+      $selection->build();
+      return $selection;
+    }
+  }
+
+  static function block_markup__selection_releases_make($page, $args = []) {
     if (!empty($args['id_project'])) {
       token::insert('id_project_context', 'text', $args['id_project']);
       $selection = core::deep_clone(selection::get('releases'));
