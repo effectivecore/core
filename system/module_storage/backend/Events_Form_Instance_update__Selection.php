34ddf0375e0d69d39df3a65e9046b388d0717320
Maxim Rysevets <root@effcore.com>
2021-08-17 15:11:53 +0300

    sprint_1044_68

--- system/module_storage/backend/Events_Form_Instance_update__Selection.php
+++ system/module_storage/backend/Events_Form_Instance_update__Selection.php
@@ -16,10 +16,10 @@ namespace effcore\modules\storage {
       $entity = entity::get($form->entity_name);
       if ($entity) {
         if ($entity->name === 'selection' && !empty($form->_instance)) {
-          $fieldset_query_settings   = new fieldset('Query settings');
-          $fieldset_decorator_params = new fieldset('Decorator parameters');
-          $fieldset_conditions       = new fieldset('Conditions');
-          $fieldset_sequence         = new fieldset('Sequence');
+          $fieldset_query_settings     = new fieldset('Query settings');
+          $fieldset_decorator_settings = new fieldset('Decorator settings');
+          $fieldset_conditions         = new fieldset('Conditions');
+          $fieldset_sequence           = new fieldset('Sequence');
         # insert field 'Limit'
           $field_limit = new field_number('Limit');
           $field_limit->build();
@@ -28,11 +28,11 @@ namespace effcore\modules\storage {
           $field_limit->min_set(1    );
           $field_limit->max_set(10000);
         # fill the form
-          $fieldset_query_settings     ->child_insert($fieldset_conditions,       'conditions'      );
-          $fieldset_query_settings     ->child_insert($fieldset_sequence,         'sequence'        );
-          $fieldset_query_settings     ->child_insert($field_limit,               'limit'           );
-          $form->child_select('fields')->child_insert($fieldset_query_settings,   'query_settings'  );
-          $form->child_select('fields')->child_insert($fieldset_decorator_params, 'decorator_params');
+          $fieldset_query_settings     ->child_insert($fieldset_conditions,         'conditions'        );
+          $fieldset_query_settings     ->child_insert($fieldset_sequence,           'sequence'          );
+          $fieldset_query_settings     ->child_insert($field_limit,                 'limit'             );
+          $form->child_select('fields')->child_insert($fieldset_query_settings,     'query_settings'    );
+          $form->child_select('fields')->child_insert($fieldset_decorator_settings, 'decorator_settings');
         }
       }
     }
