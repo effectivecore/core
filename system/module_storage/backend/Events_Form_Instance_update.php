73c6f9767b9d7c1bbe712e197e3d4c50ae932646
Maxim Rysevets <root@effcore.com>
2021-08-17 18:44:11 +0300

    sprint_1044_72

--- system/module_storage/backend/Events_Form_Instance_update.php
+++ system/module_storage/backend/Events_Form_Instance_update.php
@@ -122,7 +122,7 @@ namespace effcore\modules\storage {
                 if     ($items[$c_prefix.$c_name] instanceof complex_control        ) $c_value = $items[$c_prefix.$c_name]->value_get_complex();
                 elseif ($items[$c_prefix.$c_name] instanceof field_checkbox !== true) $c_value = $items[$c_prefix.$c_name]->value_get        ();
                 elseif ($items[$c_prefix.$c_name] instanceof field_checkbox === true) $c_value = $items[$c_prefix.$c_name]->checked_get      () ? 1 : 0;
-                if (!empty($c_field->managing_control_value_manual_get_if_empty) && $c_value === '') continue;
+                if (!empty($c_field->managing_control_value_manual_get_if_empty) && empty($c_value)) continue;
                 if (!empty($c_field->managing_control_value_manual_get         )                   ) continue;
                 $form->_instance->{$c_name} = $c_value;
               }
