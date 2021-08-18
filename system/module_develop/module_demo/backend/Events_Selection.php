6c29fb6de0a72960b7d6cf07eb47f3cb2a3e23a9
Maxim Rysevets <root@effcore.com>
2021-08-18 22:40:53 +0300

    sprint_1044_79

--- system/module_develop/module_demo/backend/Events_Selection.php
+++ system/module_develop/module_demo/backend/Events_Selection.php
@@ -8,7 +8,7 @@ namespace effcore\modules\demo {
           abstract class events_selection {
 
   static function on_selection_build_before_demo_selection_field_types($event, $selection) {
-    $selection->field_insert_entity('type_field_code', 'demo_data', 'id', 'Type "field" from code', [], 380);
+    $selection->field_insert_entity('type_field_code', 'demo_data', 'id', 'Type "field" from code', [], 390);
   }
 
 }}
