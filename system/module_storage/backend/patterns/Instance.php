cf099bb1012452ca98f575973de872bd842c0807
Maxim Rysevets <root@effcore.com>
2021-08-18 14:03:26 +0300

    sprint_1044_74

--- system/module_storage/backend/patterns/Instance.php
+++ system/module_storage/backend/patterns/Instance.php
@@ -106,14 +106,14 @@ namespace effcore {
     return static::$cache_orig[$name] ?? [];
   }
 
-  static function selection_make($entity_name, $conditions, $params = []) {
+  static function selection_make($entity_name, $conditions, $settings = []) {
     $entity = entity::get($entity_name);
     $selection = new selection;
     $selection->id = $entity_name.'-'.core::hash_get($conditions);
     $selection->template = 'content';
     $selection->query_settings['conditions'] = $entity->storage_get()->prepare_attributes($conditions);
     foreach ($entity->fields as $c_name => $c_field)
-      $selection->field_insert_entity(null, $entity->name, $c_name, $params[$c_name] ?? []);
+      $selection->field_insert_entity(null, $entity->name, $c_name, $settings[$c_name] ?? []);
     return $selection;
   }
 
