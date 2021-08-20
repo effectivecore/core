acee7592b585921db21f3c0685b81d156eaf4f0f
Maxim Rysevets <root@effcore.com>
2021-08-20 18:06:11 +0300

    sprint_1044_93

--- system/module_develop/module_demo/backend/Events_Selection.php
+++ system/module_develop/module_demo/backend/Events_Selection.php
@@ -10,7 +10,7 @@ namespace effcore\modules\demo {
 
   static function on_selection_build_before_demo_selection_field_types($event, $selection) {
     $selection->field_insert_entity('type_field_code', 'demo_data', 'id', new text('Type "%%_type" from code', ['type' => 'field']), [], 390);
-    $selection->field_insert_text('type_text_code', '   item #%%_number', new text('Type "%%_type" from code', ['type' => 'text']), ['filters' => [500 => 'ltrim', 400 => 'translate', 300 => 'tokenized']], 370);
+    $selection->field_insert_text('type_text_code', '   item #%%_number', new text('Type "%%_type" from code', ['type' => 'text']), ['filters' => [500 => 'ltrim', 400 => 'translate', 300 => 'tokenized']], 210);
   }
 
 }}
