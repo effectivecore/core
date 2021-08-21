057501f67c48b885bc5c705369919f2528f8752f
Maxim Rysevets <root@effcore.com>
2021-08-21 14:29:09 +0300

    sprint_1044_101

--- system/module_develop/module_demo/backend/Events_Selection.php
+++ system/module_develop/module_demo/backend/Events_Selection.php
@@ -10,13 +10,13 @@ namespace effcore\modules\demo {
           abstract class events_selection {
 
   static function on_selection_build_before_demo_selection_field_types($event, $selection) {
-    $selection->field_insert_entity('type_field_code', new text('Type "%%_type" from code', ['type' => 'field']), 'demo_data', 'id', [], 390);
-    $selection->field_insert_entity_join('type_field_join_code', new text('Type "%%_type" from code', ['type' => 'join_field']), 'demo_join', 'email', [], 370);
-    $selection->field_insert_text('type_text_with_translation_code', 'text with translation', new text('Type "%%_type" from code', ['type' => 'text + translation']), ['filters' => [500 => 'trim', 400 => 'translate']], 190);
-    $selection->field_insert_text('type_text_with_translation_with_token_code', 'text with translation and token demo_text = "%%_demo_text"', new text('Type "%%_type" from code', ['type' => 'text + translation + token']), ['filters' => [500 => 'trim', 400 => 'translate', 300 => 'tokenized']], 170);
-    $selection->field_insert_text('type_text_with_token_code', 'text with token demo_text = "%%_demo_text"', new text('Type "%%_type" from code', ['type' => 'text + token']), ['filters' => [500 => 'trim', 300 => 'tokenized']], 150);
-    $selection->field_insert_markup('type_markup_code', new text('Type "%%_type" from code', ['type' => 'markup']), new markup('span', [], 'markup'), [], 130);
-    $selection->field_insert_checkbox('checkbox-select', new text('Type "%%_type" from code', ['type' => 'checkbox']), ['name' => 'is_checked[]'], 90);
+    $selection->field_insert_entity     ('type_field_code',                            new text('Type "%%_type" from code', ['type' => 'field'                     ]), 'demo_data', 'id',                                            [                                                                    ], 390);
+    $selection->field_insert_entity_join('type_field_join_code',                       new text('Type "%%_type" from code', ['type' => 'join_field'                ]), 'demo_join', 'email',                                         [                                                                    ], 370);
+    $selection->field_insert_text       ('type_text_with_translation_code',            new text('Type "%%_type" from code', ['type' => 'text + translation'        ]), 'text with translation',                                      ['filters' => [500 => 'trim', 400 => 'translate'                    ]], 190);
+    $selection->field_insert_text       ('type_text_with_translation_with_token_code', new text('Type "%%_type" from code', ['type' => 'text + translation + token']), 'text with translation and token demo_text = "%%_demo_text"', ['filters' => [500 => 'trim', 400 => 'translate', 300 => 'tokenized']], 170);
+    $selection->field_insert_text       ('type_text_with_token_code',                  new text('Type "%%_type" from code', ['type' => 'text + token'              ]), 'text with token demo_text = "%%_demo_text"',                 ['filters' => [500 => 'trim',                     300 => 'tokenized']], 150);
+    $selection->field_insert_markup     ('type_markup_code',                           new text('Type "%%_type" from code', ['type' => 'markup'                    ]), new markup('span', [], 'markup'),                             [                                                                    ], 130);
+    $selection->field_insert_checkbox   ('checkbox-select',                            new text('Type "%%_type" from code', ['type' => 'checkbox'                  ]),                                                               ['name' => 'is_checked[]'                                            ], 90);
   }
 
 }}
