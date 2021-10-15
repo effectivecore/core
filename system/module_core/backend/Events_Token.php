48f2cf9d10269b611e329eddcdde04df2768b762
Maxim Rysevets <root@effcore.com>
2021-10-15 14:15:01 +0300

    sprint_1044_315

--- system/module_core/backend/Events_Token.php
+++ system/module_core/backend/Events_Token.php
@@ -11,15 +11,23 @@ namespace effcore\modules\core {
           abstract class events_token {
 
   static function on_apply($name, $args = []) {
+    if ($name === 'request_scheme'      && count($args) === 0                             ) return            request::scheme_get();
+    if ($name === 'request_host'        && count($args) === 0                             ) return            request::host_get();
+    if ($name === 'request_host_decode' && count($args) === 0                             ) return            request::host_get(true);
+    if ($name === 'request_addr'        && count($args) === 0                             ) return            request::addr_get();
+    if ($name === 'request_addr_remote' && count($args) === 0                             ) return            request::addr_remote_get();
+    if ($name === 'request_uri'         && count($args) === 0                             ) return            request::uri_get();
+    if ($name === 'request_path'        && count($args) === 0                             ) return            request::path_get();
+    if ($name === 'request_query'       && count($args) === 0                             ) return            request::query_get();
+    if ($name === 'request_scheme'      && count($args) === 1 && $args[0] === 'preg_quote') return preg_quote(request::scheme_get());
+    if ($name === 'request_host'        && count($args) === 1 && $args[0] === 'preg_quote') return preg_quote(request::host_get());
+    if ($name === 'request_host_decode' && count($args) === 1 && $args[0] === 'preg_quote') return preg_quote(request::host_get(true));
+    if ($name === 'request_addr'        && count($args) === 1 && $args[0] === 'preg_quote') return preg_quote(request::addr_get());
+    if ($name === 'request_addr_remote' && count($args) === 1 && $args[0] === 'preg_quote') return preg_quote(request::addr_remote_get());
+    if ($name === 'request_uri'         && count($args) === 1 && $args[0] === 'preg_quote') return preg_quote(request::uri_get());
+    if ($name === 'request_path'        && count($args) === 1 && $args[0] === 'preg_quote') return preg_quote(request::path_get());
+    if ($name === 'request_query'       && count($args) === 1 && $args[0] === 'preg_quote') return preg_quote(request::query_get());
     switch ($name) {
-      case 'request_scheme'     : return request::scheme_get();
-      case 'request_host'       : return request::host_get();
-      case 'request_host_decode': return request::host_get(true);
-      case 'request_addr'       : return request::addr_get();
-      case 'request_addr_remote': return request::addr_remote_get();
-      case 'request_uri'        : return request::uri_get();
-      case 'request_path'       : return request::path_get();
-      case 'request_query'      : return request::query_get();
       case 'return_if_token':
         if (count($args) > 2) {
           $text = token::text_decode($args[0]);
