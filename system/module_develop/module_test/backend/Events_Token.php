d8ebe877d64fd8005a9afad971cba1ed489a9a11
Maxim Rysevets <root@effcore.com>
2021-10-18 18:48:58 +0300

    sprint_1044_334

--- system/module_develop/module_test/backend/Events_Token.php
+++ system/module_develop/module_test/backend/Events_Token.php
@@ -56,6 +56,7 @@ namespace effcore\modules\test {
         if ($type === 'http_code'      && isset($last_response['info'])    && array_key_exists('http_code',                  $last_response['info']   )) return    (int)$last_response['info']['http_code'];
         if ($type === 'location'       && isset($last_response['headers']) && array_key_exists('Location',                   $last_response['headers'])) return (string)$last_response['headers']['Location'];
         if ($type === 'content_length' && isset($last_response['headers']) && array_key_exists('Content-Length',             $last_response['headers'])) return    (int)$last_response['headers']['Content-Length'];
+        if ($type === 'content_range'  && isset($last_response['headers']) && array_key_exists('Content-Range',              $last_response['headers'])) return (string)$last_response['headers']['Content-Range'];
         if ($type === 'submit_error'   && isset($last_response['headers']) && array_key_exists('X-Form-Submit-Errors-Count', $last_response['headers'])) return    (int)$last_response['headers']['X-Form-Submit-Errors-Count'];
         if ($type === 'time_total'     && isset($last_response['headers']) && array_key_exists('X-Time-total',               $last_response['headers'])) return  (float)$last_response['headers']['X-Time-total'];
         if ($type === 'return_level'   && isset($last_response['headers']) && array_key_exists('X-Return-level',             $last_response['headers'])) return (string)$last_response['headers']['X-Return-level'];
