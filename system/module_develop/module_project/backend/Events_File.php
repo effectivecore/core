0ee22b47e4c1b5e05518d1a46d61087134f298db
Maxim Rysevets <root@effcore.com>
2021-10-19 13:26:47 +0300

    sprint_1044_341

---
+++ system/module_develop/module_project/backend/Events_File.php
@@ -0,0 +1,26 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2021 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\project {
+          use \effcore\dynamic;
+          use \effcore\instance;
+          abstract class events_file {
+
+  static function on_load_static_release($event, &$type_info, &$file) {
+    if ($type_info->type === 'zip' || $type_info->type === '7z') {
+      if ($file->dirs === dynamic::dir_files.'releases/') {
+        $release = new instance('release', [
+          'path' => $file->path_get_relative()
+        ]);
+        if ($release->select()) {
+          $release->downloads_num++;
+          $release->update();
+        }
+      }
+    }
+  }
+
+}}
