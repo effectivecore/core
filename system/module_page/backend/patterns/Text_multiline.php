0708a3f7ea22727bb2426ed3756d64633fce979e
Maxim Rysevets <root@effcore.com>
2021-10-22 12:33:33 +0300

    sprint_1044_365

--- system/module_page/backend/patterns/Text_multiline.php
+++ system/module_page/backend/patterns/Text_multiline.php
@@ -26,11 +26,15 @@ namespace effcore {
   function text_line_update($key, $new_text) {$this->text[$key] = $new_text;}
   function text_line_delete($key)            {unset($this->text[$key]);}
 
-  function text_length()          {return strlen($this->render());}
   function text_select()          {return $this->text;}
   function text_update($new_text) {$this->text   = $new_text;}
   function text_append($new_line) {$this->text[] = $new_line; return count($this->text);}
   function text_delete()          {$this->text   = [];}
+
+  function text_length($is_in_bytes = false) {
+    if ($is_in_bytes === true) return    strlen($this->render());
+    if ($is_in_bytes !== true) return mb_strlen($this->render());
+  }
 
   function render() {
     $result = [];
--- system/module_page/backend/patterns/Text_simple.php
+++ system/module_page/backend/patterns/Text_simple.php
@@ -22,11 +22,15 @@ namespace effcore {
   function text_line_update($key, $new_text) {$lines = explode($this->delimiter, $this->text);       $lines[$key] = $new_text;                                                                     $this->text = implode($this->delimiter, $lines);}
   function text_line_delete($key)            {$lines = explode($this->delimiter, $this->text); unset($lines[$key]);                                                                                $this->text = implode($this->delimiter, $lines);}
 
-  function text_length()          {return strlen($this->render());}
   function text_select()          {return $this->text;}
   function text_update($new_text) {$this->text = $new_text;}
   function text_append($new_text) {$this->text.= $new_text;}
   function text_delete()          {$this->text = '';}
+
+  function text_length($is_in_bytes = false) {
+    if ($is_in_bytes === true) return    strlen($this->render());
+    if ($is_in_bytes !== true) return mb_strlen($this->render());
+  }
 
   function render() {
     return $this->text;
