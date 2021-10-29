7ec3fdea6d2b0784e8e06a322af3521eafc3a025
Maxim Rysevets <root@effcore.com>
2021-10-29 21:06:58 +0300

    sprint_1044_410

--- system/module_core/backend/Core.php
+++ system/module_core/backend/Core.php
@@ -932,7 +932,7 @@ namespace effcore {
 
   static function exponencial_string_normalize($value) {
     if (is_string($value) && is_numeric($value))
-      if ($value !== (string)(int)$value && $value[0] !== '0')
+      if ($value !== (string)(int)$value && $value[0] !== '0' && strpbrk($value, 'eE'))
         return core::format_number($value, 10);
     return $value;
   }
