bd69081b1875e59d39643b30e02442c6feb66bb8
Maxim Rysevets <root@effcore.com>
2021-11-15 16:53:08 +0300

    sprint_1044_453

--- system/module_core/backend/patterns/Step_Validate_error_register.php
+++ system/module_core/backend/patterns/Step_Validate_error_register.php
@@ -12,9 +12,8 @@ namespace effcore {
   function run(&$data_validator, $c_dpath_scenario, $c_dpath_value, $c_value, &$c_results) {
     $result = core::deep_clone($this->message);
     if (is_string($result)) $result = new text($result);
-    if ($result instanceof text) {
-      $result->args['dpath_scenario'] = $c_dpath_scenario;
-      $result->args['dpath_value'   ] = $c_dpath_value; }
+    if ($result instanceof text)
+        $result->args += ['dpath_scenario' => $c_dpath_scenario, 'dpath_value' => $c_dpath_value];
     $c_results['trace_info'][$c_dpath_value][] = $c_dpath_scenario;
     $c_results['errors'][$c_dpath_value.'|'.$c_dpath_scenario] = $result;
   }
