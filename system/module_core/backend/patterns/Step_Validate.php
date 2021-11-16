595c18fd04ae9df0e5c9c6784f5d6282fd3c36ca
Maxim Rysevets <root@effcore.com>
2021-11-16 16:15:05 +0300

    sprint_1044_457

--- system/module_core/backend/patterns/Step_Validate.php
+++ system/module_core/backend/patterns/Step_Validate.php
@@ -30,6 +30,25 @@ namespace effcore {
         if ($result !== true && isset($this->on_failure)) foreach ($this->on_failure as $c_dpath_in_cycle => $c_step) $c_step->run($data_validator, $c_dpath_scenario.':on_failure/'.$c_dpath_in_cycle, $c_dpath_value, $c_value, $c_results);
       }
     }
+    if (strpos($this->check, 'parent_') === 0) {
+      $depth = (int)substr($this->check, strlen('parent_'));
+      if ($depth > 0 && isset($c_results['parents_info'][                                    $depth])) $parent = $c_results['parents_info'][                                    $depth];
+      if ($depth < 0 && isset($c_results['parents_info'][count($c_results['parents_info']) + $depth])) $parent = $c_results['parents_info'][count($c_results['parents_info']) + $depth];
+      if (isset($parent)) {
+        $c_results['trace_info'][$c_dpath_value][] = $c_dpath_scenario;
+        $is_regexp = is_string($this->match) && strlen($this->match) && $this->match[0] === '%';
+        $match = token::apply((string)$this->match);
+        if ($this->where === 'dpath' && $is_regexp === true                               ) $result = (bool)preg_match($match,    $c_dpath_value);
+        if ($this->where === 'dpath' && $is_regexp !== true                               ) $result =                  $match === $c_dpath_value;
+        if ($this->where === 'type'  && $is_regexp === true                               ) $result = (bool)preg_match($match,    core::gettype($parent));
+        if ($this->where === 'type'  && $is_regexp !== true                               ) $result =                  $match === core::gettype($parent);
+        if ($this->where === 'value' && $is_regexp === true && is_string($parent) !== true) $result = false;
+        if ($this->where === 'value' && $is_regexp === true && is_string($parent) === true) $result = (bool)preg_match($match,    $parent);
+        if ($this->where === 'value' && $is_regexp !== true                               ) $result =                  $match === $parent;
+        if ($result === true && isset($this->on_success)) foreach ($this->on_success as $c_dpath_in_cycle => $c_step) $c_step->run($data_validator, $c_dpath_scenario.':on_success/'.$c_dpath_in_cycle, $c_dpath_value, $c_value, $c_results);
+        if ($result !== true && isset($this->on_failure)) foreach ($this->on_failure as $c_dpath_in_cycle => $c_step) $c_step->run($data_validator, $c_dpath_scenario.':on_failure/'.$c_dpath_in_cycle, $c_dpath_value, $c_value, $c_results);
+      }
+    }
   }
 
 }}
