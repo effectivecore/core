e8a095fc23836907954684dc5f28f884c38f2342
Maxim Rysevets <root@effcore.com>
2021-11-20 14:57:35 +0300

    sprint_1044_489

--- system/module_page/backend/patterns/Field_Textarea_data.php
+++ system/module_page/backend/patterns/Field_Textarea_data.php
@@ -10,6 +10,7 @@ namespace effcore {
   public $title = 'Data in "data" format';
   public $classes_allowed = [];
   public $class_std_by_default = true;
+  public $data_validator_id;
 
   function value_data_get() {
     return storage_nosql_data::text_to_data($this->value_get(), $this->classes_allowed)->data;
@@ -39,60 +40,72 @@ namespace effcore {
   ###########################
 
   static function validate_value($field, $form, $element, &$new_value) {
-    $result = storage_nosql_data::text_to_data($new_value, $field->classes_allowed);
-    if (count($result->errors)) {
-      foreach ($result->errors as $c_error) {
-        switch ($c_error->code) {
-          case storage_nosql_data::ERR_CODE_EMPTY_LINE_WAS_FOUND:
-            $field->error_set(new text_multiline([
-              'Field "%%_title" contains an error!',
-              'An empty line was found.',
-              'Line: %%_line'], [
-              'title' => (new text($field->title))->render(),
-              'line'  => $c_error->line]));
-            break;
-          case storage_nosql_data::ERR_CODE_LEADING_TAB_CHARACTER_FOUND:
-            $field->error_set(new text_multiline([
-              'Field "%%_title" contains an error!',
-              'Leading tab character found.',
-              'Line: %%_line'], [
-              'title' => (new text($field->title))->render(),
-              'line'  => $c_error->line]));
-            break;
-          case storage_nosql_data::ERR_CODE_INDENT_SIZE_IS_NOT_EVEN:
-            $field->error_set(new text_multiline([
-              'Field "%%_title" contains an error!',
-              'Indent size is not even.',
-              'Line: %%_line'], [
-              'title' => (new text($field->title))->render(),
-              'line'  => $c_error->line]));
-            break;
-          case storage_nosql_data::ERR_CODE_INDENT_OVERSIZE:
-            $field->error_set(new text_multiline([
-              'Field "%%_title" contains an error!',
-              'Indent oversize is detected.',
-              'Line: %%_line'], [
-              'title' => (new text($field->title))->render(),
-              'line'  => $c_error->line]));
-            break;
-          case storage_nosql_data::ERR_CODE_CLASS_WAS_NOT_FOUND:
-            $field->error_set(new text_multiline([
-              'Field "%%_title" contains an error!',
-              'Class "%%_class_name" was not found.',
-              'Line: %%_line'], [
-              'title'      => (new text($field->title))->render(),
-              'line'       => $c_error->line,
-              'class_name' => $c_error->args['class_name']]));
-            break;
-          case storage_nosql_data::ERR_CODE_CLASS_NOT_ALLOWED:
-            $field->error_set(new text_multiline([
-              'Field "%%_title" contains an error!',
-              'Class "%%_class_name" not allowed.',
-              'Line: %%_line'], [
-              'title'      => (new text($field->title))->render(),
-              'line'       => $c_error->line,
-              'class_name' => $c_error->args['class_name']]));
-            break;
+    if (strlen($new_value)) {
+      $parsed_result = storage_nosql_data::text_to_data($new_value, $field->classes_allowed);
+      if (count($parsed_result->errors)) {
+        foreach ($parsed_result->errors as $c_error) {
+          switch ($c_error->code) {
+            case storage_nosql_data::ERR_CODE_EMPTY_LINE_WAS_FOUND:
+              $field->error_set(new text_multiline([
+                'Field "%%_title" contains an error!',
+                'An empty line was found.',
+                'Line: %%_line'], [
+                'title' => (new text($field->title))->render(),
+                'line'  => $c_error->line]));
+              break;
+            case storage_nosql_data::ERR_CODE_LEADING_TAB_CHARACTER_FOUND:
+              $field->error_set(new text_multiline([
+                'Field "%%_title" contains an error!',
+                'Leading tab character found.',
+                'Line: %%_line'], [
+                'title' => (new text($field->title))->render(),
+                'line'  => $c_error->line]));
+              break;
+            case storage_nosql_data::ERR_CODE_INDENT_SIZE_IS_NOT_EVEN:
+              $field->error_set(new text_multiline([
+                'Field "%%_title" contains an error!',
+                'Indent size is not even.',
+                'Line: %%_line'], [
+                'title' => (new text($field->title))->render(),
+                'line'  => $c_error->line]));
+              break;
+            case storage_nosql_data::ERR_CODE_INDENT_OVERSIZE:
+              $field->error_set(new text_multiline([
+                'Field "%%_title" contains an error!',
+                'Indent oversize is detected.',
+                'Line: %%_line'], [
+                'title' => (new text($field->title))->render(),
+                'line'  => $c_error->line]));
+              break;
+            case storage_nosql_data::ERR_CODE_CLASS_WAS_NOT_FOUND:
+              $field->error_set(new text_multiline([
+                'Field "%%_title" contains an error!',
+                'Class "%%_class_name" was not found.',
+                'Line: %%_line'], [
+                'title'      => (new text($field->title))->render(),
+                'line'       => $c_error->line,
+                'class_name' => $c_error->args['class_name']]));
+              break;
+            case storage_nosql_data::ERR_CODE_CLASS_NOT_ALLOWED:
+              $field->error_set(new text_multiline([
+                'Field "%%_title" contains an error!',
+                'Class "%%_class_name" not allowed.',
+                'Line: %%_line'], [
+                'title'      => (new text($field->title))->render(),
+                'line'       => $c_error->line,
+                'class_name' => $c_error->args['class_name']]));
+              break;
+          }
+        }
+      } else {
+        if ($field->data_validator_id) {
+          $validate_result = data_validator::get($field->data_validator_id)->validate($parsed_result->data);
+          if (count($validate_result['errors'])) {
+            foreach ($validate_result['errors'] as $c_error_message)
+              $field->error_set($c_error_message);
+          } else {
+            return true;
+          }
         }
       }
     } else {
